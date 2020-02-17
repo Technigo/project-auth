fcd4570d15d83f7131ff59f812b896aff99e8dad
nasimmhn <nasim.mahzoun@gmail.com>
2020-02-17 11:28:51 +0100

    implemented registration endpoint and error handling

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import User from '../lib/user/usermodel.js'
+import bcrypt from 'bcrypt-nodejs'
 
 const app = express()
 
@@ -14,9 +15,44 @@ app.get('/', (req, res) => {
   res.send('Hej')
 })
 
-app.get('/user', async (req, res) => {
-  const users = await User.find()
-  res.json(users)
+app.get('/user', async (req, res, next) => {
+  try {
+    const users = await User.find()
+    res.json(users)
+  } catch (err) {
+    next(err)
+  }
 })
+
+app.post('/registration', async (req, res, next) => {
+  try {
+    /*
+    * TODO
+    * name => Firstname + lastname
+    * email => email + username
+    * password => string (to be hashed)
+    */
+
+    const {
+      name,
+      email,
+      password
+    } = req.body
+
+    const newUser = await new User({ name, email, password: bcrypt.hashSync(password) }).save()
+    res.json(newUser)
+  } catch (err) {
+    next(err)
+  }
+})
+
+app.use((req, res) => {
+  res.status(404).json({ error: `route ${req.originalUrl} doesn't exist` })
+})
+
+app.use((err, req, res, next) => {
+  res.status(500).json({ error: err.message })
+})
+
 
 module.exports = app
