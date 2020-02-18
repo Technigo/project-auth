f66be60d25af4730988b88a6502ee9db58ca7241
Angelica Brodin <brodin.angelica@gmail.com>
2020-02-18 14:44:19 +0100

    added login and create user

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,21 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+const User = mongoose.model('User', {
+  username: {
+    type: String,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString('hex')
+  }
+})
+
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -23,6 +34,27 @@ app.use(bodyParser.json())
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+app.post('/users', async (req, res) => {
+  try {
+    const { username, password } = req.body
+    const user = new User({ username, password: bcrypt.hashSync(password) })
+    user.save()
+    res.status(201).json({ id: user._id, accessToken: user.accessToken })
+  } catch (err) {
+    res.status(403).json({ message: 'Could not create user! Are you sure you are a wizard?', errors: err.errors })
+  }
+})
+
+
+app.post('/login', async (req, res) => {
+  const user = await User.findOne({ username: req.body.username })
+  if (user && bcrypt.compareSync(req.body.password, user.password)) {
+    res.json({ userId: user._id, accessToken: user.accessToken })
+  } else {
+    res.json({ notFound: true })
+  }
 })
 
 // Start the server
