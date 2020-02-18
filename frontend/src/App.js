1841ddbde258c44eac2d78ae9c43ba7a78817948
Michel Elmoznino Laufer <mitchel_85@hotmail.com>
2020-02-18 18:44:36 +0100

    Added some new states in App.js for showing form and ContentPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import React, {useState} from 'react'
 import {Registration} from 'components/Registration'
 import {Signin} from 'components/Signin'
+import {ContentPage} from 'components/ContentPage'
 
 export const App = () => {
   const [showRegister, setShowRegister] = useState(false)
+  const [showForm, setShowForm] = useState(true)
+  const [showContentPage, setShowContentPage] = useState(false)
   
   const clickSignin = () => {
     setShowRegister(false)
@@ -13,8 +16,17 @@ export const App = () => {
     setShowRegister(true)
   }
 
+  const handleSignOut = event => {
+    event.preventDefault()
+    //make user sign out
+    setShowForm(true)
+    setShowContentPage(false)
+}
+
   return (
     <div>
+      {showForm && 
+      <div>
       <div className="btn-container">
         <button 
           className="btn-chooseform"
@@ -37,6 +49,15 @@ export const App = () => {
 
       {showRegister &&
         <Registration />
+      }
+      </div>
+      }
+      {showContentPage && 
+        <div className="welcome-sign">
+          <ContentPage/>
+          <button className="btn-signout" type="button"
+          onClick={handleSignOut}>Sign out</button>
+        </div>
       }
     </div>
   )
