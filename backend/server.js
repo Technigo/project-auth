527f181af842234c8be374f57c0d34cec7ffac26
AleksandraMuller <aleksandra.e.mueller@gmail.com>
2020-02-19 13:08:46 +0100

    basic setup backend

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,102 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import crypto from "crypto";
+import bcrypt from "bcrypt-nodejs";
+import mongoose from "mongoose";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    unique: true
+  },
+  email: {
+    type: String,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString("hex")
+  }
+});
+
+// const user = new User({ name: "Bob", password: bcrypt.hashSync("foobar") });
+// user.save();
+
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
+
+const authenticateUser = async (req, res, next) => {
+  const user = await User.findOne({ accessToken: req.header("Authorization") });
+  if (user) {
+    req.user = user;
+    next();
+  } else {
+    res.status(401).json({ loggedOut: true });
+  }
+};
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.get("/secrets", authenticateUser);
+app.get("/secrets", (req, res) => {
+  res.json({ secret: "this is the secret" });
+});
+
+//REGISTRATION
+app.post("/users", async (req, res) => {
+  try {
+    const { name, password, email } = req.body;
+    const user = new User({ name, email, password: bcrypt.hashSync(password) });
+    user.save();
+    res.status(201).json({ id: user._id, accessToken: user.accessToken });
+  } catch (err) {
+    res
+      .status(400)
+      .json({ message: "could not create user", errors: err.errors });
+  }
+});
+
+//LOGIN
+
+app.post("/sessions", async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (user && bcrypt.compareSync(req.body.password, user.password)) {
+    res.json({ userId: user._id, accessToken: user.accessToken });
+  } else {
+    res.json({ notFound: true });
+  }
+});
+
+// app.post("/tweets", authenticateUser);
+// app.post("/tweets", async (req, res) => {});
+
+// app.post("/sessions", async (req, res) => {
+//   const user = await User.findOne({ name: req.body.name });
+//   if (user && bcrypt.compareSync(req.body.password, user.password)) {
+//     res.json({ userId: user._id, accessToken: user.accessToken });
+//   } else {
+//     res.json({ notFound: true });
+//   }
+// });
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
