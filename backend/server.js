1654a41fe50bc9137d40b28de7cb991d19342951
Jens_Nilsen <jensnilsen89@gmail.com>
2020-02-19 17:27:25 +0100

    wendesdays code along

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,32 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import crypto from 'crypto'
+import bcrypt from 'bcrypt-nodejs'
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/authAPI'
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+    required: true,
+    minlength: 6,
+    maxlenght: 500
+  },
+  password: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  }
+})
+
+
+// Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
@@ -23,7 +43,35 @@ app.get('/', (req, res) => {
   res.send('Hello world')
 })
 
+//create user
+app.post('/users', (req, res) => {
+  try {
+    const {name, password} = req.body
+    const user = new User({name, password: bcrypt.hashSync(password)})
+    const saved = await user.save()
+    res.status(200).json(saved)
+  } catch (err) {
+    res.status(400).json({message: 'could not save user', errors: err.errors})
+  }
+})
+
+//find / login
+app.post('/sessions', (req, res) => {
+  try {
+    const {name, password} = req.body
+    const user = await User.findOne({ name })
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(201).json({useId: user._id, accesToken: user.accesToken})
+    }else {
+      res.json({notFound: true})
+    }
+  } catch (err) {
+    res.status(400).json({message: 'could not save user', errors: err.errors})
+  }
+})
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 })
+
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const URL = 'http://localhost:8080/users'
 
 export const App = () => {
+  const [name, setName] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+    fetch(URL, {
+      method: 'POST',
+      body: JSON.stringify({})
+    })
+      .then((res) => res.json())
+      .then((json) => console.log(json))
+  }
+
   return (
     <div>
-      Find me in src/app.js!
+      <from>
+        <h1>Sign Up</h1>
+        <label>
+          Name
+          <input
+            required
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+        </label>
+        <label>
+          Password
+          <input
+            required
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </label>
+      </from>
     </div>
   )
 }
