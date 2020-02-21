bf642e27436141b91e6335aeeeb18088bb678f46
sarah wolfson <swolfson23@gmail.com>
2020-02-21 15:23:38 +0100

    fixing frontend deployment

--- frontend/src/components /signIns.js
+++ frontend/src/components /signIns.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef } from "react";
 import { useForm } from "react-hook-form";
 import styled from "styled-components";
 
