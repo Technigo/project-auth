852f49e611e892b7013686c5993a880131dc9f05
Angelica Brodin <brodin.angelica@gmail.com>
2020-02-21 10:43:26 +0100

    started to add the form for the registration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const URL = 'https://harry-potter-auth.herokuapp.com/users'
 
 export const App = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = event => {
+    event.preventDefault()
+
+    fetch(URL, {
+      method: 'POST',
+      body: JSON.stringify({ username, password }),
+      headers: { 'Content-Type': 'application/json' }
+    })
+      .then(res => res.json())
+      .then(json => console.log(json))
+  }
+
   return (
     <div>
-      Find me in src/app.js!
+      <form onSubmit={handleSubmit}>
+        <h1>Sign up to Hogwarts:</h1>
+        <label>
+          Username:
+            <input
+            required
+            value={username}
+            onChange={event => setUsername(event.target.value)}
+          />
+        </label>
+        <label>
+          Password:
+            <input
+            required
+            value={password}
+            onChange={event => setPassword(event.target.value)}
+          />
+        </label>
+        <button type="submit" onClick={handleSubmit}>REGISTER</button>
+      </form>
     </div>
   )
 }
