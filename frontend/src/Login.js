49595761e22f221523d00fedc9df1f35759ddbb8
Clara Gyökeres <you@example.com>
2020-02-22 13:31:20 +0100

    clear field after signup

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react'
+import Profile from './Profile'
 
-const URL = 'https://harry-potter-auth.herokuapp.com/login'
+// const URL = 'https://harry-potter-auth.herokuapp.com/login'
+
+const URL = 'http://localhost:8080/login'
 
 export const Login = () => {
     // Vi måste ändra setState till rätt namn
 
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
+    const [loggedInUser, setLoggedInUser] = useState(null)
 
     const handleSubmit = event => {
         event.preventDefault()
@@ -17,33 +21,41 @@ export const Login = () => {
             headers: { 'Content-Type': 'application/json' }
         })
             .then(res => res.json())
-            .then(json => console.log(json))
-            .catch(err => console.log('error:', err))
+            .then((json) => {
+                setLoggedInUser(json)
+                setPassword('')
+                setUsername('')
+            }).catch(err => console.log('error:', err))
     }
+    if (loggedInUser === null) {
 
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <h1>Login to Hogwarts:</h1>
-                <label>
-                    Username:
+        return (
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <h1>Login to Hogwarts:</h1>
+                    <label>
+                        Username:
             <input
-                        required
-                        value={username}
-                        onChange={event => setUsername(event.target.value)}
-                    />
-                </label>
-                <label>
-                    Password:
+                            required
+                            value={username}
+                            onChange={event => setUsername(event.target.value)}
+                        />
+                    </label>
+                    <label>
+                        Password:
             <input
-                        required
-                        type="password"
-                        value={password}
-                        onChange={event => setPassword(event.target.value)}
-                    />
-                </label>
-                <button type="submit" onClick={handleSubmit}>LOGIN</button>
-            </form>
-        </div>
-    )
-}
+                            required
+                            type="password"
+                            value={password}
+                            onChange={event => setPassword(event.target.value)}
+                        />
+                    </label>
+                    <button type="submit" onClick={handleSubmit}>LOGIN</button>
+                </form>
+            </div>
+        )
+    } else {
+        //If user is logged in, show profile
+        return (<Profile loggedInUser={loggedInUser} />);
+    }
+} 
