4411db319e93784a37c994de501debf795814e44
tavferreira <tiagoferreira@tuta.io>
2020-02-22 15:09:57 +0100

    Store and router implemented.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,21 +3,32 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Login } from './pages/Login'
 import { Signup } from './pages/Signup'
 import { Secret } from './pages/Secret'
+import { Provider } from "react-redux";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { auth } from "./reducers/auth";
+
+const reducer = combineReducers({
+  auth: auth.reducer
+});
+
+const store = configureStore({ reducer });
 
 export const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route path='/' exact>
-          <Login />
-        </Route>
-        <Route path='/signup' exact>
-          <Signup />
-        </Route>
-        <Route path='/secrets' exact>
-          <Secret />
-        </Route>
-      </Switch>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          <Route path='/' exact>
+            <Login />
+          </Route>
+          <Route path='/signup' exact>
+            <Signup />
+          </Route>
+          <Route path='/secrets' exact>
+            <Secret />
+          </Route>
+        </Switch>
+      </Router>
+    </Provider>
   )
 }
--- frontend/src/pages/Secret.js
+++ frontend/src/pages/Secret.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from "react-redux"
+import { auth } from "../reducers/auth"
 
 export const Secret = () => {
+    const [secrets, setSecrets] = useState([])
+    const [status, setStatus] = useState()
+    const dispatch = useDispatch()
+    const token = useSelector(store => store.auth.accessToken);
+
+    const logout = () => {
+        dispatch(auth.actions.logout())
+    }
+
+    useEffect(() => {
+        fetch('http://localhost:8080/secrets', {
+            headers: {
+                "Authorization": token
+            }
+        })
+            .then(res => {
+                setStatus(res.status)
+                return res.json()
+            })
+            .then(json => {
+                setSecrets(json.secret)
+            })
+    }, [token])
+
+    if (status !== 200) {
+        return (
+            <div>
+                <h1>Unauthorized</h1>
+            </div>
+        )
+    }
     return (
-        <div>
-            Secret page!
-        </div>
+        <main>
+
+            <h1>Secret page!</h1>
+            {secrets}
+
+            <Link to='/'><button type='button' onClick={logout}>Logout</button></Link>
+        </main>
     )
 }
