9faa309ef041840782cc3b518b26b163b5ec3913
tavferreira <tiagoferreira@tuta.io>
2020-02-22 13:44:24 +0100

    Frontend: Added routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Login } from './pages/Login'
+import { Signup } from './pages/Signup'
+import { Secret } from './pages/Secret'
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/' exact>
+          <Login />
+        </Route>
+        <Route path='/signup' exact>
+          <Signup />
+        </Route>
+        <Route path='/secrets' exact>
+          <Secret />
+        </Route>
+      </Switch>
+    </Router>
   )
 }
