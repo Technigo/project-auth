ab6ecaaaf36cfd54c343768aabf8717d8b3ea7d9
Ivett Backlund <ivett.backlund@gmail.com>
2020-02-22 19:19:27 +0100

    sign up form frontend

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Signup = () => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
     return (
         <div>
-            Signup page!
+            <h1>Signup form!</h1>
+            <form>
+                <input required
+                    value={name}
+                    onChange={event => setName(event.target.value)}
+                    placeholder="name"
+                />
+                <input required
+                    value={email}
+                    onChange={event => setEmail(event.target.value)}
+                    placeholder="email"
+                />
+                <input required
+                    value={password}
+                    onChange={event => setPassword(event.target.value)}
+                    placeholder="password"
+                />
+            </form>
+
         </div>
     )
 }
