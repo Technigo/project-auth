5c051add5e30734b6e7a5540bd3bb9875eba3204
SaraBorg <50766676+SaraBorg@users.noreply.github.com>
2020-02-22 16:52:51 +0100

    Added lokal storage for token

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,8 +6,13 @@ import {SignIn} from './components/SignIn'
 // TODO: Local storage for token
 // TODO: Make the profile for signed in users
 // TODO: Styling
+// TODO: Separete sign in and sign up
+
+
+
 
 export const App = () => {
+  
   return (
     <div>
       <SignUp />
--- frontend/src/components/SignIn.js
+++ frontend/src/components/SignIn.js
@@ -6,37 +6,62 @@ export const SignIn = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [signedIn, setSignedIn] = useState(false)
+  const [accessToken, setAccessToken] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
+
+  const fetchProfile = async() => {
+   const response = await
+    fetch(URL, {
+      method: 'POST',
+      body: JSON.stringify({ email, password}),
+      headers: { 'Content-Type': 'application/json' }
+    })
+    const json = await response.json()
+      return json
+    }
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    fetch(URL, {
-      method: 'POST',
-      body: JSON.stringify({email: email, password: password}),
-      headers: {'Content-Type':'application/json'} 
-    }).then((res) => res.json) 
-    .then((json) => {
-      if (json.notFound) {
+    fetchProfile().then(data => {
+      if (data.notFound) {
+        setErrorMessage('Username or password is invalid')
         setSignedIn(false)
       } else {
-        setSignedIn(true)
+             window.localStorage.setItem('accessToken', data.accessToken)
+             setErrorMessage('')
+             setSignedIn(true)
       }
-    })
+    })   
+  }
+  const signOut = () => {
+    window.localStorage.clear()
+    setSignedIn(false)
   }
 
-  return (
+  const accessTokenTest = window.localStorage.getItem('accessToken')
+  return ( 
     <>
-    <form className='signin-form' onSubmit={handleSubmit}>
-      <label>
-        Email
-        <input type='email' value={email} onChange={(event) => {setEmail(event.target.value)}}/>
-      </label>
-      <label>
-        Password
-      <input type='password' value={password} onChange={(event) => {setPassword(event.target.value)}} />
-      </label>
-      <button className='signin-button' type='submit'>Sign in</button>
-    </form>
-    <p>{`Signed in: ${signedIn}`}</p>
+    {signedIn &&
+    <button type='button' onClick={signOut}>Sign out </button>}
+    {accessTokenTest && <p>{accessTokenTest}</p>}
+    {errorMessage && <p>{errorMessage}</p>}
+   {!signedIn && 
+   <>
+    <form className='signin-form' onSubmit={handleSubmit} >
+    <label>
+    Email 
+    <input type = 'email'
+    value = { email }
+    onChange = {(event) => { setEmail(event.target.value) }} /> 
+    </label>
+    <label>
+    Password 
+    <input type = 'password'
+    value = { password }
+    onChange = {(event) => { setPassword(event.target.value) }} /> 
+    </label> 
+    <button className='signin-button' type='submit'> Sign in </button> 
+    </form> 
+    <p> { `Signed in: ${signedIn}`} </p></> }
     </>
-  )
-}
+  )}
