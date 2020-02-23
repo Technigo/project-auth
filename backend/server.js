9761129e27fa0d04de1ee7a1006daef970b07021
tavferreira <tiagoferreira@tuta.io>
2020-02-23 09:36:27 +0100

    Frontend: adding loader. Backend: Removing password from signup response

--- backend/server.js
+++ backend/server.js
@@ -73,7 +73,7 @@ app.post('/users', async (req, res) => {
       // it is very important to encrypt the passwords and store them encrypted in our db!
       const user = new User({ name, email, password: bcrypt.hashSync(password) })
       await user.save()
-      res.status(201).json({ id: user._id, name: user.name, password: user.password, accessToken: user.accessToken })
+      res.status(201).json({ id: user._id, name: user.name, accessToken: user.accessToken })
       // if the user is not registered, then we catch the error
     } else res.status(400).json({ message: 'Invalid email', errors: err.errors })
   } catch (err) {
