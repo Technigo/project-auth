4632696a2e653e4706eed6a5b90ab737f0e5791a
AleksandraMuller <aleksandra.e.mueller@gmail.com>
2020-02-23 18:04:35 +0100

    register page styled

--- frontend/src/Register.js
+++ frontend/src/Register.js
@@ -1,6 +1,124 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { registerUser } from "./services/authorization";
+import styled from "styled-components";
+
+import woman from "./images/woman.jpg";
+
+const Container = styled.div`
+  background: #eee;
+  height: 100vh;
+`;
+
+const Left = styled.div`
+  width: 35%;
+  height: 100%;
+  background: black;
+  text-align: center;
+  display: grid;
+  grid-template-columns: 10% 10% 60% 10% 10%;
+  grid-template-rows: 10% 25% 40% 10% 15%;
+`;
+
+const Right = styled.div`
+  width: 65%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  letter-spacing: 2px;
+`;
+
+const Top = styled.div`
+  color: white;
+  font-family: "Open Sans";
+  letter-spacing: 2px;
+  grid-column: 2 / span 3;
+  grid-row: 2 / span 1;
+  z-index: 100;
+  font-size: 0.5rem;
+  @media (min-width: 600px) {
+    font-size: 1rem;
+  }
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  opacity: 0.5;
+  grid-column: 1 / -1;
+  grid-row: 1 / -1;
+`;
+
+const Signin = styled.button`
+  grid-column: 3 / span 1;
+  grid-row: 4 / span 1;
+  opacity: 1;
+  background: transparent;
+  color: white;
+  border-radius: 30px;
+  z-index: 100;
+  boder: 2px white solid;
+  text-decoration: none;
+  cursor: pointer;
+  :hover {
+    background: white;
+    color: black;
+  }
+`;
+
+const Label = styled.label`
+  margin: 0 auto;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+  color: grey;
+`;
+
+const Input = styled.input`
+  margin: 0 auto;
+  width: 70%;
+  border-top: none;
+  border-left: none;
+  border-right: none;
+
+  padding: 1rem;
+`;
+
+const HeaderRight = styled.h2`
+  text-align: center;
+`;
+
+const Signup = styled.button`
+  margin: 0 auto;
+  text-align: center;
+  margin-top: 3rem;
+  width: 30%;
+  padding: 1rem;
+  background: #a46c4d;
+  color: white;
+  border: none;
+  border-radius: 30px;
+  font-family: "Open Sans";
+  letter-spacing: 1px;
+  :hover {
+    background: #cfac99;
+  }
+`;
+
+const Form = styled.form`
+  width: 80%;
+  height: 70%;
+  margin: 0 auto;
+  display: flex;
+  background: white;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  font-family: "Open Sans";
+  margin: auto;
+`;
 
 export const Register = () => {
   const [name, setName] = useState();
@@ -20,30 +138,48 @@ export const Register = () => {
   };
 
   return (
-    <form>
-      <label>Name:</label>
-      <input
-        type="text"
-        value={name}
-        onChange={event => setName(event.target.value)}
-      ></input>
-      <label>Email:</label>
-      <input
-        type="email"
-        value={email}
-        onChange={event => setEmail(event.target.value)}
-      ></input>
-      <label>Password:</label>
-      <input
-        type="password"
-        value={password}
-        onChange={event => setPassword(event.target.value)}
-      ></input>
-      <button onClick={event => handleRegister(event)}>REGISTER</button>
-      {errorText}
-      <button onClick={() => history.push("/login")}>
-        Already have an account? Login
-      </button>
-    </form>
+    <Container>
+      <Form>
+        <Left>
+          <Image src={woman} />
+          <Top>
+            <h2>One of us?</h2>
+            <p>
+              If you already have an account, just sign in. We have missed you!
+            </p>
+          </Top>
+          <Signin onClick={() => history.push("/login")}>SIGN IN</Signin>
+        </Left>
+        <Right>
+          {/* <InputContainer> */}
+          <HeaderRight>Register with us, let´s travel together:</HeaderRight>
+          <Label>Name</Label>
+          <Input
+            type="text"
+            value={name}
+            onChange={event => setName(event.target.value)}
+          ></Input>
+          {/* </InputContainer> */}
+          {/* <InputContainer> */}
+          <Label>Email</Label>
+          <Input
+            type="email"
+            value={email}
+            onChange={event => setEmail(event.target.value)}
+          ></Input>
+          {/* </InputContainer> */}
+          {/* <InputContainer> */}
+          <Label>Password</Label>
+          <Input
+            type="password"
+            value={password}
+            onChange={event => setPassword(event.target.value)}
+          ></Input>
+          {/* </InputContainer> */}
+          <Signup onClick={event => handleRegister(event)}>REGISTER</Signup>
+          {errorText}
+        </Right>
+      </Form>
+    </Container>
   );
 };
