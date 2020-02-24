8fbe71b61751c77d60eca8c5689ab2e80c0c5a37
Petra B <petrabrinck@icloud.com>
2020-02-24 12:54:30 +0100

    text and css adjustments

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -45,15 +45,16 @@ export const Login = () => {
         <section>
             <form className='loginForm' onSubmit={handleFormSubmit}>
                 <h1><strong>Member login</strong></h1>
-                <h2>Welcome back - we've missed you!</h2>
+                <h2>Welcome back - we've missed you! </h2>
                 <div className='loginContainer'>
-                    <label >Username/Email:  </label>
+                    <label >Username/Email  </label>
                     <input value={email} placeholder='Enter Email' type='email' name='email' onChange={event => { setEmail(event.target.value) }} required></input>
 
-                    <label>Password:     </label>
+                    <label>Password     </label>
                     <input value={password} placeholder='Enter Password' type='password' name='password' onChange={event => { setPassword(event.target.value) }} required>
                     </input>
                     {errorMsg && <div className='error-message'> {errorMsg} </div>}
+                    {/* <label>Reset password here</label> */}
                     <button onClick={handleFormSubmit} type="submit">Login</button>
                 </div>
             </form>
--- frontend/src/components/Registration.js
+++ frontend/src/components/Registration.js
@@ -10,7 +10,7 @@ export const Registration = () => {
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    const [repeat, setRepeat] = useState('')
+    // const [repeat, setRepeat] = useState('')
     const [errorMsg, setErrorMsg] = useState(null)
     const [successMsg, setSuccessMsg] = useState(null)
 
@@ -48,19 +48,19 @@ export const Registration = () => {
                 <h1><strong>Sign up</strong></h1>
                 <h2>Not a member? Fill in this form and you're set!</h2>
                 <div className='infoContainer'>
-                    <label>Name:</label>
+                    <label>Name</label>
                     <input value={name} placeholder='Enter Name' type='text' name='name' onChange={event => setName(event.target.value)} required></input>
 
-                    <label >Email:</label>
+                    <label >Email</label>
                     <input value={email} placeholder='Enter Email' type='email' name='email' onChange={event => setEmail(event.target.value)} required></input>
 
-                    <label>Password:</label>
+                    <label>Password</label>
                     <input value={password} placeholder='Enter Password' type='password' name='password' onChange={event => setPassword(event.target.value)} required>
                     </input>
 
-                    <label>Repeat Password:</label>
+                    {/* <label>Repeat Password:</label>
                     <input value={repeat} placeholder='Repeat Password' type='password' name='repeat' onChange={event => setRepeat(event.target.value)} required>
-                    </input>
+                    </input> */}
                     {/* success and error messages */}
                     {errorMsg && <div className='error-message'> {errorMsg} </div>}
                     {successMsg && <div className='success-message'> {successMsg} </div>}
