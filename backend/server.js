4ba0b4b983db1ee638c7ba68373de19cc7145d2b
Petra B <petrabrinck@icloud.com>
2020-02-24 14:13:17 +0100

    changed url in backend to see if login works

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,8 @@ import mongoose from 'mongoose'
 import crypto from 'crypto'
 import bcrypt from 'bcrypt-nodejs'
 
-const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/auth'
+// const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/auth'
+const mongoUrl = process.env.MONGO_URL || 'https://pb-auth-api.herokuapp.com/users'
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
