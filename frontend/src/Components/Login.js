8024ff28f26218b82a84bef9808c9e17e3169460
Jenfi <jennifer_fick@hotmail.com>
2020-02-25 19:39:37 +0100

    added type password

--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -37,7 +37,7 @@ export const Login = () => {
           <label>
             Password
       <input
-              // type="password"
+              type="password"
               required
               value={password}
               onChange={event => setPassword(event.target.value)} />
