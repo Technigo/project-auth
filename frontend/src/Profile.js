520f126c7c5d699d9f5d968e877784a3d6ca6180
visua77 <nicznet@hotmail.com>
2020-02-29 18:48:47 +0100

    not gonna make a login or logout, cant follow along vans very confusing lecture

--- frontend/src/Profile.js
+++ frontend/src/Profile.js
@@ -1,32 +1,37 @@
-import React, { useState, useEffect } from 'react'
+ import React, { useState, useEffect } from 'react'
 
 const URL = 'http://localhost:8080/users'
 
-export const Profile = ({ loggedInUser }) => {
-   const[userId, setUserId] = useState(loggedInuser.userID)
-   const[accessToken, setAccessToken] = useState(loggedInuser.accessToken)
+export const Profile = ( loggedInUser ) => {
+   const[userId, setUserId] = useState(0)
+   const[accessToken, setAccessToken] = useState('')
    
    const changeName = (userObject) => {
         document.getElementById('name').innerHTML = userObject.name 
    }
 
-    useEffect(() => {
-        console.log(loggedInuser)
+   useEffect(() => {
+       console.log(loggedInUser)
         // Never do this unless you messed up somwhere else
-        //loggedInuser = loggedInuser.loggedInuser
-        setUserId(loggedInuser._id)
-        setAccessToken(loggedInuser.accessToken)
-        fetch(`${URL}/${userId}`, {
+        loggedInUser = loggedInUser.loggedInUser
+        setUserId(loggedInUser._id);
+        setAccessToken(loggedInUser.accessToken);
+        
+     fetch(`${URL}/${userId}`, {
             method: 'GET',
-            headers: {'Authorization':accessToken}
+            // Include the accessToken to get the protected endpoint
+            headers: {'Authorization': accessToken}
         })
             .then(res => res.json())
+            // SUCCESS: Do something with the information we got back
             .then(json => changeName(json))
-            .catch(err => console.log('error:', err))
+            .catch(err => console.log('error:', err)) //401
     
     })
 
 return (
-    <div>Profile <div id="name"></div></div>
+    <div>Profile <div id="name"></div>
+    </div>
 )
-}
+} 
+
