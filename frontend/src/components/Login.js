092b151cac5f499d2e0daf14c7ab9eb4e14eb8d4
Theresa Ulwahn <theresaulwahn@gmail.com>
2020-04-06 16:15:16 +0200

    changed  window.location.href to history.push('/MemberPage')

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import {useHistory} from 'react-router'
 // import { Link } from "react-router-dom";
 import './login.css'
 
@@ -8,6 +9,7 @@ export const Login = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [errorMsg, setErrorMsg] = useState(null)
+  const history = useHistory()
 
   // To log in an exicting member
   const handleFormSubmit = event => {
@@ -18,46 +20,92 @@ export const Login = () => {
       body: JSON.stringify({ email, password }),
       headers: { 'Content-Type': 'application/json' }
     })
-      .then(res => {
+      .then((res) => {
         if (res.ok) {
           return res.json()
         }
         else {
-          return res.text().then(json => { throw new Error(json) })
+          return res.text().then((json) => { throw new Error(json) })
         }
       })
-      .then(user => {
+      .then((user) => {
         if (user['message']) {
           setErrorMsg(user.message)
         }
         else {
           window.localStorage.setItem("userId", user.userId)
           window.localStorage.setItem("accessToken", user.accessToken)
-          window.location.href = "/MemberPage"
+          // window.location.href = "/MemberPage"
+          // if (email < 0) {
+          //   return alert('Please fill in email and password')
+          // } else {
+            history.push('/MemberPage')
+          
         }
       })
-      .catch(err => console.log('error:', err))
+      .catch((err) => console.log('error:', err))
   };
 
-  // If user is logged out, show login form
-  return (
-    <section>
-      <form className="loginForm" onSubmit={handleFormSubmit}>
-        <h1><strong>Member login</strong></h1>
-        <h2>Welcome back - we've missed you!</h2>
-        <div className="loginContainer">
-          <label >Username/Email:  </label>
-          <input value={email} placeholder="Enter Email" type="email" name="email" onChange={event => { setEmail(event.target.value) }} required></input>
+    // If user is logged out, show login form
+    return (
+      <section>
+        <form className="loginForm" onSubmit={handleFormSubmit}>
+          <h1>
+            <strong>Member login</strong>
+          </h1>
+          <h2>Welcome back - we've missed you!</h2>
+          <div className="loginContainer">
+            <label>Username/Email: </label>
+            <input
+              value={email}
+              placeholder="Enter Email"
+              minLength="0"
+              type="email"
+              name="email"
+              onChange={(event) => {
+                setEmail(event.target.value);
+              }}
+              required
+            ></input>
+            <label>Password: </label>
+            <input
+              value={password}
+              placeholder="Enter Password"
+              type="password"
+              name="password"
+              onChange={(event) => {
+                setPassword(event.target.value);
+              }}
+              required
+            ></input>
+            {errorMsg && <div className="error-message"> {errorMsg} </div>}
+            <button type="submit">Login</button>
+          </div>
+        </form>
+      </section>
+    );
+  };
+  export default Login;
 
-          <label>Password:     </label>
-          <input value={password} placeholder="Enter Password" type="password" name="password" onChange={event => { setPassword(event.target.value) }} required>
-          </input>
-          {errorMsg && <div className="error-message"> {errorMsg} </div>}
-          <button onClick={handleFormSubmit} type="submit">Login</button>
-        </div>
-      </form>
-    </section>
-  )
-}
+//   // If user is logged out, show login form
+//   return (
+//     <section>
+//       <form className="loginForm" onSubmit={handleFormSubmit}>
+//         <h1><strong>Member login</strong></h1>
+//         <h2>Welcome back - we've missed you!</h2>
+//         <div className="loginContainer">
+//           <label >Username/Email:  </label>
+//           <input value={email} placeholder="Enter Email" type="email" name="email" onChange={event => { setEmail(event.target.value) }} required></input>
 
-export default Login
+//           <label>Password:     </label>
+//           <input value={password} placeholder="Enter Password" type="password" name="password" onChange={event => { setPassword(event.target.value) }} required>
+//           </input>
+//           {errorMsg && <div className="error-message"> {errorMsg} </div>}
+//           <button onClick={handleFormSubmit} type="submit">Login</button>
+//         </div>
+//       </form>
+//     </section>
+//   )
+// }
+
+// export default Login
