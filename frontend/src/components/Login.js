56632517fe10a5c7f35e072c2c34646a4044b16e
Theresa Ulwahn <theresaulwahn@gmail.com>
2020-04-14 21:08:22 +0200

    css added

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -35,12 +35,7 @@ export const Login = () => {
         else {
           window.localStorage.setItem("userId", user.userId)
           window.localStorage.setItem("accessToken", user.accessToken)
-          // window.location.href = "/MemberPage"
-          // if (email < 0) {
-          //   return alert('Please fill in email and password')
-          // } else {
-            history.push('/MemberPage')
-          
+          history.push('/MemberPage') 
         }
       })
       .catch((err) => console.log('error:', err))
@@ -86,26 +81,3 @@ export const Login = () => {
     );
   };
   export default Login;
-
-//   // If user is logged out, show login form
-//   return (
-//     <section>
-//       <form className="loginForm" onSubmit={handleFormSubmit}>
-//         <h1><strong>Member login</strong></h1>
-//         <h2>Welcome back - we've missed you!</h2>
-//         <div className="loginContainer">
-//           <label >Username/Email:  </label>
-//           <input value={email} placeholder="Enter Email" type="email" name="email" onChange={event => { setEmail(event.target.value) }} required></input>
-
-//           <label>Password:     </label>
-//           <input value={password} placeholder="Enter Password" type="password" name="password" onChange={event => { setPassword(event.target.value) }} required>
-//           </input>
-//           {errorMsg && <div className="error-message"> {errorMsg} </div>}
-//           <button onClick={handleFormSubmit} type="submit">Login</button>
-//         </div>
-//       </form>
-//     </section>
-//   )
-// }
-
-// export default Login
