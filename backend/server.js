3e132464857a2ed7fc6e93b6beaa6f1b8eb1d55d
Siri <siri.gyllander@gmail.com>
2020-05-22 14:13:05 +0200

    project started. Built user login, sign up, user authentification

--- backend/server.js
+++ backend/server.js
@@ -14,15 +14,75 @@ mongoose.Promise = Promise
 const port = process.env.PORT || 8080
 const app = express()
 
+
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(bodyParser.json())
+
+const User = mongoose.model('User', {
+  name: {
+    type : String,
+    unique: true
+  },
+  email: {
+    type: String,
+    unique: true
+  },
+  password: {
+    type: String,
+    unique: true
+  },
+  accestoken: {
+   type: String,
+   default: () => crypto.randomBytes(128).toString('hex') 
+  }
+})
+
+const authenticateUser = async (req, res) => {
+  const user = await User.findOne({accessToken: req.header('Authorization')})
+  if (user) {
+    req.user = user
+    next()
+  } else {
+    res.status(401).json({loggedOut: true})
+  }
+}
+
+
 
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
 
+app.post('/users', async (req, res) => {
+  try {
+    const {name, email, password} = req.body
+    const user = new User ({name, email, password: bcrypt.hashSync(password)})
+    user.save()
+    res.status(201).json({id: user._id, accessToken: user.accessToken})
+  } catch (err) {
+    res.status(400).json({message:'Could not create userinfo', errors: err.errors})
+  }
+})
+app.get('/secrets', authenticateUser)
+app.get('/secrets', (req, res) => {
+  res.json({secret: 'This is a secret message'})
+})
+
+app.post('/sessions', async (req, res) => {
+  try {
+    const { name, password } = req.body
+    const user = await User.findOne({ name })
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(201).json({ useId: user._id, accessToken: user.accessToken })
+    } else {
+      res.json({ notFound: true })
+    }
+  } catch (err) {
+    res.status(400).json({ message: 'Unable to find user', errors: err.errors })
+  }
+  })
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
