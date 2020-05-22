7b3343ed48992aa13802519d6e35c2fd26e40667
palhamel <pal.hamel@gmail.com>
2020-05-22 11:19:22 +0200

    first commit and setup of mongoose, user model and endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,47 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import crypto from 'crypto';
+import bcrypt from 'bcrypt-nodejs';
 
+// Mongoose & Database setup:
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
+
+// Mongoose model setup:
+
+const User = mongoose.model('User', {
+  name: {
+    type: String, 
+    unique: true
+  },
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString('hex'),
+  },
+});
+
+// middleware to authenticate user:
+
+const authenticateUser = async (req, res, next) => {
+  const user = await User.findOne({ accessToken: req.header("Authorization") });
+  if (user) {
+    req.user = user;
+    next();
+  } else {
+    res.status(401).json({ loggedOut: true });
+  }
+};
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -18,10 +54,47 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
+
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('Hello world - snart är det helg!')
 })
+
+
+// add user endpoint - the registration endpoint:
+// POST - http://localhost:8080/users
+
+app.post('/users', async (req, res) => {
+  try {
+    const {name, email, password} = req.body;
+    const user = new User({name, email, password: bcrypt.hashSync(password)});
+    user.save();
+    res.status(201).json({ id: user._id, accessToken: user.accessToken });
+  } catch (err) {
+    res.status(400).json({ message: 'Could not create user', errors: err.errors });
+  }
+});
+
+// supersecret endpoint - the protected endpoint
+// restrict access - - using authenticateUser:
+// GET - http://localhost:8080/secrets
+app.get('/secrets', authenticateUser);
+app.get('/secrets', (req, res) => {
+  // can do anything here, but we just put in a message
+  res.json({ secret: "All ok - secret test here!" });
+});
+
+
+// endpoint to login a User - and check email and password:
+// POST - http://localhost:8080/sessions
+app.post('/sessions', async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (user && bcrypt.compareSync(req.body.password, user.password)) {
+    res.json({ userId: user._id, accessToken: user.accessToken });
+  } else {
+    res.status(401).json({ notFound: true, error: 'Login failed' });
+  }
+});
 
 // Start the server
 app.listen(port, () => {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const App = () => {
   return (
     <div>
-      Find me in src/app.js!
+      Find me in src/app.js! Eller gå ut i solen i stället!
     </div>
   )
 }
