135f218dce49d9b31b183d3b1f1a5eb6441ecf95
Ragna <ragnaulfars@gmail.com>
2020-05-23 10:41:32 +0200

    Backend up and running

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,26 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import bcrypt from 'bcrypt-nodejs'
+
+import { User } from './model/user'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
+
+// Middleware function
+const authenticateUser = async (req, res, next) => {
+  const user = await User.findOne({ accessToken: req.header('Authorization') })
+  if (user) {
+    req.user = user
+    next()
+  } else {
+    res.status(403).json({ message: 'Access forbidden!' })
+  }
+}
+
 //   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
@@ -20,7 +32,36 @@ app.use(bodyParser.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('Hi there Allan!')
+})
+
+// Sign up endpoint
+app.post('/register', async (req, res) => {
+  try {
+    const { name, email, password } = req.body
+    const user = new User({ name, email, password: bcrypt.hashSync(password) })
+    user.save()
+    res.status(201).json({ id: user._id, accessToken: user.accessToken })
+  } catch (err) {
+    res.status(400).json({ message: 'Could not create user!', errors: err.errors })
+  }
+})
+
+// Login endpoint
+app.post('/login', async (req, res) => {
+  const user = await User.findOne({ email: req.body.email })
+  if (user && bcrypt.compareSync(req.body.password, user.password)) {
+    res.json({ userId: user._id, accessToken: user.accessToken })
+  } else {
+    res.json({ notFound: true })
+  }
+})
+
+// Auth endpoint
+app.get('/narnia', authenticateUser)
+
+app.get('/narnia', (req, res) => {
+  res.json({ secret: "Welcome to Narnia!" })
 })
 
 // Start the server
