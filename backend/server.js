e77d7e820663e97f027a0750d8dab288015f33f4
ebbabw <ebbabergholtz@gmail.com>
2020-05-25 19:59:41 +0200

    added sign in and sign out in backend

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import crypto from 'crypto'
 import bcrypt from 'bcrypt-nodejs'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 mongoose.Promise = Promise
 
 const User = mongoose.model('User', {
@@ -16,7 +16,7 @@ const User = mongoose.model('User', {
     unique: true
   },
   email: {
-    type: String,
+    type: String,    
     unique: true
   },
   password: {
@@ -25,23 +25,47 @@ const User = mongoose.model('User', {
   },
   accessToken: {
     type: String,
-    default: () => crypto.randomBytes(128).toString('hex')
+    default: () => crypto.randomBytes(128).toString('hex'),
   }
 
 })
 
-// const authenticateUser = async (req, res, next) 
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
 const port = process.env.PORT || 8080
+
 const app = express()
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(bodyParser.json())
+
+const authenticateUser = async (req, res, next) => {
+
+  try {
+    const user = await User.findOne({
+      accessToken: req.header('Authorization')
+    });
+
+    if (user) {
+      req.user = user;
+      next();
+    } else {
+      res
+        .status(401)
+        .json({ loggedOut: true, message:'Please try logging in again' });
+     } 
+    
+   }  catch (err) {
+     res
+        .status(403)
+        .json({ message: 'Access token is missing or wrong', errors: err })
+    }
+}
+
 
 app.post('/users', async (req, res) => {
 
@@ -55,10 +79,34 @@ app.post('/users', async (req, res) => {
   }
 })
 
-// Start defining your routes here
-app.get('/secret', (req, res) => {
-  res.json({secret: 'Hello world'})
+// // Start defining your routes here
+// app.get('/secret', (req, res) => {
+//   res.json({secret: 'Hello world'})
+// })
+
+app.get('/users/:id', authenticateUser);
+app.get('/users/:id', (req, res) => {
+  res.status(201).json({ name: req.user.name, id: req.user._id });
 })
+
+app.post('/sessions', async (req, res) => {
+
+  try {
+    const { name, password } = req.body;
+    const user = await User.findOne({ name });
+
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(201).json({ id: user._id, accessToken: user.accessToken });
+    } else {
+      res.status(404).json({ notFound: true })
+    }
+
+  } catch (err) {
+    res.status(404).json({ notFound: true });
+  }
+
+});
+
 
 
 
