bef6bc2fd7fd0a8cbf4fa7f782d7a3322fd16ee7
johannaraden <51335476+johannaraden@users.noreply.github.com>
2020-05-26 10:26:22 +0200

    added styling, style components

---
+++ frontend/src/lib/button.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import styled from 'styled-components/macro'
+
+
+const ButtonInput = styled.button`
+  margin-top: 0;
+  color: #4f4f4f;
+  text-align: center;
+  font-size: 1.2em;
+  font-size: 0.825rem;
+  text-align: center;
+  height: 2.5em;
+  margin-top: 1em;
+  border-radius: 300px;
+  &:hover {
+    background: palevioletred;
+    color: white;
+  }
+`
+
+
+export const Button = ({title}) => {
+  return (
+    <ButtonInput>
+      {title}
+    </ButtonInput>
+  )
+}
