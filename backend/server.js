41d5783ecfc672391f6f57fb5a5ec1f7d42a7361
Olle Alm <Olle@Olles-MacBook-Pro-2.local>
2020-05-26 12:53:36 +0200

    backend

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,11 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
+import bcrypt from "bcrypt"
+
+import User from './models/users'
+
+
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
@@ -18,10 +23,110 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
+
+
+const authenticator = async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      accessToken: req.header("Authorization")
+    })
+    if (user) {
+      req.user = user
+      next()
+    } else {
+      res.status(401).json({
+        loggedOut: true,
+        message: "Try again.."
+      })
+    }
+  } catch (err) {
+    res.status(403).json({
+      message: "access token missing or wrong",
+      errors: err.errors
+    })
+  }
+}
+
+
+
+
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
+
+
+
+
+
+
+
+//make a user -- google creating a signup flow for gmail
+app.post("/users", async (req, res) => {
+  try {
+    const {
+      name,
+      password
+    } = req.body
+    const user = new User({
+      name,
+      password: bcrypt.hashSync(password)
+    })
+    const saved = await user.save()
+    res.status(201).json(saved)
+  } catch (err) {
+    res.status(400).json({
+      message: "could not save user",
+      errors: err.errors
+    })
+  }
+})
+// log in
+app.post("/sessions", async (req, res) => {
+  try {
+    const {
+      name,
+      password
+    } = req.body
+    const user = await User.findOne({
+      name
+    })
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(201).json({
+        userId: user._id,
+        accessToken: user.accessToken
+      })
+    } else {
+      res.json({
+        notFound: true
+      })
+    }
+  } catch (err) {
+    res.status(400).json({
+      message: "could not log in user",
+      error: err.errors
+    })
+  }
+})
+// secure endpoint after log in
+app.get("/users/:id", authenticator)
+app.get("/users/:id", (req, res) => {
+  try {
+    res.status(201).json(req.user)
+  } catch (err) {
+    res.status(400).json({
+      message: "could not save user",
+      errors: err.errors
+    })
+  }
+})
+
+
+
+
+
+
+
 
 // Start the server
 app.listen(port, () => {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+
+const URL = "http://localhost:8080/users";
 
 export const App = () => {
+
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+
+
   return (
     <div>
       Find me in src/app.js!
