f4932db42e13e4e63b6b07065068c28568b2a460
sara <sara.lovens.lovn@gmail.com>
2020-05-27 16:55:47 +0200

    added status messages

--- frontend/src/components/LogIn.js
+++ frontend/src/components/LogIn.js
@@ -1,67 +1,64 @@
-import React, { useState } from 'react'
-import { Form } from '../shared/shared'
-import { Button } from '../shared/shared'
+import React, { useState } from 'react';
+import { Form } from '../shared/shared';
+import { Button } from '../shared/shared';
 
 export const LogIn = ({ currentUser, setCurrentUser, setLoggedIn }) => {
-  const [userName, setUserName] = useState('')
-  const [userPassword, setUserPassword] = useState('')
+	const [ userName, setUserName ] = useState('');
+	const [ userPassword, setUserPassword ] = useState('');
+	const [ message, setMessage ] = useState('');
+	const handleLogin = async (event) => {
+		event.preventDefault();
+		let user = {
+			name: userName,
+			password: userPassword
+		};
+		console.log(user);
+		let response = await fetch('http://localhost:8080/sessions', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json;charset=utf-8'
+			},
+			body: JSON.stringify(user)
+		});
 
-  const handleLogin = async (event) => {
-    event.preventDefault()
-    let user = {
-      name: userName,
-      password: userPassword
-    }
-    console.log(user)
-    let response = await fetch('http://localhost:8080/sessions', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8'
-      },
-      body: JSON.stringify(user)
-    })
+		let result = await response.json();
+		if (result.userId) {
+			console.log('fetch result', result);
 
-    let result = await response.json()
-    console.log("fetch result", result)
+			setUserName('');
+			setUserPassword('');
+			setCurrentUser({ userId: result.userId, accessToken: result.accessToken });
+			setLoggedIn(true);
+		} else {
+			setMessage('Sorry user not found');
+		}
+	};
 
-    setUserName('')
-    setUserPassword('')
-    setCurrentUser({userId:result.userId, accessToken:result.accessToken})
-    setLoggedIn(true)
-   
-  }
-
-
-
-  return (
-    <Form onSubmit={handleLogin}>
-      <h1>
-        LOG IN HERE:
-      </h1>
-      <label>
-        Name:
-      <input
-          type='text'
-          required
-          value={userName}
-          onChange={e => setUserName(e.target.value)}
-          placeholder='name'>
-        </input>
-      </label>
-      <label>
-        Password:
-      <input
-          type='password'
-          required
-          value={userPassword}
-          onChange={e => setUserPassword(e.target.value)}
-          placeholder="password">
-        </input>
-      </label>
-      <Button
-        type='submit'>
-        ENTER
-      </Button>
-    </Form>
-  )
-}
+	return (
+		<Form onSubmit={handleLogin}>
+			<h1>LOG IN HERE:</h1>
+			{message && <p>{message}</p>}
+			<label>
+				Name:
+				<input
+					type="text"
+					required
+					value={userName}
+					onChange={(e) => setUserName(e.target.value)}
+					placeholder="name"
+				/>
+			</label>
+			<label>
+				Password:
+				<input
+					type="password"
+					required
+					value={userPassword}
+					onChange={(e) => setUserPassword(e.target.value)}
+					placeholder="password"
+				/>
+			</label>
+			<Button type="submit">ENTER</Button>
+		</Form>
+	);
+};
--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -1,47 +1,43 @@
-import React, { useState, useEffect } from 'react'
-import { Button } from '../shared/shared'
+import React, { useState, useEffect } from 'react';
+import { Button } from '../shared/shared';
 
 export const Welcome = ({ currentUser, setCurrentUser, setLoggedIn }) => {
-  const username = "bob"
-  const [user, setUser] = useState('')
-  console.log(currentUser)
-  useEffect(() => {
-    const fetchData = async () => {
-      let response = await fetch(`http://localhost:8080/users/${currentUser._id}`, {
-        method: 'GET',
-        headers: {
-          'Authorization': currentUser.accessToken,
-          'Content-Type': 'application/json;charset=utf-8'
-        },
+	const username = 'bob';
+	const [ user, setUser ] = useState('');
+	console.log(currentUser);
+	useEffect(
+		() => {
+			const fetchData = async () => {
+				let response = await fetch(`http://localhost:8080/users/${currentUser._id}`, {
+					method: 'GET',
+					headers: {
+						Authorization: currentUser.accessToken,
+						'Content-Type': 'application/json;charset=utf-8'
+					}
+				});
 
-      })
+				let result = await response.json();
+				console.log('authorization result', result);
 
-      let result = await response.json()
-      console.log("authorization result", result)
-     
-      if (result.name) {
-         setUser(result.name)
-      }
-      else {
-        setLoggedIn(false)
-      }
-    }
-    fetchData()
+				if (result.name) {
+					setUser(result.name);
+				} else {
+					setLoggedIn(false);
+				}
+			};
+			fetchData();
 
-    return () => {
-      setCurrentUser({})
-    }
-  }, [currentUser])
+			return () => {
+				setCurrentUser({});
+			};
+		},
+		[ currentUser ]
+	);
 
-
-  return (
-    <div>
-      <h1>
-        HELLO {user} 👋🏼
-      </h1>
-      <Button onClick={()=>setLoggedIn(false)}>
-        SIGN OUT
-      </Button>
-    </div>
-  )
-}
+	return (
+		<div>
+			<h1>HELLO {user.toUpperCase()} 👋🏼</h1>
+			<Button onClick={() => setLoggedIn(false)}>SIGN OUT</Button>
+		</div>
+	);
+};
