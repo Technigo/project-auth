35d51cff9cc363f00850a8ec8f737813660c0d7d
ebbabw <ebbabergholtz@gmail.com>
2020-05-29 11:36:02 +0200

    added changes in wrapper

--- frontend/src/lib/Card.js
+++ frontend/src/lib/Card.js
@@ -3,7 +3,7 @@ import styled from 'styled-components/macro'
 
 export const Card = styled.div`
 
-    width: 500px;
+    width: 800px;
     height: 400px;
     background-color: white;
     display: flex;
--- frontend/src/lib/Wrapper.js
+++ frontend/src/lib/Wrapper.js
@@ -4,6 +4,7 @@ export const Wrapper = styled.div`
     display: flex;
     flex-direction: column;
     margin-top: 100px;
+    justify-content: center;
     @media (min-width: 768px) {
         flex-direction: row;
       
