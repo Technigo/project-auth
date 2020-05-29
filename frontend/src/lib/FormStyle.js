2fe045bcdc35941b0db0adf7e12f8666e93246c0
ebbabw <ebbabergholtz@gmail.com>
2020-05-29 11:29:23 +0200

    updated formstyle

--- frontend/src/lib/FormStyle.js
+++ frontend/src/lib/FormStyle.js
@@ -2,18 +2,19 @@ import styled from 'styled-components/macro'
 
 
 export const Article = styled.div`
-
     display: flex;
     width: 400px;
+    height: 300px; 
     flexdirection: column;
     flexwrap: wrap;
     border: solid 2px black;
     margin: 10px;
-
+    @media (min-width: 768px) {
+        width: 400px;
+        height: 300px; 
 `
 
 export const From = styled.form`
-
     margin: 10px;
     width: 200px
  
@@ -21,8 +22,5 @@ export const From = styled.form`
 
 
 export const Input = styled.input`
-
    margin: 10px;
-
-
 `
