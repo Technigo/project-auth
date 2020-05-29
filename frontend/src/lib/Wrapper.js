430d0e8f2c54fba22d273fdf75f79ba64483281f
ebbabw <ebbabergholtz@gmail.com>
2020-05-29 12:14:19 +0200

    small change

--- frontend/src/lib/Wrapper.js
+++ frontend/src/lib/Wrapper.js
@@ -3,7 +3,7 @@ import styled from 'styled-components/macro'
 export const Wrapper = styled.div`
     display: flex;
     flex-direction: column;
-    margin-top: 100px;
+    margin-top: 50px;
     align-items: center;
     width: 100%;
     
