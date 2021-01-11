4165e8a53bdc2d1cd835d7daa21976524003372e
Annika Melin <melin.anki>
2021-01-11 20:11:48 +0100

    added log in endpoint and authenticated endpoint

--- backend/server.js
+++ backend/server.js
@@ -11,12 +11,12 @@ mongoose.Promise = Promise
 mongoose.set('useCreateIndex', true)
 
 const User = mongoose.model('User', {
-  name: {
+  name: { // use min and maxlength?
     type: String,
     unique: true,
     required: true
   },
-  password: {
+  password: { // use min and maxlength, maybe have a couple of numbers?
     type: String,
     required: true
   },
@@ -37,6 +37,20 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
+const authenticateUser = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ accessToken: req.header('Authorization') })
+    if (user) {
+      req.user = user
+      next()
+    } else {
+      res.status(401).json({ loggedOut: true, message: 'Please try and log in again' })
+    }
+  } catch (err) {
+    res.status(403).json({ message: 'Access token is missing or not valid', errors: err })
+  }
+}
+
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
@@ -52,6 +66,29 @@ app.post('/users', async (req, res) => {
   } catch (err) {
     res.status(400).json({ message: 'Could not create user', errors: err })
   }
+})
+
+// Endpoint that creates a login
+app.post('/sessions', async (req, res) => {
+  try {
+    const { name, password } = req.body
+    const user = await User.findOne({ name })
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(200).json({ userId: user._id, accessToken: user.accessToken })
+    } else {
+      res.status(404).json({ notFound: true })
+    }
+  } catch (err) {
+    res.status(404).json({ message: 'Could not log in user', errors: err })
+  }
+})
+
+// Endpoint that shows a page to the user when logged in
+// update endpoints to /users/:id/settings
+app.get('/secrets', authenticateUser)
+app.get('/secrets', (req, res) => {
+  const secretMessage = `This is a secret message for ${req.user.name}`
+  res.status(201).json({ secretMessage })
 })
 
 // Start the server
