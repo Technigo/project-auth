3aebc3a373bc8c19002b3bbc54ce2e520e5d9cec
Joel Harde <joelharde@Joels-MacBook-Pro.local>
2021-01-11 16:50:13 +0100

    Added form for sign up and login

--- frontend/src/Start.js
+++ frontend/src/Start.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Start = () => {
-  return <h1>hi</h1>;
+  return (
+    <>
+      <h1>hi</h1>
+      <form>
+        <label>
+          <p>Username:</p>
+          <input type="text" />
+        </label>
+        <label>
+          <p>Password:</p>
+          <input type="text" />
+        </label>
+        <button type="submit">Sign up</button>
+      </form>
+    </>
+  );
 };
 
 export default Start;
