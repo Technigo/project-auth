dda5ee4ceea0ffc52366a53d336dbf8085269e93
Nadia Mazzoni <Nadia@Nadias-MBP.lan>
2021-01-11 21:11:42 +0100

    working users endpoint

--- backend/server.js
+++ backend/server.js
@@ -4,31 +4,36 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import crypto from 'crypto';
 import bcrypt from 'bcrypt';
+import listEndpoints from 'express-list-endpoints';
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.set('useCreateIndex', true)
 mongoose.Promise = Promise
 
 const User = mongoose.model('User', {
   name: {
-    type: String
-    // unique: true,
-    // required: true
+    type: String,
+    unique: true,
+    required: true
   },
   email: {
-    type: String
-    // unique: true,
+    type: String,
+    unique: true
   },
   password: {
-    type: String
-    //required: true,
-  },
+    type: String,
+    required: true,
+  }, 
   accessToken: {
     type: String,
-    default: () => crypto.randomBytes(128).toString('hex'),
+    default: () => crypto.randomBytes(128).toString('hex')
 
   }
 })
+
+
+
 
 const port = process.env.PORT || 8080
 const app = express()
@@ -40,36 +45,25 @@ app.use(bodyParser.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world');
+  res.send(listEndpoints(app));
 })
 
-//? SIGN UP
+// SIGN UP
 app.post('/users', async (req, res) => {
   try {
-    const { name } = req.body;
-    const user = await new User({ name });
+    const { name, email, password } = req.body;
+    const salt = bcrypt.genSaltSync(10);
     
+    const user = new User({ name, email, password: bcrypt.hashSync(password, salt) });
     const newUser = await user.save();
 
-    res.status(201).json({ 
-      id: newUser._id, 
-      accessToken: newUser.accessToken
-    });
-  } catch (err) {
-    res.status(400).json({message:'Could not create user', errors: err.errors})
-
+    res.status(201).json({ userId: newUser._id, accessToken: newUser.accessToken });
+  } catch (error) {
+    res.status(400).json({ message: 'Could not create user', error });
   }
-})
+});
 
-app.get('/users', async (req, res) => {
-  try {
-  const savedUsers = await user.find();
-  res.json(savedUsers);
-  // res.status(201).json({ message: 'Succes!'});
-} catch (err) {
-  res.status(400).json({ message: 'Bad request, could not find saved user', errors: err.errors });
-}
-})
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
