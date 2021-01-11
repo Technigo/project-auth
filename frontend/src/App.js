2f2374c097336983f4678ec41434d3ecbfb59359
Hanna Skeppe <hannaskeppe@gmail.com>
2021-01-11 16:55:19 +0100

    Started setting up components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Provider } from 'react-redux'
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+
+import { LoginForm } from './components/LoginForm'
+import { Profile } from './components/Profile'
+import { SignUpForm } from './components/SignUpForm'
+import { user } from './reducers/user'
+
+//FETCH (THUNK?)
+
+const URL = "http://localhost:8080/users"
+
+const reducer = combineReducers({ user: user.reducer });
+
+const store = configureStore({ reducer });
 
 export const App = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+
+  //Sign up
+
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <Provider store={store}>
+      <SignUpForm />
+      <LoginForm />
+      <Profile />
+    </Provider>
   )
 }
