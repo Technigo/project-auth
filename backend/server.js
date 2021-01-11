923377dbbd99828abffbf9d53621cd05978844ec
Moa Blomkvist <moa@birgerochblom.se>
2021-01-11 15:40:11 +0100

    authorization

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,43 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import crypto from 'crypto'
+import bcrypt from 'bcrypt-nodejs'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
+
+const User = mongoose.model('User', {
+  name:{
+    type: String,
+    unique: true
+  },
+  email:{
+    type: String,
+    unique: true
+  },
+  password:{
+    type: String,
+    required: true
+  },
+  accessToken:{
+    type: String,
+    default: () => crypto.randomBytes(128).toString('hex')
+  }
+});
+
+//Create a middleware
+
+const authenticateUser = async (req, res, next) => {
+  const user = await User.findOne({accessToken: req.header('Authorization')});
+  if (user) {
+    req.user = user;
+    next();
+  } else {
+    res.status(401).json({loggedOut: true});
+  }
+};
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -22,6 +55,31 @@ app.use(bodyParser.json())
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
+
+app.post('/users', async (req, res) => {
+  try {
+    const {name, email, password} = req.body;
+    const user = new User({name, email, password: bcrypt.hashSync(password)})
+    user.save()
+    res.status(201).json({id: user._id, accessToken: user.accessToken})
+  } catch (err) {
+    res.status(400).json({message: 'Could not create user', error:err,error});
+  }
+});
+
+app.get('/secrets', authenticateUser);
+app.get('/secrets', (req, res) => {
+  res.json({secret: 'Secret message'});
+});
+
+app.post('/sessions', async (req, res) => {
+  const user = await User.findOne({email: req.body.email})
+  if (user && bcrypt.compareSync(req.body.password, user.password)) {
+    res.json({userId: user._id, accessToken: user.accessToken})
+  } else {
+    res.json({notFound: true})
+  }
+});
 
 // Start the server
 app.listen(port, () => {
