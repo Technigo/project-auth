b76152dc65688bee48e24b3edc28ca3f38168522
Annika Melin <melin.anki>
2021-01-11 16:08:55 +0100

    created endpoint that creates new user and user model

--- backend/server.js
+++ backend/server.js
@@ -2,16 +2,35 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import crypto from 'crypto'
+import bcrypt from 'bcrypt'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
+mongoose.set('useCreateIndex', true)
+
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString('hex')
+  }
+})
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8081
 const app = express()
 
 // Add middlewares to enable cors and json body parsing
@@ -21,6 +40,18 @@ app.use(bodyParser.json())
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+// Endpoint that creates a new user
+app.post('/users', async (req, res) => {
+  try {
+    const { name, password } = req.body
+    const salt = bcrypt.genSaltSync(10)
+    const user = await new User({ name, password: bcrypt.hashSync(password, salt) }).save()
+    res.status(201).json({ userId: user._id, accessToken: user.accessToken })
+  } catch (err) {
+    res.status(400).json({ message: 'Could not create user', errors: err })
+  }
 })
 
 // Start the server
