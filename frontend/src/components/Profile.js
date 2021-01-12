5ab791ed5e200dc7b7e0e6dad74618bd66e6077b
Karan Mann <62376840+karanmann@users.noreply.github.com>
2021-01-12 11:59:18 +0100

    Front End structure done

---
+++ frontend/src/components/Profile.js
@@ -0,0 +1,51 @@
+import React, {useEffect, useState} from 'react'
+import {user} from '../reducers/user'
+import {useDispatch, useSelector} from 'react-redux'
+
+const URL = 'http://localhost:8080/users'
+
+export const Profile = () => {
+  const dispatch = useDispatch()
+  const accessToken = useSelector((store) => store.user.login.accessToken)
+  const userId = useSelector((store) => store.user.login.userId)
+  const statusMessage = useSelector((store) => store.user.login.statusMessage)
+
+  const loginSuccess = (loginResponse) => {}
+
+  const loginFailed = (loginError) => {}
+
+  const logout = () => {}
+
+  const login = () => {
+    // Include userId in the path
+    fetch(`${URL}/${userId}`, {
+      method: 'GET',
+      // Include the accessToken to get the protected endpoint
+      headers: { Authorization: accessToken },
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw 'Profile test failed'
+        }
+        return res.json()
+      })
+      // SUCCESS: Do something with the information we got back
+      .then((json) => loginSuccess(json))
+      .catch((err) => loginFailed(err))//401
+  }
+  if (!accessToken) {
+    return <></>
+  }
+
+  return (
+    <section class='profile'>
+      <h2>Profile:</h2>
+      <h4>userId:</h4>
+      <p> {`${userId}`}</p>
+      <h4>accessToken:</h4>
+      <p> {`${accessToken}`}</p>
+      <input type='submit' onClick={login} value='Test Login' />
+      <input type='submit' onClick={logout} value='Test Logout' />
+    </section>
+  )
+}
---
+++ frontend/src/reducers/user.js
@@ -0,0 +1,35 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState= {
+  login: {
+    accessToken: null,
+    userId: 0,
+    statusMessage:'',
+  }
+}
+
+export const user = createSlice({
+  name:'user',
+  initialState: initialState,
+  reducers: {
+    setAccessToken: ( state, action) => {
+      const {accessToken} = action.payload
+      console.log(`Access token: ${accessToken}`)
+      state.login.accessToken = accessToken
+    },
+    SetUserId: (state, action) => {
+      const { userId } = action.payload
+      console.log(`User id: ${userId}`)
+      state.login.userId = userId
+    },
+    setStatusMessage: (state, action) => {
+      const { statusMessage } = action.payload
+      state.login.statusMessage = statusMessage
+    },
+    logout: (state, action) => {
+      console.log('logging out')
+      state.login.userId = 0
+      state.login.accessToken = null
+    }, 
+  }
+})
