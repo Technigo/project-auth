051ad59ac78596857b28a8df196e1512c238a998
Katsiez <katarinajovanovic.a@gmail.com>
2021-01-12 17:35:50 +0100

    set up basic structure in app.js, partially

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {Login} from "./components/Login"
+import {Provider} from "react-redux"
+import {configureStore, combineReducers} from "@reduxjs/toolkit"
+import {user} from "./reducers/user"
+
+const URL = "https://project-auth-liza-kat.herokuapp.com/users"
+
+const reducer = combineReducers({user: user.reducer})
+const store = configureStore({reducer})
 
 export const App = () => {
+  const [name, setName] = useState("")
+  const [password, setPassword] = useState("")
+
+  //Sing up a user
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+		fetch(URL, {
+			method: "POST",
+			body: JSON.stringify({name, password}),
+			headers: {"Content-Type": "application/json"}
+		})
+		.then((res) => res.json())
+		.then((json) => console.log(json))
+		.catch((err) => console.log("error:", err))
+  }
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <Provider store ={store}>
+     <Login/>
+     </Provider>
   )
 }
