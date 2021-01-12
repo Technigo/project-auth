21e7505da50c598e83bdb1871c6341ede80a3f56
Olof Lindahl <olindahl@gmail.com>
2021-01-12 19:00:28 +0100

    cant get basic post to work

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,28 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import crypto from 'crypto'
+import bcrypt from 'bcrypt'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
+
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+    required: true,
+  },
+  // password: {
+  //   type: String,
+  //   required: true,
+  // },
+  // accessToken: {
+  //   type: String,
+  //   default: () => crypto.randomBytes(128).toString('hex'),
+  //   unique: true,
+  // },
+});
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -22,6 +40,23 @@ app.use(bodyParser.json())
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
+
+//registration endpoint
+app.post('/users', async (req, res) => {
+  try {
+    // const {name, password} = req.body
+    const {name} = req.body
+    
+    // const user = new User({name, password: bcrypt.hashSync(password)})
+    const user = await new User({name}).save()
+    // res.status(201).json({id: user._id, accessToken: user.accessToken})
+  } catch (err) {
+    res.status(400).json({message: 'Could not create user', errors: err.errors})
+  }
+
+}
+
+)
 
 // Start the server
 app.listen(port, () => {
