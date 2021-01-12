c130aff0d38caefccd5fff5e4a5f87cbc16f0b61
Nadia Mazzoni <Nadia@Nadias-MBP.lan>
2021-01-12 12:14:47 +0100

    Added enpoints: login + secure w/ authentication. Working. Basic frontend, not working

--- backend/server.js
+++ backend/server.js
@@ -42,13 +42,32 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
+// ? Checking users access token in db, setting user in req so can be used in protected endpoint
+const authenticateUser = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ 
+      accessToken: req.header('Authorization'),
+    });
+
+    if (user) {
+      req.user = user;
+      next();
+    } else {
+      res.status(401).json({ loggedOut: true, message: 'Please try logging in again' });
+    }
+
+  } catch (err) {
+    res.status(403).json({ message: 'Access denied', errors: err });
+  }
+};
+
 
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send(listEndpoints(app));
 })
 
-// SIGN UP
+// SIGN UP - create user
 app.post('/users', async (req, res) => {
   try {
     const { name, email, password } = req.body;
@@ -58,8 +77,30 @@ app.post('/users', async (req, res) => {
     const newUser = await user.save();
 
     res.status(201).json({ userId: newUser._id, accessToken: newUser.accessToken });
-  } catch (error) {
-    res.status(400).json({ message: 'Could not create user', error });
+  } catch (err) {
+    res.status(400).json({ message: 'Could not create user', errors: err });
+  }
+});
+
+//? secure endpoint, user needs to be logged access this
+app.get('/users/:id', authenticateUser);
+app.get('/users/:id', (req, res) => {
+  res.status(201).json({ name: req.user.name, userId: req.user._id });
+
+})
+
+// Login user and compare password match user
+app.post('/sessions', async (req, res) => {
+  try {
+    const { name, password } = req.body;
+    const user = await User.findOne({ name });
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(201).json({ userId: user._id, accessToken: user.accessToken });
+    } else {
+      res.json({ notFound: true });
+    }
+  } catch (err) {
+    res.status(400).json({ message: 'Could not log in', errors: err });
   }
 });
 
