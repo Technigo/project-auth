da77473df729c550ae811a8711941bce6a586845
Joel Harde <joelharde@Joels-MacBook-Pro.local>
2021-01-12 20:08:15 +0100

    Signup form done in frontend

--- frontend/src/Start.js
+++ frontend/src/Start.js
@@ -1,19 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Start = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleOnClick = (event) => {
+    event.preventDefault();
+
+    fetch("http://localhost:8080/users", {
+      method: "POST",
+      body: JSON.stringify({ name: username, password: password }),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        setUsername("");
+        setPassword("");
+      });
+  };
+
   return (
     <>
       <h1>hi</h1>
       <form>
         <label>
           <p>Username:</p>
-          <input type="text" />
+          <input
+            type="text"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+            placeholder="Username"
+          />
         </label>
         <label>
           <p>Password:</p>
-          <input type="text" />
+          <input
+            type="text"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            placeholder="Password"
+          />
         </label>
-        <button type="submit">Sign up</button>
+        <button type="submit" onClick={handleOnClick}>
+          Sign up
+        </button>
       </form>
     </>
   );
