deaf86acd97ae63db06cd66941df5fa52dff523d
Sara Sundblom <sundblomsara@gmail.com>
2021-01-13 11:09:03 +0100

    login post request

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,6 @@ import mongoose from 'mongoose'
 import crypto from 'crypto'
 import bcrypt from 'bcrypt'
 
-
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
@@ -25,6 +24,7 @@ const User = mongoose.model('User', {
     default: () => crypto.randomBytes(128).toString('hex')
   }
 })
+
 const authenticateUser = async (req, res, next) => {
   const user = await User.findOne({ accessToken: req.header('Authorization') })
   if (user) {
@@ -45,6 +45,7 @@ app.get('/', (req, res) => {
   res.send('Hello world')
 })
 
+//Sign up 
 app.post('/users', async (req, res) => {
   try {
     const { name, password } = req.body
@@ -57,6 +58,22 @@ app.post('/users', async (req, res) => {
     res.status(400).json({ message: 'Could not create user', errors: err.errors })
   }
 })
+
+//Login
+app.post('/sessions', async (req, res) => {
+  try {
+    const {name, password} = req.body
+    const user = await User.findOne({name})
+    if(user && bcrypt.compareSync(password, user.password)){
+      res.status(200).json({userId:user._id, accessToken: user.accessToken})
+    } else {
+      throw 'User not found'
+    }
+  } catch(err) {
+    res.status(404).json({error:'User not found'})
+  }
+})
+
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
