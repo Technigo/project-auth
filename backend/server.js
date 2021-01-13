fc53f0c08c3a1fda3139a5ac32a3d3b59ad10b50
Agnes Harrysson Hansson <aagneshansson@hotmail.com>
2021-01-13 10:35:24 +0100

    Backend works with postman to create user

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,9 @@ import mongoose from 'mongoose';
 import crypto from "crypto";
 import bcrypt from 'bcrypt';
 
-const SALT = bcrypt.genSaltSync(10) // the 10 is for how much variation you want in the password 8
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
-
 
 const User = mongoose.model("User", { 
   name: {
@@ -22,17 +20,14 @@ const User = mongoose.model("User", {
   },
   accessToken: {
     type: String,
-    default: () => crypto.randomBytes(128).toString("Hex"),
+    default: () => crypto.randomBytes(128).toString('hex'),
     unique: true,
   },
 });
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
@@ -53,21 +48,15 @@ app.post("/users", async (req, res) => {
     const { name, password } = req.body;
       console.log(`Name: ${name}`);
       console.log(`Password: ${password}`);
+    const SALT = bcrypt.genSaltSync(); // the number between () is for how much variation you want in the password 8
     const user = await new User({
       name,
       password: bcrypt.hashSync(password, SALT),
     }).save();
-    res.status(200).json({ userId: user._id });
-
+    res.status(200).json({ userId: user._id, accessToken: user.accessToken });
   } catch (err) {
     res.status(400).json({ message: 'Could not create user', errors: err });
   }
-});
-
-// Secure endpoint, user needs to be logged in to access this 
-app.get("/users/:id", authenticateUser);
-app.get("/users/:id", (req, res) => {
-  res.status(501).send();
 });
 
 // Login user
@@ -76,14 +65,21 @@ app.post("/sessions", async (req, res) => {
     const { name, password } = req.body;
     const user = await User.findOne({ name });
 
-    if (user) {
+    if (user && bcrypt.compareSync(password, user.password)) {
       //Compare passwords
+      res.status(200).json({ userId: user._id, accessToken: user.accessToken });
     } else { 
-      throw "User not found"
+      throw 'User not found';
     }
   } catch (err) { 
-    res.status(401).json({ error: err });
+    res.status(404).json({ error: 'User not found' });
   }
+});
+
+// Secure endpoint, user needs to be logged in to access this 
+app.get("/users/:id", authenticateUser); // Do we need to keep this? 
+app.get("/users/:id", (req, res) => {
+  res.status(501).send();
 });
 
 // Start the server
