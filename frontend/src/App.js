9a35e6b086dc224d65dd43f2cccf5e38d1142865
caroline birgersson <caroline@birgerochblom.se>
2021-01-13 16:05:02 +0100

    added some styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,139 +1,268 @@
-import React, { useState, useEffect } from 'react'
-import styled from 'styled-components'
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 
 export const App = () => {
-  const [signup, setSignup] = useState(false)
-  const [login, setLogin] = useState(false)
-  const [welcome, setWelcome] = useState(false)
+  const [signup, setSignup] = useState(false);
+  const [login, setLogin] = useState(false);
+  const [welcome, setWelcome] = useState(false);
 
-  const [name, setName] = useState('')
-  const [password, setPassword] = useState('')
-  const [userId, setUserId] = useState('') // use for authenticated endpoint?
-  const [accessToken, setAccessToken] = useState('') // use for authenticated endpoint? enough with thise one?
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [userId, setUserId] = useState(""); // use for authenticated endpoint?
+  const [accessToken, setAccessToken] = useState(""); // use for authenticated endpoint? enough with thise one?
 
-  const SIGNUP_URL = 'http://localhost:8081/users'
-  const LOGIN_URL = 'http://localhost:8081/sessions'
+  const SIGNUP_URL = "http://localhost:8081/users";
+  const LOGIN_URL = "http://localhost:8081/sessions";
   // const WELCOME_URL = 'http://localhost:8081/welcome' // use for authenticated endpoint
 
-  const signupUser = event => {
-    event.preventDefault()
+  const signupUser = (event) => {
+    event.preventDefault();
     fetch(SIGNUP_URL, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ name, password })
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name, password }),
     })
-      .then(res => {
+      .then((res) => {
         if (!res.ok) {
-          throw new Error('Could not create user')
+          throw new Error("Could not create user");
         } else {
-          return res.json()
+          return res.json();
         }
       })
 
-      .then(json => {
-        console.log(json)
-        setUserId(json.userId)
-        setAccessToken(json.accessToken)
-        setSignup(false)
-        setWelcome(true)
+      .then((json) => {
+        console.log(json);
+        setUserId(json.userId);
+        setAccessToken(json.accessToken);
+        setSignup(false);
+        setWelcome(true);
       })
-      .catch(err => alert(err))
+      .catch((err) => alert(err))
       .finally(() => {
-        setName('')
-        setPassword('')
-      })
-  }
+        setName("");
+        setPassword("");
+      });
+  };
 
-  const loginUser = event => {
-    event.preventDefault()
+  const loginUser = (event) => {
+    event.preventDefault();
     fetch(LOGIN_URL, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ name, password })
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name, password }),
     })
-      .then(res => {
+      .then((res) => {
         if (!res.ok) {
-          throw new Error('Could not log in user')
+          throw new Error("Could not log in user");
         } else {
-          return res.json()
+          return res.json();
         }
       })
-      .then(json => {
-        console.log(json)
-        setUserId(json.userId)
-        setAccessToken(json.accessToken)
-        setLogin(false)
-        setWelcome(true)
+      .then((json) => {
+        console.log(json);
+        setUserId(json.userId);
+        setAccessToken(json.accessToken);
+        setLogin(false);
+        setWelcome(true);
       })
-      .catch(err => alert(err))
+      .catch((err) => alert(err))
       .finally(() => {
-        setName('')
-        setPassword('')
-      })
-  }
+        setName("");
+        setPassword("");
+      });
+  };
 
   return (
-    <div>
-      {!signup && !login && !welcome && // startpage
-        <>
-          <button onClick={() => setSignup(true)}>Sign up</button>
-          <button onClick={() => setLogin(true)}>Log in</button>
-        </>
-      }
-      {signup && !login && !welcome && // sign up page
-        <>
-          <p>Hi! Please sign up.</p>
-          <form onSubmit={signupUser}>
-            <label>Name
-              <input
-                value={name}
-                onChange={event => setName(event.target.value)}
-                required
-                minLength='5'
-              />
-            </label>
-            <label>Password
-              <input
-                value={password}
-                onChange={event => setPassword(event.target.value)}
-                required
-                minLength='5'
-              />
-            </label>
-            <button type='submit'>Submit</button>
-          </form>
-        </>
-      }
-      {login && !signup && !welcome && // log in page
-        <>
-          <p>Hi! Please login.</p>
-          <form onSubmit={loginUser}>
-            <label>Name
-              <input
-                value={name}
-                onChange={event => setName(event.target.value)}
-                required
-                minLength='5'
-              />
-            </label>
-            <label>Password
-              <input
-                value={password}
-                onChange={event => setPassword(event.target.value)}
-                required
-                minLength='5'
-              />
-            </label>
-            <button type='submit'>Submit</button>
-          </form>
-        </>
-      }
-      {welcome && !signup && !login && // welcome page
-        <>
-          <p>Hi!</p>
-          <button onClick={() => { setWelcome(false) && setLogin(true) && setAccessToken('') }}>Log out</button>
-        </>
-      }
-    </div >
-  )
-}
+      <Container>
+      {!signup &&
+        !login &&
+        !welcome && ( // startpage
+          <>
+            <Intro>Please join us by creating an account or log in.</Intro>
+            <SignupButton onClick={() => setSignup(true)}>Create account</SignupButton>
+            <LoginButton onClick={() => setLogin(true)}>Log in</LoginButton>
+          </>
+        )}
+      {signup &&
+        !login &&
+        !welcome && ( // sign up page
+          <>
+            <Intro>You're soon a part of us.</Intro>
+            <Form onSubmit={signupUser}>
+              <Label>
+                Create username
+                <SignupInput
+                  value={name}
+                  onChange={(event) => setName(event.target.value)}
+                  required
+                  minLength="5"
+                />
+              </Label>
+              <Label>
+                Create password
+                <SignupInput
+                  value={password}
+                  onChange={(event) => setPassword(event.target.value)}
+                  required
+                  minLength="5"
+                />
+              </Label>
+              <FinalSignupButton type="submit">Sign up</FinalSignupButton>
+            </Form>
+          </>
+        )}
+      {login &&
+        !signup &&
+        !welcome && ( // log in page
+          <>
+            <Intro>Log in to your account</Intro>
+            <Form onSubmit={loginUser}>
+              <Label>
+                Name
+                <LoginInput
+                  value={name}
+                  onChange={(event) => setName(event.target.value)}
+                  required
+                  minLength="5"
+                />
+              </Label>
+              <Label>
+                Password
+                <LoginInput
+                  value={password}
+                  onChange={(event) => setPassword(event.target.value)}
+                  required
+                  minLength="5"
+                />
+              </Label>
+              <FinalLoginButton type="submit">Log in</FinalLoginButton>
+            </Form>
+          </>
+        )}
+      {welcome &&
+        !signup &&
+        !login && ( // welcome page
+          <>
+            <Intro>Hi!</Intro>
+            <LogoutButton
+              onClick={() => {
+                setWelcome(false) && setLogin(true) && setAccessToken("");
+              }}
+            >
+              Log out
+            </LogoutButton>
+          </>
+        )}
+    </Container>
+  ); 
+};
+
+// Styling Startpage
+const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100vh;
+  padding: 100px 0;
+  background: #608578;
+`;
+
+const SignupButton = styled.button`
+  border: none;
+  border-radius: 25px;
+  padding: 20px;
+  background: #E3E2FF;
+  font-size: 24px;
+  font-weight: bold;
+  text-transform: uppercase;
+`;
+
+const LoginButton = styled.button`
+  margin: 20px;
+  padding: 20px;
+  border: none;
+  border-radius: 25px;
+  background: #1a7763;
+  font-size: 24px;
+  font-weight: bold;
+  text-transform: uppercase;
+`;
+
+// General styling
+
+const Intro = styled.p`
+  margin-bottom: 80px;
+  font-size: 32px;
+  font-weight: bold;
+  text-align: center;
+  color: #1F4E3F;
+`
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`
+
+const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+  margin: 10px;
+  font-size: 20px;
+  font-weight: bold;
+`
+
+// Styling Signup-page
+
+const SignupInput = styled.input`
+  border: none;
+  border-radius: 10px;
+  padding: 15px;
+  margin: 5px;
+  background: #E3E2FF;
+`
+
+const FinalSignupButton = styled.button`
+  margin-top: 60px;
+  padding: 15px;
+  border: none;
+  border-radius: 15px;
+  background: #E3E2FF;
+  font-weight: bold;
+  font-size: 18px;
+  text-transform: uppercase;
+  color: #1F4E3F;
+`
+
+// Styling Login-page
+
+const LoginInput = styled.input`
+  margin: 5px;
+  padding: 15px;
+  border: none;
+  border-radius: 10px;
+  background: #1a7763;
+`
+
+const FinalLoginButton = styled.button`
+  margin-top: 60px;
+  padding: 15px;
+  border: none;
+  border-radius: 15px;
+  background: #1a7763;
+  font-size: 18px;
+  font-weight: bold;
+  text-transform: uppercase;
+`
+
+// Styling Welcome-page
+
+const LogoutButton = styled.button`
+  margin-top: 60px;
+  padding: 15px;
+  border: none;
+  border-radius: 15px;
+  background: #1a7763;
+  font-size: 18px;
+  font-weight: bold;
+  text-transform: uppercase;
+`
