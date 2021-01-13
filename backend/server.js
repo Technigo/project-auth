22afe54ce37ebfccd6ab219fed66baab951e64ba
AnnaHellkvist <51534839+AnnaHellkvist@users.noreply.github.com>
2021-01-13 13:55:37 +0100

    added userSchema

--- backend/server.js
+++ backend/server.js
@@ -10,57 +10,7 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
 // Added schema to be able to make the validation on choosen password and not hashed value
-// const userSchema = new mongoose.Schema({
-//   name: {
-//     type: String,
-//     unique: true,
-//     required: true,
-//     minlength: 4,
-//     maxlength: 15,
-//   },
-//   password: {
-//     type: String,
-//     required: true,
-//     minlength: 8,
-//   },
-//   accessToken: {
-//     type: String,
-//     default: () => crypto.randomBytes(128).toString('hex'),
-//     unique: true,
-//   }
-// });
-
-// userSchema.pre('save', async function (next) {
-//   const user = this;
-
-//   if (!user.isModified('password')) {
-//     return next(); 
-//   }
-//   const salt = bcrypt.genSaltSync();
-//   user.password = bcrypt.hashSync(password, salt);
-
-//   next(); // Values are written to the database
-// })
-
-// const authenticateUser = async (req, res, next) => {
-//   try {
-//     const accessToken = req.header('Authorization'); //Another part of the request as body
-//     const user = await User.findOne({ accessToken });
-
-//     if (!user) {
-//       throw 'User not found!';
-//     }
-//     req.user = user; // have access to req.user in endpoint
-//     next();
-//   } catch (err) {
-//     const errorMessage = 'Please try logging in again';
-//     console.log(errorMessage);
-//     res.status(401).json({error: errorMessage})
-//   }
-// }
-
-// const User = mongoose.model('User', userSchema);
-const User = mongoose.model('User', {
+const userSchema = new mongoose.Schema({
   name: {
     type: String,
     unique: true,
@@ -79,6 +29,38 @@ const User = mongoose.model('User', {
     unique: true,
   }
 });
+
+userSchema.pre('save', async function (next) {
+  const user = this;
+
+  if (!user.isModified('password')) {
+    return next(); 
+  }
+  const salt = bcrypt.genSaltSync();
+  user.password = bcrypt.hashSync(password, salt);
+
+  next(); // Values are written to the database
+})
+
+// const authenticateUser = async (req, res, next) => {
+//   try {
+//     const accessToken = req.header('Authorization'); //Another part of the request as body
+//     const user = await User.findOne({ accessToken });
+
+//     if (!user) {
+//       throw 'User not found!';
+//     }
+//     req.user = user; // have access to req.user in endpoint
+//     next();
+//   } catch (err) {
+//     const errorMessage = 'Please try logging in again';
+//     console.log(errorMessage);
+//     res.status(401).json({error: errorMessage})
+//   }
+// }
+
+const User = mongoose.model('User', userSchema);
+
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -100,10 +82,9 @@ app.get('/', (req, res) => {
 app.post('/users', async (req, res) => {
   try {
     const { name, password } = req.body; // If we didnt use the schema we could do a validation here instead
-    const salt = bcrypt.genSaltSync();
     const user = await new User({
       name,
-      password: bcrypt.hashSync(password, salt),
+      password,
     }).save();
     res.status(200).json({ userId: user._id, accessToken : user.accessToken });
 
@@ -117,7 +98,10 @@ app.post('/sessions', async (req, res) => {
     const { name, password } = req.body;
     const user = await User.findOne({ name });
     if (user && bcrypt.compareSync(password, user.password)) {
-      res.status(200).json({userId: user._id, accessToken: user.accessToken })
+      res.status(200).json({
+        userId: user._id,
+        accessToken: user.accessToken
+      })
     } else {
       throw 'User not found';
     }
