2e07cf369f9340f3b2212430571aa6a0467aeff8
Anna <hasselstromanna@gmail.com>
2021-01-14 16:28:35 +0100

    Add deployed endpoints.

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -3,9 +3,9 @@ import { useDispatch, useSelector } from "react-redux";
 
 import { user } from "../reducers/user";
 
-const SIGNUP_URL = "http://localhost:8080/users";
+const SIGNUP_URL = "https://project-signup.herokuapp.com/users";
 //change to Heroku-url
-const LOGIN_URL = "http://localhost:8080/sessions";
+const LOGIN_URL = "https://project-signup.herokuapp.com/sessions";
 //change to Heroku-url
 
 export const Login = () => {
