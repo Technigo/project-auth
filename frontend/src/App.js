9c4157800e3f9c0f5c09d418d6ee9d183f4e5e9d
Sofia Sousa <sofiavazsousa@gmail.com>
2021-01-14 17:45:48 +0100

    Remove unused code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import { Provider } from "react-redux";
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 
 import { user } from "./reducers/user";
-import { SignIn} from "./components/SignIn";
-
-const URL = "http://localhost:8080/users";
+import { SignIn } from "./components/SignIn";
 
 const reducer = combineReducers({ user: user.reducer });
-
 const store = configureStore({ reducer });
 
 export const App = () => {
-    fetch(URL, {
-      method: "POST",
-      body: JSON.stringify({ name, password }),
-      headers: { "Content-Type": "application/json" },
-    })
-      .then((res) => res.json())
-      .then((json) => console.log(json))
-      .catch((err) => console.log("error:", err));
-      
   return (
     <Provider store={store}>
       <SignIn />
