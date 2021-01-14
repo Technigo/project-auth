d76b93aa6a578ad4cd5f55004b4957c2f67203ab
idsintehittapa <jblom15@gmail.com>
2021-01-14 10:44:06 +0100

    refactoring code

--- frontend/src/reducers/user.js
+++ frontend/src/reducers/user.js
@@ -43,7 +43,7 @@ export const user = createSlice({
 //____________THUNKS____________//
 //_______Sign up
 export const signUp = (name, email, password) => {
-  const SIGNUP_URL = "http://the-secret.herokuapp.com/signup"
+  const SIGNUP_URL = "https://the-secret.herokuapp.com/signup"
   return (dispatch) => {
     fetch(SIGNUP_URL, {
       method: 'POST',
@@ -86,7 +86,7 @@ export const signUp = (name, email, password) => {
 
 //_______Login
 export const login = (name, email, password) => {
-  const LOGIN_URL = 'http://the-secret.herokuapp.com/login'
+  const LOGIN_URL = 'https://the-secret.herokuapp.com/login'
   return (dispatch) => {
     fetch(LOGIN_URL, {
       method: 'POST',
@@ -119,7 +119,7 @@ export const login = (name, email, password) => {
 
 //_______Secret message
 export const getSecretMessage = () => {
-  const USERS_URL = 'http://the-secret.herokuapp.com/users'
+  const USERS_URL = 'https://the-secret.herokuapp.com/users'
   return (dispatch, getState) => {
     const accessToken = getState().user.login.accessToken
     const userId = getState().user.login.userId
