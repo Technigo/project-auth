08dabe90c6b2f1785ca8b443406390d9ad71da9a
Sara Heijkenskjöld <sara.heijkenskjold@gmail.com>
2021-01-14 13:40:53 +0100

    Restructured code, changed to schema, added endpoint and error variables

--- backend/server.js
+++ backend/server.js
@@ -1,117 +1,175 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import mongoose from 'mongoose'
-import crypto from 'crypto'
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import crypto from 'crypto';
 import bcrypt from 'bcrypt';
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/authAPI';
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
 
-const User = mongoose.model('User', {
+// Error variables
+const SERVICE_UNAVAILABLE = 'Service unavailable';
+const LOGIN_FAILED = 'Please try logging in again';
+const POST_FAILED = 'Could not create user';
+const USER_NOT_FOUND = 'User not found';
+const LOGOUT_FAILED = 'Could not log out';
+const ACCESS_DENIED = 'Access denied';
+
+// Schema
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: [2, 'Name is too short'],
+  },
   email: {
     type: String,
     unique: true,
     required: true,
-    minlength: [6, 'e-mail is too short']
   },
   password: {
     type: String,
-    required: true,
-    minlength: [5, 'Password must be at least 5 characters']
-  },
-  name: {
-    type: String,
-    required: false,
-    minlength: [2, 'Name is too short']
+    required: [true, 'Password is required'],
+    minlength: [5, 'Password must be at least 5 characters'],
   },
   accessToken: {
     type: String,
-    default: () => crypto.randomBytes(128).toString('hex')
-  }
+    default: () => crypto.randomBytes(128).toString('hex'),
+  },
 });
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+// Middleware to hash password before new user is saved
+userSchema.pre('save', async function (next) {
+  const user = this;
 
-// Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
+  if (!user.isModified('password')) {
+    return next();
+  }
 
+  const salt = bcrypt.genSaltSync();
+  user.password = bcrypt.hashSync(user.password, salt);
+
+  next();
+});
+
+// Middleware to authenticate user
 const authenticateUser = async (req, res, next) => {
   try {
     const user = await User.findOne({
       accessToken: req.header('Authorization'),
-    })
+    });
 
-    if (user) {
-      req.user = user;
-      next();
-    } else {
-      res.status(401).json({ loggedOut: true, message: 'Log in failed. Please try to log in again'});
+    if (!user) {
+      throw USER_NOT_FOUND;
     }
+    req.user = user;
+    next();
   } catch (err) {
-    res.status(403).json({ message: ' Acces token is missing or wrong', errors: err });
+    res.status(401).json({ message: LOGIN_FAILED, errors: err.errors });
   }
 };
 
-// Registration endpoint (creates user)
-app.post('/users', async (req, res) => {
-  try {
-    const { email, name, password } = req.body
-    const salt = bcrypt.genSaltSync();
-    const user = await new User({ 
-      email,
-      name, 
-      password: bcrypt.hashSync(password, salt)
-    }).save();
-    res.status(201).json({ userId: user._id, accessToken: user.accessToken }); //send user id and access token back to the user
-  } catch (err) {
-    res.status(400).json({ message: 'Could not create user', errors: err });
+// Model
+const User = mongoose.model('User', userSchema);
+
+// Defines the port
+const port = process.env.PORT || 8080;
+const app = express();
+
+// Add middlewares to enable cors and json body parsing
+app.use(cors());
+app.use(bodyParser.json());
+
+// Error message if server is down
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState === 1) {
+    next();
+  } else {
+    res.status(503).send({ error: SERVICE_UNAVAILABLE });
   }
 });
 
-//Secure endpoint, protected by authenticateUser. Should :id be here or not?
-//Looks up the user based on the access token stored in the header
-app.get('/users/secret', authenticateUser);
-app.get('/users/secret', (req, res) => {
-  const secretMessage = 'This is a secret message!'
-  res.status(201).json({ secretMessage });
+// GET - list of all endpoints
+const listEndpoints = require('express-list-endpoints');
+app.get('/endpoints', (req, res) => {
+  res.send(listEndpoints(app));
 });
 
-//Login user
+// POST - registration endpoint (creates user)
+app.post('/users', async (req, res) => {
+  try {
+    const { email, name, password } = req.body;
+    const user = await new User({
+      email,
+      name,
+      password,
+    }).save();
+    res.status(200).json({ userId: user._id, accessToken: user.accessToken }); //send user id and access token back to the user
+  } catch (err) {
+    res.status(400).json({ message: POST_FAILED, errors: err.errors });
+  }
+});
+
+// POST - login user
 app.post('/sessions', async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
     if (user && bcrypt.compareSync(password, user.password)) {
-      res.status(201).json({ userId: user._id, accessToken: user.accessToken });
+      user.accessToken = crypto.randomBytes(128).toString('hex');
+
+      const updatedUser = await user.save();
+      res.status(200).json({
+        userId: updatedUser._id,
+        accessToken: updatedUser.accessToken,
+      });
     } else {
-      throw "Login failed. User not found.";
+      throw USER_NOT_FOUND;
     }
   } catch (err) {
-    res.status(404).json({ message: 'Login failed. User not found.', errors: err });
+    res.status(404).json({ message: USER_NOT_FOUND, errors: err.errors });
   }
 });
 
-//Log out
-app.post('/users/logout', authenticateUser)
+//POST - logout user
+app.post('/users/logout', authenticateUser);
 app.post('/users/logout', async (req, res) => {
   try {
     req.user.accessToken = null;
     await req.user.save();
     res.status(200).json({ loggedOut: true });
   } catch (err) {
-    res.status(400).json({ error: 'Could not log out'});
+    res.status(400).json({ error: LOGOUT_FAILED });
+  }
+});
+
+// GET - secure endpoint, protected by authenticateUser. Should :id be here or not?
+//Looks up the user based on the access token stored in the header
+app.get('/users/secret', authenticateUser);
+app.get('/users/secret', (req, res) => {
+  const secretMessage = 'This is a secret message!';
+  res.status(201).json({ secretMessage });
+});
+
+// GET - user specific information
+app.get('/users/:id/secret', authenticateUser);
+app.get('/users/:id/secret', async (req, res) => {
+  try {
+    const userId = req.params.id;
+    if (userId != req.user._id) {
+      console.log('Wrong user');
+      throw ACCESS_DENIED;
+    }
+    const secretMessage = `This is a secret message for ${req.user.name}`;
+    res.status(200).json({ secretMessage });
+  } catch (err) {
+    res.status(403).json({ error: ACCESS_DENIED });
   }
 });
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
