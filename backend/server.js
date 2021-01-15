cbbc650359d3b2580386220e6f4bb905c70361e1
caroline birgersson <caroline@birgerochblom.se>
2021-01-15 10:40:55 +0100

    formatted the code in backend

--- backend/server.js
+++ backend/server.js
@@ -1,98 +1,107 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import mongoose from 'mongoose'
-import crypto from 'crypto'
-import bcrypt from 'bcrypt'
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+import crypto from "crypto";
+import bcrypt from "bcrypt";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
-mongoose.set('useCreateIndex', true)
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
+mongoose.set("useCreateIndex", true);
 
-const User = mongoose.model('User', {
-  name: { 
+const User = mongoose.model("User", {
+  name: {
     type: String,
     unique: true,
     required: true,
-    minlength: 5
+    minlength: 5,
   },
-  password: { 
+  password: {
     type: String,
     required: true,
-    minlength: 5
+    minlength: 5,
   },
   accessToken: {
     type: String,
-    default: () => crypto.randomBytes(128).toString('hex')
-  }
-})
+    default: () => crypto.randomBytes(128).toString("hex"),
+  },
+});
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
+// Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8081
-const app = express()
+const port = process.env.PORT || 8081;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(bodyParser.json());
 
 const authenticateUser = async (req, res, next) => {
   try {
-    const user = await User.findOne({ accessToken: req.header('Authorization') })
+    const user = await User.findOne({
+      accessToken: req.header("Authorization"),
+    });
     if (user) {
-      req.user = user
-      next()
+      req.user = user;
+      next();
     } else {
-      res.status(401).json({ loggedOut: true, message: 'Please try and log in again' })
+      res
+        .status(401)
+        .json({ loggedOut: true, message: "Try to log in again" });
     }
   } catch (err) {
-    res.status(403).json({ message: 'Access token is missing or not valid', errors: err })
+    res
+      .status(403)
+      .json({ message: "Access token is missing or not valid", errors: err });
   }
-}
+};
 
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
 
 // Endpoint that creates a new user
-app.post('/users', async (req, res) => {
+app.post("/users", async (req, res) => {
   try {
-    const { name, password } = req.body
-    const salt = bcrypt.genSaltSync(10)
-    const user = await new User({ name, password: bcrypt.hashSync(password, salt) }).save()
-    res.status(201).json({ userId: user._id, accessToken: user.accessToken })
+    const { name, password } = req.body;
+    const salt = bcrypt.genSaltSync(10);
+    const user = await new User({
+      name,
+      password: bcrypt.hashSync(password, salt),
+    }).save();
+    res.status(201).json({ userId: user._id, accessToken: user.accessToken });
   } catch (err) {
-    res.status(400).json({ message: 'Could not create user', errors: err })
+    res.status(400).json({ message: "Could not create user", errors: err });
   }
-})
+});
 
 // Endpoint that creates a login
-app.post('/sessions', async (req, res) => {
+app.post("/sessions", async (req, res) => {
   try {
-    const { name, password } = req.body
-    const user = await User.findOne({ name })
+    const { name, password } = req.body;
+    const user = await User.findOne({ name });
     if (user && bcrypt.compareSync(password, user.password)) {
-      res.status(200).json({ userId: user._id, accessToken: user.accessToken })
+      res.status(200).json({ userId: user._id, accessToken: user.accessToken });
     } else {
-      res.status(404).json({ notFound: true })
+      res.status(404).json({ notFound: true });
     }
   } catch (err) {
-    res.status(404).json({ message: 'Could not log in user', errors: err })
+    res.status(404).json({ message: "Could not log in user", errors: err });
   }
-})
+});
 
 // Endpoint that shows a page to the user when logged in
-app.get('/welcome', authenticateUser)
-app.get('/welcome', (req, res) => {
-  const welcomeMessage = `Hi ${req.user.name}! Nice to see you here.`
-  res.status(201).json({ welcomeMessage })
-})
+app.get("/welcome", authenticateUser);
+app.get("/welcome", (req, res) => {
+  const welcomeMessage = `Hi ${req.user.name}! Nice to see you here.`;
+  res.status(201).json({ welcomeMessage });
+});
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
