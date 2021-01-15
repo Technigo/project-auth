dd6e904d23324ebf17e3756aa3a7be3747cb06f2
Ylva Nilsson <ylva.z.nilsson@teliacompany.com>
2021-01-15 08:24:11 +0100

    clean up the server code, removed console log's

--- backend/server.js
+++ backend/server.js
@@ -13,12 +13,12 @@ mongoose.connect(mongoUrl, {
 });
 mongoose.Promise = Promise;
 
-const userSchema = new mongoose.Schema ({
+const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
-    minlength: 2,
-    maxlength: 40
+    minlength: [2, 'Name is too short'],
+    maxlength: [40, 'Name is too long']
   },
   email: {
     type: String,
@@ -27,28 +27,28 @@ const userSchema = new mongoose.Schema ({
   },
   password: {
     type: String,
-    required: true,
-    // minlength: 5
+    required: [true, 'Password is required'],
+    minlength: [5, 'Password must be at least 5 characters']
   },
   accessToken: {
     type: String,
     default: () => crypto.randomBytes(128).toString('hex')
   }
-})
+});
 
+// Middleware to hash password before new user is saved
 userSchema.pre('save', async function (next) {
   const user = this;
-  
+
   if (!user.isModified('password')) {
     return next();
   }
   const salt = bcrypt.genSaltSync();
-  console.log(`PRE-password before hash: ${user.password}`)
   user.password = bcrypt.hashSync(user.password, salt);
-  console.log(`PRE-password after hash: ${user.password}`)
   next();
-})
+});
 
+// Middleware to authenticate user
 const authenticateUser = async (req, res, next) => {
   try {
     const accessToken = req.header('Authorization');
@@ -59,18 +59,17 @@ const authenticateUser = async (req, res, next) => {
     req.user = user;
     next();
   } catch (err) {
-    const errorMessage = 'Please try loggin in again'
-    console.log(errorMessage)
-    res.status(401).json({ error: errorMessage })
+    const errorMessage = 'Please try logging in again';
+    res.status(401).json({ error: errorMessage });
   }
 };
 
-const User = mongoose.model('User', userSchema );
+// Model
+const User = mongoose.model('User', userSchema);
 
-//   PORT=9000 npm start
+// PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
-const endPointList = require('express-list-endpoints');
 
 // Add middlewares to enable cors and json body parsing
 
@@ -82,6 +81,8 @@ const endPointList = require('express-list-endpoints');
 app.use(cors());
 app.use(bodyParser.json());
 
+// GET - list of all endpoints
+const endPointList = require('express-list-endpoints');
 app.get('/', (req, res) => {
   if (!res) {
     res
@@ -109,16 +110,16 @@ app.get('/', (req, res) => {
 // Const's for error messages instead of text in error handling
 const SERVICE_UNAVAILABLE = 'Service unavailable.';
 
-// Error message in case database is down
+// Error message in case server is down
 app.use((req, res, next) => {
   if (mongoose.connection.readyState === 1) {
-    next(); // To execute next get response
+    next();
   } else {
     res.status(503).send({ error: SERVICE_UNAVAILABLE });
   }
 });
 
-// Signup a user
+// POST - Signup a user
 app.post('/signup', async (req, res) => {
   try {
     const { name, email, password } = req.body;
@@ -127,9 +128,7 @@ app.post('/signup', async (req, res) => {
       email,
       password
     }).save();
-    console.log('user', user)
-    res.status(201)
-    .json({ userId: user._id, accessToken: user.accessToken })
+    res.status(201).json({ userId: user._id, accessToken: user.accessToken });
   } catch (err) {
     res
       .status(400)
@@ -147,7 +146,6 @@ app.post('/login', async (req, res) => {
     } else {
       res.status(404).json({
         notFound: true,
-        //Test and checkagain the error message
         message: 'Username and/or password is not correct'
       });
     }
@@ -160,18 +158,19 @@ app.post('/login', async (req, res) => {
 });
 
 // Secure endpoint, user needs to be logged in to access this
+// Wanted to add authorization check (403) on :id but didn't get it fully to work
+// Decided to keep the endpoint as-is
 app.get('/users/:id/secret', authenticateUser);
 app.get('/users/:id/secret', async (req, res) => {
   try {
-    const user = await User.findOne({ _id: req.params.id });
     const secretMessage = `This is a super secret message for  ${req.user.name}`;
-      res.status(201).json({ secretMessage });
+    res.status(201).json({ secretMessage });
   } catch (err) {
     res.status(404).json({
-    notFound: true,
-    message: 'Oops! Something goes wrong. Try again later!'
-  });
-}
+      notFound: true,
+      message: 'Oops! Something goes wrong. Try again later!'
+    });
+  }
 });
 
 // Start the server
