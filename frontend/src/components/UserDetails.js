00055e939774402982974ba34f09c193502291ac
MindstormingAB <s.elander@mindstorming.se>
2021-01-15 11:01:12 +0100

    Improved logged out

--- frontend/src/components/UserDetails.js
+++ frontend/src/components/UserDetails.js
@@ -5,15 +5,27 @@ import { useHistory } from "react-router-dom";
 import LogoutButton from "./LogoutButton";
 
 const UserDetails = () => {
+  const accessToken = useSelector((store) => store.user.login.accessToken);
   const userId = useSelector((store) => store.user.login.userId);
   const history = useHistory();
 
   return (
     <section>
-      <h1>This is your profile page</h1>
-      <p>You are successfully logged in as member {userId}!</p>
-      <button onClick={() => history.push("/")}>Back</button>
-      <LogoutButton />
+      {accessToken &&
+        <>
+          <h1>This is your profile page</h1>
+          <p>You are successfully logged in as member {userId}!</p>
+          <button onClick={() => history.push("/")}>Back</button>
+          <LogoutButton />
+        </>
+      }
+      {!accessToken &&
+        <>
+          <h1>Logged out</h1>
+          <p>Your session has been terminated. Please log in again.</p>
+          <button onClick={() => history.push("/")}>Back to login</button>
+        </>
+      }
     </section>
   )
 };
