3b189f0b54fbe2d3dbb0448a3ec78d5d1af58900
AnnaHellkvist <51534839+AnnaHellkvist@users.noreply.github.com>
2021-01-15 10:28:14 +0100

    added/changed fetch on UserPage

--- frontend/src/components/UserPage.js
+++ frontend/src/components/UserPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useParams, useHistory } from 'react-router-dom'
 import { user } from '../reducer/user';
@@ -7,11 +7,29 @@ import { Button } from '../lib/Button';
 
 
 export const UserPage = ({ id }) => {
+    const [inlogMessage, setInlogMessage] = useState();
     const dispatch = useDispatch();
     const history = useHistory();
     const myUser = useSelector(store => store.user.login.userName);
     const statusMessage = useSelector(store => store.user.login.statusMessage);
     const accessToken = useSelector(store => store.user.login.accessToken);
+
+    useEffect(() => {
+        getUserInfo();
+    }, []); 
+
+    const getUserInfo = () => {
+        fetch(`https://auth-project-api.herokuapp.com/users/${id}/secret`, {
+            method: 'GET',
+            headers: { 'Authorization': accessToken},
+        })
+            .then(res => res.json())
+            .then(data => {
+                setInlogMessage(data);
+                console.log(data)
+            })
+    }
+
     
     const loginSuccess = (loginResponse) => {
         dispatch(user.actions.setStatusMessage({
