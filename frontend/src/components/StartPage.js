477f8815d830387a1a64c7f9e9987eb3d98f9228
MindstormingAB <s.elander@mindstorming.se>
2021-01-15 16:25:44 +0100

    Updated Start page

--- frontend/src/components/StartPage.js
+++ frontend/src/components/StartPage.js
@@ -1,15 +1,17 @@
 import React from "react";
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
 
 import DetailsButton from "./DetailsButton";
 import LogoutButton from "./LogoutButton";
 
 const StartPage = ({ SIGNUP_URL }) => {
-  const userId = useSelector((store) => store.user.login.userId)
+  // const userId = useSelector((store) => store.user.login.userId);
   return (
     <section>
       <h1>Max and Sandrine's app</h1>
-      <p>You are successfully logged in as member {userId}!</p>
+      {/* <p>You are successfully logged in as member {userId}!</p> */}
+      <p>You are successfully logged in!</p>
+      <p>You can update your profile by clicking on the button below.</p>
       <DetailsButton SIGNUP_URL={SIGNUP_URL} />
       <LogoutButton />
     </section>
