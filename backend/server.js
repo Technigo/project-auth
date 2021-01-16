1bc4c99a0f2c808e4c0a30f5c6136d4330c93e74
destinyleia <destinylindgrenc@gmail.com>
2021-01-16 18:29:34 +0100

    code didnt work so back to the old code

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-import express from 'express'
+import express from 'express' 
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
@@ -6,13 +6,14 @@ import crypto from 'crypto'
 import bcrypt from 'bcrypt'
 import listEndpoints from 'express-list-endpoints'
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/authAPI'
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
 const User = mongoose.model('User', {
   name: {
     type: String,
+    minlength:3,
     unique: true,
     required: true
   },
@@ -22,84 +23,65 @@ const User = mongoose.model('User', {
   },
   accessToken: {
     type: String,
-    default: () => crypto.randomBytes(128).toString('hex'),
-    unique: true
+    default: () => crypto.randomBytes(128).toString('hex')
   }
 })
 
 const authenticateUser = async (req, res, next) => {
   try {
-    const accessToken = req.header('Authorization')
-    const user = await User.findOne({ accessToken })
-    if (!user) {
-      throw 'Login error '
+    const user = await User.findOne({ accessToken: req.header('Authorization')})
+
+    if (user) {
+      req.user = user
+      next()
+    } else {
+      res.status(401).json({ loggedOut: true, message: 'Please try logging in again' })
     }
-    req.user = user
-    next()
   } catch (err) {
-    const errorMessage = 'Try to login again'
-    console.log(errorMessage)
-    res.status(401).json({ error: errorMessage })
+    res.status(403).json({ message: 'Access token is missing or wrong', errors: err })
   }
 }
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
 const port = process.env.PORT || 8080
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here
 app.get('/', (req, res) => {
   res.send(listEndpoints(app))
 })
 
-//salt adds som variation to the hash function, per user
-app.post('/users', async (req, res) => {
-  try {
+app.post('/users', async (req, res) => { 
+  try { 
     const { name, password } = req.body
-    //Do not store plaintext passwords!
-    const salt = bcrypt.genSaltSync(10)
-    const user = await new User({
-      name,
-      password: bcrypt.hashSync(password, salt)
-    }).save()
-    res.status(201).json({
-      userId: user._id,
-      accessToken: user.accessToken
-    })
+    const SALT = bcrypt.genSaltSync(10)
+    const user = await new User({ name, password: bcrypt.hashSync(password, SALT)}).save()
+    res.status(201).json({id: user._id, accessToken: user.accessToken})
   } catch (err) {
-    res.status(400).json({
-      message: 'Could not create user', errors: err.errors
-    })
+    res.status(400).json({message: "Could not create user", errors:err.errors})
   }
 })
 
-app.get('/secret', authenticateUser)
-app.get('/secret', (req, res) => {
-  const secretMessage = `This is a secret message for ${req.user.name}`
-  res.status(201).json({ secretMessage })
+app.get('/users/:id', authenticateUser)
+app.get('/users/:id', (req, res) => {
+  res.status(401).send()
 })
 
 app.post('/sessions', async (req, res) => {
   try {
-    const user = await User.findOne({ name: req.body.name })
-    if (user && bcrypt.compareSync(req.body.password, user.password)) {
+    const { name, password } = req.body
+    const user = await User.findOne({ name })
+    if (user && bcrypt.compareSync(password, user.password)) {
       res.status(201).json({ userId: user._id, accessToken: user.accessToken })
     } else {
-      throw 'User not found'
+      res.status(404).json({ notFound: true, message: "Check if username and/or password is correct" })
     }
-  } catch (error) {
-    res.status(404).json({ error: "User not found" })
+  } catch (err) {
+    res.status(404).json({ notFound: true, message: "Check if username and/or password is correct"})
   }
 })
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 })
