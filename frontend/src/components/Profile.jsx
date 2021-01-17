dff0bb897df8c7f988662224fafc7bf19c39735f
jonna ru <jonna.ru@icloud.com>
2021-01-17 10:57:03 +0100

    logout

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -3,6 +3,8 @@ import { user } from "../reducers/user";
 import { useDispatch, useSelector } from "react-redux";
 
 const URL = "http://localhost:8080/users";
+const SECRET_URL = "http://localhost:8080/secret";
+
 export const Profile = () => {
   const dispatch = useDispatch();
   const accessToken = useSelector((store) => store.user.login.accessToken);
@@ -10,15 +12,57 @@ export const Profile = () => {
   const name = useSelector((store) => store.user.login.name);
   const statusMessage = useSelector((store) => store.user.login.statusMessage);
 
-  const loginSuccess = (loginResponse) => {};
+  const loginSuccess = (loginResponse) => {
+    dispatch(
+      user.actions.setStatusMessage({
+        statusMessage: loginResponse.secretMessage,
+      })
+    );
+  };
 
-  const loginFailed = (loginError) => {};
+  const loginFailed = (loginError) => {
+    dispatch(user.actions.setAccessToken({ accessToken: null }));
+    dispatch(user.actions.setStatusMessage({ statusMessage: loginError }));
+  };
 
-  const logout = () => {};
+  const logoutSuccess = () => {
+    dispatch(
+      user.actions.setStatusMessage({
+        statusMessage: "Logout success",
+      })
+    );
+    dispatch(user.actions.setAccessToken({ accessToken: null }));
+  };
 
-  const login = () => {
+  const logoutFailed = (logoutError) => {
+    dispatch(
+      user.actions.setStatusMessage({
+        statusMessage: logoutError,
+      })
+    );
+  };
+
+  const logout = () => {
     // Include userId in the path
-    fetch(`${URL}/${userId}`, {
+    fetch(`${URL}/logout`, {
+      method: "POST",
+      // Include the accessToken to get the protected endpoint
+      headers: { Authorization: accessToken },
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw "Failed to logout";
+        }
+        return res.json();
+      })
+      // SUCCESS: Do something with the information we got back
+      .then((json) => logoutSuccess(json))
+      .catch((err) => logoutFailed(err));
+  };
+
+  const testSecret = () => {
+    // Include userId in the path
+    fetch(`${SECRET_URL}`, {
       method: "GET",
       // Include the accessToken to get the protected endpoint
       headers: { Authorization: accessToken },
@@ -46,7 +90,7 @@ export const Profile = () => {
       <p> {`${userId}`}</p>
       <h4>accessToken:</h4>
       <p> {`${accessToken}`}</p>
-      <input type="submit" onClick={login} value="Test Login" />
+      <input type="submit" onClick={testSecret} value="test secret" />
       <input type="submit" onClick={logout} value="Test Logout" />
     </section>
   );
