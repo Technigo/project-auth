1aef596f134cfed4f40df22bff251024b14b9457
Maksymilian Olszewski <olszewski.maksymilian@gmail.com>
2021-05-05 12:28:38 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import ReactDOM from 'react-dom';
-
-console.log('Hello');
 
 export const App = () => {
   return (
     <div>
+      Hello from Technigo!
       Find me in src/app.js!
     </div>
   )
