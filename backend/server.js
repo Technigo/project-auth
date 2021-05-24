2810232a1d822207b6a902b0b159da74be7c6ff7
Laima Duhovnaja <laima@macbook-air.mynet>
2021-05-24 20:55:07 +0200

    updated backend

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,133 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import crypto from "crypto";
+import bcrypt from "bcrypt";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI";
+mongoose.connect(mongoUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const Thoughts = mongoose.model("Thoughts", {
+  message: String,
+});
+
+const User = mongoose.model("User", {
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: 5,
+    maxlenght: 15,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    maxlenght: 15,
+  //   validate: {
+  //     validator: (value) => {
+  //       return /^(?=.*\d+)(?=.*[a-zA-Z])[0-9a-zA-Z!@#$%]{6,10}$/.test(value);
+  //     },
+  // },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString("hex"),
+  },
+});
+
+const authenticateUser = async (req, res, next) => {
+  const accessToken = req.header("Authorization");
+
+  try {
+    const user = await User.findOne({ accessToken });
+    if (user) {
+      next();
+    } else {
+      res.status(401).json({ message: "Not authorized" });
+    }
+  } catch (error) {
+    res.status(400).json({ message: "Invalid request", error });
+  }
+};
+
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(bodyParser.json())
+app.use(cors());
+app.use(express.json());
 
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.get("/thoughts", authenticateUser);
+app.get("/thoughts", async (req, res) => {
+  const thoughts = await Thoughts.find();
+  res.json(thoughts);
+});
+
+app.post("/thoughts", authenticateUser);
+app.post("/thoughts", async (req, res) => {
+  const { message } = req.body;
+  try {
+    const newThought = await new Thoughts({ message }).save();
+    res.json(newThought);
+  } catch (error) {
+    res.status(400).json({ message: "Invalid request", error });
+  }
+});
+
+// Post request for signing up
+app.post("/signup", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const salt = bcrypt.genSaltSync();
+
+    const newUser = await new User({
+      username,
+      password: bcrypt.hashSync(password, salt),
+    }).save();
+    res.json({
+      userID: newUser._id,
+      username: newUser.username,
+      accesToken: newUser.accessToken,
+    });
+  } catch (error) {
+    res.status(400).json({ message: "Invalid request", error });
+  }
+});
+
+// Post request for signing in
+app.post("/signin", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username });
+
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.json({
+        userID: user._id,
+        username: user.username,
+        accesToken: user.accessToken,
+      });
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    res.status(400).json({ message: "Invalid request", error });
+  }
+});
 
 // Start the server
 app.listen(port, () => {
   // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
