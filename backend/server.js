420a72e4171a80c460cd1a6412d30473625848ee
katliebig <liebig.kat@gmail.com>
2021-05-24 22:09:49 +0200

    created user and secret model, set up endpoints for registration, login, authorized endpoint for secrets

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,129 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import crypto from 'crypto'
+import bcrypt from 'bcrypt'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+const User = mongoose.model('User', {
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString('hex')
+  }
+})
+
+const Secret = mongoose.model('Secret', {
+  message: {
+    type: String,
+    required: true
+  }
+})
+
+const authenticateUser = async (req, res, next) => {
+  const accessToken = req.header('Authorization')
+
+  try {
+    const user = await User.findOne({ accessToken })
+    if (user) {
+      next()
+    } else {
+      res.status(401).json({ message: 'Not authorized' })
+    }
+  } catch (error) {
+    res.status(400).json({ message: "Invalid request", error })
+  }
+}
 const port = process.env.PORT || 8080
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors())
-app.use(bodyParser.json())
+app.use(express.json())
 
-// Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
 
-// Start the server
+app.get('/secrets', authenticateUser)
+app.get('/secrets', async (req, res) => {
+  const secrets = await Secret.find()
+  res.json(secrets)
+})
+
+app.post('/secrets', authenticateUser)
+app.post('/secrets', async (req, res) => {
+  const { message } = req.body
+
+  try {
+    const newSecret = await new Secret({
+      message
+    }).save()
+
+    res.json({
+      newSecret
+    })
+  } catch (error) {
+    res.status(400).json({ message: "Invalid request", error })
+  }
+})
+
+app.post('/users', async (req, res) => {
+  const { username, password, email } = req.body
+
+  try {
+    const salt = bcrypt.genSaltSync()
+    const newUser = await new User({
+      username,
+      email,
+      password: bcrypt.hashSync(password, salt)
+    }).save()
+
+    res.json({
+      userID: newUser._id,
+      username: newUser.username,
+      accessToken: newUser.accessToken
+    })
+  } catch (error) {
+    res.status(400).json({ message: "Invalid request", error })
+  }
+})
+
+app.post('/sessions', async (req, res) => {
+  const { username, password } = req.body
+
+  try {
+    const user = await User.findOne({ username })
+
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.json({
+        userID: user._id,
+        username: user.username,
+        accessToken: user.accessToken
+      })
+    } else {
+      res.status(404).json({ message: 'User not found' })
+    }
+  } catch (error) {
+    res.status(404).json({ message: 'Invalid request', error: error })
+  }
+})
+
 app.listen(port, () => {
   // eslint-disable-next-line
   console.log(`Server running on http://localhost:${port}`)
