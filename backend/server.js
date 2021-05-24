a80dac62b8fbda68d1f8f8d8184c78f69168cf08
M4L5 <malin.gu2017@gmail.com>
2021-05-24 22:30:39 +0200

    setting up endpoints in backend

--- backend/server.js
+++ backend/server.js
@@ -4,27 +4,62 @@ import cors from 'cors'
 import mongoose from 'mongoose'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+const Thought = mongoose.model('Thought', {
+  message: String
+})
+
+const User = mongoose.model('User', {
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+})
+
 const port = process.env.PORT || 8080
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
 
-// Start the server
+app.get('/thoughts', async (_, res) => {
+  const thoughts = await Thought.find()
+  res.json(thoughts)
+})
+
+app.post('/thoughts', async (req, res) => {
+  const { message } = req.body
+
+  try{
+    const newThought = await new Thought ({ message }).save()
+    res.json(newThought)
+  } catch (error) {
+    res.status(400).json({ message: 'Invalid request',  error })
+  }
+})
+
+app.post('/signup', async (req, res) => {
+  const { username, password } = req.body
+
+  try {
+
+  } catch (error) {
+    
+  }
+})
+
+
 app.listen(port, () => {
-  // eslint-disable-next-line
   console.log(`Server running on http://localhost:${port}`)
 })
