bd2c1de8fbb3082c65c98ee9c9a7f7c1fc82853b
“[ida <[ida.t.sylven@gmail.com]>
2021-05-25 11:48:59 +0200

    Made endpoints for register and sign-in

--- backend/server.js
+++ backend/server.js
@@ -1,126 +1,114 @@
-import express from 'express'
-import cors from 'cors'
-import mongoose from 'mongoose'
-import crypto from 'crypto'
-import bcrypt from 'bcrypt'
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import crypto from "crypto";
+import bcrypt from "bcrypt";
+import listEndpoints from "express-list-endpoints";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
-mongoose.Promise = Promise
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI";
+mongoose.connect(mongoUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+mongoose.Promise = Promise;
 
-const Thought = mongoose.model('Thought', {
-  message: String
-})
-
-const User = mongoose.model('User', {
+const User = mongoose.model("User", {
   username: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
   password: {
     type: String,
-    required: true
+    required: true,
   },
   accessToken: {
     type: String,
-    default: () => crypto.randomBytes(128).toString('hex')
-  }
-})
+    default: () => crypto.randomBytes(128).toString("hex"),
+  },
+});
 
-// Middlewear function
+//MIDDLEWARE FOR AUTHENTICATION
 const authenticateUser = async (req, res, next) => {
-  const accessToken = req.header('Autorisation')
+  const accessToken = req.header("Authorization");
 
   try {
-    const user = await User.findOne({ accessToken })
+    const user = await User.findOne({ accessToken });
     if (user) {
-      next()
+      next();
     } else {
-      res.status(401).json({ message: 'Not authenticated'})
+      res.status(401).json({ message: "Not authenticated" });
     }
   } catch (error) {
-    res.status(400).json({ message: 'Invalid request', error})
+    res.status(400).json({ message: "Invalid requset", error });
   }
-}
+};
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
-// Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-// Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+app.get("/", (req, res) => {
+  res.send(listEndpoints(app));
+});
 
-app.get('/thoughts', authenticateUser)
-app.get('/thoughts', async (req, res) => {
-  const thoughts = await Thought.find()
-  res.json(thoughts) 
-})
+// app.post("/welcome", async (req, res) => {
+//   const { message } = req.body;
 
-app.post('/thoughts', async (req, res) => {
-  const { message } = req.body
+//   try {
+//   } catch (error) {}
+// });
+
+// USER REGISTER
+app.post("/register", authenticateUser);
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
 
   try {
-    const newThought = await new Thought({ message }).save()
-    res.json(newThought)
-  } catch (error) {
-    res.status(400).json({ message: 'Invalid request', error})
-  }
-})
-
-// Or /users or /register
-app.post('/signup', async (req, res) => {
-  const { username, password } = req.body
-
-  try {
-    const salt = bcrypt.genSaltSync()
+    const salt = bcrypt.genSaltSync();
 
     const newUser = await new User({
       username,
-      password: bcrypt.hashSync(password, salt)
-    }).save()
+      password: bcrypt.hashSync(password, salt),
+    }).save();
 
     res.json({
       userID: newUser._id,
-      username: newUser.username,
-      accessToken: newUser.accessToken
-    })
+      user: newUser.username,
+      accessToken: newUser.accessToken,
+    });
   } catch (error) {
-    res.status(400).json({ message: 'Invalid request', error })
+    res.status(400).json({ message: "Invalid request", error });
   }
-})
+});
 
-app.post('/signin', async (req, res) => {
-  const { username, password } = req.body
+// USER SIGNIN
+app.post("/signin", authenticateUser);
+app.post("/signin", async (req, res) => {
+  const { username, password } = req.body;
 
   try {
-    const user = await User.findOne({ username })
+    const user = await User.findOne({ username });
 
     if (user && bcrypt.compareSync(password, user.password)) {
       res.json({
-      userID: user._id,
-      username: user.username,
-      accessToken: user.accessToken
-      })
+        userID: user._id,
+        username: user.username,
+        accessToken: user.accessToken,
+      });
     } else {
-      res.status(404).json({ message: 'User not found '})
+      res.status(404).json({ message: "User not found" });
     }
   } catch (error) {
-    res.status(400).json({ message: 'Invalid request', error })
+    res.status(400).json({ message: "Invalid request", error });
   }
-})
+});
 
 // Start the server
 app.listen(port, () => {
   // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
