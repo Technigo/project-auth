a9b7fd04375be0eaf32ab25773324c60e7dc4195
liaback <felicia.liaback@gmail.com>
2021-05-25 16:12:48 +0200

    create middleware authenticateUser

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose'
 import listEndpoints from 'express-list-endpoints'
 import crypto from 'crypto'
 import bcrypt from 'bcrypt'
+import { restart } from 'nodemon'
 
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
@@ -32,6 +33,22 @@ const User = mongoose.model('User', {
   }
 })
 
+// add autehntication 
+const authenticateUser = async (req, res, next) => {
+  const accessToken = req.header('Authorization');
+
+  try {
+    const User = await User.findOne({ accessToken })
+    if(user) {
+      next()
+    } else {
+      res.status(401).json({ message: 'Not authorized'  })
+    }
+  } catch (error) {
+    res.status(400).json({ message: 'Invalid request', error })
+  }
+}
+
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -43,13 +60,15 @@ app.get('/', (_, res) => {
   res.send(listEndpoints(app))
 })
 
-// endpoint - get request 
+// endpoint - get thoughts 
+app.get('/thoughts', authenticateUser);
 app.get('/thoughts', async (_, res) => {
   const thoughts = await Thought.find()
   res.json(thoughts)
 })
 
-// edpoints - post request
+// edpoints - post thoughts
+app.get('/thoughts', authenticateUser);
 app.post('/thoughts', async (req, res) => {
   const { message } = req.body
 
@@ -61,7 +80,7 @@ app.post('/thoughts', async (req, res) => {
   }
 })
 
-// edpoint - get request for signup
+// endpoint - get request for signup
 app.post('/signup', async (req, res) => {
   const { username, password } = req.body
   try {
@@ -77,6 +96,25 @@ app.post('/signup', async (req, res) => {
     });
   } catch (error) {
     res.status(400).json({ message: 'Invalid request', error });
+  }
+})
+
+// endpoint for signin (add erro messages for user/ password)
+app.post('/signin', async (req,res) => {
+  const { username, password} = req.body
+  try {
+    const user = await User.findOne({ username });
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.json({
+        userId: user._id,
+        username: user.username,
+        accessToken: user.accessToken
+      })
+    } else {
+      res.status(404).json({ message: 'User not found' })
+    }
+  } catch {
+    res.status(400).json({ message: 'Invalid request', error})
   }
 })
 
