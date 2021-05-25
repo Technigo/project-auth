4f842a70fc3e0f1f6ced8d4e2a117a2862300e07
DANIELA <DANIELA.ZACARIAS@OUTLOOK.COM>
2021-05-25 09:26:46 -0500

    Created thought model and post endpoint for new thoughts

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,16 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import crypto from 'crypto'
+
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
+
+const Thought = mongoose.model('Thought', {
+  message: String
+})
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -21,6 +27,17 @@ app.use(bodyParser.json())
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+app.post('/thoughts', async (req, res) => {
+  const { message } = req.body
+
+  try {
+    const newThought = await new Thought ({ message }).save()
+    res.json(newThought)
+  } catch (error) {
+    res.status(400).json({ message: 'Invalid request', error })
+  }
 })
 
 // Start the server
