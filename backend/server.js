055523346695072af8fe20b04362557178ffc916
DANIELA <DANIELA.ZACARIAS@OUTLOOK.COM>
2021-05-25 09:52:24 -0500

    Created model for user

--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,37 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import listEndpoints from 'express-list-endpoints'
 import crypto from 'crypto'
 
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
 mongoose.Promise = Promise
 
+//A model for new Thought
 const Thought = mongoose.model('Thought', {
   message: String
 })
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+const User = mongoose.model('User', {
+  username: {
+    type:String,
+    required:true,
+    unique:true       
+  },
+  password: {
+    type:String,
+    required:true
+  },
+  accessToken: {
+    type:String,
+    default:() => crypto.randomBytes(128).toString('hex')
+  }
+})
+
+
+
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -26,7 +42,12 @@ app.use(bodyParser.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send(listEndpoints(app))
+})
+//An endpoint to get all thoughts
+app.get('/thoughts', async (req, res) => {
+  const thoughts = await Thought.find()
+  res.json(thoughts)
 })
 
 app.post('/thoughts', async (req, res) => {
