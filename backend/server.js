134dbdc57c4de9e1c64b112dce2d5d826c890d56
liaback <felicia.liaback@gmail.com>
2021-05-25 14:26:35 +0200

    import listEndpoints

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,18 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import listEndpoints from 'express-list-endpoints'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 mongoose.Promise = Promise
 
+// model thoughts
 const Thought = mongoose.model('Thought', {
   message: String
 })
 
+// model users
 const User = mongoose.model('User', {
   username: {
     type: String,
@@ -27,17 +29,20 @@ const port = process.env.PORT || 8080
 const app = express()
 
 app.use(cors())
-app.use(bodyParser.json())
+app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello world')
+// list endpoints
+app.get('/', (_, res) => {
+  res.send(listEndpoints(app))
 })
 
+// endpoint - get request 
 app.get('/thoughts', async (_, res) => {
   const thoughts = await Thought.find()
   res.json(thoughts)
 })
 
+// edpoints - post request
 app.post('/thoughts', async (req, res) => {
   const { message } = req.body
 
@@ -49,6 +54,7 @@ app.post('/thoughts', async (req, res) => {
   }
 })
 
+// edpoint - get request for signup
 app.post('/signup', async (req, res) => {
   const { username, password } = req.body
 
