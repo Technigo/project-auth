2b7fe8c96f025ac997667b60d3da717e252cfbc5
Elaine Bergstrom <Elainebergstrom26@gmail.com>
2021-05-25 19:59:52 +0200

    added User model, post request

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,11 @@ import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
 import crypto from 'crypto'
+import bcrypt from 'bcrypt'
+import { request } from 'http'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 mongoose.Promise = Promise
 
 const User = mongoose.model('User', {
@@ -46,6 +48,27 @@ app.use(express.json())
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+app.post('/signup', async (req, res) => {
+  const { username, password, email } = req.body
+
+  try {
+    const salt = bcrypt.genSaltSync()
+    const newUser = await new User({
+      username,
+      email,
+      password: bcrypt.hashSync(password, salt)
+    }).save()
+    res.json({
+      userId: newUser._id,
+      username: newUser.username,
+      email: newUser.email,
+      accessToken:newUser.accessToken
+    })
+  } catch(error) {
+    res.status(400).json({ message: 'invalid request', error})
+  }
 })
 
 // Start the server
