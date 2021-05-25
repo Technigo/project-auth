d732b02f40afda7a013c4220c54edc054b0180fa
Sofarz <sofia.wallen@gmail.com>
2021-05-25 19:37:42 +0200

    added user model

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,47 @@
 import express from 'express'
-import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import crypto from 'crypto'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+const User = mongoose.model('User', {
+  username: {
+    type: String,
+    required: true, 
+    unique: true 
+  }, 
+  password: {
+    type: String,
+    required: true,
+    minlength: 8, // add error message
+  }, 
+  email: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    unique: true,
+    validate: {
+      validator: (value) => {
+        return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value)
+      },
+      message: "Please enter a valid email address"
+    }
+  }, 
+  accessToken: {
+    type: String, 
+    default: () => crypto.randomBytes(128).toString('hex')
+  }
+})
+
 const port = process.env.PORT || 8080
 const app = express()
 //testing!! Elaine was here, writing by your test!
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
-app.use(bodyParser.json())
+app.use(express.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
