fe80250dfe6df189fbb8fa34c1a329ffb2b048fe
Your Name <isabellamarkskog@live.se>
2021-05-26 16:56:37 +0200

    Adjusted BASE_URL

--- frontend/src/reusable/urls.js
+++ frontend/src/reusable/urls.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'https://project-auth-isma-toha.herokuapp.com/'
+export const BASE_URL = 'https://project-auth-isma-toha.herokuapp.com'
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`
