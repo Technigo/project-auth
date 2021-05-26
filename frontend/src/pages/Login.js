7af88acc85a579e077817edf741b74f0c5c1ca90
CarolineF3 <caroline.fahlgren@gmail.com>
2021-05-27 00:40:41 +0200

    added some logic to Login.js and some styling in LoginStyle.css

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,9 +1,69 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch, batch } from "react-redux";
+import { useHistory } from "react-router-dom";
+
+import user from "../reducers/user";
+
+//import { API_URL } from "../reusable/urls";
 
 import "./LoginStyle.css";
 
 const Login = () => {
-  return <div>This is the login page</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [mode, setMode] = useState(null);
+
+  const accessToken = useSelector((store) => store.user.accessToken);
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  useEffect(() => {
+    console.log("Checking access token, accessToken");
+    if (accessToken) {
+      history.push("/");
+    }
+  }, [accessToken, history]);
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    };
+  };
+
+  return (
+    <div className='form-wrapper'>
+      <div className='form-container'>
+        <form onSubmit={onFormSubmit}>
+          <label>Username</label>
+          <input
+            className='input'
+            type='text'
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            className='input'
+            type='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button
+            className='login-button'
+            type='submit'
+            onClick={() => setMode("signin")}
+          >
+            Log in
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -14,29 +14,41 @@ const Register = () => {
   };
 
   return (
-    <div className="form-wrapper">
-      <div className="form-container">
-        <form className="form" onSubmit={onFormSubmit}>
-          <label>Sign up here</label>
-          <input
-            placeholder="Username"
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <input
-            className="input"
-            placeholder="Password"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button className="signup-button" type="submit">
-            Sign up
-          </button>
-        </form>
+    <>
+      <div className='form-wrapper'>
+        <div className='form-container'>
+          <div className='pink-circle'></div>
+          <div className='blue-circle'></div>
+          <div className='green-circle'></div>
+          <form className='form' onSubmit={onFormSubmit}>
+            <h2>Sign up here</h2>
+            <label>Full name</label>
+            <input
+              className='input'
+              placeholder='Username'
+              type='text'
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <input
+              className='input'
+              placeholder='Password'
+              type='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button className='signup-button' type='submit'>
+              Sign up
+            </button>
+          </form>
+        </div>
+        <div className='member-container'>
+          <p>
+            Already a member? <b>Login here</b>
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
