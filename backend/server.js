c2b8599acfb2eabcef4365be0b8094278e967727
Caroline Byström <frivridnadagar@gmail.com>
2021-05-26 11:39:25 +0200

    add success to BE

--- backend/server.js
+++ backend/server.js
@@ -99,12 +99,13 @@ app.post('/signup', async (req, res) => {
   }).save()
   console.log(newUser)
   res.json({
+    success: true,
     userId: newUser._id,
     email: newUser.email,
     accessToken: newUser.accessToken
   })
  } catch (error) {
-  res.status(400).json({ message: 'Invalid request', error })
+  res.status(400).json({ success: false, message: 'Invalid request', error })
  }
 })
 
@@ -123,10 +124,10 @@ app.post('/signin', async (req, res) => {
           accessToken: user.accessToken
         })
       } else {
-        res.status(404).json({ message: 'User not found'})
+        res.status(404).json({ success: false, message: 'User not found'})
       }
    } catch (error) {
-      res.status(400).json({ message: 'Invalid request', error })
+      res.status(400).json({ success: false, message: 'Invalid request', error })
    }
 })
 
