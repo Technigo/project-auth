ee9d976cb0542331220827d86cc2ee0ff3d903ee
newmarkdev <newmarkdev@gmail.com>
2021-05-26 19:04:08 +0200

    set basic backend done

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,48 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import crypto from 'crypto'
+import bcrypt from 'bcrypt'
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+const Thought = mongoose.model('Thought', {
+  message: String
+}); 
+
+const User = mongoose.model('User', {
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString('hex')
+  }
+});
+
+const authenticateUser = async (req, res, next) => {
+  const accessToken = req.header('Authorization');
+
+  try {
+    const user = await User.findOne({ accessToken });
+    if (user) {
+      next();
+    } else {
+      res.status(401).json({ message: 'Not authorized' });
+    }
+  } catch (error) {
+    res.status(400).json({ message: 'Invalid request', error });
+  }
+}
+
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -22,6 +55,69 @@ app.use(bodyParser.json())
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
+
+app.get('/thoughts', authenticateUser);
+app.get('/thoughts', async (req, res) => {
+  const thoughts = await Thought.find();
+  res.json(thoughts);
+});
+
+app.get('/thoughts', authenticateUser);
+app.post('/thoughts', async (req, res) => {
+  const { message } = req.body;
+
+  try {
+    const newThought = await new Thought({ message }).save();
+    res.json(newThought);
+  } catch (error) {
+    res.status(400).json({ message: 'Invalid request', error });
+  }
+});
+
+// or '/users' or '/register'
+app.post('/signup', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const salt = bcrypt.genSaltSync();
+
+    const newUser = await new User({
+      username,
+      password: bcrypt.hashSync(password, salt)
+    }).save();
+
+    res.json({
+      userID: newUser._id,
+      username: newUser.username,
+      accessToken: newUser.accessToken
+    });
+  } catch (error) {
+    res.status(400).json({ message: 'Invalid request', error });
+  }
+}); 
+
+// or '/session' or '/login'
+app.post('/signin', async (req, res) => {
+  const { username, password } = req.body;
+
+  console.log(username, password);
+
+  try {
+    const user = await User.findOne({ username });
+
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.json({
+        userID: user._id,
+        username: user.username,
+        accessToken: user.accessToken
+      });
+    } else {
+      res.status(404).json({ message: 'User not found' });
+    }
+  } catch (error) {
+    res.status(400).json({ message: 'Invalid request', error });
+  }
+});
 
 // Start the server
 app.listen(port, () => {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Provider } from 'react-redux';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 
 export const App = () => {
   return (
