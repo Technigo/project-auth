fce07f4e4e52440ee507654b7d71dc60bfee9912
Your Name <isabellamarkskog@live.se>
2021-05-26 16:14:25 +0200

    Added deployed API link

--- frontend/src/reusable/urls.js
+++ frontend/src/reusable/urls.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'http://localhost:8080' // Later on we change this to the deployed one
+export const BASE_URL = 'https://project-auth-isma-toha.herokuapp.com/'
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`
