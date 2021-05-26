b7d882aa72601416fb6474d724f15e14734ba812
Rasmus Persson <rrasmuspersson@gmail.com>
2021-05-26 15:07:52 +0200

    Added styling for the register page

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -14,26 +14,28 @@ const Register = () => {
   };
 
   return (
-    <div className="form-container">
-      <form onSubmit={onFormSubmit}>
-        <input
-          className="input"
-          placeholder="Username"
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <input
-          className="input"
-          placeholder="Password"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button className="signup-button" type="submit">
-          Sign up
-        </button>
-      </form>
+    <div className="form-wrapper">
+      <div className="form-container">
+        <form className="form" onSubmit={onFormSubmit}>
+          <input
+            className="input"
+            placeholder="Username"
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            className="input"
+            placeholder="Password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className="signup-button" type="submit">
+            Sign up
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
