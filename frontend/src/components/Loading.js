b153690f12d39c84b62ddaf5797db3c582037c70
Laima Duhovnaja <laima@macbook-air.mynet>
2021-05-27 16:13:48 +0200

    pics updated

--- frontend/src/components/Loading.js
+++ frontend/src/components/Loading.js
@@ -1,32 +1,25 @@
 import React from "react";
 import styled, { keyframes } from "styled-components/macro";
 
+import girlLoading from "../assets/girl_loading.png"
+
 export const Loading = ({ loadingText }) => {
   return (
     <LoaderContainer>
       <Title>{loadingText}</Title>
-      <Load></Load>
+      <Image src={girlLoading}></Image>
     </LoaderContainer>
   );
 };
 
-const breatheAnimation = keyframes`
- 0% { height: 100px; width: 100px; }
- 30% { height: 400px; width: 400px; opacity: 1 }
- 40% { height: 405px; width: 405px; opacity: 0.3; }
- 100% { height: 100px; width: 100px; opacity: 0.6; }
+const rotating = keyframes`
+from { transform: rotate(-30deg) }
+  to { transform: rotate(30deg) }
 `;
 
-const Load = styled.div`
-  height: 100px;
-  width: 100px;
-  border-style: solid;
-  border-width: 5px;
-  border-radius: 50%;
-  border-color: #00bbff;
-  animation-name: ${breatheAnimation};
-  animation-duration: 8s;
-  animation-iteration-count: infinite;
+const Image = styled.img`
+  height: 60%;
+  animation: ${rotating} 1s linear infinite alternate-reverse;
 `;
 
 const LoaderContainer = styled.div`
