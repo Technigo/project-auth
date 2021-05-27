cd18fa58d53454beb7cfca72e414edc1486dd15d
Anna Lindgren <annalindgren@MacBook-Pro-som-tillhor-Anna.local>
2021-05-27 20:16:23 +0200

    fixed indentations for HomePage component

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -4,20 +4,20 @@ import { Link } from 'react-router-dom'
 import { MainContainer, SubContainer, ButtonContainer, Button, Header, Text } from './styled-components/homepage-style'
 
 const HomePage = () => {
-    return (
-        <MainContainer>
-          <SubContainer>
-            <Header>Welcome</Header>
-            <Text>Would you like to know our super secret?</Text>
-              <ButtonContainer>
-                  <Link to="/login">
-                      <Button>
-                          Yes!
-                      </Button>
-                  </Link>
-              </ButtonContainer>
-            </SubContainer>
-        </MainContainer>
+  return (
+    <MainContainer>
+      <SubContainer>
+        <Header>Welcome</Header>
+        <Text>Would you like to know our super secret?</Text>
+        <ButtonContainer>
+          <Link to="/login">
+          <Button>
+            Yes!
+          </Button>
+          </Link>
+        </ButtonContainer>
+      </SubContainer>
+    </MainContainer>
     )
 }
 
