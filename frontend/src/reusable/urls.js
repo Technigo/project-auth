e38381fdc6f4b801f76dbc338adc67dd83fbe169
Maria Pogosjan <Maria.pogosjan@gmail.com>
2021-05-27 20:45:56 +0200

    test

--- frontend/src/reusable/urls.js
+++ frontend/src/reusable/urls.js
@@ -2,4 +2,4 @@ const BASE_URL = 'https://moody-test-technigo.herokuapp.com'
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`
 
-export const FEELING_URL = (id) => `${BASE_URL}/users/${id}/feelings` 
+export const FEELING_URL = (id) => `${BASE_URL}/users/${id}/feelings`
