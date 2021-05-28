3bd7e72a5800b9672d818e63c397aecc1d82e600
newmarkdev <newmarkdev@gmail.com>
2021-05-28 14:16:53 +0200

    added logout btn

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { batch, useSelector, useDispatch } from 'react-redux';
+import Button from '@material-ui/core/Button';
+
+
+import user from '../reducers/user';
+
+const Header = () => {
+    const accessToken = useSelector(store => store.user.accessToken);
+    const dispatch = useDispatch();
+
+    const onButtonClick = () => {
+        batch(() => {
+            dispatch(user.actions.setUsername(null));
+            dispatch(user.actions.setAccessToken(null));
+        });
+    };
+
+    return (
+        <header>
+            {accessToken && <Button 
+            fullWidth
+            color="primary" 
+            onClick={onButtonClick}>Logout
+            </Button>}
+        </header>
+    );
+};
+
+export default Header;
--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,9 +1,39 @@
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import Avatar from '@material-ui/core/Avatar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Box from '@material-ui/core/Box';
+import LockOpenOutlinedIcon from '@material-ui/icons/LockOpenOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
+import Header from './Header';
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.primary.main,
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  }));
 
 const Main = () => {
+    const classes = useStyles();
+
     const accessToken = useSelector(store => store.user.accessToken);
 
     const history = useHistory();
@@ -15,11 +45,24 @@ const Main = () => {
     }, [accessToken, history]);
 
     return (
-        <div>
-            <h1>
-            Secret Page
-            </h1>
+
+    <>
+        <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+            <LockOpenOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+           Welcome to the Secret Page!
+        </Typography>
         </div>
+        <Header />
+
+        <Box mt={8}>
+        </Box>
+        </Container>
+    </>
     );
 };
 
