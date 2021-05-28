c04ef2682591aa5b9a3cf16d8d7bcbc1f41de643
M4L5 <malin.gu2017@gmail.com>
2021-05-28 16:20:54 +0200

    removed link from import

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react'
 import { useSelector, useDispatch, batch } from "react-redux";
-import { useHistory, Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 import {API_URL} from "../reusables/urls";
 
