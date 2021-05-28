d160dba64845f80ba8ddcd8cd21fe73f394cbb89
newmarkdev <newmarkdev@gmail.com>
2021-05-28 09:17:09 +0200

    added material ui form design

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,12 +1,43 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch, batch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
 import user from '../reducers/user';
 
 import { API_URL } from '../reusable/urls';
 
+const useStyles = makeStyles((theme) => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  }));
+
 const Login = () => {
+    const classes = useStyles();
+
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [mode, setMode] = useState(null);
@@ -51,20 +82,72 @@ const Login = () => {
     };
 
     return (
-        <form onSubmit={onFormSubmit}>
-            <input
-                type="text"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
+    <>
+        <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+            Login or Signup
+        </Typography>
+        <form className={classes.form} noValidate onSubmit={onFormSubmit}>
+            <TextField
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="User Name"
+            name="username"
+            autoComplete="username"
+            autoFocus
             />
-            <input
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
+            <TextField
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            id="password"
+            autoComplete="current-password"
             />
-            <button type="submit" onClick={() => setMode('signin')}>Sign in</button>
-            <button type="submit" onClick={() => setMode('signup')}>Sign up</button>
+        
+            <Button
+            type="submit"
+            onClick={() => setMode('signin')}
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            >
+            Login
+            </Button>
+            <Button
+            type="submit"
+            onClick={() => setMode('signup')}
+            fullWidth
+            variant="contained"
+            color="secondary"
+            className={classes.submit}
+            >
+            Signup
+            </Button>
         </form>
+        </div>
+        <Box mt={8}>
+        </Box>
+        </Container>
+    </>
+
     );
 };
 
--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,16 +1,11 @@
 import React, { useEffect } from 'react';
-import { useSelector, useDispatch, batch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
-import { API_URL } from '../reusable/urls';
-
-import thoughts from '../reducers/thoughts';
 
 const Main = () => {
     const accessToken = useSelector(store => store.user.accessToken);
-    // const thoughtsItems = useSelector(store => store.thoughts.items);
 
-    const dispatch = useDispatch();
     const history = useHistory();
 
     useEffect(() => {
@@ -19,36 +14,11 @@ const Main = () => {
         }
     }, [accessToken, history]);
 
-    useEffect(() => {
-        const options = {
-            method: 'GET',
-            headers: {
-                Authorization: accessToken
-            }
-        }
-
-        fetch(API_URL('thoughts'), options)
-            .then(res => res.json())
-            .then(data => {
-                if (data.success) {
-                    batch(() => {
-                        dispatch(thoughts.actions.setThoughts(data.thoughts));
-                        dispatch(thoughts.actions.setErrors(null));
-                    });
-                } else {
-                    dispatch(thoughts.actions.setErrors(data));
-                }
-            });
-    }, [accessToken]);
-    
-    // console.log(thoughtsItems)
-    
     return (
         <div>
-            <div>MAIN</div>
-            {/* {thoughtsItems.map(thought => (
-                <div key={thought._id}>{thought.message}</div>
-            ))} */}
+            <h1>
+            Secret Page
+            </h1>
         </div>
     );
 };
