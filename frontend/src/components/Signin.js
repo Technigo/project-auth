559beb4b2b47fa5caa616ccec01b232d8e5db87a
Irina <jekabsone.irina@gmail.com>
2021-05-29 19:49:26 +0200

    Form styling

--- frontend/src/components/Signin.js
+++ frontend/src/components/Signin.js
@@ -1,10 +1,81 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch, batch } from "react-redux";
 import { useHistory } from "react-router-dom";
+import styled from 'styled-components/macro'
 
 import { API_URL } from "../reusable/urls";
 
 import user from "../reducers/user";
+
+const SigninWrapper = styled.div`
+  background-color: #4838a8;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+`
+
+const WelcomeMessage = styled.div`
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 30px;
+  width: 50%;
+`
+
+const WelcomeText = styled.p`
+  font-size: 24px;
+`
+
+const InputForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #fff;
+  min-width: 450px;
+  min-height: 650px;
+  border-radius: 20px;
+`
+
+const FormWrapper = styled.form`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const InputField = styled.input`
+  margin: 5px;
+  border-radius: 5px;
+  min-width: 200px;
+`
+
+const Button = styled.button`
+  min-width: 200px;
+  background-color: #4838a8;
+  color: #fff;
+  border-radius: 5px;
+  margin-top: 20px;
+  height: 40px;
+
+  &:hover {
+    background-color: #211661;
+  }
+`
+
+const Username = styled.label`
+  align-self: flex-start;
+  margin-left: 130px;
+`
+
+const Password = styled.label`
+  align-self: flex-start;
+  margin-left: 130px;
+`
 
 const Signin = () => {
   const [username, setUsername] = useState("");
@@ -49,20 +120,31 @@ const Signin = () => {
   };
 
   return (
-    <form onSubmit={onFormSubmit}>
-      <input
-        type="text"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button type="submit">Sign in</button>
-    </form>
-  );
+    <SigninWrapper>
+      <WelcomeMessage>
+        <h1>Welcome To Happy Thoughts Website</h1>
+        <WelcomeText>Let´s Sign in here</WelcomeText>
+      </WelcomeMessage>
+      <FormWrapper>
+        <InputForm onSubmit={onFormSubmit}>
+          <Username for="username">Username</Username>
+          <InputField
+            id="username" 
+            type="text" 
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <Password for="password">Password</Password>
+          <InputField 
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button type='submit'>Sign in</Button>
+        </InputForm>
+      </FormWrapper>
+    </SigninWrapper>
+  )
 };
 
 export default Signin;
--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,6 +1,77 @@
 import React, { useState } from 'react'
+import styled from 'styled-components/macro'
 
 import { API_URL } from '../reusable/urls'
+
+const SignupWrapper = styled.div`
+  background-color: #4838a8;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+`
+
+const WelcomeMessage = styled.div`
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 30px;
+  width: 50%;
+`
+
+const WelcomeText = styled.p`
+  font-size: 24px;
+`
+
+const InputForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #fff;
+  min-width: 450px;
+  min-height: 650px;
+  border-radius: 20px;
+`
+
+const FormWrapper = styled.form`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const InputField = styled.input`
+  margin: 5px;
+  border-radius: 5px;
+  min-width: 200px;
+`
+
+const Button = styled.button`
+  min-width: 200px;
+  background-color: #4838a8;
+  color: #fff;
+  border-radius: 5px;
+  margin-top: 20px;
+  height: 40px;
+
+  &:hover {
+    background-color: #211661;
+  }
+`
+
+const Username = styled.label`
+  align-self: flex-start;
+  margin-left: 130px;
+`
+
+const Password = styled.label`
+  align-self: flex-start;
+  margin-left: 130px;
+`
 
 const Signup = () => {
   const [username, setUsername] = useState('')
@@ -22,19 +93,30 @@ const Signup = () => {
   }
 
   return (
-    <form onSubmit={onFormSubmit}>
-      <input 
-        type="text" 
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input 
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button type='submit'>Sign up</button>
-    </form>
+    <SignupWrapper>
+      <WelcomeMessage>
+        <h1>Welcome To Happy Thoughts Website</h1>
+        <WelcomeText>Let´s Sign up here</WelcomeText>
+      </WelcomeMessage>
+      <FormWrapper>
+        <InputForm onSubmit={onFormSubmit}>
+          <Username for="username">Username</Username>
+          <InputField
+            id="username" 
+            type="text" 
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <Password for="password">Password</Password>
+          <InputField 
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button type='submit'>Sign up</Button>
+        </InputForm>
+      </FormWrapper>
+    </SignupWrapper>
   )
 }
 
