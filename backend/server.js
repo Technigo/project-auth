57284eceb98f7017091e0ac567d249d6f5cf30e1
johlofg <jh.lofgren@gmail.com>
2021-05-29 09:58:18 +0200

    changed name of db

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import crypto from 'crypto'
 import bcrypt from 'bcrypt'
 
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/thoughts"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
 mongoose.Promise = Promise
 
