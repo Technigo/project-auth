5ea72ca4b946f58fd3ba3cb8bf1c87d7169b1665
Atefeh H.Takrami <ati.takrami@gmail.com>
2021-05-30 11:31:11 +0200

    fix api base url

--- frontend/src/reusable/urls.js
+++ frontend/src/reusable/urls.js
@@ -1,5 +1,4 @@
 /* eslint-disable linebreak-style */
-const BASE_URL = 'http://localhost:8080'
+const BASE_URL = process.env.API_URL || "http://localhost:8080";
 
-export const API_URL = (slug) => `${BASE_URL}/${slug}`
-
+export const API_URL = (slug) => `${BASE_URL}/${slug}`;
