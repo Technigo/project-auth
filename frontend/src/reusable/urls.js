e09b6de54a247eeb7604b1381d02f4e180ba8923
Atefeh H.Takrami <ati.takrami@gmail.com>
2021-05-30 11:35:38 +0200

    add REACT_APP prefix to env variable

--- frontend/src/reusable/urls.js
+++ frontend/src/reusable/urls.js
@@ -1,4 +1,4 @@
 /* eslint-disable linebreak-style */
-const BASE_URL = process.env.API_URL || "http://localhost:8080";
+const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:8080";
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`;
