05cc9b486917cbcd51361206419e0560ae3a801b
Rasmus Persson <rrasmuspersson@gmail.com>
2021-05-30 18:06:47 +0200

    added auth to certain endpoints

--- backend/server.js
+++ backend/server.js
@@ -72,8 +72,8 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // This is a GET endpoint that will return all the thougut that are available in the database and if there are no thoughts the API will throw and error
-app.get("/thoguhts", authenticateUser);
 app.get("/thoughts", async (req, res) => {
+  authenticateUser();
   try {
     const thoughts = await Thought.find();
     res.json(thoughts);
@@ -83,8 +83,8 @@ app.get("/thoughts", async (req, res) => {
 });
 
 // This is a POST endpoint and it will allow the used to submit a new thought that will be stored in the database. The post request takes a message in the json body.
-app.post("/thoughts", authenticateUser);
 app.post("/thoughts", async (req, res) => {
+  authenticateUser();
   const { message } = req.body;
   try {
     const newThought = await new Thought({ message }).save();
