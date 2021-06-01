0450007d41ba20a023094a1e7a9940e093a65fb3
DANIELA <DANIELA.ZACARIAS@OUTLOOK.COM>
2021-06-01 07:17:26 -0500

    Corrected base url

--- frontend/src/reusable/urls.js
+++ frontend/src/reusable/urls.js
@@ -1,3 +1,3 @@
-const BASE_URL = 'https://our-happy-thoughts-app.herokuapp.com/'
+const BASE_URL = 'https://our-happy-thoughts-app.herokuapp.com'
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`
