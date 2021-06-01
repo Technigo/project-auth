3fb1bbc1d0e6f0397c303f9ecf88352a20ae358c
Laima Duhovnaja <laima@macbook-air.mynet>
2021-06-01 17:43:23 +0200

    local storage added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,13 @@ import React from "react";
 import styled from "styled-components";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { Provider } from "react-redux";
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { combineReducers, createStore } from "@reduxjs/toolkit";
 
 import user from "reducers/user";
 import { Main } from "./pages/Main";
 import { SignIn } from "pages/SignIn";
 import { SignUp } from "pages/SignUp";
 import { Joke } from "pages/Joke";
-
 
 const MainContainer = styled.main`
   display: flex;
@@ -25,11 +24,28 @@ const MainContainer = styled.main`
   background-color: #f2f3ff;
 `;
 
+const store = configureStore({ reducer });
+
 const reducer = combineReducers({
   user: user.reducer,
 });
 
-const store = configureStore({ reducer });
+const persistedStateJSON = localStorage.getItem("reduxState");
+let persistedState = {};
+
+if (persistedStateJSON) {
+  persistedState = JSON.parse(persistedStateJSON)
+};
+
+const store = createStore(
+  reducer,
+  persistedState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+store.subscribe(() => {
+  localStorage.setItem("reduxState", JSON.stringify(store.getState()));
+});
 
 export const App = () => {
   return (
