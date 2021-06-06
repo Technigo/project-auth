670cc16264c02302715c35de9eab13b76fac49b3
Ylva Landoff Lindberg <ylva.lan.lin@gmail.com>
2021-06-06 20:48:10 +0200

    cleaned code and removed _redirect file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,34 @@
-import React from 'react'
-// import styled from 'styled-components'
+import React from 'react';
 
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import { Provider } from 'react-redux'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 
-import { thoughts } from './reducers/thoughts'
-import { user } from './reducers/user'
+import { thoughts } from './reducers/thoughts';
+import { user } from './reducers/user';
 
-import {Thoughts} from './components/Thoughts'
-import {RegistrationForm} from './components/RegistrationForm'
-// import {Start} from './components/Start'
+import { Thoughts } from './components/Thoughts';
+import { RegistrationForm } from './components/RegistrationForm';
 
+const reducer = combineReducers({
+	thoughts: thoughts.reducer,
+	user: user.reducer,
+});
 
-const reducer = combineReducers ({
-  thoughts: thoughts.reducer,
-  user: user.reducer,
-})
-
-const store = configureStore({ reducer })
+const store = configureStore({ reducer });
 
 export const App = () => {
-  return (
-    <>
-    <BrowserRouter>
-      <Provider store={store}>
-        <Switch>
-           <Route exact path='/' component={RegistrationForm} /> 
-          <Route path='/registration' component={RegistrationForm} />
-          <Route path='/thoughts' component={Thoughts} /> 
-        </Switch>
-      </Provider>
-    </BrowserRouter>
-    </>
-  )
-}
+	return (
+		<>
+			<BrowserRouter>
+				<Provider store={store}>
+					<Switch>
+						<Route exact path='/' component={RegistrationForm} />
+						<Route path='/registration' component={RegistrationForm} />
+						<Route path='/thoughts' component={Thoughts} />
+					</Switch>
+				</Provider>
+			</BrowserRouter>
+		</>
+	);
+};
