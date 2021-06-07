c723884ddf042a1cf8514ac93d3d42eeb2a41aed
Ylva Landoff Lindberg <ylva.lan.lin@gmail.com>
2021-06-07 11:34:03 +0200

    Did all changes asked for in the code review. added so users stay logedin when reload the pageXOffsee

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { combineReducers, createStore } from '@reduxjs/toolkit';
 
 import { thoughts } from './reducers/thoughts';
 import { user } from './reducers/user';
@@ -11,11 +11,26 @@ import { Thoughts } from './components/Thoughts';
 import { RegistrationForm } from './components/RegistrationForm';
 
 const reducer = combineReducers({
-	thoughts: thoughts.reducer,
 	user: user.reducer,
+	thoughts: thoughts.reducer,
 });
 
-const store = configureStore({ reducer });
+const persistedStateJSON = localStorage.getItem('reduxState');
+let persistedState = {};
+
+if (persistedStateJSON) {
+	persistedState = JSON.parse(persistedStateJSON);
+}
+
+const store = createStore(
+	reducer,
+	persistedState,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+store.subscribe(() => {
+	localStorage.setItem('reduxState', JSON.stringify(store.getState()));
+});
 
 export const App = () => {
 	return (
