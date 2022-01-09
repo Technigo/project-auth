a62f374c07f624458c8eb08365a8992e8a7b90dd
Priscila Alfaro <priscila24n@hotmail.com>
2022-01-09 16:18:14 +0100

    add basic endpoints following video codealong

--- README.md
+++ README.md
@@ -1,8 +1,25 @@
+# Week 20 - Technigo Bootcamp
+
 # Project Auth
 
-Replace this readme with your own information about your project.
+Your project needs two parts; a backend API, and a React frontend. You'll need to create a `User` model using mongoose, with properties for your registered user, and to store a user's access token.
 
-Start by briefly describing the assignment in a sentence or two. Keep it short and to the point.
+Then, on the frontend side of things, you'll need to build up a registration form that POSTs to your API. You'll need to store the access token you get back in the browser using local storage, and then use that token when making other requests to your API.
+
+Once a user is logged in, you will need to have one last endpoint which returns some content which only logged-in users should be able to access. You can choose what you want this endpoint to return, and if you want to make multiple endpoints, that's fine too. It could return hard-coded data or something from the database - either is fine. Whatever you choose, it should be displayed in the frontend after you've logged in.
+
+To summarise, your API needs:
+
+- Registration endpoint, to create a new user.
+- Sign-in endpoint, to authenticate a returning user.
+- An authenticated endpoint which only returns content if the `Authorization` header with the user's token was correct.
+
+Your frontend needs:
+
+- A registration form.
+- A sign-in form.
+- A page to show the authenticated content from the API.
+- A 'sign out' button that removes the saved access token and redirects the user to the login form.
 
 ## The problem
 
@@ -10,4 +27,5 @@ Describe how you approached to problem, and what tools and techniques you used t
 
 ## View it live
 
-Every project should be deployed somewhere. Be sure to include the link to the deployed project so that the viewer can click around and see what it's all about.
+See backend: 
+See frontend: 
--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,105 @@
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import crypto from "crypto"
+import bcrypt from "bcrypt-nodejs"
+import dotenv from 'dotenv'
+import listEndpoints from 'express-list-endpoints';
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+dotenv.config();
+
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI" //database name
+
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+  // eslint-disable-next-line no-console
+  console.log(`database connected`)
+}, (onreject) => {
+  // eslint-disable-next-line no-console
+  console.log(onreject);
+})
 mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 6000
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
+
 app.use(cors())
 app.use(express.json())
 
-// Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.json(listEndpoints(app));
 })
+
+
+//collection name
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    unique: true
+  },
+  email: {
+    type: String,
+    unique: true
+  },
+  password: {
+    type: String,
+    Required: true,
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString('hex')
+  }
+})
+
+const authenticateUser = async (req, res, next) => {
+  const user = await User.findOne({ accessToken: req.header('Authorization') })
+  if (user) {
+    req.user = user;
+    next();
+  } else {
+    res.status(401).json({ loggetOut: true })
+  }
+}
+
+app.post('/register', async (req, res) => {
+  try {
+    const { name, email, password } = req.body
+    const user = new User({ name, email, password: bcrypt.hashSync(password) });
+    user.save();
+    res.status(201).json({ id: user._id, accessToken: user.accessToken });
+  } catch (e) {
+    res.status(400).json({ message: "Could not create user", errors: e.errors });
+  }
+})
+
+//para retornar info cuando ya esta logueado 
+app.get('/user', authenticateUser, async (req, res) => {
+  try {
+    res.status(200).json({ message: "correctToken" })
+  } catch (e) {
+    res.status(500).json({ errors: e.errors });
+  }
+})
+
+
+app.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body
+    const user = await User.findOne({ email: email })
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.json({ userId: user._id, accessToken: user.accessToken })
+    } else {
+      //user does not exist
+      //encripted password does not match
+      res.json({ notFound: true })
+    }
+
+
+  } catch (e) {
+    res.status(500).json({ errors: e.errors });
+  }
+})
+
 
 // Start the server
 app.listen(port, () => {
