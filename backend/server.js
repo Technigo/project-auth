9993460b82ab08e8cb37bbe58d1f2847b1bba9bb
Skrosen <76884712+Skrosen@users.noreply.github.com>
2022-01-10 11:54:22 +0100

    run npm install in frontend and backend folders, installed crypto and bcrypt in backend, updated backend with enpoints for signup, signin, authentication

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,127 @@
-import express from 'express'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import crypto from "crypto";
+import bcrypt from "bcrypt";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
+const userSchema = new mongoose.Schema({
+	username: {
+		type: String,
+		unique: true,
+		required: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	accessToken: {
+		type: String,
+		default: () => crypto.randomBytes(128).toString("hex"),
+	},
+});
+
+const User = mongoose.model("User", userSchema);
+
+// Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+
+// authenticates user with access token
+const authenticateUser = async (req, res, next) => {
+	const accessToken = req.header("Authorization");
+
+	try {
+		const user = await User.findOne({ accessToken });
+		if (user) {
+			next();
+		} else {
+			res.status(401).json({ response: "Please log in", success: false });
+		}
+	} catch (error) {
+		res.status(400).json({ response: error, success: false });
+	}
+};
 
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+app.get("/", (req, res) => {
+	res.send("Hello world");
+});
+
+// not used endpoint but set up for authentication
+app.get("/", authenticateUser);
+app.get("/", async (req, res) => {});
+
+// endpoint for signing up
+app.post("/signup", async (req, res) => {
+	const { username, password } = req.body;
+
+	try {
+		const salt = bcrypt.genSaltSync();
+
+		// ensures the password length is minimum 5 characters
+		if (password.length < 5) {
+			throw "Password has to be at least 5 characters";
+		}
+
+		const newUser = await new User({
+			username,
+			password: bcrypt.hashSync(password, salt), //encrypts / hashes password
+		});
+
+		res.status(201).json({
+			response: {
+				userId: newUser._id,
+				username: newUser.username,
+				accessToken: newUser.accessToken,
+			},
+			success: true,
+		});
+	} catch (error) {
+		res.status(400).json({ response: error, success: false });
+	}
+});
+
+// endpoint for signing in
+app.post("/signin", async (req, res) => {
+	const { username, password } = req.body;
+
+	try {
+		const user = await User.findOne({ username });
+
+		// checks if user and password matches the ones in database
+		if (user && bcrypt.compareSync(password, user.password)) {
+			res.status(200).json({
+				response: {
+					userId: newUser._id,
+					username: newUser.username,
+					accessToken: newUser.accessToken,
+				},
+				success: true,
+			});
+		} else {
+			res.status(404).json({
+				response: "Username or password doesn't match",
+				success: false,
+			});
+		}
+	} catch (error) {
+		res.status(400).json({ response: error, success: false });
+	}
+});
 
 // Start the server
 app.listen(port, () => {
-  // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`)
-})
+	// eslint-disable-next-line
+	console.log(`Server running on http://localhost:${port}`);
+});
