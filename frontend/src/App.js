40e0c33b9b3d1dfa4fba1ccfe31eb903e06da5f5
madeleinesvensson <madeleinesvensson13@gmail.com>
2022-01-10 18:51:51 +0100

    removing cached files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { combineReducers, createStore } from "@reduxjs/toolkit";
 
 export const App = () => {
+  // We need to change name to whatever out store/reducer is called. This is the local storage
+  const reducer = combineReducers({
+    name: name.reducer,
+  });
+  const persistedStateJSON = localStorage.getItem("nameReduxState");
+  let persistedState = {};
+
+  if (persistedStateJSON) {
+    persistedState = JSON.parse(persistedStateJSON);
+  }
+  const store = createStore(reducer, persistedState);
+
+  store.subscribe(() => {
+    localStorage.setItem("nameReduxState", JSON.stringify(store.getState()));
+  });
+
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
-  )
-}
+    <Router>
+      <Link to="/">Start</Link>
+      <Link to="/signin">Sign in</Link>
+      <Link to="/signup">Sign up</Link>
+      {/* This route needs a name :P */}
+      <Link to="/sessions">Secret riddles</Link>
+
+      <Routes>
+        <Route exact path="/">
+          {/* here we put a component */}
+        </Route>
+        <Route path="/signin">{/* here we put a component */}</Route>
+        <Route path="/signup">{/* here we put a component */}</Route>
+        <Route path="/sessions">{/* here we put a component */}</Route>
+      </Routes>
+    </Router>
+  );
+};
---
+++ frontend/src/components/SignIn.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const SignIn = () => {
+  return (
+    <>
+      <h1>This is the sign in page</h1>
+    </>
+  );
+};
