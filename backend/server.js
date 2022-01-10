737179b3ebbd4389e2e15f14ba22d723bb4a07be
Reinis <reinis@Reiniss-MacBook-Pro.local>
2022-01-10 18:42:51 +0100

    2 post routes difined - signup and login

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,91 @@
-import express from 'express'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import crypto from "crypto";
+import bcrypt from "bcrypt-nodejs";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString("hex"),
+  },
+});
+
+const User = mongoose.model("User", UserSchema);
+
+// Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+
+// const authenticateUser = async (req, res, next) =>
 
 // Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.post("/signup", async (req, res) => {
+  const { username, password, email } = req.body;
+  try {
+    const newUser = await new User({
+      username,
+      password: bcrypt.hashSync(password),
+      email,
+    }).save();
+    // need to return part of the newUser only
+    res.status(201).json({ response: newUser, success: true });
+  } catch (error) {
+    res.status(400).json({ response: error, success: false });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const loggedUser = await User.findOne({ username });
+    if (loggedUser && bcrypt.compareSync(password, loggedUser.password)) {
+      res.status(200).json({
+        response: {
+          username: loggedUser.username,
+          accessToken: loggedUser.accessToken,
+        },
+        success: true,
+      });
+    } else {
+      res
+        .status(401)
+        .json({ response: "User or password not found", success: false });
+    }
+  } catch (error) {
+    res.status(400).json({ response: error, success: false });
+  }
+});
 
 // Start the server
 app.listen(port, () => {
   // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
