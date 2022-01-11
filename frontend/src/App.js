c8b8c57efd2c579e2502655d34c8c50641b6af28
HedvigM <hedvig@mejstedt.se>
2022-01-11 20:26:53 +0100

    new register component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import { RegistrationForm } from 'Components/RegistrationForm';
+import { Header } from 'Components/Header';
+import { Footer } from 'Components/Footer';
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
-  )
-}
+    <Container>
+      <Header />
+      <InnerContainer>
+        <RegistrationForm />
+      </InnerContainer>
+      <Footer />
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  min-height: 100vh;
+  background-color: #017c80;
+`;
+const InnerContainer = styled.div`
+  border: 5px dotted black;
+  min-width: 334px;
+  max-width: 500px;
+  margin: 0 auto;
+  height: 100%;
+  background-color: #92dea0;
+  padding: 30px;
+
+  :hover {
+    background-color: #92dea0;
+  }
+
+  @media (min-width: 0px) and (max-width: 767px) {
+    min-width: 200px;
+    max-width: 300px;
+  }
+`;
---
+++ frontend/src/Components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const Header = () => {
+  return <H1></H1>;
+};
+
+const H1 = styled.h1`
+  color: #92dea0;
+`;
