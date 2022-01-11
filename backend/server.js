4f63b1ddf046afe9c5a2e72b6dcdfb2bfed68718
MT-dotse <madelene.trang333@gmail.com>
2022-01-11 17:43:34 +0200

    created three enpoints, an authentication function and a schema for a model

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,141 @@
-import express from 'express'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt-nodejs';
+import crypto from 'crypto';
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/authAPI';
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+//Schema
+const UserSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		unique: true,
+	},
+	username: {
+		type: String,
+		//minlength: [5, 'You need at least 5 letters for your username'],
+		unique: true,
+	},
+	email: {
+		type: String,
+		unique: true,
+	},
+	password: {
+		type: String,
+		//minlength: [6, 'You need at least 6 letters for your password'],
+		unique: true,
+	},
+	accessToken: {
+		type: String,
+		default: () => crypto.randomBytes(128).toString('hex'),
+	},
+});
 
-// Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(express.json())
+//Here we create a model for creating user via schema
+const User = mongoose.model('User', UserSchema);
 
-// Start defining your routes here
+//A shield asking for accessToken
+const authenticateUser = async (req, res, next) => {
+	const accessToken = req.header('Authorization');
+	try {
+		const user = await User.findOne({ accessToken });
+		if (user) {
+			next();
+		} else {
+			res.status(401).json({ response: 'Please log in', success: false });
+		}
+	} catch (err) {
+		res.status(400).json({ response: error, success: false });
+	}
+};
+
+//PORT=9000 npm start
+const port = process.env.PORT || 8080;
+const app = express();
+
+// Middleware to enable cors and json body parsing
+app.use(cors());
+app.use(express.json());
+
+//--------------ENDPOINTS--------------
+
 app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+	res.send('Hello world');
+});
+
+//--------------Authentication test--------------
+app.get('/secrets', authenticateUser);
+app.get('/secrets', (req, res) => {
+	res.json({ secret: 'This is a super secret message' });
+});
+
+//--------------Register--------------
+app.post('/signup', async (req, res) => {
+	const { name, username, email, password } = req.body;
+	try {
+		const salt = bcrypt.genSaltSync();
+		//a conditon for creating a password
+		if (password.length < 6 && username.length < 5) {
+			//redirecting to catch
+			throw 'Password must be at least 6 characters long and username must be longer than 5 characters';
+		}
+
+		const newUser = await new User({
+			name,
+			username,
+			email,
+			password: bcrypt.hashSync(password, salt),
+		}).save();
+		res.status(201).json({
+			response: {
+				userId: newUser._id,
+				username: newUser.userName,
+				acccessToken: newUser.accessToken,
+			},
+			success: true,
+		});
+	} catch (error) {
+		res.status(400).json({
+			response: error,
+			success: false,
+			message: 'New user could not be created',
+		});
+	}
+});
+
+//--------------Log in--------------
+app.post('/signin', async (req, res) => {
+	const { username, password } = req.body; //object destructuring
+	try {
+		const user = await User.findOne({ username });
+		if (user && bcrypt.compareSync(password, user.password)) {
+			res.status(200).json({
+				response: {
+					userId: user._id,
+					username: user.username,
+					accessToken: user.accessToken,
+				},
+				success: true,
+			});
+		} else {
+			res.status(404).json({
+				response: 'User not found or password does not match',
+				success: false,
+			});
+		}
+	} catch (error) {
+		res.status(400).json({
+			response: error,
+			success: false,
+		});
+	}
+});
 
 // Start the server
 app.listen(port, () => {
-  // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`)
-})
+	// eslint-disable-next-line
+	console.log(`Server running on http://localhost:${port}`);
+});
