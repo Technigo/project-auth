366deb339848b9912f26ec150e2391086115eb96
Birgit Nehrwein <nehrwein@posteo.de>
2022-01-11 15:50:57 +0100

    added authenticateUser-function and authenticated endpoint /secrets

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
-import express from 'express'
-import cors from 'cors'
-import mongoose from 'mongoose'
-import crypto from 'crypto'
-import bcrypt from 'bcrypt'
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import crypto from 'crypto';
+import bcrypt from 'bcrypt';
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI"
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/authAPI';
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
 
 const UserSchema = new mongoose.Schema({
 	username: {
@@ -26,23 +26,38 @@ const UserSchema = new mongoose.Schema({
 
 const User = mongoose.model('User', UserSchema);
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
+// Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
-const app = express()
+const port = process.env.PORT || 8080;
+const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(cors())
-app.use(express.json())
+// v1 - Allow all domains
+app.use(cors());
+app.use(express.json());
 
-// Start defining your routes here
-app.get('/', (req, res) => {
-  res.send('Hello world')
-})
+const authenticateUser = async (req, res, next) => {
+	const accessToken = req.header('Authorization');
 
-// create a new user 
+	try {
+		const user = await User.findOne({ accessToken });
+		if (user) {
+			next();
+		} else {
+			res.status(401).json({ response: 'Please, log in', success: false });
+		}
+	} catch (error) {
+		res.status(400).json({ response: error, success: false });
+	}
+};
+
+app.get('/secrets', authenticateUser);
+app.get('/secrets', (req, res) => {
+	res.send('Here is the secret');
+});
+
 app.post('/signup', async (req, res) => {
 	const { username, password } = req.body;
 
@@ -71,7 +86,6 @@ app.post('/signup', async (req, res) => {
 	}
 });
 
-// check for user in the db
 app.post('/signin', async (req, res) => {
 	const { username, password } = req.body;
 
@@ -100,6 +114,6 @@ app.post('/signin', async (req, res) => {
 
 // Start the server
 app.listen(port, () => {
-  // eslint-disable-next-line
-  console.log(`Server running on http://localhost:${port}`)
-})
+	// eslint-disable-next-line
+	console.log(`Server running on http://localhost:${port}`);
+});
