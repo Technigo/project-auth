19d95588b847c67729fe89ff0a1d719a497d47d9
Lisa Blomé <81302626+annaester@users.noreply.github.com>
2022-01-12 19:47:16 +0100

    started with styled components

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,89 +1,188 @@
-import React, { useState, useEffect } from 'react'
-import { useSelector, useDispatch, batch } from 'react-redux'
-import { Link, useNavigate } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch, batch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
-import { API_URL } from '../utils/constants'
-import user from '../reducers/user'
+import { API_URL } from "../utils/constants";
+import user from "../reducers/user";
+
+const Background = styled.div`
+  background: linear-gradient(
+    to bottom right,
+    rgb(0, 0, 0) 0%,
+    rgb(50, 50, 50) 100%
+  );
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  padding-top: 20px;
+  align-items: center;
+  font-family: Helvetica Neue;
+`;
+
+const BackLink = styled.div`
+  background: black;
+  color: white;
+  align-self: start;
+  padding-left: 20px;
+
+  a {
+    color: white;
+  }
+`;
+
+const Title = styled.h1`
+  text-align: center;
+  width: 700px;
+  color: white;
+`;
+
+const InputBox = styled.div`
+  margin-top: 50px;
+  padding: 20px;
+  border: 1px solid white;
+  border-radius: 40px;
+  height: 300px;
+  width: 500px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  color: white;
+
+  input {
+    padding: 10px;
+    border: none;
+    border-radius: 6px;
+    margin: 5px;
+  }
+
+  label {
+    color: white;
+    margin: 5px;
+    font-weight: bold;
+  }
+
+  button {
+    margin: 20px 3px;
+    padding: 10px;
+    border-radius: 50px;
+    border: 1px solid white;
+    color: white;
+    background: black;
+  }
+`;
+
+const RadioBox = styled.div`
+  margin: 5px;
+  padding: 5px;
+`;
+
+const FormBox = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
 
 const Login = () => {
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-    const [mode, setMode] = useState('signup')
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [mode, setMode] = useState("signup");
 
-    const accessToken = useSelector((store) => store.user.accessToken)
+  const accessToken = useSelector((store) => store.user.accessToken);
 
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        if (accessToken) {
-            navigate('/')
+  useEffect(() => {
+    if (accessToken) {
+      navigate("/");
+    }
+  }, [accessToken, navigate]);
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    };
+
+    fetch(API_URL(mode), options)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.success) {
+          batch(() => {
+            dispatch(user.actions.setUserId(data.response.userId));
+            dispatch(user.actions.setUserName(data.response.username));
+            dispatch(user.actions.setAccessToken(data.response.accessToken));
+            dispatch(user.actions.setError(null));
+          });
+        } else {
+          batch(() => {
+            dispatch(user.actions.setUserId(null));
+            dispatch(user.actions.setUserName(null));
+            dispatch(user.actions.setAccessToken(null));
+            dispatch(user.actions.setError(data.response));
+            alert("Username already taken!");
+          });
         }
-    }, [accessToken, navigate])
+      });
+  };
 
-    const onFormSubmit = (event) => {
-        event.preventDefault()
+  return (
+    <Background>
+      <BackLink>
+        <Link to="/">To '/'!</Link>
+      </BackLink>
+      <Title>
+        Welcome to our special site, please log in OR sign up to access the
+        secret information.
+      </Title>
+      <InputBox>
+        <RadioBox>
+          <label htmlFor="signup">Sign up</label>
+          <input
+            id="signup"
+            type="radio"
+            checked={mode === "signup"}
+            onChange={() => setMode("signup")}
+          />
+          <label htmlFor="signin">Log in</label>
+          <input
+            id="signin"
+            type="radio"
+            checked={mode === "signin"}
+            onChange={() => setMode("signin")}
+          />
+        </RadioBox>
+        <FormBox onSubmit={onFormSubmit}>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button
+            type="submit"
+            disabled={password.length < 5}
+            onClick={onFormSubmit}
+          >
+            submit
+          </button>
+        </FormBox>
+      </InputBox>
+    </Background>
+  );
+};
 
-        const options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ username, password })
-        }
-
-        fetch(API_URL(mode), options)
-        .then((res) => res.json())
-        .then((data) => {
-            if (data.success) {
-                batch(() => {
-                dispatch(user.actions.setUserId(data.response.userId))
-                dispatch(user.actions.setUserName(data.response.username))
-                dispatch(user.actions.setAccessToken(data.response.accessToken))
-                dispatch(user.actions.setError(null))
-            })
-            } else {
-                batch(() => {
-                dispatch(user.actions.setUserId(null))
-                dispatch(user.actions.setUserName(null))
-                dispatch(user.actions.setAccessToken(null))
-                dispatch(user.actions.setError(data.response))
-                alert('Username already taken!')
-            })
-        }
-    })}
-
-    return (
-        <>  
-            <div>
-                <Link to="/">To '/'!</Link>
-            </div>
-            <label htmlFor="signup">Signup</label>
-                <input 
-                id="signup" 
-                type="radio" 
-                checked={mode === 'signup'} 
-                onChange={() => setMode('signup')}/>
-            <label htmlFor="signin">Signin</label>
-                <input 
-                id="signin" 
-                type="radio" 
-                checked={mode === 'signin'} 
-                onChange={() => setMode('signin')} />
-            <form onSubmit={onFormSubmit}>
-            <label htmlFor="username">Username</label>
-                <input id="username" type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-                <label htmlFor="password">Password</label>
-                <input 
-                id="password" 
-                type="password" 
-                
-                value={password} 
-                onChange={(e) => setPassword(e.target.value)} />
-                <button type="submit" disabled={password.length < 5} onClick={onFormSubmit}>submit</button>
-            </form>
-        </>
-    )
-}
-
-export default Login
+export default Login;
--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,67 +1,103 @@
-import React, { useEffect } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { useNavigate, Link } from 'react-router-dom'
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate, Link } from "react-router-dom";
+import styled from "styled-components";
 
-import { API_URL } from '../utils/constants'
-import thoughts from '../reducers/thoughts' 
-import user from '../reducers/user'
+import { API_URL } from "../utils/constants";
+import thoughts from "../reducers/thoughts";
+import user from "../reducers/user";
 
+const Background = styled.div`
+  background: linear-gradient(
+    to bottom right,
+    rgb(0, 0, 0) 0%,
+    rgb(50, 50, 50) 100%
+  );
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  padding-top: 20px;
+
+  h1 {
+    text-align: center;
+    color: white;
+  }
+`;
+
+const HeaderBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 97vw;
+  padding: 10px 20px;
+
+  a {
+    color: white;
+  }
+
+  button {
+    margin: 20px 3px;
+    padding: 10px;
+    border-radius: 50px;
+    border: 1px solid white;
+    color: white;
+    background: black;
+    width: 100px;
+  }
+`;
 
 const Main = () => {
-    const thoughtsItems = useSelector((store) => store.thoughts.items)
-    const accessToken = useSelector((store) => store.user.accessToken)
+  const thoughtsItems = useSelector((store) => store.thoughts.items);
+  const accessToken = useSelector((store) => store.user.accessToken);
 
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const logout = () => {
-        // event.preventDefault()
-        dispatch(user.actions.setAccessToken(''))
-        // localStorage.clear()
+  const logout = () => {
+    // event.preventDefault()
+    dispatch(user.actions.setAccessToken(""));
+    // localStorage.clear()
+  };
+
+  useEffect(() => {
+    if (!accessToken) {
+      navigate("/login");
     }
+  }, [accessToken, navigate]);
 
-    useEffect(() => {
-        if (!accessToken) {
-            navigate('/login')
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      headers: {
+        Authorization: accessToken,
+      },
+    };
+    fetch(API_URL("thoughts"), options)
+      .then((res) => res.json)
+      .then((data) => {
+        if (data.success) {
+          dispatch(thoughts.actions.setItems(data.response));
+          dispatch(thoughts.actions.setError(null));
+        } else {
+          dispatch(thoughts.actions.setError(data.response));
+          dispatch(thoughts.actions.setItems([]));
         }
-    }, [accessToken, navigate])
+      });
+  }, [accessToken]);
 
-    useEffect(() => {
-        const options = {
-            method: 'GET',
-            headers: {
-                'Authorization': accessToken
-            },
-        }
-        fetch(API_URL('thoughts'), options)
-        .then((res) => res.json)
-        .then((data) => {
-            if (data.success) {
-                dispatch(thoughts.actions.setItems(data.response))
-                dispatch(thoughts.actions.setError(null))
-            } else {
-                dispatch(thoughts.actions.setError(data.response))
-                dispatch(thoughts.actions.setItems([]))
-            }
-        })
-    }, [accessToken])
+  return (
+    <Background>
+      <div>
+        <HeaderBox>
+          <Link to="/login">To '/login'!</Link>
+          <button onClick={logout}>Sign out!</button>
+        </HeaderBox>
+        <h1>Protected happy thoughts:</h1>
+        {thoughtsItems.map((item) => (
+          <div key={item._id}>{item.message}</div>
+        ))}
+      </div>
+    </Background>
+  );
+};
 
-    return (
-        <>
-        <div>
-            <div>
-                <Link to="/login">To '/login'!</Link>
-            </div>
-            <h1>Protected happy thoughts:</h1>
-			{thoughtsItems.map((item) => (
-				<div key={item._id}>{item.message}</div>
-			))}
-        </div>
-        <div>
-            <button onClick={logout}>Sign out!</button>
-        </div>
-        </>
-    )
-}
-
-export default Main
+export default Main;
