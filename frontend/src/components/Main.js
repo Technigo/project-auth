a3f41f8f980d9125c8b7d7eb6fb36a913ef332c3
isomoth <ic.gonzalez35@hotmail.com>
2022-01-12 18:54:04 +0100

    Feat: Add logout button, remove Selector and import for secrets in Main.js

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,23 +1,33 @@
 import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector, useDispatch, batch } from 'react-redux';
 import { useNavigate, Link } from 'react-router-dom';
 
 import { API_URL } from '../utils/constants';
-import secrets from '../reducers/secrets';
+// import secrets from '../reducers/secrets';
+import user from '../reducers/user';
 
 const Main = () => {
-  const secretsItems = useSelector((store) => store.secrets.items);
+  // const secretsItems = useSelector((store) => store.secrets.items);
   const accessToken = useSelector((store) => store.user.accessToken);
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  const logout = () => {
+    batch(() => {
+      dispatch(user.actions.setUsername(null));
+      dispatch(user.actions.setAccessToken(null));
+
+      localStorage.removeItem('user');
+    });
+  };
 
   useEffect(() => {
     if (!accessToken) {
       navigate('/login');
     }
   }, [accessToken, navigate]);
-
+  /* 
   useEffect(() => {
     const options = {
       method: 'GET',
@@ -38,16 +48,19 @@ const Main = () => {
         }
       });
   }, [accessToken]);
-
+ */
   return (
     <div>
       <div>
         <Link to='/login'>To '/login' !</Link>
       </div>
       <h1>It's a secret!</h1>
-      {secretsItems.map((item) => (
+      {/* {secretsItems.map((item) => (
         <div key={item._id}>{item.message}</div>
-      ))}
+      ))} */}
+      <button className='button' onClick={logout}>
+        Logout
+      </button>
     </div>
   );
 };
