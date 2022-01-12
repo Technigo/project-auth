9e32f5e7c635f74d091462c836cb974786efb911
Jenny Quach <quach.jen@gmail.com>
2022-01-12 23:17:37 +0100

    Created Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
+
+import Main from './components/Main'
+import Signin from './components/Signin'
+import NotFound from './components/NotFound'
+
+import user from './reducers/user'
+import profiles from './reducers/profile'
+
+
+const reducer = combineReducers({
+  user: user.reducer,
+  profiles: profiles.reducer
+})
+
+const store = configureStore({ reducer })
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <Provider store={store}> 
+      <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/signin" element={<Signin />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+      </BrowserRouter>
+    </Provider>
   )
 }
