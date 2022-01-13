bd88d904933fb61a3d77d6315c8a9e6f2c55d55e
isomoth <ic.gonzalez35@hotmail.com>
2022-01-13 10:26:22 +0100

    Fix: Updates BASE_URL to /secrets

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,3 +1,3 @@
-const BASE_URL = 'https://project-auth-secrets.herokuapp.com/';
+const BASE_URL = 'https://project-auth-secrets.herokuapp.com/secrets';
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`;
