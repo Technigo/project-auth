30ee3e3f7a5a0753a813951952b9bbff6259bcdf
Sabrina Zancotti <zancotti@gmail.com>
2022-01-13 16:06:40 +0100

    removed a console log

--- frontend/src/screen/Screen.js
+++ frontend/src/screen/Screen.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useSelector } from "react-redux";
-
 import { Login } from "components/Login";
 import { LoggedIn } from "components/LoggedIn";
 
 export const Screen = () => {
   const token = useSelector((store) => store.user.user.accessToken);
-  console.log(token);
 
   return (
     <BrowserRouter>
