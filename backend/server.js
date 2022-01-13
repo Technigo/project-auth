87029d24de2b696bfcd403acefc7c3634c2e0ac8
Rephili <72921631+Rephili@users.noreply.github.com>
2022-01-13 18:57:01 +0100

    trying to fix heroku error

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,37 @@
-import express from 'express';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import crypto from 'crypto';
-import bcrypt from 'bcrypt';
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import crypto from "crypto";
+import bcrypt from "bcrypt";
 
-const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/authAPI';
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI";
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
 const UserSchema = new mongoose.Schema({
-	username: {
-		type: String,
-		unique: true,
-		required: true,
-	},
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
   email: {
     type: String,
-		unique: true,
-		required: true,   
+    unique: true,
+    required: true,
   },
-	password: {
-		type: String,
-		required: true,
-	},
-	accessToken: {
-		type: String,
-		default: () => crypto.randomBytes(128).toString('hex'),
-	},
+  password: {
+    type: String,
+    required: true,
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString("hex"),
+  },
 });
 
-const User = mongoose.model('User', UserSchema);
+const User = mongoose.model("User", UserSchema);
 
 // Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
@@ -46,83 +46,93 @@ app.use(cors());
 app.use(express.json());
 
 const authenticateUser = async (req, res, next) => {
-	const accessToken = req.header('Authorization');
+  const accessToken = req.header("Authorization");
 
-	try {
-		const user = await User.findOne({ accessToken });
-		if (user) {
-			next();
-		} else {
-			res.status(401).json({ response: 'Please, log in', success: false });
-		}
-	} catch (error) {
-		res.status(400).json({ response: error, success: false });
-	}
+  try {
+    const user = await User.findOne({ accessToken });
+    if (user) {
+      next();
+    } else {
+      res.status(401).json({ response: "Please, log in", success: false });
+    }
+  } catch (error) {
+    res.status(400).json({ response: error, success: false });
+  }
 };
 
-app.get('/secrets', authenticateUser);
-app.get('/secrets', (req, res) => {
-	res.send('Here is the secret');
+app.get("/", (req, res, next) => {
+  res.status(200).json({
+    status: "success",
+    data: {
+      name: "Project Auth",
+      version: "0.1.0",
+    },
+  });
 });
 
-app.post('/signup', async (req, res) => {
-	const { username, password, email } = req.body;
+app.get("/secrets", authenticateUser);
+app.get("/secrets", (req, res) => {
+  res.send("Here is the secret");
+});
 
-	try {
-		const salt = bcrypt.genSaltSync();
+app.post("/signup", async (req, res) => {
+  const { username, password, email } = req.body;
 
-		if (password.length < 5) {
-			throw 'Password must be at least 5 characters long';
-		}
+  try {
+    const salt = bcrypt.genSaltSync();
 
-		const newUser = await new User({
-			username,
+    if (password.length < 5) {
+      throw "Password must be at least 5 characters long";
+    }
+
+    const newUser = await new User({
+      username,
       email,
-			password: bcrypt.hashSync(password, salt),
-		}).save();
+      password: bcrypt.hashSync(password, salt),
+    }).save();
 
-		res.status(201).json({
-			response: {
-				userId: newUser._id,
-				username: newUser.username,
+    res.status(201).json({
+      response: {
+        userId: newUser._id,
+        username: newUser.username,
         email: newUser.email,
-				accessToken: newUser.accessToken,
-			},
-			success: true,
-		});
-	} catch (error) {
-		res.status(400).json({ response: error, success: false });
-	}
+        accessToken: newUser.accessToken,
+      },
+      success: true,
+    });
+  } catch (error) {
+    res.status(400).json({ response: error, success: false });
+  }
 });
 
-app.post('/signin', async (req, res) => {
-	const { username, password } = req.body;
+app.post("/signin", async (req, res) => {
+  const { username, password } = req.body;
 
-	try {
-		const user = await User.findOne({ username });
+  try {
+    const user = await User.findOne({ username });
 
-		if (user && bcrypt.compareSync(password, user.password)) {
-			res.status(200).json({
-				response: {
-					userId: user._id,
-					username: user.username,
-					accessToken: user.accessToken,
-				},
-				success: true,
-			});
-		} else {
-			res.status(404).json({
-				response: "Username or password doesn't match",
-				success: false,
-			});
-		}
-	} catch (error) {
-		res.status(400).json({ response: error, success: false });
-	}
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(200).json({
+        response: {
+          userId: user._id,
+          username: user.username,
+          accessToken: user.accessToken,
+        },
+        success: true,
+      });
+    } else {
+      res.status(404).json({
+        response: "Username or password doesn't match",
+        success: false,
+      });
+    }
+  } catch (error) {
+    res.status(400).json({ response: error, success: false });
+  }
 });
 
 // Start the server
 app.listen(port, () => {
-	// eslint-disable-next-line
-	console.log(`Server running on http://localhost:${port}`);
+  // eslint-disable-next-line
+  console.log(`Server running on http://localhost:${port}`);
 });
