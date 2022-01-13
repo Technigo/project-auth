3deb3a516281fc1ff872f559320296b86b2a0b9d
Mattsson717 <83518778+Mattsson717@users.noreply.github.com>
2022-01-13 09:10:11 +0100

    started  with frontend

--- backend/server.js
+++ backend/server.js
@@ -37,6 +37,15 @@ const UserSchema = new mongoose.Schema({
 
 const User = mongoose.model("User", UserSchema);
 
+const ThoughtSchema = new mongoose.Schema({
+  message: {
+    type: String,
+    required: true,
+  },
+});
+
+const Thought = mongoose.model("Thought", ThoughtSchema);
+
 // Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
 const port = process.env.PORT || 8080;
@@ -52,10 +61,14 @@ const authenticateUser = async (req, res, next) => {
   try {
     const user = await User.findOne({ accessToken });
     if (user) {
-      // req.user = user ?
       next();
     } else {
-      res.status(401).json({ response: "Please, log in", success: false });
+      res.status(401).json({
+        response: {
+          message: "Please, log in",
+        },
+        success: false,
+      });
     }
   } catch (error) {
     res.status(400).json({ response: error, success: false });
@@ -67,8 +80,20 @@ const authenticateUser = async (req, res, next) => {
 
 // Start defining your routes here
 app.get("/thoughts", authenticateUser);
-app.get("/thoughts", (req, res) => {
-  res.send("Here are your thoughts");
+app.get("/thoughts", async (req, res) => {
+  const thoughts = await Thought.find({});
+  res.status(201).json({ response: thoughts, success: true });
+});
+
+app.post("/thoughts", async (req, res) => {
+  const { message } = req.body;
+
+  try {
+    const newThought = await new Thought({ message }).save();
+    res.status(201).json({ response: newThought, success: true });
+  } catch (error) {
+    res.status(400).json({ response: error, success: false });
+  }
 });
 
 // Signup endpoint
---
+++ frontend/src/components/Login.js
@@ -0,0 +1,107 @@
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch, batch } from "react-redux";
+import { useNavigate, Link } from "react-router-dom";
+
+import { API_URL } from "../utils/constants";
+import user from "../reducers/user";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [mode, setMode] = useState("signup");
+
+  const accessToken = useSelector((store) => store.user.accessToken);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (accessToken) {
+      navigate("/");
+    }
+  }, [accessToken, navigate]);
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password, email }),
+    };
+
+    fetch(API_URL(mode), options)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.success) {
+          batch(() => {
+            dispatch(user.actions.setUserId(data.response.userId));
+            dispatch(user.actions.setUsername(data.response.username));
+            dispatch(user.actions.setAccessToken(data.response.accessToken));
+            dispatch(user.actions.setEmail(data.response.email));
+            dispatch(user.actions.setError(null));
+          });
+        } else {
+          batch(() => {
+            dispatch(user.actions.setUserId(null));
+            dispatch(user.actions.setUsername(null));
+            dispatch(user.actions.setAccessToken(null));
+            dispatch(user.actions.setEmail(null));
+            dispatch(user.actions.setError(data.response));
+          });
+        }
+      });
+  };
+
+  return (
+    <>
+      <div>
+        <Link to="/">To '/' !</Link>
+      </div>
+      <label htmlFor="signup">Signup</label>
+      <input
+        id="signup"
+        type="radio"
+        checked={mode === "signup"}
+        onChange={() => setMode("signup")}
+      />
+      <label htmlFor="signin">Signin</label>
+      <input
+        id="signin"
+        type="radio"
+        checked={mode === "signin"}
+        onChange={() => setMode("signin")}
+      />
+      <form onSubmit={onFormSubmit}>
+        <label htmlFor="username">Username</label>
+        <input
+          id="username"
+          type="text"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          id="password"
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          type="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
+};
+
+export default Login;
