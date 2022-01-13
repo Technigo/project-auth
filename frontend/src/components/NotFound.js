14482385321a83e630738bd040cded3ced4cbd5a
Naushin Malik <nosheen.malik@gmail.com>
2022-01-13 15:18:00 +0100

    styled the Notfound page

--- frontend/src/components/NotFound.js
+++ frontend/src/components/NotFound.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
 
+import { useNavigate, Link } from 'react-router-dom';
+
+const ThoughtsContainer = styled.div`
+  background-color: green;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
+`;
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  font-size: 20px;
+  color: #fff;
+`;
 const NotFound = () => {
-  return <h1>Not found...</h1>;
+  return (
+    <ThoughtsContainer>
+      Page not found...
+      <StyledLink to="/signin">Please sign in</StyledLink>
+    </ThoughtsContainer>
+  );
 };
 
 export default NotFound;
