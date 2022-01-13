0635405f63983185521a52380f37e4de75de8f48
madeleinesvensson <madeleinesvensson13@gmail.com>
2022-01-13 18:28:21 +0100

    securitylevel i header, additional styling to put everyting in header

--- frontend/src/components/SignOut.js
+++ frontend/src/components/SignOut.js
@@ -4,20 +4,22 @@ import { useDispatch, useSelector } from "react-redux";
 import { users } from "../reducers/users";
 
 export const SignOut = () => {
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    const username = useSelector((state) => state.users.username);
+  const username = useSelector((state) => state.users.username);
+  const securityLevel = useSelector((state) => state.users.securityLevel);
 
-    const onSignOut = () => {
-        dispatch(users.actions.setUserToLoggedOut());
-        navigate("/");
-    };
+  const onSignOut = () => {
+    dispatch(users.actions.setUserToLoggedOut());
+    navigate("/");
+  };
 
-    return (
-        <div>
-            <h4>Signed in as: {username}</h4>
-            <button onClick={onSignOut}>Sign out</button>
-        </div>
-    );
+  return (
+    <div>
+      <h4>Signed in as: {username}</h4>
+      <h5>Your securitylevel is: {securityLevel}</h5>
+      <button onClick={onSignOut}>Sign out</button>
+    </div>
+  );
 };
