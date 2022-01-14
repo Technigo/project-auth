181a59e9d470a86f82cded08de3f8edc504fcd29
Anna Dimitrakopoulos <dimitrakopoulos.anna@gmail.com>
2022-01-14 14:13:17 +0100

    minor changes again

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,3 +1,3 @@
-const BASE_URL = "https://admj-authentication.herokuapp.com/";
+const BASE_URL = "https://admj-authentication.herokuapp.com";
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`;
