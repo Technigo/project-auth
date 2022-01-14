9310488326d3a8a8886f6853d33ac7b2b7c5a352
NinaAlejandra <Niinaa___@hotmail.com>
2022-01-14 16:33:20 +0100

    added styling logout button and changed sign in and sign out button

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,29 +1,29 @@
-import React, { useState, useEffect } from "react"
-import { useSelector, useDispatch, batch } from "react-redux"
-import { useNavigate, Link } from "react-router-dom"
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch, batch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-import { API_URL } from "../utils/constants"
-import user from "../reducers/user"
+import { API_URL } from "../utils/constants";
+import user from "../reducers/user";
 
 const Login = () => {
-  const [username, setUsername] = useState("")
-  const [password, setPassword] = useState("")
-  const [mode, setMode] = useState("signup")
-  const [error, setError] = useState("")
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [mode, setMode] = useState("signup");
+  const [error, setError] = useState("");
 
-  const accessToken = useSelector((store) => store.user.accessToken)
+  const accessToken = useSelector((store) => store.user.accessToken);
 
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (accessToken) {
-      navigate("/")
+      navigate("/");
     }
-  }, [accessToken, navigate])
+  }, [accessToken, navigate]);
 
   const onFormSubmit = (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
     const options = {
       method: "POST",
@@ -31,44 +31,54 @@ const Login = () => {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ username, password }),
-    }
+    };
 
     fetch(API_URL(mode), options)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data)
+        console.log(data);
         if (data.success) {
           batch(() => {
-            dispatch(user.actions.setUserId(data.response.userId))
-            dispatch(user.actions.setUsername(data.response.username))
-            dispatch(user.actions.setAccessToken(data.response.accessToken))
-            dispatch(user.actions.setError(null))
-          })
+            dispatch(user.actions.setUserId(data.response.userId));
+            dispatch(user.actions.setUsername(data.response.username));
+            dispatch(user.actions.setAccessToken(data.response.accessToken));
+            dispatch(user.actions.setError(null));
+          });
         } else {
           batch(() => {
-            dispatch(user.actions.setUserId(null))
-            dispatch(user.actions.setUsername(null))
-            dispatch(user.actions.setAccessToken(null))
-            dispatch(user.actions.setError(data.response))
-          })
-          setError("Sorry, this is an invalid username or password")
+            dispatch(user.actions.setUserId(null));
+            dispatch(user.actions.setUsername(null));
+            dispatch(user.actions.setAccessToken(null));
+            dispatch(user.actions.setError(data.response));
+          });
+          setError("Sorry, this is an invalid username or password");
         }
-      })
-  }
+      });
+  };
 
   return (
     <>
-      <div>
-        <Link to="/">To '/' !</Link>
-      </div>
-
-      <button type="submit" onClick={() => setMode("signin")}>
-        Sign in
-      </button>
-      <button type="submit" onClick={() => setMode("signup")}>
-        Sign up
-      </button>
-      {/* <label htmlFor="signup">Signup</label>
+      <div className="main-container">
+        <div className="btn-container">
+          {mode === "signin" ? (
+            <button
+              className="sign-btn"
+              type="submit"
+              onClick={() => setMode("signup")}
+            >
+              Sign up
+            </button>
+          ) : (
+            <button
+              className="sign-btn"
+              type="submit"
+              onClick={() => setMode("signin")}
+            >
+              Sign in
+            </button>
+          )}
+        </div>
+        {/* <label htmlFor="signup">Signup</label>
       <input
         id="signup"
         type="radio"
@@ -82,28 +92,33 @@ const Login = () => {
         checked={mode === "signin"}
         onChange={() => setMode("signin")}
       /> */}
-      <form onSubmit={onFormSubmit}>
-        <label htmlFor="username">Username</label>
-        <input
-          id="username"
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          id="password"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
+        <div className="form-container">
+          <form onSubmit={onFormSubmit}>
+            <label htmlFor="username">Username</label>
+            <input
+              id="username"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <label htmlFor="password">Password</label>
+            <input
+              id="password"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
 
-        <p>{error}</p>
+            <p>{error}</p>
 
-        <button type="submit">Submit</button>
-      </form>
+            <button className="submit-btn" type="submit">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,22 +1,23 @@
-import React, { useEffect } from "react"
-import { useSelector, useDispatch } from "react-redux"
-import { useNavigate, Link } from "react-router-dom"
+import React, { useEffect } from "react";
+import { useSelector, useDispatch, batch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-import { API_URL } from "../utils/constants"
-import thoughts from "../reducers/thoughts"
+import { API_URL } from "../utils/constants";
+import thoughts from "../reducers/thoughts";
+import user from "../reducers/user";
 
 const Main = () => {
-  const thoughtsItems = useSelector((store) => store.thoughts.items)
-  const accessToken = useSelector((store) => store.user.accessToken)
+  const thoughtsItems = useSelector((store) => store.thoughts.items);
+  const accessToken = useSelector((store) => store.user.accessToken);
 
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (!accessToken) {
-      navigate("/login")
+      navigate("/login");
     }
-  }, [accessToken, navigate])
+  }, [accessToken, navigate]);
 
   useEffect(() => {
     const options = {
@@ -24,32 +25,43 @@ const Main = () => {
       headers: {
         Authorization: accessToken,
       },
-    }
+    };
 
     fetch(API_URL("thoughts"), options)
       .then((res) => res.json())
       .then((data) => {
         if (data.success) {
-          dispatch(thoughts.actions.setItems(data.response))
-          dispatch(thoughts.actions.setError(null))
+          dispatch(thoughts.actions.setItems(data.response));
+          dispatch(thoughts.actions.setError(null));
         } else {
-          dispatch(thoughts.actions.setItems([]))
-          dispatch(thoughts.actions.setError(data.response))
+          dispatch(thoughts.actions.setItems([]));
+          dispatch(thoughts.actions.setError(data.response));
         }
-      })
-  }, [accessToken, dispatch])
+      });
+  }, [accessToken, dispatch]);
+
+  const logOutUser = () => {
+    batch(() => {
+      dispatch(user.actions.setUsername(null));
+      dispatch(user.actions.setAccessToken(null));
+
+      localStorage.removeItem("user");
+    });
+  };
 
   return (
-    <div>
+    <>
       <div>
-        <Link to="/login">To "/login" !</Link>
+        <h1>welcome to the chamber of secrets..</h1>
+        {thoughtsItems.map((item) => (
+          <div key={item._id}>{item.message}</div>
+        ))}
       </div>
-      <h1>welcome to the chamber of secrets..</h1>
-      {thoughtsItems.map((item) => (
-        <div key={item._id}>{item.message}</div>
-      ))}
-    </div>
-  )
-}
+      <button className="logout-btn" onClick={logOutUser}>
+        Log out
+      </button>
+    </>
+  );
+};
 
-export default Main
+export default Main;
