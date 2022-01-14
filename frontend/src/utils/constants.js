e14d915bac520e66c6b0f0a0859bc6e08a2079e9
Anna Dimitrakopoulos <dimitrakopoulos.anna@gmail.com>
2022-01-14 13:47:14 +0100

    changed api url

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,10 +1,3 @@
-// export const API_URL = "https://admj-authentication.herokuapp.com/";
-// export const SIGNIN_URL = "https://admj-authentication.herokuapp.com/signin";
-// export const SIGNUP_URL = "https://admj-authentication.herokuapp.com/signup";
-
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://admj-authentication.herokuapp.com/";
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`;
-
-export const STARS_URL =
-  "https://brightest-stars.p.rapidapi.com/brightstars/?api_key=e3551275c7mshe47070c1bcf512dp147705jsn5e5a262864d1";
