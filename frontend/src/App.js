e43697a15c1a908a4aa246c999eb731220c89413
Amanda Tilly <amandatilly@Amandas-2.lan>
2022-01-14 13:27:51 +0100

    started styling with styling components in App.js and Login.js components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Provider } from "react-redux";
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import styled from "styled-components";
 
 // Imported components
 import Main from "./components/Main";
@@ -11,6 +12,16 @@ import NotFound from "./components/NotFound";
 // Imported reducers
 import user from "./reducers/user";
 import thoughts from "./reducers/thoughts";
+
+import img from "./assets/background.png";
+
+const MainWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-image: url(${img});
+`;
 
 // Combining reducers: user & thoughts
 const reducer = combineReducers({
@@ -22,14 +33,16 @@ const store = configureStore({ reducer });
 
 export const App = () => {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+    <MainWrapper>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    </MainWrapper>
   );
 };
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,9 +1,89 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch, batch } from "react-redux";
 import { useNavigate, Link } from "react-router-dom";
+import styled from "styled-components";
 
 import { API_URL } from "../utils/urls";
 import user from "../reducers/user";
+
+const Wrapper = styled.main`
+  display: flex;
+  flex-direction: column;
+  width: 90%;
+  padding: 20px;
+  /* border: solid black 2px; */
+  background-color: rgba(255, 255, 255, 0.063);
+  backdrop-filter: blur(12px);
+  --webkit-backdrop-filter: blur(12px);
+
+  @media (min-width: 200px) {
+    margin: 0 auto;
+    max-width: 300px;
+  }
+`;
+
+const RadioWrapper = styled.section`
+  display: flex;
+  justify-content: center;
+  padding: 20px 20px;
+  /* border: 2px solid red; */
+  margin: 20px;
+`;
+
+const RadioButtons = styled.div`
+  display: flex;
+  padding: 0 10px;
+
+  input {
+    opacity: 0.5;
+  }
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  button {
+    width: 100px;
+    padding: 5px 10px;
+    align-items: center;
+    text-align: center;
+    border: orange solid 2px;
+    background-color: transparent;
+    border-radius: 50px;
+    cursor: pointer;
+    color: white;
+    font-weight: bold;
+    text-transform: uppercase;
+    transition: all 0.3s ease;
+    margin-top: 30px;
+
+    :hover {
+      background-color: orange;
+      transform: scale(1.1, 1.1);
+    }
+  }
+`;
+
+const InputWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 10px 10px;
+  /* border: 2px solid blue; */
+
+  label {
+    padding-bottom: 10px;
+    text-align: center;
+  }
+
+  input {
+    padding: 10px;
+    border-radius: 50px;
+    border: none;
+    text-align: center;
+  }
+`;
 
 const Login = () => {
   const [username, setUsername] = useState(""); // default value empty string
@@ -74,42 +154,55 @@ const Login = () => {
 
   return (
     // label is just informative, htmlFor so that we can connect to the input
-    <>
+    <Wrapper>
       <div>
         <Link to="/">To '/' !</Link>
       </div>
-      <label htmlFor="signup">Sign Up</label>
-      <input
-        id="signup" // connects to label so we can also click the text and not only radio button
-        type="radio"
-        checked={mode === "signup"} // this input should be checked/selected only if the mode is equal to signup
-        onChange={() => setMode("signup")} // change handler to change the mode to signup when we click on it
-      />
-      <label htmlFor="signin">Sign In</label>
-      <input
-        id="signin"
-        type="radio"
-        checked={mode === "signin"}
-        onChange={() => setMode("signin")}
-      />
-      <form onSubmit={onFormSubmit}>
-        <label htmlFor="username">Username</label>
-        <input
-          id="username"
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <label htmlFor="password">Password</label>
-        <input
-          id="password"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
+      <RadioWrapper>
+        <RadioButtons>
+          <label htmlFor="signup">Sign Up</label>
+          <input
+            id="signup" // connects to label so we can also click the text and not only radio button
+            type="radio"
+            checked={mode === "signup"} // this input should be checked/selected only if the mode is equal to signup
+            onChange={() => setMode("signup")} // change handler to change the mode to signup when we click on it
+          />
+        </RadioButtons>
+        <RadioButtons>
+          <label htmlFor="signin">Sign In</label>
+          <input
+            id="signin"
+            type="radio"
+            checked={mode === "signin"}
+            onChange={() => setMode("signin")}
+          />
+        </RadioButtons>
+      </RadioWrapper>
+
+      <Form onSubmit={onFormSubmit}>
+        <InputWrapper>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            value={username}
+            placeholder="enter your username..."
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </InputWrapper>
+        <InputWrapper>
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            value={password}
+            placeholder="enter your password..."
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </InputWrapper>
         <button type="submit">Submit</button>
-      </form>
-    </>
+      </Form>
+    </Wrapper>
   );
 };
 export default Login;
