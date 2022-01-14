f2f71175e2707096db8676900d20eff8c8b79f2e
MT-dotse <madelene.trang333@gmail.com>
2022-01-14 19:28:20 +0200

    trying to resolve deployment issue

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,2 +1,2 @@
-const BASE_URL = 'https://project-auth-secret-week20.herokuapp.com/';
+const BASE_URL = 'https://project-auth-secret-week20.herokuapp.com';
 export const API_URL = (slug) => `${BASE_URL}/${slug}`;
