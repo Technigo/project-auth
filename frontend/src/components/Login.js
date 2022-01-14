3cbf104c9bfea0d1f4d716b8b845b688f50b51c5
Darya <darya.lapata@gmail.com>
2022-01-14 08:11:18 +0100

    added styling

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch, batch } from 'react-redux'
 import { useNavigate, Link } from 'react-router-dom'
 
+import styled from 'styled-components/macro'
+
 import { API_URL } from '../utils/urls'
 
 import user from '../reducers/user'
@@ -14,7 +16,7 @@ export const Login = () => {
   const [mode, setMode] = useState('signup')
 
   const accessToken = useSelector(store => store.user.accessToken)
-
+  const error = useSelector(store => store.user.error)
 
   const dispatch = useDispatch()
   const navigate = useNavigate()
@@ -41,47 +43,55 @@ export const Login = () => {
       .then((res) => res.json())
       .then((data) => {
         // setting info from backend to redux store
-        if(data.success) {
-          // batch function combines multiple dispatches, improves loading time 
-          batch(() => {
-            // if user is saved to the database, then we save it to the reduxstore
-            dispatch(user.actions.setUserId(data.response.userId))
-            dispatch(user.actions.setUsername(data.response.username))
-            dispatch(user.actions.setAccessToken(data.response.accessToken))
-            // cleaning all of the erros that were before
-            dispatch(user.actions.setError(null))
-          })
-        } else {
-          batch(() => {
-            // cleaning store (defensive approach)
-            dispatch(user.actions.setUserId(null))
-            dispatch(user.actions.setUsername(null))
-            dispatch(user.actions.setAccessToken(null))
-            // saving error msg from database to the redux store 
-            dispatch(user.actions.setError(data.response))
-
-          })
-
-        }
-      })
+      if(data.success) {
+        // batch function combines multiple dispatches, improves loading time 
+        batch(() => {
+          // if user is saved to the database, then we save it to the reduxstore
+          dispatch(user.actions.setUserId(data.response.userId))
+          dispatch(user.actions.setUsername(data.response.username))
+          dispatch(user.actions.setAccessToken(data.response.accessToken))
+          // cleaning all of the erros that were before
+          dispatch(user.actions.setError(null))
+        })
+      } else {
+        batch(() => {
+          // cleaning store (defensive approach)
+          dispatch(user.actions.setUserId(null))
+          dispatch(user.actions.setUsername(null))
+          dispatch(user.actions.setAccessToken(null))
+          // saving error msg from database to the redux store 
+          dispatch(user.actions.setError(data.response))
+        })
+      }
+    })
   }
 
   return (
     <> 
-      <div>
-        <Link to="/">To "/" !</Link>
+      <div className="background">
+        <div className="shape"></div>
+        <div className="shape"></div>
       </div>
-      <label htmlFor='signup'>Sign up</label>
-      <input id="signup" type="radio" checked={mode === 'signup'} onChange={() => setMode('signup')} />
-      <label htmlFor='signin'>Sign in</label>
-      <input id="signin" type="radio" checked={mode === 'signin'} onChange={() => setMode('signin')} />
       <form onSubmit={onFormSubmit}>
+      <div style={{display: 'flex'}} className="toggle">
+        {/*<label htmlFor='signup' className="label">Sign up</label>*/}
+        <input id="signup" type="radio" checked={mode === 'signup'} onChange={() => setMode('signup')} />
+        {/*<label htmlFor='signin'>Sign in</label>*/}
+        <input id="signin" type="radio" checked={mode === 'signin'} onChange={() => setMode('signin')} />
+      </div>
+        <h3>{mode === 'signup' ? 'SIGN UP' : 'LOG IN'}</h3>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+        <input type="text" placeholder="Enter your email" id="username" value={username} onChange={(e) => setUsername(e.target.value)} required />
         <label htmlFor="password">Password</label>
-        <input id="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-        <button type="submit">Submit</button>
+        <input type="password" placeholder="Password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+        {error && 
+          <div>
+            <p className="error-text">{error}</p>
+          </div>
+        }
+        <button>{mode === 'signup' ? 'Submit' : 'Log in'}</button>
       </form>
     </>
   )
 }
+
--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -4,16 +4,18 @@ import { useNavigate, Link } from 'react-router-dom'
 
 import { API_URL } from 'utils/urls';
 import thoughts from '../reducers/thoughts'
+import user from '../reducers/user'
 
 export const Main = () => {
-// this comp available only for users 
+// this comp available only for users, secret compo
   const thoughtsItems = useSelector(store => store.thoughts.items)
   // hämtar accesstoken from resux store 
   const accessToken = useSelector(store => store.user.accessToken)
+  const username = useSelector(store => store.user.username)
+  console.log(username)
 
   const navigate = useNavigate()
   const dispatch = useDispatch()
-
 
   useEffect(() => {
     if(!accessToken) {
@@ -39,19 +41,21 @@ export const Main = () => {
           dispatch(thoughts.actions.setItems([]))
           dispatch(thoughts.actions.setError(data.response))
         }
-
       })
-  }, [accessToken, navigate])
+  }, [accessToken, dispatch])
+
+  const logOut = () => {
+    dispatch(user.actions.setAccessToken(null))
+    navigate('/login')
+  }
 
   return (
-    <div>
-      <div>
-        <Link to="/login">To "/login" !</Link>
-      </div>
-      <h1>Protected Happy Thoughts</h1>
-      {/*{thoughtsItems.map(item => (
-        <div key={item._id}>{item.message}</div>
-      ))}*/}
+    <div className="wrapper"> 
+      <h3>Here are your protected quotes, {username}</h3>
+      {thoughtsItems.map(item => (
+        <p className="quote" key={item._id}>{item.message}</p>
+      ))}
+    <button type="button" onClick={logOut}>Log out</button>
     </div>
   )
 }
