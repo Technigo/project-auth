e9d24aa9497a7041e7b3180d217f32ab9d104c91
madeleinesvensson <madeleinesvensson13@gmail.com>
2022-01-14 17:30:57 +0100

    testing fail

--- backend/server.js
+++ backend/server.js
@@ -71,11 +71,7 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 // Add middlewares to enable cors and json body parsing
-app.use(
-  cors({
-    origin: "https://riddlemaster.netlify.app/",
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 const authenticateUser = async (req, res, next) => {
