040fdbc496344beea57b221c4e00455e9183807a
e-delsol <80672920+e-delsol@users.noreply.github.com>
2022-01-15 16:08:20 +0100

    fix: A bug fix related to old thoughts code. build: Updating npm.

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,16 +1,12 @@
 import React, { useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import { useNavigate, Link } from "react-router-dom";
 import { Logout } from "./Logout";
-
-import { API_URL } from "../utils/constants";
-import thoughts from "../reducers/thoughts";
 
 const Main = () => {
 	const thoughtsItems = useSelector((store) => store.thoughts.items);
 	const accessToken = useSelector((store) => store.user.accessToken);
 
-	const dispatch = useDispatch();
 	const navigate = useNavigate();
 
 	useEffect(() => {
@@ -18,27 +14,6 @@ const Main = () => {
 			navigate("/login");
 		}
 	}, [accessToken, navigate]);
-
-	useEffect(() => {
-		const options = {
-			method: "GET",
-			headers: {
-				Authorization: accessToken,
-			},
-		};
-
-		fetch(API_URL("thoughts"), options)
-			.then((res) => res.json())
-			.then((data) => {
-				if (data.success) {
-					dispatch(thoughts.actions.setItems(data.response));
-					dispatch(thoughts.actions.setError(null));
-				} else {
-					dispatch(thoughts.actions.setItems([]));
-					dispatch(thoughts.actions.setError(data.response));
-				}
-			});
-	}, [accessToken, dispatch]);
 
 	return (
 		<div>
