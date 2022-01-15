6effbebe8c9af1ea6f5caf84bdcfac56af27f00c
vrill <52007698+vrill@users.noreply.github.com>
2022-01-16 00:46:05 +0100

    Added the new components logic to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import Main from './components/Main'
+import Login from './components/Login'
+import NotFound from './components/NotFound'
+
 export const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route  />
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   )
