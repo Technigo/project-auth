d56cfc1574b592e6617afc3bcd818126be828a55
e-delsol <80672920+e-delsol@users.noreply.github.com>
2022-01-16 20:19:35 +0100

    Bringing back the thoughts code

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,12 +1,17 @@
 import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
+// import { useDispatch } from "react-redux";
 import { useNavigate, Link } from "react-router-dom";
 import { Logout } from "./Logout";
 
+// import { API_URL } from "../utils/constants";
+// import thoughts from "../reducers/thoughts";
+
 const Main = () => {
-	// const thoughtsItems = useSelector((store) => store.thoughts.items);
+	const thoughtsItems = useSelector((store) => store.thoughts.items);
 	const accessToken = useSelector((store) => store.user.accessToken);
 
+	// const dispatch = useDispatch();
 	const navigate = useNavigate();
 
 	useEffect(() => {
@@ -15,15 +20,36 @@ const Main = () => {
 		}
 	}, [accessToken, navigate]);
 
+	// useEffect(() => {
+	// 	const options = {
+	// 		method: "GET",
+	// 		headers: {
+	// 			Authorization: accessToken,
+	// 		},
+	// 	};
+
+	// 	fetch(API_URL("thoughts"), options)
+	// 		.then((res) => res.json())
+	// 		.then((data) => {
+	// 			if (data.success) {
+	// 				dispatch(thoughts.actions.setItems(data.response));
+	// 				dispatch(thoughts.actions.setError(null));
+	// 			} else {
+	// 				dispatch(thoughts.actions.setItems([]));
+	// 				dispatch(thoughts.actions.setError(data.response));
+	// 			}
+	// 		});
+	// }, [accessToken, dispatch]);
+
 	return (
 		<div>
 			<div>
 				<Link to="/login">To '/login' !</Link>
 			</div>
-			<h1>Secret heading:</h1>
-			{/* {thoughtsItems.map((item) => (
+			<h1>Protected happy thoughts:</h1>
+			{thoughtsItems.map((item) => (
 				<div key={item._id}>{item.message}</div>
-			))} */}
+			))}
 			<Logout />
 		</div>
 	);
