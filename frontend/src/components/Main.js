ea17906f4880d072b667692e97b47a50d6d6f948
vrill <52007698+vrill@users.noreply.github.com>
2022-01-16 21:54:32 +0100

    Updated the Main.js with logic to display the item.

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Main = () => {
-    return <h1>Main</h1>
+    
+    const thoughtsItems = useSelector(store => store.thoughts.items)
+
+    return (
+        <section>
+            <h1>Protected Happy Thoughts:</h1>
+            {thoughtsItems.map((item) => (
+                <article key={item._id}>{item.message}</article>
+            ))}
+        </section>
+    )
 }
 
 export default Main
