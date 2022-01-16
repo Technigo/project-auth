a3449115a0f74cc560b9eecc8b1b851658711f66
Rosanna Dahlberg <wernerrosanna86@gmail.com>
2022-01-16 21:41:17 +0100

    url typo

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,4 @@
 //const BASE_URL = 'http://localhost:8080'
-const BASE_URL = 'https://week20-auth-backend.herokuapp.com/' 
+const BASE_URL = 'https://week20-auth-backend.herokuapp.com'
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`
