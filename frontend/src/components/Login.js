0fa216761e51bbf4bb48a489ceff0896051b8d63
camekman <ekman.camilla@gmail.com>
2022-01-17 13:55:04 +0100

    styling

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,23 +1,18 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch, batch } from "react-redux";
 import { useNavigate } from "react-router-dom";
-// import styled from "styled-components";
+import styled from "styled-components";
 
 import { API_URL } from "../utils/url";
 import user from "../reducers/user";
-// import order from "../reducers/order";
 
 const Login = () => {
   const [username, setUsername] = useState("");
-  // const [order, setOrder] = useState("");
-  // const [message, setMessage] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [mode, setMode] = useState("signup");
   const [errorMessage, setErrorMessage] = useState(null);
 
-  // const email = useSelector((store) => store.user.email)
-  // const message = useSelector((store) => store.order.message);
   const accessToken = useSelector((store) => store.user.accessToken);
   const error = useSelector((store) => store.user.error);
   console.log(error);
@@ -69,54 +64,87 @@ const Login = () => {
 
   return (
     <>
-      <label htmlFor="signup">Signup</label>
-      <input
-        id="signup"
-        type="radio"
-        checked={mode === "signup"}
-        onChange={() => setMode("signup")}
-      />
-      <label htmlFor="signin">Signin</label>
-      <input
-        id="signin"
-        type="radio"
-        checked={mode === "signin"}
-        onChange={() => setMode("signin")}
-      />
-      <form onSubmit={onFormSubmit}>
-        <p> User Name </p>
-        <input
-          id="username"
-          type="text"
-          value={username}
-          onChange={(event) => setUsername(event.target.value)}
-        />
-        <p> Password </p>
-        <input
-          id="password"
-          type="password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-        />
-        <p> email </p>
-        <input
-          id="email"
-          type="email"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-        />
-        {/* <p> order </p>
-        <input
-          id="order"
-          type="text"
-          value={message}
-          onChange={(event) => setMessage(event.target.value)}
-        /> */}
-        <button type="submit">Submit</button>
-        {errorMessage !== null && <p>{error.message}</p>}
-      </form>
+      <LoginContainer>
+        <form onSubmit={onFormSubmit}>
+          <p> Username </p>
+          <input
+            id="username"
+            type="text"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+          <p> Password </p>
+          <input
+            id="password"
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <p> Email </p>
+          <input
+            id="email"
+            type="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+          <RadioButtonContainer>
+            <label htmlFor="signup">Signup</label>
+            <input
+              id="signup"
+              type="radio"
+              checked={mode === "signup"}
+              onChange={() => setMode("signup")}
+            />
+            <label htmlFor="signin">Signin</label>
+            <input
+              id="signin"
+              type="radio"
+              checked={mode === "signin"}
+              onChange={() => setMode("signin")}
+            />
+          </RadioButtonContainer>
+          <ButtonContainer>
+            <SubmitButton type="submit">Submit</SubmitButton>
+          </ButtonContainer>
+          {errorMessage !== null && <p>{error.message}</p>}
+        </form>
+      </LoginContainer>
     </>
   );
 };
 
 export default Login;
+
+const LoginContainer = styled.div`
+  background-color: rgba(42, 101, 42, 0.783);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: fit-content;
+  padding: 20px;
+  border-radius: 30px;
+  border: 1px solid black;
+  box-shadow: 5px 5px;
+  color: white;
+`;
+
+const RadioButtonContainer = styled.div`
+  background-color: gray;
+  display: flex;
+  flex-direction: row;
+  margin-top: 15px;
+  padding: 15px;
+  border-radius: 10px;
+  color: white;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  margin-top: 20px;
+  justify-content: center;
+`;
+
+const SubmitButton = styled.button`
+  padding: 10px;
+  border-radius: 10px;
+`;
--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -2,17 +2,13 @@ import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
-// import { API_URL } from "../utils/url";
 import user from "../reducers/user";
 
-// import { RON_SWANSSON_URL } from "../utils/url";
 import { MOVIE_URL } from "../utils/url";
-// import order from "../reducers/order";
+import "./Main.css";
 
 const Main = () => {
   const [movies, setMovies] = useState([]);
-  // const [message, setMessage] = useState("");
-  // const orderMessage = useSelector((store) => store.order.message);
   const accessToken = useSelector((store) => store.user.accessToken);
 
   const dispatch = useDispatch();
@@ -23,10 +19,6 @@ const Main = () => {
       navigate("/login");
     }
   }, [accessToken, navigate]);
-
-  // useEffect(() => {
-  //   fetchMovie();
-  // }, []);
 
   useEffect(() => {
     const options = {
@@ -42,18 +34,6 @@ const Main = () => {
         console.log(json);
         setMovies(json.results);
       }, []);
-
-    // fetch(API_URL("order"), options)
-    //   .then((res) => res.json())
-    //   .then((data) => {
-    //     if (data.success) {
-    //       dispatch(order.actions.setMessage(data.response));
-    //       dispatch(order.actions.setError(null));
-    //     } else {
-    //       dispatch(order.actions.setMessage(null));
-    //       dispatch(order.actions.setError(data.response));
-    //     }
-    //   });
   }, [accessToken]);
 
   const handleRestart = () => {
@@ -85,29 +65,3 @@ const Main = () => {
 };
 
 export default Main;
-
-// const List = ({ movies }) => {
-//   return (
-//     <>
-//       <section className="full-page-container">
-//         {movies.map((movie) => (
-//           <Link to={`/details/${movie.id}`} key={movie.id}>
-//             <img
-//               className="movie-list-image"
-//               src={`https://image.tmdb.org/t/p/w342/${movie.poster_path}`}
-//               alt="{movie.title}"
-//             />
-
-//             <div className="movie-date-title-text-box">
-//               <h1>{movie.title}</h1>
-//               <p> Released:{movie.release_date}</p>
-//             </div>
-//           </Link>
-//         ))}
-//       </section>
-//     </>
-//   );
-// };
-
-// export default List;
-// © 2022 GitHub, Inc.
