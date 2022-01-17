8621d76e5d1337f24ca343180f9bb318c3623822
loulunds <lousanne.lundstrom@gmail.com>
2022-01-17 12:45:46 +0100

    change base url

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,30 @@
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
-import crypto from "crypto";
-import bcrypt from "bcrypt";
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import crypto from 'crypto';
+import bcrypt from 'bcrypt';
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/authAPI";
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/authAPI';
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
 const UserSchema = new mongoose.Schema({
-	username: {
-		type: String,
-		unique: true,
-		required: true,
-	},
-	password: {
-		type: String,
-		required: true,
-	},
-	accessToken: {
-		type: String,
-		default: () => crypto.randomBytes(128).toString("hex"),
-	},
+  username: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString('hex')
+  }
 });
 
-const User = mongoose.model("User", UserSchema);
+const User = mongoose.model('User', UserSchema);
 
 // Defines the port the app will run on. Defaults to 8080, but can be
 // overridden when starting the server. For example:
@@ -38,46 +38,47 @@ const app = express();
 app.use(cors());
 
 // v2 - Allow only one specific domain
-app.use(
-	cors({
-		origin: "https://my-project-frontend.com",
-	})
-);
+// app.use(
+//   cors({
+//     origin: 'https://my-project-frontend.com'
+//   })
+// );
 
 // v3 - Allow multiple domains
 
-const allowedDomains = [
-	"https://project-authentication.netlify.app/login",
-	"https://authentication-signup-project.herokuapp.com/",
-	"http://localhost:3000",
-];
-app.use(
-	cors({
-		origin: (origin, callback) => {
-			if (allowedDomains.includes(origin)) {
-				return callback(null, true);
-			} else {
-				return callback(new Error("This domain is not allowed"), false);
-			}
-		},
-	})
-);
+// const allowedDomains = [
+//   'https://project-authentication.netlify.app/login',
+//   'https://authentication-signup-project.herokuapp.com/',
+//   'http://localhost:3000',
+//   'http://localhost:8080'
+// ];
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (allowedDomains.includes(origin)) {
+//         return callback(null, true);
+//       } else {
+//         return callback(new Error('This domain is not allowed'), false);
+//       }
+//     }
+//   })
+// );
 
 app.use(express.json());
 
 const authenticateUser = async (req, res, next) => {
-	const accessToken = req.header("Authorization");
+  const accessToken = req.header('Authorization');
 
-	try {
-		const user = await User.findOne({ accessToken });
-		if (user) {
-			next();
-		} else {
-			res.status(401).json({ response: "Please, log in", success: false });
-		}
-	} catch (error) {
-		res.status(400).json({ response: error, success: false });
-	}
+  try {
+    const user = await User.findOne({ accessToken });
+    if (user) {
+      next();
+    } else {
+      res.status(401).json({ response: 'Please, log in', success: false });
+    }
+  } catch (error) {
+    res.status(400).json({ response: error, success: false });
+  }
 };
 
 // Authentication - 401 (Unauthorized) But should be unauthenticated
@@ -85,75 +86,75 @@ const authenticateUser = async (req, res, next) => {
 
 // Start defining your routes here
 
-app.get("/thoughts", authenticateUser);
-app.get("/thoughts", (req, res) => {
-	res.send("Here are your thoughts");
+app.get('/thoughts', authenticateUser);
+app.get('/thoughts', (req, res) => {
+  res.send('Here are your thoughts');
 });
 
-app.post("/signup", async (req, res) => {
-	const { username, password } = req.body;
+app.post('/signup', async (req, res) => {
+  const { username, password } = req.body;
 
-	try {
-		const salt = bcrypt.genSaltSync();
+  try {
+    const salt = bcrypt.genSaltSync();
 
-		if (password.length < 5) {
-			throw "Password must be at least 5 characters long";
-		}
+    if (password.length < 5) {
+      throw 'Password must be at least 5 characters long';
+    }
 
-		if (!username) {
-			throw "Please enter a username";
-		}
+    if (!username) {
+      throw 'Please enter a username';
+    }
 
-		if (username.unique === false) {
-			throw "Username is not available";
-		}
+    if (username.unique === false) {
+      throw 'Username is not available';
+    }
 
-		const newUser = await new User({
-			username,
-			password: bcrypt.hashSync(password, salt),
-		}).save();
+    const newUser = await new User({
+      username,
+      password: bcrypt.hashSync(password, salt)
+    }).save();
 
-		res.status(201).json({
-			response: {
-				userId: newUser._id,
-				username: newUser.username,
-				accessToken: newUser.accessToken,
-			},
-			success: true,
-		});
-	} catch (error) {
-		res.status(400).json({ response: error, success: false });
-	}
+    res.status(201).json({
+      response: {
+        userId: newUser._id,
+        username: newUser.username,
+        accessToken: newUser.accessToken
+      },
+      success: true
+    });
+  } catch (error) {
+    res.status(400).json({ response: error, success: false });
+  }
 });
 
-app.post("/signin", async (req, res) => {
-	const { username, password } = req.body;
+app.post('/signin', async (req, res) => {
+  const { username, password } = req.body;
 
-	try {
-		const user = await User.findOne({ username });
+  try {
+    const user = await User.findOne({ username });
 
-		if (user && bcrypt.compareSync(password, user.password)) {
-			res.status(200).json({
-				response: {
-					userId: user._id,
-					username: user.username,
-					accessToken: user.accessToken,
-				},
-				success: true,
-			});
-		} else {
-			res.status(404).json({
-				response: "Username or password doesn't match",
-				success: false,
-			});
-		}
-	} catch (error) {
-		res.status(400).json({ response: error, success: false });
-	}
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(200).json({
+        response: {
+          userId: user._id,
+          username: user.username,
+          accessToken: user.accessToken
+        },
+        success: true
+      });
+    } else {
+      res.status(404).json({
+        response: "Username or password doesn't match",
+        success: false
+      });
+    }
+  } catch (error) {
+    res.status(400).json({ response: error, success: false });
+  }
 });
 
 // Start the server
 app.listen(port, () => {
-	// eslint-disable-next-line
-	console.log(`Server running on http://localhost:${port}`);
+  // eslint-disable-next-line
+  console.log(`Server running on http://localhost:${port}`);
 });
