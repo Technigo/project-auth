8a17e1f5cc85f20d3d28c8a54fc62921203b43b6
Amanda Tilly <amandatilly@Amandas-2.lan>
2022-02-18 17:47:33 +0100

    removed slash in url

--- frontend/src/utils/urls.js
+++ frontend/src/utils/urls.js
@@ -1,3 +1,3 @@
-const BASE_URL = "https://amanda-fatima-insider-jokes.herokuapp.com/";
+const BASE_URL = "https://amanda-fatima-insider-jokes.herokuapp.com";
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`;
