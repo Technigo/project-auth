1c3e07ccbe0414888c86c48f905032072b436a78
Maria Thomasson <mariathomasson@hotmail.se>
2022-02-18 09:22:22 +0100

    indentation fix

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -6,95 +6,94 @@ import { API_URL } from '../utils/constants'
 import user from '../reducers/user'
 
 const Login = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [mode, setMode] = useState('signup')
 
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-    const [mode, setMode] = useState('signup')
+  const accessToken = useSelector((store) => store.user.accessToken)
 
-    const accessToken = useSelector((store) => store.user.accessToken)
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
 
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
+   useEffect(() => {
+      if (accessToken) {
+          navigate('/')
+      }
+  }, [accessToken, navigate])
 
-    useEffect(() => {
-        if (accessToken) {
-            navigate('/')
-        }
-    }, [accessToken, navigate])
+  const onFormSubmit = (event) => {
+      event.preventDefault()
 
-    const onFormSubmit = (event) => {
-        event.preventDefault()
-
-        const options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ username, password })
-        }
-
-        fetch(API_URL(mode), options)
-            .then((response) => response.json())
-            .then((data) => {
-                if (data.success) {
-                    batch(() => {
-                        dispatch(user.actions.setUserId(data.response.userId))
-                        dispatch(user.actions.setUsername(data.response.username))
-                        dispatch(user.actions.setAccessToken(data.response.accessToken))
-                        dispatch(user.actions.setError(null))
-                    })
-                } else {
-                    batch(() => {
-                        dispatch(user.actions.setUserId(null))
-                        dispatch(user.actions.setUsername(null))
-                        dispatch(user.actions.setAccessToken(null))
-                        dispatch(user.actions.setError(data.response))
-                    })
-                }
-            })
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, password })
     }
 
-    return (
-        <>
-            <section>
-            <Link to="/">To '/'!</Link>
-            </section>
-            <label htmlFor="signup">Signup</label>
-            <input
-                id="signup"
-                type="radio"
-                checked={mode === 'signup'}
-                onChange={() => setMode('signup')}    
-            />
+    fetch(API_URL(mode), options)
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.success) {
+                batch(() => {
+                    dispatch(user.actions.setUserId(data.response.userId))
+                    dispatch(user.actions.setUsername(data.response.username))
+                    dispatch(user.actions.setAccessToken(data.response.accessToken))
+                    dispatch(user.actions.setError(null))
+                })
+            } else {
+                batch(() => {
+                    dispatch(user.actions.setUserId(null))
+                    dispatch(user.actions.setUsername(null))
+                    dispatch(user.actions.setAccessToken(null))
+                    dispatch(user.actions.setError(data.response))
+                })
+            }
+        })
+ }
 
-            <label htmlFor="signin">Sign in</label>
-            <input
-                id="signin"
-                type="radio"
-                checked={mode === 'signin'}
-                onChange={() => setMode('signin')}    
-            />
+  return (
+    <>
+      <section>
+      <Link to="/">To '/'!</Link>
+      </section>
+      <label htmlFor="signup">Signup</label>
+      <input
+          id="signup"
+          type="radio"
+          checked={mode === 'signup'}
+          onChange={() => setMode('signup')}    
+    />
 
-            <form onSubmit={onFormSubmit}>
-                <label htmlFor="username">Username</label>
-                <input
-                    id="username"
-                    type="text"
-                    value={username}
-                    onChange={(event) => setUsername(event.target.value)}
-                />
+      <label htmlFor="signin">Sign in</label>
+      <input
+            id="signin"
+            type="radio"
+            checked={mode === 'signin'}
+            onChange={() => setMode('signin')}    
+      />
 
-                <label htmlFor="password">Password</label>
-                <input
-                    id="password"
-                    type="password"
-                    value={password}
-                    onChange={(event) => setPassword(event.target.value)}
-                />
-                <button type="submit">Submit</button>
-            </form>
-        </>
-    )
+      <form onSubmit={onFormSubmit}>
+      <label htmlFor="username">Username</label>
+      <input
+              id="username"
+              type="text"
+              value={username}
+              onChange={(event) => setUsername(event.target.value)}
+      />
+
+        <label htmlFor="password">Password</label>
+        <input
+                id="password"
+                type="password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+        />
+            <button type="submit">Submit</button>
+        </form>
+      </>
+  )
 }
 
 export default Login
