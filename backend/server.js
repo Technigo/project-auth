2876d09ad889944bdd9a815e4135f3ea10904fef
Maria Thomasson <mariathomasson@hotmail.se>
2022-02-18 10:57:22 +0100

    clean up and error fix

--- backend/server.js
+++ backend/server.js
@@ -70,7 +70,7 @@ app.post('/signup', async (req, res) => {
     const salt = bcrypt.genSaltSync()
 
     if (password.length < 5) {
-      throw new Error ("Password must be at least 5 characters long")
+      throw new Error("Password must be at least 5 characters long")
     }
 
     const newUser = await new User({
