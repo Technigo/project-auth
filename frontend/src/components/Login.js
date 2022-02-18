0135ae96765c8592db75a1b4fa9411b714359513
Maria Thomasson <mariathomasson@hotmail.se>
2022-02-18 09:38:07 +0100

    clean up

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -15,85 +15,85 @@ const Login = () => {
   const dispatch = useDispatch()
   const navigate = useNavigate()
 
-   useEffect(() => {
-      if (accessToken) {
-          navigate('/')
-      }
+  useEffect(() => {
+    if (accessToken) {
+      navigate('/')
+    }
   }, [accessToken, navigate])
 
   const onFormSubmit = (event) => {
-      event.preventDefault()
+    event.preventDefault()
 
     const options = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ username, password })
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ username, password })
     }
 
     fetch(API_URL(mode), options)
-        .then((response) => response.json())
-        .then((data) => {
-            if (data.success) {
-                batch(() => {
-                    dispatch(user.actions.setUserId(data.response.userId))
-                    dispatch(user.actions.setUsername(data.response.username))
-                    dispatch(user.actions.setAccessToken(data.response.accessToken))
-                    dispatch(user.actions.setError(null))
-                })
-            } else {
-                batch(() => {
-                    dispatch(user.actions.setUserId(null))
-                    dispatch(user.actions.setUsername(null))
-                    dispatch(user.actions.setAccessToken(null))
-                    dispatch(user.actions.setError(data.response))
-                })
-            }
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.success) {
+        batch(() => {
+          dispatch(user.actions.setUserId(data.response.userId))
+          dispatch(user.actions.setUsername(data.response.username))
+          dispatch(user.actions.setAccessToken(data.response.accessToken))
+          dispatch(user.actions.setError(null))
         })
- }
+      } else {
+        batch(() => {
+          dispatch(user.actions.setUserId(null))
+          dispatch(user.actions.setUsername(null))
+          dispatch(user.actions.setAccessToken(null))
+          dispatch(user.actions.setError(data.response))
+        })
+      }
+    })
+  }
 
   return (
     <>
       <section>
-      <Link to="/">To '/'!</Link>
-      </section>
-      <label htmlFor="signup">Signup</label>
-      <input
-          id="signup"
-          type="radio"
-          checked={mode === 'signup'}
-          onChange={() => setMode('signup')}    
-    />
-
-      <label htmlFor="signin">Sign in</label>
-      <input
-            id="signin"
-            type="radio"
-            checked={mode === 'signin'}
-            onChange={() => setMode('signin')}    
-      />
-
-      <form onSubmit={onFormSubmit}>
-      <label htmlFor="username">Username</label>
-      <input
-              id="username"
-              type="text"
-              value={username}
-              onChange={(event) => setUsername(event.target.value)}
-      />
-
-        <label htmlFor="password">Password</label>
+        <Link to="/">To '/'!</Link>
+        </section>
+        <label htmlFor="signup">Signup</label>
         <input
-                id="password"
-                type="password"
-                value={password}
-                onChange={(event) => setPassword(event.target.value)}
+        id="signup"
+        type="radio"
+        checked={mode === 'signup'}
+        onChange={() => setMode('signup')}
         />
-            <button type="submit">Submit</button>
-        </form>
-      </>
-  )
-}
+        
+        <label htmlFor="signin">Sign in</label>
+        <input
+        id="signin"
+        type="radio"
+        checked={mode === 'signin'}
+        onChange={() => setMode('signin')}
+        />
+        
+        <form onSubmit={onFormSubmit}>
+          <label htmlFor="username">Username</label>
+          <input
+          id="username"
+          type="text"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+          />
+          
+          <label htmlFor="password">Password</label>
+          <input
+          id="password"
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+          />
+          <button type="submit">Submit</button>
+          </form>
+          </>
+          )
+        }
 
 export default Login
--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -6,51 +6,50 @@ import { API_URL } from '../utils/constants'
 import thoughts from '../reducers/thoughts'
 
 const Main = () => {
+  const thoughtsItems = useSelector((store) => store.thoughts.items)
+  const accessToken = useSelector((store) => store.user.accessToken)
+  
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  
+  useEffect(() => {
+    if (!accessToken) {
+      navigate('/login')
+    }
+  }, [accessToken, navigate])
+  
+  useEffect(() => {
+    const options = {
+      method: 'GET',
+      headers: {
+        'Authorization': accessToken,
+      },
+    }
     
-    const thoughtsItems = useSelector((store) => store.thoughts.items)
-    const accessToken = useSelector((store) => store.user.accessToken)
-
-    const navigate = useNavigate()
-    const dispatch = useDispatch()
-
-    useEffect(() => {
-        if (!accessToken) {
-            navigate('/login')
-        }
-    }, [accessToken, navigate])
-
-    useEffect(() => {
-        const options = {
-            method: 'GET',
-            headers: {
-                'Authorization': accessToken,
-            },
-        }
-
-        fetch(API_URL('thoughts'), options)
-            .then((response) => response.json())
-            .then((data) => {
-                if (data.success) {
-                    dispatch(thoughts.actions.setItems(data.response))
-                    dispatch(thoughts.actions.setError(null))
-                } else {
-                    dispatch(thoughts.actions.setItems([]))
-                    dispatch(thoughts.actions.setError(data.response))
-                }
-            })
-    }, [])
-
-    return (
-        <section>
-            <div>
-                <Link to="/login">To '/login'!</Link>
-            </div>
-            <h1>Protected Happy Thoughts:</h1>
-            {thoughtsItems.map((item) => (
-                <article key={item._id}>{item.message}</article>
-            ))}
-        </section>
-    )
-}
+    fetch(API_URL('thoughts'), options)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.success) {
+        dispatch(thoughts.actions.setItems(data.response))
+        dispatch(thoughts.actions.setError(null))
+      } else {
+        dispatch(thoughts.actions.setItems([]))
+        dispatch(thoughts.actions.setError(data.response))
+      }
+    })
+  }, [])
+  
+  return (
+  <section>
+    <div>
+      <Link to="/login">To '/login'!</Link>
+      </div>
+      <h1>Protected Happy Thoughts:</h1>
+      {thoughtsItems.map((item) => (
+      <article key={item._id}>{item.message}</article>
+      ))}
+      </section>
+      )
+    }
 
 export default Main
