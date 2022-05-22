f5c0bb75269a571247e9ba2a990cea3c29fcc1bf
Emma Lindell <emma.lindell4@gmail.com>
2022-05-22 16:52:59 +0200

    login endpoint works

--- backend/server.js
+++ backend/server.js
@@ -8,45 +8,13 @@ import crypto from 'crypto'
 import bcrypt from 'bcrypt-nodejs'
 import listEndpoints from 'express-list-endpoints'
 
-// Connected to our database
+// Connected to database
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/auth"
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+
 mongoose.Promise = Promise
 
-// Creating a user model with validation rules: name, email, password and default accessToken with crypto library
-const User = mongoose.model('User', {
-  name: {
-      type: String,
-      unique: true
-  },
-  email: {
-      type: String,
-      unique: true
-  },
-  password:{
-      type: String,
-      required: true
-  },
-  accessToken: {
-      type: String,
-      default: () => crypto.randomBytes(128).toString('hex')
-  }
-})
-
-// Created a middleware funcion, which looks up the user based on the accessToken stored in the header, which we can test. Then calling next() which allows protected endpoint to continue execution
-const authenticateUser = async (req, res, next) => {
-  const user = await User.findOne({accessToken: req.header('Authorization')})
-  if (user) {
-    req.user = user
-    next()
-  } else {
-    res.status(401).json({loggedOut: true})
-  }
-}
-
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
+// Defines port the app will run on
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -59,29 +27,94 @@ app.get("/", (req, res) => {
   res.json(listEndpoints(app))
 });
 
-// Registration endpoint to assign name, email and password to a user in the database
+// User model with validation rules: name, email, password and default accessToken with crypto library
+const User = mongoose.model('User', {
+  name: {
+      type: String,
+      unique: true,
+      required: true
+  },
+  email: {
+      type: String,
+      unique: true,
+      required: true
+  },
+  password:{
+      type: String,
+      required: true
+  },
+  accessToken: {
+      type: String,
+      default: () => crypto.randomBytes(128).toString('hex')
+  }
+})
+
+// Middleware function which looks up the user based on the accessToken stored in the header, which we can test. Then calling next() which allows protected endpoint to continue execution
+const authenticateUser = async (req, res, next) => {
+  const user = await User.findOne({ accessToken: req.header('Authorization') })
+
+  if (user) {
+    req.user = user
+    next()
+  } else {
+    res.status(401).json({loggedOut: true})
+  }
+}
+
+//---------------------------SIGN UP ENDPOINT---------------------------//
 app.post('/signup', async (req, res) => {
+
   try {
     const { name, email, password } = req.body
-    
+
     // Not storing the password in plain text
     const user = new User({ 
       name, 
       email, 
       password: bcrypt.hashSync(password)})
     user.save()
-    res.status(201).json({id: user._id, accessToken:user.accessToken})
+    res.status(201).json({
+      id: user._id, 
+      accessToken:user.accessToken})
+
   } catch (err) {
     res.status(400).json({message: 'Could not create user', errors: err.errors})
   }
 })
 
-// Protected endpoint, allow only logged-in users see this info
+
+//---------------------------USER LOGIN ENDPOINT---------------------------//
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body
+
+  try {
+    const user = await User.findOne({ email })
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(200).json(
+        {
+          response: {
+            id: user._id,
+            name: user.name,
+            email: user.email,
+            accessToken: user.accessToken
+          },
+          success: true
+        })
+    } else {
+      // User do not exist or encrypted password doesnt match
+      res.status(404).json({ response: "Email or password doesn't match", success: false })
+    }
+  } catch (error) {
+    res.status(500).json({ errors: error })
+  }
+})
+
+//---------------------------PROFILE PROTECTED ENDPOINT---------------------------//
 app.get('/profile', authenticateUser, async (req, res) => {
   res.json({secret: 'This is a super secret message'})
 })
 
-// Sessions endpoint. Similar to registration endpoint, except this doesnt create a user, it finds one
+//---------------------------SESSIONS ENDPOINT---------------------------//
 app.post('/sessions', async (req, res) => {
   const user = await User.findOne({email: req.body.email})
   if (user && bcrypt.compareSync(req.body.password, user.password)) {
@@ -91,7 +124,7 @@ app.post('/sessions', async (req, res) => {
 }
 })
 
-// Start the server
+// Start server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 });
