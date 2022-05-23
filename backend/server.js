c44cc7c8b869aafddd6bee7d246d6f00a7a7182a
idanaslund <idanaslund@users.noreply.github.com>
2022-05-23 11:25:48 +0200

    codealong

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import crypto from "crypto"
+import bcrypt from "bcrypt"
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -15,6 +17,138 @@ const app = express();
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String, 
+    required: true
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString("hex")
+  }
+})
+
+const User = mongoose.model("User", UserSchema)
+
+// Only for registration of new user
+app.post("/register", async (req, res) => {
+
+  const { username, password } = req.body
+  try {
+    const salt = bcrypt.genSaltSync()
+
+    if(password.length < 8) {
+      res.status(400).json({
+        respons: "Password must be at least 8 characters long",
+        success: false
+      })
+    } else {
+      const newUser = await new User({
+        username: username,
+        password: bcrypt.hashSync(password, salt)
+      }).save()
+  
+      res.status(201).json({
+        response: {
+          username: newUser.username,
+          accessToken: newUser.accessToken,
+          userId: newUser._id
+        },
+        success: true
+      })
+  }
+  } catch(error) {
+    res.status(400).json({
+    respons: error,
+    success: false
+    })
+  }
+})
+
+// Only for logging in
+app.post("/login", async(req, res) => {
+  const { username, password } = req.body
+
+  try {
+    const user = await User.findOne({username})
+    if(user && bcrypt.compareSync(password, user.password))
+    res.status(200).json({
+      repsonse: {
+        username: user.username,
+        accessToken: user.accessToken,
+        userId: user._id
+      },
+      success: true
+    })
+  } catch(error) {
+    res.status(400).json({
+      respons: "Username and password don't match",
+      success: false
+    })
+  }
+})
+
+// Autheticating the user is logged in and have access to asked function
+const authenticateUser = async (req, res, next) => {
+  const accessToken = req.header("Authorization")
+
+  try {
+    const user = await User.findOne({accessToken: accessToken})
+    if(user) {
+      next()
+    } else {
+      res.status(401).json({
+        respons: "Please log in",
+        success: false
+      })
+    }
+  } catch(error) {
+    res.status(400).json({
+      respons: error,
+      success: false
+    })
+  }
+}
+
+//Example text to show when authenticated
+app.get("/thoughts", authenticateUser)
+app.get("/thoughts", (req, res) => {
+  res.send("here are your thoughts")
+})
+
+
+
+///// CORS version1 (allows all origins)
+// app.use(cors())
+
+
+////// CORS version2
+// app.use(cors({
+//   origin: "https://my-origin.com"
+// }))
+
+
+///// CORS version3
+// const allowedDomains = [
+//   "http://lalala.io",
+//   "http://something.com",
+//   "https://lorem.com",
+// ]
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if(allowedDomains.includes(origin)){
+//       return callback(null, true)
+//     } else {
+//       return callback(new Error("domain not allowed"), false)
+//     }
+//   }
+// }))
 
 // Start defining your routes here
 app.get("/", (req, res) => {
