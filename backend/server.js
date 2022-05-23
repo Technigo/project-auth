ddf66c7e84616652b3bcb63f2423350369a057cd
rijad90 <rijad.i@hotmail.com>
2022-05-23 16:33:56 +0200

    created signin

--- backend/server.js
+++ backend/server.js
@@ -70,7 +70,7 @@ app.post("/signup", async (req, res) => {
     const newUser = await new User({
       username,
       email,
-      password: bcrypt.hashSync(password, salt), 
+      password: bcrypt.hashSync(password, salt),
     }).save()
 
     res.status(201).json({
@@ -108,6 +108,51 @@ app.post("/signup", async (req, res) => {
         success: false,
       })
     }
+  }
+})
+
+app.post("/signin", async (req, res) => {
+  const { username, password, email } = req.body
+
+  try {
+    const user = await User.findOne({ username })
+
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(200).json({
+        response: {
+          userId: user._id,
+          username: user.username,
+          accessToken: user.accessToken,
+        },
+        success: true,
+      })
+    } else {
+      if (username === "") {
+        res.status(404).json({
+          message: "missing username",
+          response: "missing username",
+          success: false,
+        })
+      } else if (password === "") {
+        res.status(404).json({
+          message: "missing password",
+          response: "missing password",
+          success: false,
+        })
+      } else {
+        res.status(404).json({
+          message: "wrong username or password",
+          response: "wrong username or password",
+          success: false,
+        })
+      }
+    }
+  } catch (error) {
+    res.status(400).json({
+      message: "wrong entry",
+      response: error,
+      success: false,
+    })
   }
 })
 
