7e5b39595f190d5bd90310fc4db5920f586ba5a5
mathildakarlsson <86979661+mathildakarlsson@users.noreply.github.com>
2022-05-23 11:19:02 +0200

    mongoose user created,registration and login endpoints created and authorization added

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import crypto from "crypto";
+import bcrypt from "bcrypt";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -16,9 +18,145 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString("hex")
+  }
+});
+
+const User = mongoose.model("User", UserSchema);
+
+
+//register
+
+app.post("/register", async (req, res) => {
+  
+  const { username, password } = req.body;
+  try {
+    const salt = bcrypt.genSaltSync();
+    if(password.length < 8) {
+      res.status(400).json({
+        response: "Password must be at least 8 characters long",
+        success: false
+      })
+    } else {
+    const newUser = await new User({
+      username: username,
+      password: bcrypt.hashSync(password, salt)
+    }).save();
+    res.status(201).json({
+      response: {
+        username: newUser.username,
+        accessToken: newUser.accessToken,
+        userId: newUser._id
+      },
+      success: true
+    })
+  }
+  } catch (error) {
+    res.status(400).json({
+      response: error,
+      success: false
+    })
+  }
+
+});
+
+
+//authentication
+
+const authenticateUser = async (req, res, next) => {
+  const accessToken = req.header("Authorization");
+  
+  try {
+    const user = await User.findOne({accessToken: accessToken});
+
+    if(user) {
+      next();
+    } else {
+      res.status(401).json({
+        response: "Please log in",
+        success: false
+      });
+    }
+
+  } catch (error) {
+    res.status(400).json({
+      response: error,
+      success: false
+    });
+  }
+} 
+
+app.get("/thoughts", authenticateUser);
+app.get("/thoughts", (req, res,) => {res.send("here are your thoughts")})
+
+
+//cors version 2
+app.use(cors({
+  origin: "https://my-origin.com"
+}));
+
+//cors version 3
+const allowedDomains = [
+  "https://lalala.io",
+  "https://something.com",
+  "http://lorem.com"
+];
+app.use(cors({
+  origin: (origin, callback) => {
+    if(allowedDomains.includes(origin)) {
+      return callback(null, true)
+    } else {
+      return callback(new Error("domain not allowed"), false);
+    }
+  }
+}));
+
+//login
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({username});
+
+    if(user && bcrypt.compareSync(password, user.password)) {
+      res.status(200).json({
+        success: true,
+        username: user.username,
+        accessToken: user.accessToken,
+        userId: user._id
+      });
+    } else {
+      res.status(400).json({
+        response: "username and password don't match",
+        success: false
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      response: error,
+      success: false
+    })
+  }
+});
+
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send("This is the backend");
 });
 
 // Start the server
