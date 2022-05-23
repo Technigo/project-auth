5100e6f39c2ff6cb8a58609d8a225ec609c04f24
Sherin Susan Thomas <sherinsusanthomas95@gmail.com>
2022-05-23 10:56:18 +0200

    initial steps done login and register

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import crypto from "crypto";
+import bcrypt from "bcrypt";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -16,11 +18,85 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Schema
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString("hex"),
+  },
+});
+
+//////
+const User = mongoose.model("User", UserSchema);
+
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const salt = bcrypt.genSaltSync();
+    if (password.length < 8) {
+      res.status(400).json({
+        response: "password must contain atleast 8 characters",
+        success: false,
+      });
+    } else {
+      const newUser = await new User({
+        username: username,
+        password: bcrypt.hashSync(password, salt),
+      }).save();
+      res.status(201).json({
+        response: {
+          username: newUser.username,
+          accessToken: newUser.accessToken,
+          userId: newUser._id,
+        },
+        success: true,
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      response: error,
+      success: false,
+    });
+  }
+});
+//login
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await user.findOne(username);
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.json({
+        username: newUser.username,
+        accessToken: newUser.accessToken,
+        userId: newUser._id,
+        success: true,
+      });
+    } else {
+      res.status(400).json({
+        response: "User not found",
+        success: false,
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      response: error,
+      success: false,
+    });
+  }
+});
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
-
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
