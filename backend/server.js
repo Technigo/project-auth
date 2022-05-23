0a24858eb3548ed8878aafd923ecec87d9cddd66
rijad90 <rijad.i@hotmail.com>
2022-05-23 16:16:31 +0200

    created user schema

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,47 @@
-import express from "express";
-import cors from "cors";
-import mongoose from "mongoose";
+import express from "express"
+import cors from "cors"
+import mongoose from "mongoose"
+import bcrypt from "bcrypt"
+import crypto from "crypto"
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.Promise = Promise;
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo"
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = Promise
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
-const port = process.env.PORT || 8080;
-const app = express();
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString('hex')
+  }
+})
 
-// Add middlewares to enable cors and json body parsing
-app.use(cors());
-app.use(express.json());
+const port = process.env.PORT || 8080
+const app = express()
 
-// Start defining your routes here
+
+app.use(cors())
+app.use(express.json())
+
+
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
-});
+  res.send("Hello Technigo!")
+})
 
-// Start the server
+
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+  console.log(`Server running on http://localhost:${port}`)
+})
