b0597febb29dc35e2e4a93b5e5ef00dda3dc60ea
Therese Nyman <theresenyman@Thereses-MacBook-Air.local>
2022-05-24 17:10:37 +0200

    page-wrapper fix

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -4,17 +4,20 @@ import { Link, NavLink } from "react-router-dom";
 export const Nav = () => {
   return (
     <section className="navbar">
-      <h1>
-        <Link to="/">Navbar here</Link>
-      </h1>
-      <button>
-        <NavLink to="/login">Log in</NavLink>
-      </button>
-      <button>
-        <NavLink to="/register">Register</NavLink>
-      </button>
-
-      <button>Log out</button>
+      <div className="title-box">
+        <h1>
+          <Link to="/">Navbar here</Link>
+        </h1>
+      </div>
+      <div className="link-box">
+        <button>
+          <NavLink to="/login">Log in</NavLink>
+        </button>
+        <button>
+          <NavLink to="/register">Register</NavLink>
+        </button>
+        <button>Log out</button>
+      </div>
     </section>
   );
 };
