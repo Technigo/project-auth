7b8723fa63f0e6ed8b51dc5a25be3d90128e5e57
Kyparn <93533136+Kyparn@users.noreply.github.com>
2022-05-24 16:47:01 +0200

    header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,18 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Main from './components/Main'
 import { Welcome } from './components/Welcome'
 import { NotFound } from './components/NotFound'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 export const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Header />
         <Route path="/signin" element={<Main />} />
         <Route path="/welcome" element={<Welcome />} />
         <Route path="notfound" element={<NotFound />} />
+        <Footer />
       </Routes>
     </BrowserRouter>
   )
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Heeeey there and welcome!</h1>
+    </div>
+  )
+}
+
+export default Header
