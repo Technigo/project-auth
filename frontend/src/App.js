8aef349124e40abf594b9b80aa736fce00a2c2bd
Dorothea-B <barre.dorothea@gmail.com>
2022-05-24 22:35:17 +0200

    basic frontend setup - components, routes  and redux reducers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,42 @@
 import React from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Provider } from "react-redux"
+import { configureStore, combineReducers } from "@reduxjs/toolkit"
+
+//Components
+import Main from "./Components/Main"
+import Login from "./Components/Login"
+import NotFound from "./Components/NotFound"
+
+//Redux reducers
+import user from "./reducers/user"
+import thoughts from "./reducers/thoughts"
+
+
+
+
+
+const reducer = combineReducers({
+  user: user.reducer,
+  thoughts: thoughts.reducer
+})
+
+const store = configureStore({ reducer })
+
 
 export const App = () => {
-  return <div>Let's do this!</div>
+  return (
+
+    <Provider store={store}>
+      <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+    </BrowserRouter>
+    </Provider>
+
+  )
+  
 }
