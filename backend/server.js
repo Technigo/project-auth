1c21173fe1e3b8e485540ae5eb7ee1e9c7394cda
HWallberg <91497486+HWallberg@users.noreply.github.com>
2022-05-24 10:21:22 +0200

    created endoints and a mongoose model

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import crypto from "crypto";
+import bcrypt from "bcrypt";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-auth";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
@@ -15,6 +17,109 @@ const app = express();
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString("hex")
+  }
+});
+
+const User = mongoose.model("User", UserSchema);
+
+app.post("/register", async (req, res) => {
+  
+  const { username, password } = req.body;
+  try {
+    const salt = bcrypt.genSaltSync();
+
+    if(password.length < 8) {
+      res.status(400).json({
+        response: "Password must be at least 8 characters long",
+        success: false
+      });
+    } else {
+      const newUser = await new User({
+        username: username,
+        password: bcrypt.hashSync(password, salt) // skickar tillbaka blandade tecken typ?
+      }).save();
+      res.status(201).json({
+        response: {
+          username: newUser.username,
+          accesToken: newUser.accesToken,
+          userId: newUser._id
+        },
+        success: true
+      });
+    }
+  } catch(error) {
+    res.status(400).json({
+      response: error,
+      success: false
+    });
+  }
+});
+
+// login
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({username});
+
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(200).json({
+        success: true,
+        username: user.username,
+        accesToken: user.accesToken,
+        userId: user._id
+      });
+    } else {
+      res.status(400).json({
+        response: "username and password don't match",
+        success: false
+      });
+    }
+  } catch(error) {
+    res.status(400).json({
+      response: error,
+      success: false
+    });
+  }
+});
+
+//Authorization
+const authenticateUser = async (req, res, next) => {
+  const accesToken  = req.header("Authorization");
+  try {
+    const user = await User.findOne({accesToken: accesToken});
+    if(user) {
+      next();
+    } else {
+      res.status(401).json({
+        response: "Please log in",
+        success: false
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      response: error,
+      success: false
+    });
+  }
+}
+
+app.get("/welcome", authenticateUser);
+app.get("/welcome", (req, res,) => {res.send("you are here! welcome")});
 
 // Start defining your routes here
 app.get("/", (req, res) => {
