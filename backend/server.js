67b84225e196b6148e22ed8dcf18aec62b3b8103
Justine Zwiazek <justine.zwiazek@mentimeter.com>
2022-05-24 22:04:39 +0200

    working on signin

--- backend/server.js
+++ backend/server.js
@@ -4,14 +4,13 @@ import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import crypto from "crypto";
 import bcrypt from "bcrypt-nodejs";
-import { allowedNodeEnvironmentFlags } from "process";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/auth";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
 // Codealong with Van
-const User = mongoose.model("User", {
+const UserSchema = mongoose.Schema({
   name: { type: String, unique: true },
   email: { type: String, unique: true },
   password: { type: String, required: true },
@@ -21,25 +20,39 @@ const User = mongoose.model("User", {
   },
 });
 
-const authenticateUser = async (req, res, next) => {
-  const user = await User.findOne({ accessToken: req.header("Authorization") });
-};
+const User = mongoose.model("User", UserSchema);
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8082;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
+// Middlewares:
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const authenticateUser = async (req, res, next) => {
+  const accessToken = req.header("Authorization");
+  try {
+    const user = await User.findOne({ accessToken });
+    if (user) {
+      next();
+    } else {
+      res.status(401).json({ response: "Please log in", success: false });
+    }
+  } catch (error) {
+    res.status(400).json({ response: error, success: false });
+  }
+};
 
 // Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+app.get("/welcome", authenticateUser);
+app.get("/welcome", (req, res) => {
+  res.json("Hello world");
 });
 
-////// Codealong with Van
-app.post("/users", async (req, res) => {
+// Registration endpoint
+app.post("/signup", async (req, res) => {
   try {
     const { name, email, password } = req.body;
     //do not store plain text password, always use bcrypt or alternative
@@ -58,6 +71,7 @@ app.get("/secrets", (req, res) => {
   res.json({ secret: "This is a secret message" });
 });
 
+// Login endpoint
 app.post("/sessions", async (req, res) => {
   const user = await User.findOne({ email: req.body.email });
   if (user && bcrypt.compareSync(req.body.password, user.password)) {
