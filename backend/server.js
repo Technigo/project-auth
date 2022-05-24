413669259f3a332b8a2b39f451c0b35ef8618673
Justine Zwiazek <justine.zwiazek@mentimeter.com>
2022-05-24 08:58:48 +0200

    built add new user functionality

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import crypto from "crypto";
+import bcrypt from "bcrypt-nodejs";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -19,9 +20,10 @@ const User = mongoose.model("User", {
   },
 });
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
+const authenticateUser = async (req, res, next) => {
+  const user = await User.findOne({ accessToken: req.header("Authorization") });
+};
+
 const port = process.env.PORT || 8080;
 const app = express();
 
@@ -36,8 +38,20 @@ app.get("/", (req, res) => {
 });
 
 ////// Codealong with Van
+app.post("/users", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    //do not store plain text password, always use bcrypt or alternative
+    const user = new User({ name, email, password: bcrypt.hashSync(password) });
+    user.save();
+    res.status(201).json({ id: user.id, accessToken: user.accessToken });
+  } catch (err) {
+    res
+      .status(400)
+      .json({ message: "Could not create user", errors: err.errors });
+  }
+});
 
-// Secret
 app.get("/secrets", (req, res) => {
   res.json({ secret: "This is a secret message" });
 });
