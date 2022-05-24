989161f28fc05a03be7c0daa612b57a68d5b0e70
Kyparn <93533136+Kyparn@users.noreply.github.com>
2022-05-24 12:12:57 +0200

    routes and img for fun

---
+++ frontend/src/components/Main.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div className="main-container">
+      <h1>Hej hej</h1>
+    </div>
+  )
+}
+
+export default Main
---
+++ frontend/src/components/Welcome.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styledComponents from 'styled-components'
+
+export const Welcome = () => {
+  return (
+    <main className="welcome-container">
+      <h1>Heeeey there, lets grab a drink</h1>
+    </main>
+  )
+}
