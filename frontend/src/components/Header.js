efef5ba9d681512b1368830c20530b3022f3d518
Kyparn <93533136+Kyparn@users.noreply.github.com>
2022-05-25 09:56:40 +0200

    remove button

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -4,9 +4,6 @@ const Header = () => {
   return (
     <div className="main-header">
       <h2>Heeeey there and welcome!</h2>
-      <button>Sign In</button>
-      <button>Register</button>
-      <button>About Us</button>
     </div>
   )
 }
