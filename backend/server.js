24055f059079da20a78f4a6410d61095167157d6
Josefin Eliasson <91781497+Josse79@users.noreply.github.com>
2022-05-25 09:50:11 +0200

    added register and login to backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import crypto from "crypto";
+import bcrypt from "bcrypt";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -15,6 +17,84 @@ const app = express();
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString("hex")
+  }
+});
+
+const User = mongoose.model("User", UserSchema);
+
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const salt = bcrypt.genSaltSync();
+
+    if(password.length < 5) {
+      res.status(400).json({
+        response: "Password must be at least 5 characters long",
+        success: false
+      });
+    } else {
+      const newUser = await new User({
+        username: username,
+        password: bcrypt.hashSync(password, salt)
+      }).save();
+      res.status(201).json({
+        response: {
+          username: newUser.username,
+          accessToken: newUser.accessToken,
+          userId: newUser._id
+        },
+        sucess: true
+      });
+    }
+  } catch(error) {
+    res.status(400).json({
+      response: error,
+      sucess: false
+    });
+  }
+});
+
+app.post("/login", async (req, res) =>{
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({username});
+
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(200).json({
+        success: true,
+        username: user.username,
+        accessToken: user.accessToken,
+        userId: user._id
+      });
+    } else {
+      res.status(400).json({
+        response: "Username and password does not match",
+        success: false
+      });
+    }
+  } catch(error) {
+    res.status(400).json({
+      response: error,
+      success: false
+    });
+  }
+});
+
 
 // Start defining your routes here
 app.get("/", (req, res) => {
