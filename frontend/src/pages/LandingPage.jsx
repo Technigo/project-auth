f6c9500c732a4e0869067e0c0f42b37360bcf8b6
StepbyStepp <91485842+SteppbySteph@users.noreply.github.com>
2022-05-25 13:55:29 +0200

    edited Landingpage

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import styled from 'styled-components'
+// import background from '../assets/alper-guzeler-inQvRABkTfg-unsplash.jpg'
+// import Button from '@mui/material'
 
 const LandingPage = () => {
   const navigate = useNavigate();
@@ -13,3 +16,10 @@ const LandingPage = () => {
 };
 
 export default LandingPage;
+
+// const PageContainer = styled.div`
+// height: 100vh;
+// width: 100vw;
+// // background-image: url(${background});
+// background-color: "black";
+// `
