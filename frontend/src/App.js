f60807f9e406f8395892d3903ec07978b516ff61
Kyparn <93533136+Kyparn@users.noreply.github.com>
2022-05-25 22:27:26 +0200

    store is open

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,16 +6,31 @@ import { NotFound } from './components/NotFound'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
+import { Provider } from 'react-redux'
+import { configstore, combineReducers } from '@reduxjs/toolkit'
+
+import user from 'reducers/user'
+import thoughts from 'reducers/thoughts'
+
+const reducer = combineReducers({
+  user: user.reducer,
+  thoughts: thoughts.reducer,
+})
+
+const store = configstore({ reducer })
+
 export const App = () => {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/notfound" element={<NotFound />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </Provider>
   )
 }
