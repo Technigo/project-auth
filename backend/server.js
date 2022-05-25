0f0777685cfe669d769c7f3c37cf17de57ffc068
Louisa <lundislon@gmail.com>
2022-05-25 18:55:51 +0200

    solved the /regeister issue by changing MongoURL to /mongo instead of /project-mongo

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 import crypto from "crypto";
 import bcrypt from "bcrypt";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
