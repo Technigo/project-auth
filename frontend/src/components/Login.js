663424e62fc6d0608fb17245d0f2f578ff9be41d
Kyparn <93533136+Kyparn@users.noreply.github.com>
2022-05-25 23:52:08 +0200

    batch added

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,12 +1,64 @@
 import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector, batch } from 'react-redux'
+import { API_URL } from 'utils/API'
+import user from 'reducers/user'
 
 export const Login = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
 
+  const [mode, setMode] = useState('register')
+
+  const dispatch = useDispatch()
+
+  const onFormSubmit = (event) => {
+    event.preventDefault()
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username: username, password: password }),
+    }
+    fetch(API_URL(mode), options)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+        if (data.success) {
+          batch(() => {
+            dispatch(user.actions.setUserId(data.userId))
+            dispatch(user.actions.setAccessToken(data.accessToken))
+            dispatch(user.actions.setUserName(data.username))
+            dispatch(user.actions.setError(null))
+          })
+        } else {
+          batch(() => {
+            dispatch(user.actions.setError(data.response))
+            dispatch(user.actions.setUserId(null))
+            dispatch(user.actions.setAccessToken(null))
+            dispatch(user.actions.setUserName(null))
+          })
+        }
+      })
+  }
   return (
     <>
-      <form>
+      <label htmlFor="register">Register</label>
+      <input
+        type="radio"
+        id="register"
+        checked={mode === 'register'}
+        onChange={() => setMode('register')}
+      />
+      <label htmlFor="login">Login</label>
+      <input
+        type="radio"
+        id="login"
+        checked={mode === 'login'}
+        onChange={() => setMode('login')}
+      />
+      <form onSubmit={onFormSubmit}>
         <label htmlFor="username">Username</label>
         <input
           type="text"
@@ -17,7 +69,7 @@ export const Login = () => {
 
         <label htmlFor="username">Username</label>
         <input
-          type="text"
+          type="password"
           id="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
