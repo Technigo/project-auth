9759aa0a120feb11f52ef098712a9d38be915907
idanaslund <idanaslund@users.noreply.github.com>
2022-05-26 19:20:24 +0200

    styled 404

--- frontend/src/components/NotFound.js
+++ frontend/src/components/NotFound.js
@@ -1,10 +1,21 @@
 import React from 'react'
-import { Container } from "./Style"
+import { Link } from 'react-router-dom'
+import { Container, SubmitButton, StyledForm, MainData } from "./Style"
+
+import Header from './Header'
+import Footer from './Footer'
 
 const NotFound = () => {
     return (
     <Container>
-        <h2>Not found.. 😔</h2>
+        <Header />
+        <StyledForm>
+            <MainData>Not found.. 😔</MainData>
+            <Link to="/">
+                <SubmitButton>Go back</SubmitButton>
+            </Link>
+        </StyledForm>
+        <Footer />
     </Container>
     )
 }
