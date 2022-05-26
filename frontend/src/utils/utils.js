6b4fdb8088d789697df75422642b17d0982eae37
Camilla Hallberg <camillahallberg@Camillas-MacBook-Pro.local>
2022-05-26 13:47:48 +0200

    removed a slash

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,3 +1,3 @@
-const BASE_URL = "https://project-auth-by-ida-camilla.herokuapp.com/";
+const BASE_URL = "https://project-auth-by-ida-camilla.herokuapp.com";
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`;
