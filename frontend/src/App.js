f539da4c8a0610211b0a0321e6fb3898d915ca42
Sherin Susan Thomas <sherinsusanthomas95@gmail.com>
2022-05-26 19:26:05 +0200

    frontend login/signup done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Login from "components/Login";
+import Main from "components/Main";
+import Notfound from "components/Notfound";
+
+import { Provider } from "react-redux";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+
+import user from "reducers/user";
+import thoughtsPage from "reducers/thoughtsPage";
+
+const reducer = combineReducers({
+  user: user.reducer,
+  thoughtsPage: thoughtsPage.reducer,
+});
+
+const store = configureStore({ reducer });
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
-  )
-}
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/" element={<Main />}></Route>
+          <Route path="*" element={<Notfound />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  );
+};
---
+++ frontend/src/components/Main.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
+import user from "reducers/user";
+const Main = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <h1>Hello User </h1>
+      <h2>You are logged in </h2>
+      <button
+        type="buttton"
+        onClick={() => {
+          navigate("/login");
+          dispatch(user.actions.setAccessToken(null));
+        }}
+      >
+        Log Out
+      </button>
+    </div>
+  );
+};
+
+export default Main;
