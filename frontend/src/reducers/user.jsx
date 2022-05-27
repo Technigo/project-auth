96a5c646876306bfb00d81aba8899af41354b6ae
Dopest0727 <74298827+Dopest0727@users.noreply.github.com>
2022-05-27 12:34:44 +0200

    changed project structure

--- frontend/src/reducers/user.jsx
+++ frontend/src/reducers/user.jsx
@@ -15,29 +15,29 @@ const initialState = localStorage.getItem("user") //LOCALSTORAGE ENABELED
       error: null,
     }
 
-    const user = createSlice({
-      name: "user",
-      initialState: {
-        userId: null,
-        username: null,
-        accessToken: null,
-        error: null,
+  const user = createSlice({
+    name: "user",
+    initialState: {
+      userId: null,
+      username: null,
+      accessToken: null,
+      error: null,
+    },
+    reducers: {
+      setUserId: (store, action) => {
+        store.userId = action.payload;
       },
-      reducers: {
-        setUserId: (store, action) => {
-          store.userId = action.payload;
-        },
-        setUserName: (store, action) => {
-          store.username = action.payload;
-        },
-        setAccessToken: (store, action) => {
-          store.accessToken = action.payload;
-        },
-        setError: (store, action) => {
-          store.error = action.payload;
-        },
+      setUserName: (store, action) => {
+        store.username = action.payload;
       },
-    });
+      setAccessToken: (store, action) => {
+        store.accessToken = action.payload;
+      },
+      setError: (store, action) => {
+        store.error = action.payload;
+      },
+    },
+  });
     
 
 export default user
