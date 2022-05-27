f767cd7f74dfe364e4b3bbaf3629dfc3a730c198
Emma Högberg <emmahogberg88@gmail.com>
2022-05-27 16:30:32 +0200

    Add styling and conditional rendering for login and registration

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from "react"
 import { useDispatch, useSelector, batch } from "react-redux"
-import { useNavigate, Link } from "react-router-dom"
-// import { useFormik } from "formik"
+import { useNavigate } from "react-router-dom"
 import { Formik, Form, useField } from "formik"
 import * as Yup from "yup"
-import axios from "axios"
 import styled from "styled-components/macro"
 
 import { API_URL } from "utils/utils"
@@ -28,7 +26,7 @@ const Login = () => {
     // const [username, setUsername] = useState("")
     // const [password, setPassword] = useState("")
 
-    const [mode, setMode] = useState("register")
+    const [mode, setMode] = useState("login")
     console.log(mode)
 
     const dispatch = useDispatch()
@@ -37,7 +35,7 @@ const Login = () => {
 
     console.log("accessToken", accessToken)
 
-    ////////////////
+
     const handleLoginSuccess = (data) => {
         batch(() => {
             dispatch(user.actions.setUserId(data.userId))
@@ -54,120 +52,120 @@ const Login = () => {
             dispatch(user.actions.setUserName(null))
         })
     }    
-    ////////////////
+
     useEffect(() => {
         if (accessToken) {
             navigate("/")
         }
     }, [accessToken])
-
-
-    // const onFormSubmit = (event) => {
-    //     event.preventDefault()
-        
-    //     const options = {
-    //         method: "POST",
-    //         headers: {
-    //             "Content-Type": "application/json"
-    //         },
-    //         body: JSON.stringify({username: username, password: password})
-    //     }
-
-    //     fetch(API_URL(mode), options)
-    //         .then(res => res.json())
-    //         .then(data => {
-    //             console.log(data)
-    //             if (data.success) {
-    //                 batch(() => {
-    //                     dispatch(user.actions.setUserId(data.userId))
-    //                     dispatch(user.actions.setAccessToken(data.accessToken))
-    //                     dispatch(user.actions.setUserName(data.username))
-    //                     dispatch(user.actions.setError(null))
-    //                 })
-    //             } else {
-    //                 batch(() => {
-    //                     dispatch(user.actions.setError(data.response))
-    //                     dispatch(user.actions.setUserId(null))
-    //                     dispatch(user.actions.setAccessToken(null))
-    //                     dispatch(user.actions.setUserName(null))
-    //                 })
-    //             }
-    //         })
-    // }
-
-    
+   
     return (
         <>
-            <StyledMode>
-                <label htmlFor="register">Register</label>
-                    <input type="radio" id="register" checked={mode === "register"} onChange={() => setMode("register")} />
-                <label htmlFor="login">Login</label>
-                    <input type="radio" id="login" checked={mode === "login"} onChange={() => setMode("login")} />
-            </StyledMode>
-            
-            <Formik
-                initialValues={{ username: "", password: "", email: "" }}
-                validationSchema={Yup.object({
-                    username: Yup.string()
-                        .required("Required"),
-                    password: Yup.string()
-                        .required("Required")
-                        .min(8, "The password must contain at least 8 characters"),
-                    // email: Yup.string()
-                    //     .required("Required")
-                    //     .email("Invalid email address")
-                })}
+            <StyledLoginSection>
+                <StyledLoginWrapper>
+                    {/* <StyledMode>
+                        <label htmlFor="register">Register</label>
+                            <input type="radio" id="register" checked={mode === "register"} onChange={() => setMode("register")} />
+                        <label htmlFor="login">Login</label>
+                            <input type="radio" id="login" checked={mode === "login"} onChange={() => setMode("login")} />
+                    </StyledMode> */}
 
-                onSubmit={(values) => {
-                    fetch(API_URL(mode), {
-                        method: "POST",
-                        headers: {
-                            "Content-Type": "application/json"
-                        },
-                        body: JSON.stringify({username: values.username, password: values.password})
-                    })
-                    .then(res => res.json())
-                    .then(data => {
-                        console.log(data)
-                        handleLoginSuccess(data)
-                    })
-                    .catch((err) => {
-                        handleLoginFailure(err);
-                    })
-                }}
+                    {mode === 'login' ? <h1>Login here</h1> : <h1>Register here</h1>}
+                    
+                    <Formik
+                        initialValues={{ username: "", password: "", email: "" }}
+                        validationSchema={Yup.object({
+                            username: Yup.string()
+                                .required("Required"),
+                            password: Yup.string()
+                                .required("Required")
+                                .min(8, "The password must contain at least 8 characters"),
+                                // email: Yup.string()
+                                //     .required("Required")
+                            //     .email("Invalid email address")
+                        })}
 
-            >
-               {({ isSubmitting }) => (
-                    <StyledForm>
-                        {isSubmitting && <div>Loading...</div>}
-                        <StyledInput
-                            label="Username"
-                            name="username"
-                            type="text" 
-                        />
+                        onSubmit={(values, { setSubmitting }) => {
+                            fetch(API_URL(mode), {
+                                method: "POST",
+                                headers: {
+                                    "Content-Type": "application/json"
+                                },
+                                body: JSON.stringify({username: values.username, password: values.password})
+                            })
+                            .then(res => res.json())
+                            .then(data => {
+                                console.log(data)
+                                handleLoginSuccess(data)
+                                setMode('login')
+                            })
+                            .catch((err) => {
+                                handleLoginFailure(err);
+                            })
+                            .finally(() => setSubmitting(false))
+                        }}
+                        
+                        >
+                    {({ isSubmitting }) => (
+                        <StyledForm>
+                                {isSubmitting && <div>Loading...</div>}
+                                <StyledInput
+                                    label="Username"
+                                    name="username"
+                                    type="text" 
+                                    />
 
-                        <StyledInput
-                            label="Password"
-                            name="password"
-                            type="password" 
-                        />
+                                <StyledInput
+                                    label="Password"
+                                    name="password"
+                                    type="password" 
+                                />
 
-                        {/* <MyTextInput
-                            label="Email address"
-                            name="email"
-                            type="email" 
-                        /> */}
-
-                        <StyledButton type="submit">Submit</StyledButton>
-                    </StyledForm>
-               )}
-            </Formik>
+                                {/* <MyTextInput
+                                    label="Email address"
+                                    name="email"
+                                    type="email" 
+                                /> */}
+                                {mode === 'login' ? <StyledButton type="submit" background="#EBD0D0" boxShadow="2px 3px" width="100px">Login</StyledButton> : <StyledButton type="submit" background="#EBD0D0">Register</StyledButton>}
+                                
+                                {mode === 'login' ? 
+                                <StyledButton type="button" onClick={()=> setMode("register")} background="transparent" width="100%" textDecoration="underline">Not a member yet? Register here</StyledButton> :
+                                <StyledButton type="button" onClick={()=> setMode("login")} background="transparent" width="100%" textDecoration="underline">Already a member? Login here</StyledButton>
+                            }
+                            </StyledForm>
+                    )}
+                    
+                    </Formik>
+                </StyledLoginWrapper>
+            </StyledLoginSection>
         </>  
     )
 }
 
 export default Login
 
+
+const StyledLoginSection = styled.section`
+    display: flex;
+    flex-direction: column;   
+    align-items: center; 
+    justify-content: center;
+    height: 100vh;
+    background: #E7CFD7;
+`
+
+const StyledLoginWrapper = styled.section`
+    display: flex;
+    flex-direction: column;   
+    align-items: center; 
+    justify-content: center;
+    width: 300px;
+    height: 80vh;
+    background: #959595;
+    border: 10px solid black;
+
+`
+    
 const StyledMode = styled.div`
     display: flex;
     flex-direction: row;
@@ -186,12 +184,23 @@ const StyledForm = styled(Form)`
 `
 
 const StyledInput = styled(MyTextInput)`
-    width: 50%;
-    border-radius: 20px;
+    width: 150px;
+    height: 20px;
     margin-bottom: 1rem;
+    border: none;
+    box-shadow: 2px 3px;
 `
 
 const StyledButton = styled.button`
     margin-top: 1rem;
-    border-radius: 20px;
+    height: 25px;
+    width: ${props => props.width};
+    font-family: inherit;
+    background: ${props => props.background};
+    border: none;
+    box-shadow: ${props => props.boxShadow};
+    
+    &:hover {
+        text-decoration: ${props => props.textDecoration};
+    }
 `
