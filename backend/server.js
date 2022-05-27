e7b1adc49c7e5186d5c952dd564ff0e7c91036b0
Justine Zwiazek <justine.zwiazek@mentimeter.com>
2022-05-27 19:53:30 +0200

    "added a landing page to API"

--- backend/server.js
+++ backend/server.js
@@ -1,52 +1,58 @@
-import express from 'express'
-import cors from 'cors'
-import mongoose from 'mongoose'
-import bodyParser from 'body-parser'
-import crypto from 'crypto'
-import bcrypt from 'bcrypt-nodejs'
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import crypto from "crypto";
+import bcrypt from "bcrypt-nodejs";
 
-const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/auth'
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
-mongoose.Promise = Promise
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/auth";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
 
-const port = process.env.PORT || 8082
-const app = express()
+const port = process.env.PORT || 8082;
+const app = express();
 
 // Middlewares:
-app.use(cors())
-app.use(express.json())
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Codealong with Van, then Daniel
 const UserSchema = new mongoose.Schema({
-  name: { type: String, unique: true },
-  email: { type: String, unique: true },
+  name: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   accessToken: {
     type: String,
-    default: () => crypto.randomBytes(128).toString('hex'),
+    default: () => crypto.randomBytes(128).toString("hex"),
   },
-})
+});
 
-const User = mongoose.model('User', UserSchema)
+const User = mongoose.model("User", UserSchema);
+
+// API Landing
+app.get("/", (req, res) => {
+  res.send({
+    "Justine's and Simon's app": "Welcome to our authentication API",
+  });
+});
 
 // Registration endpoint
-app.post('/register', async (req, res) => {
-  const { username, password } = req.body
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
 
   try {
-    const salt = bcrypt.genSaltSync()
+    const salt = bcrypt.genSaltSync();
     if (password.length < 8) {
       res.status(400).json({
-        response: 'Password must be at least 8 characters long',
+        response: "Password must be at least 8 characters long",
         success: false,
-      })
+      });
     } else {
       const newUser = await new User({
         username,
         password: bcrypt.hashSync(password, salt),
-      }).save()
+      }).save();
       res.status(201).json({
         response: {
           username: newUser.username,
@@ -54,18 +60,18 @@ app.post('/register', async (req, res) => {
           userId: newUser._id,
         },
         success: true,
-      })
+      });
     }
   } catch (error) {
-    res.status(400).json({ response: error, success: false })
+    res.status(400).json({ response: error, success: false });
   }
-})
+});
 
 // Login endpoint
-app.post('/login', async (req, res) => {
-  const { username, password } = req.body
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
   try {
-    const user = await User.findOne({ username })
+    const user = await User.findOne({ username });
     if (user && bcrypt.compareSync(password, user.password)) {
       res.status(200).json({
         response: {
@@ -74,40 +80,40 @@ app.post('/login', async (req, res) => {
           accessToken: user.accessToken,
         },
         success: true,
-      })
+      });
     } else {
       res.status(404).json({
-        response: 'Username or password does not match',
+        response: "Username or password does not match",
         success: false,
-      })
+      });
     }
   } catch (error) {
-    res.status(404).json({ response: error, success: false })
+    res.status(404).json({ response: error, success: false });
   }
-})
+});
 
 // Authentication
 const authenticateUser = async (req, res, next) => {
-  const accessToken = req.header('Authorization')
+  const accessToken = req.header("Authorization");
   try {
-    const user = await User.findOne({ accessToken })
+    const user = await User.findOne({ accessToken });
     if (user) {
-      next()
+      next();
     } else {
-      res.status(401).json({ response: 'Please log in', success: false })
+      res.status(401).json({ response: "Please log in", success: false });
     }
   } catch (error) {
-    res.status(400).json({ response: error, success: false })
+    res.status(400).json({ response: error, success: false });
   }
-}
+};
 
 // Post authentication
-app.get('/info', authenticateUser)
-app.get('/info', (req, res) => {
-  res.json('Hello world')
-})
+app.get("/info", authenticateUser);
+app.get("/info", (req, res) => {
+  res.json("Hello world");
+});
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
-})
+  console.log(`Server running on http://localhost:${port}`);
+});
