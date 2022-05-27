faec56868a970ffd9daf3b7030d9e0559d50885f
Justine Zwiazek <justine.zwiazek@mentimeter.com>
2022-05-27 21:22:41 +0200

    fixing Login

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,56 +1,59 @@
-import React, { useState, useEffect } from 'react'
-import { useDispatch, useSelector, batch } from 'react-redux'
-import { useNavigate, Link } from 'react-router-dom'
-import { API_URL } from 'utils/API'
-import user from 'reducers/user'
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector, batch } from "react-redux";
+import { useNavigate, Link } from "react-router-dom";
+
+import { API_URL } from "utils/API";
+
+import user from "reducers/user";
 
 export const Login = () => {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-
-  const [mode, setMode] = useState('register')
-
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-  const accessToken = useSelector((store) => store.user.accessToken)
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [value, setValue] = useState("register");
+  const [errorCheck, setErrorCheck] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const accessToken = useSelector((store) => store.user.accessToken);
 
   useEffect(() => {
     if (accessToken) {
-      navigate('/')
+      navigate("/");
     }
-  }, [accessToken])
+  }, [accessToken]);
 
   const onFormSubmit = (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
     const options = {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify({ username: username, password: password }),
-    }
-    fetch(API_URL(mode), options)
+      body: JSON.stringify({ username, password }),
+    };
+    fetch(API_URL(value), options)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data)
+        console.log(data);
         if (data.success) {
           batch(() => {
-            dispatch(user.actions.setUserId(data.response.userId))
-            dispatch(user.actions.setAccessToken(data.response.accessToken))
-            dispatch(user.actions.setUserName(data.response.username))
-            dispatch(user.actions.setError(null))
-          })
+            dispatch(user.actions.setUserId(data.response.userId));
+            dispatch(user.actions.setAccessToken(data.response.accessToken));
+            dispatch(user.actions.setUserName(data.response.username));
+            dispatch(user.actions.setError(null));
+            setErrorCheck("User created successfully");
+          });
         } else {
           batch(() => {
-            dispatch(user.actions.setError(data.response))
-            dispatch(user.actions.setUserId(null))
-            dispatch(user.actions.setAccessToken(null))
-            dispatch(user.actions.setUserName(null))
-          })
+            dispatch(user.actions.setError(data.response));
+            dispatch(user.actions.setUserId(null));
+            dispatch(user.actions.setAccessToken(null));
+            dispatch(user.actions.setUserName(null));
+            setErrorCheck(data.response);
+          });
         }
-      })
-  }
+      });
+  };
   return (
     <>
       <div className="main-container">
@@ -60,15 +63,15 @@ export const Login = () => {
           <input
             type="radio"
             id="register"
-            checked={mode === 'register'}
-            onChange={() => setMode('register')}
+            checked={value === "register"}
+            onChange={() => setValue("register")}
           />
           <label htmlFor="login">Login</label>
           <input
             type="radio"
             id="login"
-            checked={mode === 'login'}
-            onChange={() => setMode('login')}
+            checked={value === "login"}
+            onChange={() => setValue("login")}
           />
         </div>
         <div className="form-container">
@@ -95,5 +98,5 @@ export const Login = () => {
         </div>
       </div>
     </>
-  )
-}
+  );
+};
