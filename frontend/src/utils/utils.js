fd184c8b8ea4d85940673676984f06aaae3b6178
Tiina <tiina_liukkonen@hotmail.com>
2022-05-27 23:38:52 +0300

    bug fix

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,3 +1,3 @@
-const BASE_URL = "https://project-auth-emma-tiina.herokuapp.com/"
+const BASE_URL = "https://project-auth-emma-tiina.herokuapp.com"
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`
