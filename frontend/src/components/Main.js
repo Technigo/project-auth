221a9acb6efe4778ba4d881039504dc2c594b91e
Justine Zwiazek <justine.zwiazek@mentimeter.com>
2022-05-27 21:03:39 +0200

    created Main from scratch

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,53 +1,37 @@
-import React, { useEffect } from 'react'
-import { useSelector } from 'react-redux'
-import { useNavigate, Link } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
-import { API_URL } from 'utils/API'
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate, Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { API_URL } from "utils/API";
+
+import user from "reducers/user";
 
 const Main = () => {
-  const accessToken = useSelector((store) => store.user.accessToken)
-  const infoItems = useSelector((store) => store.info.items) //what do we put here?
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
+  const accessToken = useSelector((store) => store.user.accessToken);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const username = useSelector((store) => store.user.username);
+
   useEffect(() => {
     if (!accessToken) {
-      navigate('/login')
+      navigate("/login");
     }
-  }, [accessToken])
-
-  useEffect(() => {
-    const options = {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: accessToken,
-      },
-    }
-    fetch(API_URL('info'), options) // what do we put here??
-      .then((res) => res.json)
-      .then((data) => {
-        if (data.success) {
-          dispatch(info.actions.setItems(data.response))
-          dispatch(info.actions.setError(null))
-        } else {
-          dispatch(info.actions.setError(data.response))
-          dispatch(info.actions.setItems([]))
-        }
-      })
-  }, [])
+  }, [accessToken]);
 
   return (
-    <>
-      <div className="main-container">
-        <Link to="/login">Link To /login</Link>
-        <h1>This is Main</h1>
+    <div>
+      <h1>Hello! Glad you could make it!</h1>
+      <button
+        onClick={() => {
+          navigate("/login");
+          dispatch(user.actions.setAccessToken(null));
+        }}
+      >
+        Log out
+      </button>
+    </div>
+  );
+};
 
-        {infoItems.map((item) => {
-          return <div key={item._id}>{item.message}</div>
-        })}
-      </div>
-    </>
-  )
-}
-
-export default Main
+export default Main;
