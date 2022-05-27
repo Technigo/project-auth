71ac2b8a8f1a7ef5a67ba29aa910fd70d26c6193
Justine Zwiazek <justine.zwiazek@mentimeter.com>
2022-05-27 23:52:53 +0200

    fixes

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,3 +1,5 @@
-const BASE_URL = "https://justine-simon-project-auth.herokuapp.com";
+const BASE_URL = "https://auth-m-r.herokuapp.com";
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`;
+
+//https://justine-simon-project-auth.herokuapp.com
