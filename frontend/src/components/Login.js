4bd0bcc14e26fada51798ba36bb6c62ff7f9a84b
Justine Zwiazek <justine.zwiazek@mentimeter.com>
2022-05-28 13:37:02 +0200

    fixing everyting myself, nice

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,58 +1,57 @@
-import React, { useState, useEffect } from 'react'
-import { useDispatch, useSelector, batch } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector, batch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-import { API_URL } from 'utils/utils'
+import { API_URL } from "utils/utils";
 
-import user from 'reducers/user'
+import user from "reducers/user";
 
 export const Login = () => {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const [mode, setMode] = useState('register')
-  const [error, setError] = useState('')
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
-  const accessToken = useSelector((store) => store.user.accessToken)
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [mode, setMode] = useState("register");
+  const [error, setError] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const accessToken = useSelector((store) => store.user.accessToken);
 
   useEffect(() => {
     if (accessToken) {
-      navigate('/')
+      navigate("/");
     }
-  }, [accessToken])
+  }, [accessToken]);
 
   const onFormSubmit = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     const options = {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({ username: username, password: password }),
-    }
+    };
 
     fetch(API_URL(mode), options)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data)
+        console.log(data);
         if (data.success) {
           batch(() => {
-            dispatch(user.actions.setUserId(data.response.userId))
-            dispatch(user.actions.setAccessToken(data.response.accessToken))
-            dispatch(user.actions.setUserName(data.response.username))
-            dispatch(user.actions.setError(null))
-          })
+            dispatch(user.actions.setUserId(data.userId));
+            dispatch(user.actions.setAccessToken(data.accessToken));
+            dispatch(user.actions.setUserName(data.username));
+            dispatch(user.actions.setError(null));
+          });
         } else {
           batch(() => {
-            dispatch(user.actions.setError(data.response))
-            dispatch(user.actions.setUserId(null))
-            dispatch(user.actions.setAccessToken(null))
-            dispatch(user.actions.setUserName(null))
-          })
-          setError(data.response)
+            dispatch(user.actions.setError(data.response));
+            dispatch(user.actions.setUserId(null));
+            dispatch(user.actions.setAccessToken(null));
+            dispatch(user.actions.setUserName(null));
+          });
         }
-      })
-  }
+      });
+  };
   return (
     <>
       <div className="main-container">
@@ -62,15 +61,15 @@ export const Login = () => {
             <input
               type="radio"
               id="register"
-              checked={mode === 'register'}
-              onChange={() => setMode('register')}
+              checked={mode === "register"}
+              onChange={() => setMode("register")}
             />
             <label htmlFor="login">Login</label>
             <input
               type="radio"
               id="login"
-              checked={mode === 'login'}
-              onChange={() => setMode('login')}
+              checked={mode === "login"}
+              onChange={() => setMode("login")}
             />
           </div>
           <div className="form-container">
@@ -95,5 +94,5 @@ export const Login = () => {
         </form>
       </div>
     </>
-  )
-}
+  );
+};
