ab578090af63878e5f92c2e703102a62747943e0
Kyparn <93533136+Kyparn@users.noreply.github.com>
2022-05-28 11:30:58 +0200

    login fixed :)

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,96 +1,99 @@
-import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector, batch } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector, batch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
 
-import { API_URL } from "utils/utils";
+import { API_URL } from 'utils/utils'
 
-import user from "reducers/user";
+import user from 'reducers/user'
 
 export const Login = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [mode, setMode] = useState("register");
-  const [error, setError] = useState("");
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const accessToken = useSelector((store) => store.user.accessToken);
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [mode, setMode] = useState('register')
+  const [error, setError] = useState('')
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+  const accessToken = useSelector((store) => store.user.accessToken)
 
   useEffect(() => {
     if (accessToken) {
-      navigate("/");
+      navigate('/')
     }
-  }, [accessToken]);
+  }, [accessToken])
 
   const onFormSubmit = (event) => {
-    event.preventDefault();
+    event.preventDefault()
     const options = {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({ username: username, password: password }),
-    };
+    }
 
     fetch(API_URL(mode), options)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        // console.log(data)
         if (data.success) {
           batch(() => {
-            dispatch(user.actions.setUserId(data.response.userId));
-            dispatch(user.actions.setAccessToken(data.response.accessToken));
-            dispatch(user.actions.setUserName(data.response.username));
-            dispatch(user.actions.setError(null));
-          });
+            dispatch(user.actions.setUserId(data.userId))
+            dispatch(user.actions.setAccessToken(data.accessToken))
+            dispatch(user.actions.setUserName(data.username))
+            dispatch(user.actions.setError(null))
+          })
         } else {
           batch(() => {
-            dispatch(user.actions.setError(data.response));
-            dispatch(user.actions.setUserId(null));
-            dispatch(user.actions.setAccessToken(null));
-            dispatch(user.actions.setUserName(null));
-          });
-          setError(data.response);
+            dispatch(user.actions.setError(data.response))
+            dispatch(user.actions.setUserId(null))
+            dispatch(user.actions.setAccessToken(null))
+            dispatch(user.actions.setUserName(null))
+          })
+          setError(data.response)
         }
-      });
-  };
+      })
+  }
   return (
     <>
       <div className="main-container">
         <form onSubmit={onFormSubmit}>
-          <div className="buttons">
+          <div className="login-top">
             <label htmlFor="register">Register</label>
             <input
               type="radio"
               id="register"
-              checked={mode === "register"}
-              onChange={() => setMode("register")}
+              checked={mode === 'register'}
+              onChange={() => setMode('register')}
             />
             <label htmlFor="login">Login</label>
             <input
               type="radio"
               id="login"
-              checked={mode === "login"}
-              onChange={() => setMode("login")}
+              checked={mode === 'login'}
+              onChange={() => setMode('login')}
             />
           </div>
-          <label htmlFor="username">Username</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button type="submit">Submit</button>
-          <p className="error-message">{error}</p>
+          <div className="form-container">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+
+            <button type="submit">Submit</button>
+            <p className="error-message">{error}</p>
+          </div>
         </form>
       </div>
     </>
-  );
-};
+  )
+}
--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,34 +1,37 @@
-import React, { useEffect } from "react";
-import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import React, { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import song from '../img/song.gif'
 
-import user from "reducers/user";
+import user from 'reducers/user'
 
 const Main = () => {
-  const accessToken = useSelector((store) => store.user.accessToken);
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
+  const accessToken = useSelector((store) => store.user.accessToken)
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
 
   useEffect(() => {
     if (!accessToken) {
-      navigate("/login");
+      navigate('/login')
     }
-  }, [accessToken]);
+  }, [accessToken])
 
   return (
-    <div>
+    <div className="main-container">
       <h1>Hello! Glad you could make it!</h1>
+      <img className="song" src={song} />
       <button
+        className="button-container"
         onClick={() => {
-          navigate("/login");
-          dispatch(user.actions.setAccessToken(null));
+          navigate('/login')
+          dispatch(user.actions.setAccessToken(null))
         }}
       >
         Log out
       </button>
     </div>
-  );
-};
+  )
+}
 
-export default Main;
+export default Main
