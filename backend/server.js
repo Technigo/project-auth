e4bfb8d3059d2073bbc3bdfce962f85747bb581e
Louisa <lundislon@gmail.com>
2022-05-28 15:13:25 +0200

    bcryptjs replacing bcrypt

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import crypto from "crypto";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
