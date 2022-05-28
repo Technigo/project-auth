426b81c83d8aa3273e6f6cf9be84e8225a04bea6
Louisa <lundislon@gmail.com>
2022-05-28 16:15:53 +0200

    removed / in BASE_URL at the end

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,3 +1,3 @@
-const BASE_URL = "https://project-auth-secret-garden.herokuapp.com/";
+const BASE_URL = "https://project-auth-secret-garden.herokuapp.com";
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`;
