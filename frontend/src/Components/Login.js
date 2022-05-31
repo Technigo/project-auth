2317608ade1b3b7f0f11377f9eb5e881a3182d88
LovisaBrorson <lovisabrorson@gmail.com>
2022-05-31 12:58:01 +0200

    Fixed error

--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -1,126 +1,111 @@
-import React, { useState, useEffect } from 'react'
-import { useSelector, useDispatch, batch } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
+import React, { useState, useEffect } from "react"
+import { useSelector, useDispatch, batch } from "react-redux"
+import { useNavigate } from "react-router-dom"
 
-
-
-import  { API_URL }  from '../../src/utils/constants'
-import user from '../../src/reducers/user'
+import { API_URL } from "../../src/utils/constants"
+import user from "../../src/reducers/user"
 
 const Login = () => {
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-    const [mode, setMode] = useState('signup')
-    
-    let errorMessage = useSelector((store) => store.user.error)
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [mode, setMode] = useState("signup")
 
-    const accessToken = useSelector((store) => store.user.accessToken)
+  let errorMessage = useSelector((store) => store.user.error)
 
-    const dispatch = useDispatch()
-    const navigate = useNavigate()
+  const accessToken = useSelector((store) => store.user.accessToken)
 
-    useEffect(() => {
-  
-      if (accessToken && mode === 'signin') {
-        navigate('/')
-      }
-    }, [accessToken, navigate])
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
 
-    
-    //This function posts the form to the database
-    const onFormSubmit = (e) => {
-          e.preventDefault()
+  useEffect(() => {
+    if (accessToken && mode === "signin") {
+      navigate("/")
+    }
+  }, [accessToken, navigate])
 
-          const options = {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ username, password })
-          }
+  //This function posts the form to the database
+  const onFormSubmit = (e) => {
+    e.preventDefault()
 
-          fetch(API_URL(mode), options)
-          .then(res => res.json())
-          .then((data) => {
-            console.log(data.response);
-            if (data.success) {
-              batch(() => {
-                dispatch(user.actions.setUserId(data.response.userId))
-                dispatch(user.actions.setUsername(data.response.username))
-                dispatch(user.actions.setAccessToken(data.response.accessToken))
-                dispatch(user.actions.setError(null))
-                if (mode === 'signup') {
-                  alert(`Successfully signed up!`)
-                  window.location.reload()
-                }
-              })
-              
-            } else {
-              dispatch(user.actions.setUserId(null))
-              dispatch(user.actions.setUsername(null))
-              dispatch(user.actions.setAccessToken(null))
-              dispatch(user.actions.setError(data.response))
-              errorMessage = data.response
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    }
+
+    fetch(API_URL(mode), options)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.response)
+        if (data.success) {
+          batch(() => {
+            dispatch(user.actions.setUserId(data.response.userId))
+            dispatch(user.actions.setUsername(data.response.username))
+            dispatch(user.actions.setAccessToken(data.response.accessToken))
+            dispatch(user.actions.setError(null))
+            if (mode === "signup") {
+              alert(`Successfully signed up!`)
+              // window.location.reload()
             }
           })
+        } else {
+          dispatch(user.actions.setUserId(null))
+          dispatch(user.actions.setUsername(null))
+          dispatch(user.actions.setAccessToken(null))
+          dispatch(user.actions.setError(data.response))
+          errorMessage = data.response
+        }
+      })
 
-          form.reset()
-    }
-    
+    form.reset()
+  }
 
-
-
-    console.log(mode);
+  console.log(mode)
 
   return (
-    
-    <div className="wrapper">
-
+    <div className='wrapper'>
       <h1>Welcome to our login page!</h1>
 
       <label htmlFor='signup'>Sign up</label>
-      <input 
-      id='signup' 
-      type="radio" 
-      checked={mode === 'signup'} 
-      onChange={() => setMode('signup')} 
+      <input
+        id='signup'
+        type='radio'
+        checked={mode === "signup"}
+        onChange={() => setMode("signup")}
       />
-
 
       <label htmlFor='signin'>Sign in</label>
-      <input id='signin'
-      type="radio" 
-      checked={mode === 'signin'} 
-      onChange={() => setMode('signin')} 
-
+      <input
+        id='signin'
+        type='radio'
+        checked={mode === "signin"}
+        onChange={() => setMode("signin")}
       />
 
-        <form onSubmit={onFormSubmit}>
+      <form onSubmit={onFormSubmit}>
+        <label htmlFor='username'>Username</label>
 
-          <label htmlFor='username'>Username</label>
-          
-          <input
-          id="username" 
-          type="text"
+        <input
+          id='username'
+          type='text'
           value={username}
           onChange={(e) => setUsername(e.target.value)}
-          />
+        />
 
-          <label htmlFor='password'>Password</label>
+        <label htmlFor='password'>Password</label>
 
-
-          <input
-          id="password" 
-          type="password" 
+        <input
+          id='password'
+          type='password'
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-          />
+        />
 
-          <button type="submit">Submit</button>
-          
-        </form>
-        <h3>{errorMessage}</h3>
-    
+        <button type='submit'>Submit</button>
+      </form>
+      <h3>{errorMessage}</h3>
     </div>
   )
 }
