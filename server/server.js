a07b8762b15612e91aade40b9ce81ec5d721c151
savannah-hayes <79738821+savannah-hayes@users.noreply.github.com>
2022-06-02 21:25:30 +0200

    Add middleware to fix CORS error

--- server/server.js
+++ server/server.js
@@ -15,6 +15,11 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 app.use("/", userRoutes);
 
 app.get("/", (req, res) => {
