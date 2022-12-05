4a7c8a5b180d46e57501fa4230469e33eab38e7d
Sailornina <sylcardozo.sc@gmail.com>
2022-12-05 17:20:51 +0100

    Bcrypto library was added. UserSchema and authenticateUser were created

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,50 @@
 import express from "express";
 import cors from "cors";
+import crypto from "crypto";
+import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString("hex"),
+  },
+});
+
+const User = mongoose.model("User", UserSchema);
+
+const authenticateUser = async (req, res, next) => {
+  const accessToken = req.header("Authorization");
+  try {
+    const user = await User.findOne({ accessToken: accessToken });
+    if (user) {
+      next();
+    } else {
+      res.status(401).json({
+        response: "Please log in",
+        success: false,
+      });
+    }
+  } catch (error) {
+    res.status(400).json({
+      response: error,
+      success: false,
+    });
+  }
+};
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -18,8 +58,9 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hi!");
+  res.send("This is the backend of Project-auth by Naghmeh Okhovat and Antonella Cardozo.");
 });
+
 
 // Start the server
 app.listen(port, () => {
