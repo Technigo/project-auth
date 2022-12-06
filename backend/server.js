b6257634147e1b9ee6a7bb0130d875db745e3511
Tina Bruce <tinabruce42@hotmail.com>
2022-12-06 16:37:04 +0100

    Added API endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import crypto from "crypto";
+import bcrypt from "bcrypt";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -15,6 +17,113 @@ const app = express();
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
+
+const UserSchema = mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true
+  }, 
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString('hex')
+  }
+})
+
+const User = mongoose.model('User', UserSchema);
+
+
+//REGISTER
+app.post('/register', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const salt = bcrypt.genSaltSync();
+    if (password.length < 8) {
+      res.status(400).json({
+        success: false,
+        response: "Password must be at least 8 characters long"
+      })
+    } else {
+      const newUser = await User({username: username, password: bcrypt.hashSync(password, salt)}).save()
+      res.status(201).json({
+        success: true,
+        response: {
+          username: newUser.username,
+          accessToken: newUser.accessToken,
+          id: newUser._id
+        }
+      })
+    }
+  } catch (error) {
+      res.status(400).json({
+        success: false,
+        response: error
+      })
+  }
+})
+
+
+//LOGIN
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({username})
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(200).json({
+        success: true,
+        response: {
+          username: user.username,
+          accessToken: user.accessToken,
+          id: user._id
+        }
+      }) 
+    } else {
+        res.status(400).json({
+          success: false,
+          response: "Credentials did not match"
+      })
+    }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      response: error
+    })
+  }
+})
+
+//SECRET PAGE
+const authenticateUser = async (req, res, next) => {
+  const accessToken = req.header("Authorization")
+  try {
+    const user = await User.findOne({accessToken})
+    if (user) {
+      next()
+    } else {
+      res.status(401).json({
+        success: false,
+        response: "Please log in"
+      })
+    }
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      response: error
+    })
+  }
+}
+
+app.get('/welcome', authenticateUser)
+app.get('/welcome', (req, res) => {
+  res.status(200).json({
+    success: true,
+    response: "Welcome! You are logged in"
+  })
+})
 
 // Start defining your routes here
 app.get("/", (req, res) => {
