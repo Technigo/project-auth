76cb7086258625b45ed1cfe7a7c77f546a7c77c4
Kneedler <sarahklindelow@gmail.com>
2022-12-06 12:09:17 +0100

    Authorization, login and register works in postman and compass

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
-import crypto from "crypto"
-import bcrypt from "bcrypt-nodejs"
+import crypto from "crypto";
+import bcrypt from "bcrypt";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-auth";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
@@ -18,8 +18,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// shows error if the server is not responding
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState === 1) {
+    next()
+  } else {
+    res.status(503).json({ error: 'Service unavailable' })
+  }
+})
+
+//Schemas
 const UserSchema = new mongoose.Schema({
-  name: {
+  username: {
     type: String,
     unique: true
   },
@@ -27,18 +37,131 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+/*   email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    validate: {
+      validator: (value) => {
+        return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value)
+      },
+      message: "Please enter a valid email address"
+    }
+  }, */
   accessToken: {
     type: String,
     default: () => crypto.randomBytes(128).toString("hex")
   }
 });
 
+const LoginSchema = new mongoose.Schema({
+  message: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: () => new Date()
+  },
+  hearts: {
+    type: Number,
+    default: 0
+  }
+});
+
+//models
 const User = mongoose.model("User", UserSchema)
+const LoginInfo = mongoose.model("LoginInfo", LoginSchema)
+
+// authentication 
+const authenticateUser = async (req, res, next) => {
+  const accessToken = req.header("Athorization");
+  try {
+    const user = await User.findOne({accessToken: accessToken});
+    if (user) {
+      next()
+    } else {
+      res.status(401).json({
+        success: false,
+        response: "Please log in"
+      })//401 unauthorized - inte inloggad (403 är forbidden)
+    }
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      response: error
+   })
+  }
+};
 
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
+
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+    try {
+      const salt = bcrypt.genSaltSync();
+      if (password.length < 8) {
+        res.status(400).json({
+          success: false,
+          response: "Password must be at least 8 characters long"
+        })
+      } else {
+        const newUser = await new User({username: username, password: bcrypt.hashSync(password, salt)}).save();
+        res.status(201).json({
+          success: true,
+          response: {
+            username: newUser.username,
+            accessToken: newUser.accessToken,
+            id: newUser._id
+          }
+        });
+      }
+    } catch(error) {
+      res.status(400).json({
+        success: false,
+        response: error
+      });
+    }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({username});
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(200).json({
+        success: true,
+        response: {
+          username: user.username,
+          accessToken: user.accessToken,
+          id: user._id
+        }
+      });
+    } else {
+      res.status(400).json({
+        success: false,
+        response: "Credentials didn't match"
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      response: error
+    });
+  }
+});
+
+app.get("/logininfo", authenticateUser);
+app.get("/logininfo", (req, res) => {
+  res.status(200).json({
+    success: true,
+    response: "all the information"
+  })
+})
+
 
 // Start the server
 app.listen(port, () => {
