d1b98b17c7db7b025111cac3c126638ef4f8076b
Mine Mine Mine <MasterNina@NBs-MacBook-Pro.local>
2022-12-06 20:11:06 +0100

    backend registrer login auth

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import crypto from "crypto"
+import bcrypt from "bcrypt"
+import { serialize } from "v8";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-auth";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
@@ -16,10 +19,152 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+///////// Tusday
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+    // no My_B4nK_
+  },
+  // npm install crypto, import
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString('hex')
+  }
+})
+
+const User = mongoose.model("User", UserSchema)
+
+const authenticateUser = async (req,res,next) => {
+  const accessToken = req.header("Authorization")
+  try {
+    const user = await User.findOne({accessToken: accessToken})
+    if (user) {
+      next()
+    } else {
+      //unothorized 401 not authenticated
+      //403 forbidden no Authorization
+      res.status(401).json({
+        response: "Please log in",
+        success: false
+      })
+    }
+  } catch (error) {
+    res.status(400).json({
+      response: error,
+      success: false
+    })
+  }
+}
+
+// ------- ROUTES ----------
+
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
+
+app.post("/registrer",  async (req, res) => {
+  const { username, password } = req.body
+  // npm i dcrypt
+  // const code = [1, 2, 4, 4];  (never stor the accual pw)
+  //salt
+  // const makeCodeSecret = (codeArr) => {
+    // iterate or loop
+    //const sectetMsg = codeArr.map(singlNr => singlNr + 1)
+    //return sectretMsg
+  //
+    //Salt helps decode/desipher
+  //}
+  // transformedCode = makeSodeSecret(code)
+  try {
+    const salt = bcrypt.genSaltSync()
+    if (password.length < 8) {
+      res.status(400).json({
+        success: false,
+        response: "Password need to be at least 8 characters long"
+      })
+    } else {
+      const newUser = await new User({username: username, password: bcrypt.hashSync(password, salt)}).save()
+      res.status(201).json({
+        success: true,
+        response: {
+          username: newUser.username,
+          accessToken: newUser.accessToken,
+          id: newUser._id
+        }
+      })
+      //201 created
+    }
+  } catch(error) {
+    res.status(400).json({
+      success: false,
+      response: error.message
+    })
+  }
+})
+
+app.post("/login", async (req,res) => {
+  const { username, password } = req.body
+try {
+  const user = await User.findOne({username})
+   // password = user.password (hashed) = true
+  if (user && bcrypt.compareSync(password, user.password)) {
+    res.status(200).json({
+      success: true,
+      response: {
+        username: user.username,
+        id: user._id,
+        accessToken: user.accessToken
+      }
+    })
+  } else {
+    // 400 error on client side
+    res.status(400).json({
+      success: false,
+      response: "Credentials didn't match"
+    })
+  }
+} catch (error) {
+  // error on server side
+  res.status(500).json({
+    success: false,
+    response: error
+  })
+}
+})
+
+
+
+const ThoughtSchema = new mongoose.Schema({
+  message: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: () => new Date()
+  },
+  harts: {
+    type: Number,
+    default: 0
+  }
+})
+const Thought = mongoose.model("Thought", ThoughtSchema)
+
+app.get("/thoughts", authenticateUser)
+app.get("/thoughts", async (req,res) => {
+  res.status(200).json({success: true, response: "all the thoughts"})
+})
+
+
+////////////
+
+
 
 // Start the server
 app.listen(port, () => {
