2d75df6fb9bda2e2251cfb7a3fec987cdb1889ec
Sailornina <sylcardozo.sc@gmail.com>
2022-12-06 12:20:52 +0100

    Working with POST /login

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const UserSchema = new mongoose.Schema({
     required: true,
     unique: true,
   },
-  email:{
+  email: {
     type: String,
     unique: true
   },
@@ -29,6 +29,7 @@ const UserSchema = new mongoose.Schema({
 });
 
 const User = mongoose.model("User", UserSchema);
+
 
 const authenticateUser = async (req, res, next) => {
   const accessToken = req.header("Authorization");
@@ -62,24 +63,50 @@ app.get("/", (req, res) => {
   res.send("This is the backend of Project-auth by Naghmeh Okhovat and Antonella Cardozo.");
 });
 
-
 app.post('/register', async (req, res) => {
   const { username, email, password } = req.body;
   try {
     const salt = bcrypt.genSaltSync(); // Create a randomizer to prevent to unhash it
-      const newUser = await new User({
-        username,
-        email,
-        password: bcrypt.hashSync(password, salt)
-      }).save();
-      res.status(201).json({
+    const newUser = await new User({
+      username,
+      email,
+      password: bcrypt.hashSync(password, salt)
+    }).save();
+    res.status(201).json({
+      response: {
+        userId: newUser._id,
+        username: newUser.username,
+        accessToken: newUser.accessToken
+      },
+      success: true
+    });
+  } catch (error) {
+    res.status(400).json({ response: error, success: false });
+  }
+});
+
+app.post('/login', async (req, res) => {
+  const { username, email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username, email });
+
+    if (user && bcrypt.compareSync(password, user.password, email, user.email)) {
+      res.status(200).json({
         response: {
-          userId: newUser._id,
-          username: newUser.username,
-          accessToken: newUser.accessToken
+          userId: user._id,
+          username: user.username,
+          email: user.email,
+          accessToken: user.accessToken
         },
         success: true
       });
+    } else {
+      res.status(404).json({
+        response: "Username, email or password doesn't match.",
+        success: false
+      });
+    }
   } catch (error) {
     res.status(400).json({ response: error, success: false });
   }
