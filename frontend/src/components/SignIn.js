0d2d5436d0e5299252dcb60fd25e13291d828424
Kneedler <sarahklindelow@gmail.com>
2022-12-06 14:34:14 +0100

    API URL added

--- frontend/src/components/SignIn.js
+++ frontend/src/components/SignIn.js
@@ -1,2 +1,4 @@
 import React from "react"
+
 import user from "../reducers/user";
+import { API_URL } from "../utils/URL"
---
+++ frontend/src/utils/URL.js
@@ -0,0 +1,1 @@
+export const API_URL = "https://project-auth-2ucjrblcba-lz.a.run.app"
