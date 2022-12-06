ea251c00e224e93d5c41c5313ce26b42859b15df
Fredrik Holm <karlfredrikholm@gmail.com>
2022-12-06 11:53:22 +0100

    tuesday lecture codealong

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
-import express from "express";
+import express, { response } from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import crypto from "crypto";
+import bcrypt from "bcrypt";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -16,6 +18,132 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//// Dubbelkolla första minuterna av föreläsningen ////
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+    // My_B4nK_P4$$word
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString("hex")
+  }
+});
+
+const User = mongoose.model("User", UserSchema);
+
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  // const code = [1, 2, 3, 4];
+  // const makeCodeSecret = (codeArray) => {
+     // const secretMessage = codeArr.map(singleNumber => singleNumber + 1);
+     // return secretMessage;
+  // };
+  // transformedCode = makeCodeSecret(code)
+  try {
+    const salt = bcrypt.genSaltSync();
+    if (password.length < 8) {
+      res.status(400).json({
+        success: false,
+        response: "Password must be at least 8 characters long"
+      });
+    } else {
+      const newUser = await new User({username: username, password: bcrypt.hashSync(password, salt)}).save();
+      res.status(201).json({
+        success: true,
+        response: {
+          username: newUser.username,
+          accessToken: newUser.accessToken,
+          id: newUser._id
+        }
+      });
+    }
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      response: err
+    })
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({});
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(200).json({
+        success: true,
+        response: {
+          username: user.username,
+          id: user._id,
+          accessToken: user.accessToken
+        }
+      });
+    } else {
+      res.status(400).json({
+        success: false,
+        response: "Credentials didn't match"
+      })
+    }
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      response: err
+    })
+  }
+});
+
+const authenticateUser = async (req, res, next) => {
+  const accessToken = req.header("Authorization");
+  try {
+    const user = await User.findOne({accessToken: accessToken});
+    if (user) {
+      next();
+    } else {
+      res.status(401).json({
+        success: false,
+        response: "Please log in"
+      })
+    }
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      response: err
+    })
+  }
+}
+
+const ThoughtSchema = new mongoose.Schema({
+  message: {
+    type:String,
+  },
+  createdAt: {
+    type: Date,
+    default: () => new Date()
+  },
+  hearts: {
+    type: Number,
+    default: 0
+  }
+});
+
+const Thought = mongoose.model("Thought", ThoughtSchema);
+
+app.get("/thoughts", authenticateUser);
+app.get("/thoughts", async (req, res) => {
+  res.status(200).json({
+    success: true,
+    response: "all the thoughts"
+  });
+});
+
+/////
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
