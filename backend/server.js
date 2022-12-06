4b3b9df9a9b038af5c03eeef2533ab788f178c85
Amanda Elvkull <108291356+AmandaElvkull@users.noreply.github.com>
2022-12-06 11:55:47 +0100

    added backend from tuesday session

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import crypto from "crypto";
+import bcrypt from "bcrypt";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -16,10 +18,145 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String, 
+    required: true, 
+    unique: true
+  },
+  password: {
+    type: String, 
+    required: true
+  },
+  //npm install crypto Remember to be in backend folder! 
+  accessToken: {
+    type: String, 
+    default: () => crypto.randomBytes(128).toString("hex")
+  }
+});
+
+const User = mongoose.model("User", UserSchema);
+
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send({
+    Message: "Backend for login to our project page",
+    Routes: [{
+      "/register": "POST request where you register your profile",
+      "/login" : "Login page for a user"
+    }]
+  });
 });
+
+app.post("/register", async (req, res) =>{
+  const { username, password } = req.body;
+
+// npm install bcrypt
+  try {
+    const salt = bcrypt.genSaltSync();
+    if(password.length < 8){
+      res.status(400).json({
+        success: false, 
+        response: "Password must be at least 8 characters long."
+      });
+    } else {
+      const newUser = await new User({username: username, password: bcrypt.hashSync(password, salt)}).save();
+      // 201 is created 
+      res.status(201).json({
+        success: true, 
+        response: {
+          // send the username never password, ID is to help frontend
+          username: newUser.username,
+          accessToken: newUser.accessToken,
+          id: newUser._id
+        }
+      });
+    }
+  }catch(error) {
+    res.status(400).json({
+      success: false, 
+      response: error
+    })
+  } 
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body; 
+
+  try {
+    const user = await User.findOne({username});
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(200).json({
+        success: true, 
+        response: {
+          username: user.username,
+          id: user._id,
+          accessToken: user.accessToken
+        }
+      });
+    } else {
+      res.status(400).json({
+        success: false, 
+        response: "Credentials didn't match"
+      });   
+    }
+  } catch(error) {
+    res.status(500).json({
+      success: false, 
+      response: error
+    });
+  } 
+});
+
+
+// For pages only shown for the login user we need to do a authentication of the user. 
+const authenticateUser = async (req, res, next) => {
+  const accessToken = req.header("Authorization"); 
+  try {
+    // use the accessToken when its to easy to store a username in the header, accesstoken is uniqe for each user 
+    const user = await User.findOne({accessToken: accessToken});
+    if (user) {
+      next();
+    } else {
+      res.status(401).json({
+        response: " Please log in ",
+        success: false
+      })
+    }
+  } catch (error) {
+    res.status(400).json({
+      response: error,
+      success: false
+    })
+  }
+}
+
+// aut for happy thoughts - need to change to this week project 
+
+const ThoughtsSchema = new mongoose.Schema({
+  message: {
+    type: String, 
+  }, 
+  createdAt: {
+    type: Date, 
+    default : () => new Date()
+  },
+  hearts: {
+    type: Number, 
+    default: 0
+  }
+});
+
+const Thought = mongoose.model("Thought", ThoughtsSchema);
+
+app.get("/thoughts", authenticateUser);
+app.get("/thoughts", ( req, res) => {
+  res.status(200).json({
+    success: true, 
+    response: "All the thoughts"
+  })
+})
+
 
 // Start the server
 app.listen(port, () => {
