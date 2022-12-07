6363269eea4f371230c616f261bad85062e4c634
Josefine Engdahl <josefineengdahl@MacBook-Air-som-tillhor-Josefine.local>
2022-12-07 11:18:13 +0100

    setting up App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,42 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import { Provider } from 'react-redux';
+//import { configureStore, combineReducers } from '@reduxjs/toolkit';
+
+import Start from 'components/Start'
+import Register from 'components/Register'
+import Login from 'components/Login'
+import Main from 'components/Main'
+import Notfound from 'components/Notfound'
+
+
+// import user from "./reducers/user";
+// import thoughts from "./reducers/thoughts";
+
+// const reducer = combineReducers({
+//   user: user.reducer,
+//   thoughts: thoughts.reducer,
+// });
+
+// const store = configureStore({ reducer });
+
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <div>hej</div>
+    // <BrowserRouter>
+    // {/* <Provider store={store}> */}
+    // <Routes>
+    //   {/* <Route path='/' element={<Start />} />
+    //   <Route path='/register' element={<Register />} />
+    //   <Route path='/login' element={<Login />} />
+    //   <Route path='/main' element={<Main />} />
+    //   <Route path='*' element={<Notfound />} /> */}
+    // </Routes>
+    // {/* </Provider> */}
+    // </BrowserRouter>
   );
 }
+
+// import styled from 'styled-components'
+// npm install --save styled-components
