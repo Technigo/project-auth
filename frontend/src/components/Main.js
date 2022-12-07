fa963a314281aa17b37462c23eb85132d6df35e5
Sailornina <sylcardozo.sc@gmail.com>
2022-12-07 18:25:06 +0100

    Main.js and Login.js in progress

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,3 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const Main 
+
+import { useDispatch } from 'react-redux';
+// import { useSelector } from 'react-redux'
+
+const Main = () =>{
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const dispatch = useDispatch()
+    // const userStart = useSelector((store) => store.user.username)
+
+  
+  const handleChange = (e) => {
+    setUsername(e.target.value);
+  };
+  const handleChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form onSubmit={onFormSubmit}>
+         <label htmlFor="username">Username: </label>
+      <Input
+        type="text"
+        placeholder="🖊️ Write your username here"
+        value={username}
+        onChange={(e) => handleChange(e)}
+        className="input" />
+        <label htmlFor="password">Password: </label>
+        <Input
+        type="text"
+        placeholder="🖊️ Write your password here"
+        value={password}
+        onChange={(e) => handleChange(e)}
+        className="input" />
+      <Button className="submit-btn" type="submit">Submit</Button>
+    </form>
+    )
+}
+
+export default Main;
