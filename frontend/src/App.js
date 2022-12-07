6e2347bc7450e5179ef296f492426b5ddd662476
Josefine Engdahl <josefineengdahl@MacBook-Air-som-tillhor-Josefine.local>
2022-12-07 11:26:53 +0100

    first set ut Start.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 //import { configureStore, combineReducers } from '@reduxjs/toolkit';
 
 import Start from 'components/Start'
-import Register from 'components/Register'
-import Login from 'components/Login'
-import Main from 'components/Main'
-import Notfound from 'components/Notfound'
+// import Register from 'components/Register'
+// import Login from 'components/Login'
+// import Main from 'components/Main'
+// import Notfound from 'components/Notfound'
 
 
 // import user from "./reducers/user";
@@ -23,18 +23,17 @@ import Notfound from 'components/Notfound'
 
 export const App = () => {
   return (
-    <div>hej</div>
-    // <BrowserRouter>
-    // {/* <Provider store={store}> */}
-    // <Routes>
-    //   {/* <Route path='/' element={<Start />} />
-    //   <Route path='/register' element={<Register />} />
-    //   <Route path='/login' element={<Login />} />
-    //   <Route path='/main' element={<Main />} />
-    //   <Route path='*' element={<Notfound />} /> */}
-    // </Routes>
-    // {/* </Provider> */}
-    // </BrowserRouter>
+    <BrowserRouter>
+    {/* <Provider store={store}> */}
+    <Routes>
+      <Route path='/' element={<Start />} />
+      {/* <Route path='/register' element={<Register />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/main' element={<Main />} />
+      <Route path='*' element={<Notfound />} /> */}
+    </Routes>
+    {/* </Provider> */}
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/components/Start.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Start = () => {
+return (
+<div>Start</div>
+)
+}
+
+export default Start;
