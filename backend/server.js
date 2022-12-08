2c3c1b774f0900ea15c519fc5c2eb94294ff9add
Sneezan <ceciliafraanlund@gmail.com>
2022-12-08 14:01:55 +0100

    need to add environmental variables to backend, local host added again

--- backend/server.js
+++ backend/server.js
@@ -11,49 +11,48 @@ mongoose.Promise = Promise;
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8090;
 const app = express();
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
-
-//////////////
+/////// Monday
 const UserSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
-    unique: true,
+    unique: true
   },
   password: {
     type: String,
     required: true
+    /// My_B4nK_P4$$word
   },
   // npm install crypto
   accessToken: {
     type: String,
     default: () => crypto.randomBytes(128).toString("hex")
   }
-})
+});
 
 const User = mongoose.model("User", UserSchema);
 
 app.post("/register", async (req, res) => {
   const { username, password } = req.body;
 // npm install bcrypt
-// const code = [1,2,4,4];
+// const code = [1, 2, 4, 4];
 // const makeCodeSecret = (codeArr) => {
-//    const secretMessage = codeArr.map(singleNumber => singleNumber + 1);
-//    return secretMessage
-// }
+    // const secretMessage = codeArr.map(singleNumber => singleNumber + 1);
+    // return secretMessage
+//}
 // transformedCode = makeCodeSecret(code)
-
   try {
     const salt = bcrypt.genSaltSync();
     if (password.length < 8) {
       res.status(400).json({
-        succes: false,
-        response: "Password must be atleast 8 characters long"
+        success: false,
+        response: "Password must be at least 8 characters long"
       });
     } else {
       const newUser = await new User({username: username, password: bcrypt.hashSync(password, salt)}).save();
@@ -64,9 +63,9 @@ app.post("/register", async (req, res) => {
           accessToken: newUser.accessToken,
           id: newUser._id
         }
-      })
+      });
     }
-  } catch (error) {
+  } catch(error) {
       res.status(400).json({
         success: false,
         response: error
@@ -81,7 +80,7 @@ app.post("/login", async (req, res) => {
     const user = await User.findOne({username});
     if (user && bcrypt.compareSync(password, user.password)) {
       res.status(200).json({
-        succes: true,
+        success: true,
         response: {
           username: user.username,
           id: user._id,
@@ -93,11 +92,11 @@ app.post("/login", async (req, res) => {
         success: false,
         response: "Credentials didn't match"
       });
-    } 
+    }
   } catch (error) {
-      res.status(500).json({
-        success: false,
-        response: error
+    res.status(500).json({
+      success: false,
+      response: error
     });
   }
 });
@@ -105,12 +104,12 @@ app.post("/login", async (req, res) => {
 const authenticateUser = async (req, res, next) => {
   const accessToken = req.header("Authorization");
   try {
-    const user = await User.findOne({accesToken: accessToken});
+    const user = await User.findOne({accessToken: accessToken});
     if (user) {
       next();
     } else {
       res.status(401).json({
-        response: "Please login",
+        response: "Please log in",
         success: false
       })
     }
@@ -128,29 +127,39 @@ const ThoughtSchema = new mongoose.Schema({
   },
   createdAt: {
     type: Date,
-    default: () => new Date()
+    default: () => new Date() 
   },
   hearts: {
     type: Number,
     default: 0
   }
-})
+}); 
+
 
 const Thought = mongoose.model("Thought", ThoughtSchema);
 
 app.get("/thoughts", authenticateUser);
-app.get("/thoughts", (req, res) => {
-  res.status(200).json({
-    success: true,
-    response: "all the thoughts"
-  });
+app.get("/thoughts", async (req, res)=> {
+  const thoughts = await Thought.find({});
+  res.status(200).json({success: true, response: thoughts});
 });
 
+app.post("/thoughts", authenticateUser)
+app.post("/thoughts", async (req, res) => {
+  const { message } = req.body;
+  try {
+    const newThought = await new Thought({message}).save();
+    res.status(201).json({success: true, response: newThought});
+  } catch (error) {
+    res.status(400).json({success: false, response: error});
+  }
+});
+
+///////
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
-
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
