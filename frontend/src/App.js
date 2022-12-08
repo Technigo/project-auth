3abe58bd75a02a9faca5ce681bb12828e6573f2d
Charlotte Johansson <charlotte@verismohr.com>
2022-12-08 14:58:54 +0100

    code along thursday

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from 'components/Main';
+import Login from 'components/Login';
+import NotFound from 'components/NotFound';
+import { Provider } from 'react-redux';
+import { combineReducers, combineStore } from '@reduxjs/toolkit';
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/login' element={<Login/>}></Route>
+      <Route path='/' element={<Main/>}></Route>
+      <Route path='*' element={<NotFound/>}></Route>
+    </Routes>
+    </BrowserRouter>
   );
 }
+
+
+
+
+// npm i
+// npm install react-router-dom
+// npm install react-redux
+// npm install @reduxjs/toolkit
