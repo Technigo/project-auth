be2d5c3426abb35fb5f163f5b478a60e07d17b05
Sneezan <ceciliafraanlund@gmail.com>
2022-12-08 14:07:48 +0100

    Co-authored-by: Linus Eriksson <erilinus@users.noreply.github.com>

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,3 +1,4 @@
-const BASE_URL = "https://projectauthcecilialinus-lonzreseuq-lz.a.run.app ";
+const BASE_URL = "https://projectauthcecilialinus-lonzreseuq-lz.a.run.app";
 
+export const API_BASE = "https://projectauthcecilialinus-lonzreseuq-lz.a.run.app"
 export const API_URL = (slug) => `${BASE_URL}/${slug}`;
