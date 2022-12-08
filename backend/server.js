abb5ac745cec88b18d2e5025c67586a9b53f7497
Petra Eriksson <eriksson.petra@gmail.com>
2022-12-08 15:17:51 +0100

    solved a bug in server.js

--- backend/server.js
+++ backend/server.js
@@ -3,73 +3,73 @@ import cors from "cors";
 import mongoose from "mongoose";
 import crypto from "crypto";
 import bcrypt from "bcrypt";
-//you need to install crypto/bcryto in the backend and maybe fronend as well.
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/auth";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8090;
 const app = express();
-
-//npm install bcrypt
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
-
+/////// Monday
 const UserSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
-    unique: true,
+    unique: true
   },
   password: {
     type: String,
-    required: true,
+    required: true
+    /// My_B4nK_P4$$word
   },
-
-  //npm install cryto
+  // npm install crypto
   accessToken: {
     type: String,
-    default: () => crypto.randomBytes(128).toString("hex"),
-  },
+    default: () => crypto.randomBytes(128).toString("hex")
+  }
 });
 
 const User = mongoose.model("User", UserSchema);
+
 app.post("/register", async (req, res) => {
   const { username, password } = req.body;
-
-  // transformedCode = makeCodeSecret(code)
+// npm install bcrypt
+// const code = [1, 2, 4, 4];
+// const makeCodeSecret = (codeArr) => {
+    // const secretMessage = codeArr.map(singleNumber => singleNumber + 1);
+    // return secretMessage
+//}
+// transformedCode = makeCodeSecret(code)
   try {
     const salt = bcrypt.genSaltSync();
     if (password.length < 8) {
       res.status(400).json({
         success: false,
-        response: "Password must be at least 8 characters long",
+        response: "Password must be at least 8 characters long"
       });
     } else {
-      const newUser = await new User({
-        username: username,
-        password: bcrypt.hashSync(password, salt),
-      }).save();
+      const newUser = await new User({username: username, password: bcrypt.hashSync(password, salt)}).save();
       res.status(201).json({
         success: true,
         response: {
           username: newUser.username,
           accessToken: newUser.accessToken,
-          id: newUser._id,
-        },
+          id: newUser._id
+        }
       });
     }
-  } catch (error) {
-    res.status(400).json({
-      success: false,
-      response: error,
-    });
+  } catch(error) {
+      res.status(400).json({
+        success: false,
+        response: error
+      });
   }
 });
 
@@ -77,26 +77,26 @@ app.post("/login", async (req, res) => {
   const { username, password } = req.body;
 
   try {
-    const user = await User.findOne({ username });
+    const user = await User.findOne({username});
     if (user && bcrypt.compareSync(password, user.password)) {
       res.status(200).json({
         success: true,
         response: {
           username: user.username,
           id: user._id,
-          accessToken: user.accessToken,
-        },
+          accessToken: user.accessToken
+        }
       });
     } else {
-      res.status(401).json({
+      res.status(400).json({
         success: false,
-        response: "Credentials didn't match",
+        response: "Credentials didn't match"
       });
     }
   } catch (error) {
     res.status(500).json({
       success: false,
-      response: error,
+      response: error
     });
   }
 });
@@ -104,22 +104,22 @@ app.post("/login", async (req, res) => {
 const authenticateUser = async (req, res, next) => {
   const accessToken = req.header("Authorization");
   try {
-    const user = await User.findOne({ accessToken: accessToken });
+    const user = await User.findOne({accessToken: accessToken});
     if (user) {
       next();
     } else {
       res.status(401).json({
         response: "Please log in",
-        success: false,
-      });
+        success: false
+      })
     }
   } catch (error) {
     res.status(400).json({
       response: error,
-      success: false,
-    });
+      success: false
+    })
   }
-};
+}
 
 const ThoughtSchema = new mongoose.Schema({
   message: {
@@ -127,21 +127,23 @@ const ThoughtSchema = new mongoose.Schema({
   },
   createdAt: {
     type: Date,
-    default: () => new Date(),
+    default: () => new Date() 
   },
   hearts: {
     type: Number,
-    default: 0,
-  },
-});
+    default: 0
+  }
+}); 
+
 
 const Thought = mongoose.model("Thought", ThoughtSchema);
 
-app.get("/thoughts", authenticateUser); // run authenticateUser, if true... (see above) Next() whicht means it it will fetch the data below.
-app.get("/thoughts", async (req, res) => {
+app.get("/thoughts", authenticateUser);
+app.get("/thoughts", async (req, res)=> {
   const thoughts = await Thought.find({});
-  res.status(200).json({ success: true, response: {success: true, response: thoughts }});
+  res.status(200).json({success: true, response: thoughts});
 });
+
 app.post("/thoughts", authenticateUser)
 app.post("/thoughts", async (req, res) => {
   const { message } = req.body;
@@ -153,6 +155,7 @@ app.post("/thoughts", async (req, res) => {
   }
 });
 
+///////
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
