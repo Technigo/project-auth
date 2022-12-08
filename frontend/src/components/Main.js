3f71c81487b0ad841d8bd53a5ca6c0fec60ddba2
Sailornina <sylcardozo.sc@gmail.com>
2022-12-08 12:59:10 +0100

    Main.js is done. All dependencies in FE part were added correctly

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -1,17 +1,57 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch, /*useSelector*/ } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import user from '../reducers/user';
+import { API_URL } from "../utils/urls";
 
 
 const Main = () => {
-  const [secret, setsecret] = useState("");
+  const [secret, setSecret] = useState(null);
 
-  const dispatch = useDispatch()
-  // const userStart = useSelector((store) => store.user.username)
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const accessToken = useSelector((store) => store.user.accessToken);
+
+  const logout = () => {
+    batch(() => {
+      dispatch(user.actions.setUsername(null));
+      dispatch(user.actions.setAccessToken(null));
+
+      localStorage.removeItem('user');
+    });
+  };
+
+  useEffect(() => {
+    if (!accessToken) {
+      navigate('/login')
+    }
+  }, [accessToken, navigate])
+
+  useEffect(() => {
+    const options = {
+      method: 'GET',
+      headers: {
+        Authorization: accessToken,
+      },
+    }
+
+    fetch(API_URL('secrets'), options)
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.success) {
+          setSecret(data.response)
+        }
+      })
+  }, [accessToken])
 
   return (
     <div className="secret-page">
-      <h1>It's a secret!</h1>
-      <button className="button-logout">
+      <h1>Do you want here?</h1>
+      <div>
+        <p>{secret.message}</p>
+      </div>
+      <button className="button-logout" onClick={logout}>
         Logout
       </button>
     </div>
