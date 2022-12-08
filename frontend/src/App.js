3df14e72b9a5f59f5fecb71b835b5233b37664d2
Josefine Engdahl <josefineengdahl@MacBook-Air-som-tillhor-Josefine.local>
2022-12-08 14:34:22 +0100

    changed name on component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import Start from 'components/Start'
 import Login from 'components/Login'
 import Main from 'components/Main'
-import NotFound from 'components/NotFound'
+import Notfound from 'components/Notfound'
 
 
 import user from "./reducers/user";
@@ -28,7 +28,7 @@ export const App = () => {
       <Route path='/' element={<Start />} />
       <Route path='/login' element={<Login />} />
       <Route path='/main' element={<Main />} />
-      <Route path='*' element={<NotFound />} /> 
+      <Route path='*' element={<Notfound />} /> 
     </Routes>
     </BrowserRouter>
     </Provider>
