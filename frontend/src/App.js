c0a9e76ba9b25a0d33497ba8e440dfa73bb51f6d
Tara78 <70331037+Tara78@users.noreply.github.com>
2022-12-09 09:32:33 +0100

    3 Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from "./components/Login";
+import Main from "./components/Main";
+import NotFound from "./components/NotFound"
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login/>}></Route>
+        <Route path="/" element={<Main/>}></Route>
+        <Route path="*" element={<NotFound/>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
