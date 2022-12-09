5c43a3b4c64034213c8801044c96ab518d1013db
Petra Eriksson <eriksson.petra@gmail.com>
2022-12-09 15:33:20 +0100

    changed URL

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,4 +1,4 @@
-// const BASE_URL = "https://project-auth-ww2g7bm2ra-lz.a.run.app";
-const BASE_URL = "http://localhost:8090";
+const BASE_URL = "https://project-auth-ww2g7bm2ra-lz.a.run.app";
+// const BASE_URL = "http://localhost:8090";
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`;
