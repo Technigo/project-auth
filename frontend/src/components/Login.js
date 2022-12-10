c8ae66b89a704ca4f584886443d6bbb60d59eca2
Tara78 <70331037+Tara78@users.noreply.github.com>
2022-12-10 10:33:08 +0100

    StyledComponet

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector, batch } from "react-redux";
 import { useNavigate, Link } from "react-router-dom";
 import { API_URL } from "utils/utils";
 import user from "reducers/user";
+import styled from "styled-components";
 
 const Login = () => {
   const [name, setName] = useState("");
@@ -30,7 +31,6 @@ const Login = () => {
     };
     fetch(API_URL(mode), options)
       .then((response) => response.json())
-      // .then((data) => console.log(data));
       .then((data) => {
         if (data.success) {
           batch(() => {
@@ -50,40 +50,120 @@ const Login = () => {
       });
   };
   return (
-    <>
-      <label htmlFor="register">Register</label>
-      <input
-        type="radio"
-        id="register"
-        checked={mode === "register"}
-        onChange={() => setMode("register")}
-      />
-      <label htmlFor="login">Login</label>
-      <input
-        type="radio"
-        id="login"
-        checked={mode === "login"}
-        onChange={() => setMode("login")}
-      />
-      <form onSubmit={onFormSubmit}>
-        <label htmlFor="name">Username</label>
-        <input
+    <Wrapper>
+      <Section>
+        <Label htmlFor="register">
+          <LabelText> Register</LabelText>
+          <input
+            type="radio"
+            id="register"
+            checked={mode === "register"}
+            onChange={() => setMode("register")}
+          />
+        </Label>
+        <Label htmlFor="login">
+          <LabelText> Login</LabelText>
+          <input
+            type="radio"
+            id="login"
+            checked={mode === "login"}
+            onChange={() => setMode("login")}
+          />
+        </Label>
+      </Section>
+      <Form onSubmit={onFormSubmit}>
+        <Label htmlFor="name">
+          <LabelText> Username</LabelText>
+        </Label>
+        <Input
           type="text"
           id="name"
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
-        <label htmlFor="password">Password</label>
-        <input
+        <Label htmlFor="password">
+          <LabelText>Password</LabelText>
+        </Label>
+        <Input
           type="password"
           id="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button type="submit">Submit</button>
-      </form>
-    </>
+
+        <Button type="submit">Submit</Button>
+      </Form>
+    </Wrapper>
   );
 };
 
 export default Login;
+
+
+/**Styling  */
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #d9b3ff;
+  font-family: "Inconsolata";
+  font-size: 18px;
+`;
+
+
+const Label = styled.label`
+  display: flex;
+ 
+  input {
+    font-family: "Inconsolata";
+    width: 20px;
+    border: 1px solid #000;
+    line-height: 20px;
+    padding: 5px;
+  }
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 15px;
+`;
+
+const LabelText = styled.p`
+  display: inline;
+  margin-right: 10px;
+  width: 80px;
+  text-align: right;
+  color: beige;
+`;
+const Button = styled.button`
+  font-family: "Inconsolata";
+  font-size: 18px;
+  padding: 6px 15px;
+  background: none;
+  border: 4px solid rgb(102, 0, 102);
+  width: 150px;
+  margin: 15px 30px 30px;
+  cursor: pointer;
+  color: rgb(26, 0, 26);
+  margin-top: 2rem;
+  border-radius: 8px;
+`;
+const Section = styled.div`
+  width: 100vw;
+  height: 200px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  font-size: 18px;
+`;
+const Input= styled.input`
+width: 200px;
+height: 20px;
+border-radius: 8px;
+`
