f96070c951e6be8ba8bb4873d039bc368ed33d2d
a-well <amandaemkwellington@gmail.com>
2022-12-11 15:30:51 +0100

    Add new url

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,3 +1,4 @@
-const BASE_URL = "http://localhost:8080"
+const BASE_URL = process.env.BASE_URL || "https://project-auth-vfuupab45q-uc.a.run.app"
 
+console.log('Using BASE_URL ', BASE_URL)
 export const API_URL = (slug) => `${BASE_URL}/${slug}`
