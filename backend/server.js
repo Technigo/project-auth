d538e8b62a88f50f2346fe7229ff6f7e6bfc5bbd
SandraMadeleine <sandramohlsson@yahoo.com>
2023-05-23 15:48:25 +0200

    new backend code

--- backend/server.js
+++ backend/server.js
@@ -2,23 +2,13 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import crypto from "crypto";
-import bcrypt from "bcrypt-nodejs";
+import bcrypt from "bcrypt";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1/project-auth";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
-const port = process.env.PORT || 8080;
-const app = express();
-
-// Add middlewares to enable cors and json body parsing
-app.use(cors());
-app.use(express.json());
-
-const User = mongoose.model('User', {
+const UserSchema = new mongoose.Schema({
   name: {
     type: String,
     unique: true
@@ -37,16 +27,38 @@ const User = mongoose.model('User', {
   }
 });
 
+const User = mongoose.model("User", UserSchema);
+
 const authenticateUser = async (req, res, next) => {
-  const user = await User.findOne({accessToken: req.header('Authorization')});
-  if (user) {
-    req.user = user;
+  const accessToken = req.header("Authorization");
+  try {
+    const user = await User.findOne({accessToken: accessToken});
+    if (user) {
+    // req.user = user;
     next();
-  } else {
-    res.status(401).json({loggedOut: true});
-    // message: "Authentication error"
+    } else {
+        res.status(401).json({
+          success: false,
+          response: "Please log in"
+      })
+    }
+  } catch (e) {
+    res.status(500).json({
+     success: false,
+      response: e
+    });
   }
 };
+
+// Defines the port the app will run on. Defaults to 8080, but can be overridden
+// when starting the server. Example command to overwrite PORT env variable value:
+// PORT=9000 npm start
+const port = process.env.PORT || 8080;
+const app = express();
+
+// Add middlewares to enable cors and json body parsing
+app.use(cors());
+app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
@@ -55,14 +67,27 @@ app.get("/", (req, res) => {
 
 // Register
 app.post("/users", async (req, res) => {
-  try {
-    const {name, email, password} = req.body;
-    const user = new User({name, email, password: bcrypt.hashSync(password)});
-    user.save();
-    res.status(201).json({id: user._id, accessToken: user.accessToken});
-  } catch (err) {
-    res.status(400).json({message: 'Could not create user', errors: err.errors});
-  }
+    const { username, password } = req.body;
+    try {
+      const salt = bcrypt.genSaltSync();
+      const newUser = await new User({
+        username: username,
+        password: bcrypt.hashSync(password, salt)
+      }).save();
+      res.status(201).json({
+        success: true,
+        response: {
+          username: newUser.username,
+          id: newUser._id,
+          accessToken: newUser.accessToken
+        }
+      })
+  } catch (e) {
+    res.status(400).json({
+      success: false,
+      response: e
+      })
+    }
 });
 
 // Only logged in users can see
@@ -72,12 +97,39 @@ app.get("/secrets", (req, res) => {
 });
 
 // Log-in
-app.post("/sessions", async (req, res) => {
-  const user = await User.findOne({email: req.body.email});
-  if (user && bcrypt.compareSync(req.body.password, user.password)) {
-    res.json({userId: user._id, accessToken: user.accessToken});
+// app.post("/sessions", async (req, res) => {
+//   const user = await User.findOne({email: req.body.email});
+//   if (user && bcrypt.compareSync(req.body.password, user.password)) {
+//     res.json({userId: user._id, accessToken: user.accessToken});
+//   } else {
+//     res.json({notFound: true});
+//   }
+// });
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({username: username})
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(200).json({
+        success: true,
+        response: {
+          username: user.username,
+          id: user._id,
+          accessToken: user.accessToken
+        }
+      });
   } else {
-    res.json({notFound: true});
+      res.status(400).json({
+        success: false,
+        response: "Credentials do not match"
+      });
+    }
+  } catch (e) {
+    res.status(500).json({
+      success: false,
+      response: e
+    })
   }
 });
 
