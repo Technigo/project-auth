24955294aa922418e52cca095ff0ae9192cd7b84
AnnikaSonnek <annika.sonnek@gmail.com>
2023-05-23 12:12:04 +0200

    coded along with daniel

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import crypto from "crypto";
+import bcrypt from "bcrypt";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
+const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
@@ -18,8 +20,160 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send("Hello user!!");
 });
+
+////////////////////////
+const { Schema } = mongoose; 
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String, 
+    required: true, 
+    unique: true
+  },
+  password: {
+    type: String, 
+    required: true
+  },
+  accessToken: {
+    // npm install crypto in order to use this.
+    type: String, 
+    default: () => crypto.randomBytes(128).toString("hex")
+  }
+})
+
+const User = mongoose.model("User", UserSchema)
+////////////Registration /////////////////
+app.post("/register", async (req, res) => {
+  const {username, password } = req.body
+  try {
+    const salt = bcrypt.genSaltSync()
+    const newUser = await new User({
+      username: username,
+      password: bcrypt.hashSync(password, salt)
+    }).save()
+  res.status(201).json({
+    success: true, 
+    response: {
+      username: newUser.username,
+      id: newUser._id,
+      accessToken: newUser.accessToken
+    }
+  })
+  } catch (e) {
+    res.status(400).json({
+      success: false,
+      response: e
+    })
+  }
+})
+
+////////////////LOGIN//////////////////
+app.post("/login", async (req, res) => {
+  const {username, password } = req.body
+  try {
+    const user = await User.findOne({username: username})
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(201).json({
+        success: true, 
+        response: {
+          username: user.username,
+          id: user._id,
+          accessToken: user.accessToken
+        } 
+      })
+
+    } else {
+      res.status(400).json({
+        success: false,
+        resonse: "Credentionals do not match"
+      })
+
+    }
+  } catch (e) {
+    res.status(500).json({
+      success: false,
+      resonse: e
+    })
+  }
+})
+
+//Thoughts
+
+
+const ThoughtSchema = new Schema({
+  message: {
+    // most important one
+    type: String,
+    required: true, // required true which means the user MUST provide a message in order to send it. 
+    trim: true // removes unnecessary whitespaces from string
+  },  
+  user: {
+    type: String,
+    required: true,
+  },
+  likes: {
+    type: Number,
+    default: 0
+  },
+  createdAt: {
+    type: Date,
+    default: new Date()
+  },
+  
+}); 
+
+const Thought = mongoose.model("Thought", ThoughtSchema)
+
+//Authenticate the user
+const authenticateUser = async (req, res, next) => {
+  const accessToken = req.header("Authorization")
+  try{
+    const user = await User.findOne({accessToken: accessToken})
+    if (user) {
+      next()
+    } else {
+      res.status(401).json({
+        success: false, 
+        response: "Please login"
+      })
+    }
+  } catch(e){
+    res.status(500).json({
+      success: false,
+      response: e
+    })
+  }
+}
+
+app.get("/thoughts", authenticateUser)
+app.get("/thoughts", async (req, res) => {
+  const thoughts = await Thought.find({})
+  res.status(200).json({
+    success: true, 
+    response: thoughts
+  })
+}) // add try catch blocks
+
+app.post("/thoughts", authenticateUser)
+app.post("/thoughts", async (req, res) => {
+  const { message } = req.body;
+
+  const accessToken = req.header("Authorization");
+  const user = await User.findOne({accessToken: accessToken});
+
+  const thoughts = await new Thought({message: message, user: user._id}).save();
+
+  res.status(200).json({
+    success: true, 
+    response: thoughts
+  })
+})
+
+
+///////////////////////
+
 
 // Start the server
 app.listen(port, () => {
