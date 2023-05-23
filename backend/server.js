c1413ef6b14813d241b76b545112eabf436a3cc0
cpz-manifold <carol.pinzon.ardila@gmail.com>
2023-05-24 00:10:28 +0200

    fix: typos

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import mongoose, { Mongoose } from "mongoose";
-import crypto from "crypto"
+import crypto from "crypto";
 import bcrypt from "bcrypt";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
@@ -22,52 +22,81 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
+
 ///////////////////
-const UserSchema  = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
   password: {
     type: String,
-    required: true
+    required: true,
   },
   accessToken: {
-    // npm install crypto package 
+    // npm install crypto package
     type: String,
     // create randome numbers and letters that will be the token for out log in
-    default: () => crypto.randomBytes(128).toString("hex") 
-  }
+    default: () => crypto.randomBytes(128).toString("hex"),
+  },
 });
 
 //user model
 const User = mongoose.model("User", UserSchema);
 
-app.post ("/register", async (req, res) => {
-const { username, password } =req.body;
-try {
-  const salt = bcrypt.genSaltSync(); 
-  const newUser = await new User ({
-    username: username,
-    password: bcrypt.hashSync(password, salt) // obscure the password
-  }).save ();
-  res.status(201).json ({
- success: true,
- resposne: {
-  username: newUser.username,
-  id: newUser._id,
-  accessToken: newUser.accessToken
- }
-  })
-} catch (e) {
-  res.status(400).json ({
-    success: false,
-    resposne: e
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const salt = bcrypt.genSaltSync();
+    const newUser = await new User({
+      username: username,
+      password: bcrypt.hashSync(password, salt), // obscure the password
+    }).save();
+    res.status(201).json({
+      success: true,
+      response: {
+        username: newUser.username,
+        id: newUser._id,
+        accessToken: newUser.accessToken,
+      },
+    });
+  } catch (e) {
+    res.status(400).json({
+      success: false,
+      response: e,
+    });
+  }
 });
-}
-})
+// Login
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    // tell us if the password that user put is the same that we have in the data base
+    const user = await User.findOne({ username: username });
 
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(200).json({
+        success: true,
+        response: {
+          username: user.username,
+          id: user._id,
+          accessToken: user.accessToken,
+        },
+      });
+    } else {
+      res.status(400).json({
+        success: false,
+        response: "credentials do not match",
+      });
+    }
+  } catch (e) {
+    res.status(500).json({
+      success: false,
+      response: e,
+    });
+  }
+});
 
 ///////////////////
 // Start the server
