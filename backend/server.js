b5e0ebe5425ad509f1d7ef9b5b0c86623d43675a
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2023-05-23 16:17:19 +0200

    Add get, post for users

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import crypto from "crypto";
+import bcrypt from "bcrypt";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/auth";
+const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/auth";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
@@ -21,9 +23,170 @@ app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
 
-// Register route
+const { Schema } = mongoose;
 
-// Login route
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  accessToken: {
+    // npm install crypto
+    type: String,
+    default: () => crypto.randomBytes(128).toString("hex")
+  }
+});
+
+const User = mongoose.model("User", UserSchema);
+/// Registration
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  /// const code = [1, 2, 3, 4, 5]
+  /// const makeCodeSecret = (codeArray, salt) => {
+  /// const transformedCode = codeArray.map(singleNumber => singleNumber + salt)
+  /// return transformed array
+ /// }
+  try {
+    const salt = bcrypt.genSaltSync();
+    // Do not store plaintext passwords
+    const newUser = await new User({
+      username: username,
+      password: bcrypt.hashSync(password, salt)})
+    .save();
+    res.status(201).json({
+      success: true,
+      response: {
+        username: newUser.username,
+        id: newUser._id,
+        accessToken: newUser.accessToken
+      }
+    })
+  } catch (e) {
+    // Bad request
+    res.status(400).json({
+      success: false,
+      response: e,
+      message: 'Could not create user', 
+      errors: e.errors
+    });
+  }
+});
+/// Login
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findOne({username: username})
+    // const user = await User.findOne({username})
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.status(200).json({
+        success: true,
+        response: {
+          username: user.username,
+          id: user._id,
+          accessToken: user.accessToken
+        }
+      });
+    } else {
+      res.status(400).json({
+        success: false,
+        response: "Credentials do not match"
+      });
+    }
+  } catch (e) {
+    res.status(500).json({
+      success: false,
+      response: e
+    });
+  }
+});
+////
+// Thoughts
+
+// const ThoughtSchema = new mongoose.Schema({
+//   message: {
+//     type: String
+//   },
+//   createdAt: {
+//     type: Date,
+//     default: () => new Date()
+//   },
+//   hearts: {
+//     type: Number,
+//     default: 0
+//   },
+//   user: {
+//     type: String,
+//     require: true
+//   }
+// });
+
+// const Thought = mongoose.model("Thought", ThoughtSchema);
+
+// Authenticate the user
+const authenticateUser = async (req, res, next) => {
+  const accessToken = req.header("Authorization");
+  try {
+    const user = await User.findOne({accessToken: accessToken});
+    if (user) {
+      next();
+    } else {
+      res.status(401).json({
+        success: false,
+        response: "Please log in",
+        loggedOut: true
+      })
+    }
+  } catch (e) {
+    res.status(500).json({
+      success: false,
+      response: e
+    });
+  }
+}
+
+app.get("/secrets",authenticateUser);
+app.get("/secrets", async (req, res) => {
+  res.status(200).json({secret: 'This is a super secret message'})
+});
+
+app.post('/sessions', async (req, res) => {
+  const user = await User.findOne({username: req.body.username});
+    if (user && bcrypt.compareSync(req.body.password, user.password)){
+      res.json({ userId:user._id, accessToken: user.accessToken })
+    } else {
+      res.json({notFound: true})
+    }
+});
+
+// app.post("/thoughts",authenticateUser);
+// app.post("/thoughts", async (req, res) => {
+//   const { message } = req.body;
+//   const accessToken = req.header("Authorization");
+//   const user = await User.findOne({accessToken: accessToken});
+//   const thoughts = await new Thought({message: message, user: user._id}).save();
+//   res.status(200).json({success: true, response: thoughts})
+// });
+///////////
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
+
+
+
+// Test in postman
+
+// Post: http://localhost:8080/register
+// Post: http://localhost:8080/login
+// {
+//     "username": "Sammie",
+//     "password": "passwordx"
+// }
 
 // Authenticated endpoint
 // Return a 401 or 403 with error message if someone tries to access it without an authentication
@@ -32,7 +195,4 @@ app.get("/", (req, res) => {
 
 // localStorage.removeItem() for log out (to not lose everything when logging out)
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+
