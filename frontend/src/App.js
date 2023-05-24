a50f5dcf4b67f5b604f0a9c63f120eaa0196944a
Yu Miao <yu_miao@yahoo.com>
2023-05-24 13:41:38 +0200

    create all components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,31 @@
+import NotFound from 'component/NotFound';
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import NotFound from 'component/NotFound';
+import Main from 'component/Main';
+import Login from 'component/Login';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import user from 'reducers/user';
+import thoughts from 'reducers/thoughts';
+import { Provider } from 'react-redux';
 
 export const App = () => {
+
+  const reducer = combineReducers({
+    user: user.reducer,
+    thoughts: thoughts.reducer
+  })
+  const store = configureStore({reducer})
+
   return (
-    <div>
-      Find me in src/app.js!
-    </div>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path ='/login' element={<Login/>}></Route>
+          <Route path ='' element={<Main/>}></Route>
+          <Route path ='*' element={<NotFound/>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
---
+++ frontend/src/component/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+const Login = () => {
+    rerurn (
+        <>
+        </>
+
+    )
+}
+export default Login;
