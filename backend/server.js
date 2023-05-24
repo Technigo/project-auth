14982f744b7a2efe12d40e79355977b3b510918f
AjmalZ <ajmal.zahir@hotmail.com>
2023-05-24 21:19:37 +0200

    change

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(express.json()); // Parse JSON request bodies
 
 // Define a route for the root URL ("/")
 app.get("/", (req, res) => {
-  res.send("Hello user!"); // Send a response with the message "Hello user!"
+  res.send("Hello User!"); // Send a response with the message "Hello User!"
 });
 
 // Define the User schema for the MongoDB collection
