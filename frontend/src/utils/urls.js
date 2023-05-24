02e7e888f91383232c6fcbe5da58828f70726c70
Annaselvine <120709649+AnnaElvine@users.noreply.github.com>
2023-05-24 14:23:45 +0200

    all done

--- frontend/src/utils/urls.js
+++ frontend/src/utils/urls.js
@@ -1,3 +1,5 @@
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://project-authh-oivc6zag6a-lz.a.run.app/";
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`
+
+/* http://localhost:8080 */
