b25d424c2e7ed71b60c6bb4ee6e2ce1d5f8b52ee
Joanna Philips <66673587+Joanna-Philips@users.noreply.github.com>
2023-05-25 13:41:24 +0200

    Fixed footer alignment

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,25 +1,45 @@
 import React from "react";
-import { BottomNavigation, BottomNavigationAction, Typography, Paper} from "@mui/material";
+import { BottomNavigation, Typography, Link} from "@mui/material";
 
 export const Footer = () => {
-    return (
-    <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0, color: 'black', height: 0.06 }} elevation={3}>
-        <BottomNavigation
-        showLabels >
-        <Typography variant="body2" color="text.secondary" justifyContent="center" display="flex" alignItems="center" >Made by</Typography>
-        <BottomNavigationAction
-            label="Joanna Philips" 
-            href="https://joannaphilips.se/"
-            target="_blank"
-            rel="noreferrer"
-            alt="Joannas portfolio" />
-        <BottomNavigationAction
-            label="Leo Thunell"
-            href="https://leothunellportfoliowebdev.netlify.app/"
-            target="_blank"
-            rel="noreferrer"
-            alt="Leos portfolio" />
-        </BottomNavigation>
-    </Paper>
-    );
-}
+  return (
+    <BottomNavigation
+      sx={{
+        position: "fixed",
+        bottom: 0,
+        left: 0,
+        right: 0,
+        color: "black",
+        height: 0.06,
+      }}
+    >
+      <Typography
+        variant="body2"
+        color="text.secondary"
+        fontFamily="VT323, monospace"
+      >
+        {"Made by "}
+        <Link
+          color="inherit"
+          href="https://joannaphilips.se/"
+          target="_blank"
+          rel="noreferrer"
+          alt="Joanna's portfolio"
+        >
+          Joanna Philips
+        </Link>
+        {" & "}
+        <Link
+          color="inherit"
+          href="https://leothunellportfoliowebdev.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+          alt="Leo's portfolio"
+        >
+          Leo Thunell
+        </Link>{" "}
+        {new Date().getFullYear()}
+      </Typography>
+    </BottomNavigation>
+  );
+};
