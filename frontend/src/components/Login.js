4c0d44b560ff5f6f53bf0cedba34d8aefd76eb44
AjmalZ <ajmal.zahir@hotmail.com>
2023-05-25 11:42:48 +0200

    test

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -60,7 +60,7 @@ const Login = () => {
                     <p className="sign-up">Enter your personal details<br /> and start a journey with us</p>
                     <a
                         className={`button sign-up ${mode === 'register' ? 'active' : ''}`}
-                        href="#"
+                        href=""
                         onClick={() => setMode('register')}
                     >
                         Sign Up
