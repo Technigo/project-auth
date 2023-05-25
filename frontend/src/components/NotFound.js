c04d2a375287b3d3937ad12344402f7c609314af
Amanda <amanda.sl747@gmail.com>
2023-05-25 08:27:40 +0200

    Update NotFound.js

--- frontend/src/components/NotFound.js
+++ frontend/src/components/NotFound.js
@@ -1,5 +1,28 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+
+const Title = styled.h1`
+  font-size: 2rem;
+  margin-bottom: 1rem;
+`;
+
+const Message = styled.p`
+  font-size: 1.2rem;
+`;
 
 export const NotFound = () => {
-  return <p>Page not found</p>;
+  return (
+    <Container>
+      <Title>Page Not Found</Title>
+      <Message>The page you are looking for does not exist.</Message>
+    </Container>
+  );
 };
