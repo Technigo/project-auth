ed53abd134a0b35b3ea5f947ea766442bdfd431a
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2023-05-25 09:34:57 +0200

    Change project name

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import crypto from "crypto";
 import bcrypt from "bcrypt";
 import listEndpoints from "express-list-endpoints";
 
-const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/auth";
+const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/project-auth";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
