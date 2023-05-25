792f953b36b9df156c2db3bb40cf9a950c3acb2f
Amanda <amanda.sl747@gmail.com>
2023-05-25 08:44:10 +0200

    Reducers update
    Added reducer "user" and updated App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,26 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Main } from "components/Main";
 import { Login } from "components/Login";
 import { NotFound } from "components/NotFound";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { user } from "./reducers/user";
+import { Provider } from "react-redux";
+
+const reducer = combineReducers({
+  user: user.reducer,
+});
+
+const store = configureStore({ reducer });
 
 export const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Main />}></Route>
-        <Route path="/login" element={<Login />}></Route>
-        <Route path="*" element={<NotFound />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 };
