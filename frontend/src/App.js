9b12632a873e8a74e79c5d7f73f8108bcc9d12dd
Yu Miao <yu_miao@yahoo.com>
2023-05-25 14:32:08 +0200

    Main-Login component done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
-import NotFound from 'component/NotFound';
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Main from 'component/Main';
-import Login from 'component/Login';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NotFound from 'components/NotFound';
+import Main from 'components/Main';
+import Login from 'components/Login';
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import user from 'reducers/user';
 import thoughts from 'reducers/thoughts';
@@ -13,16 +13,16 @@ export const App = () => {
   const reducer = combineReducers({
     user: user.reducer,
     thoughts: thoughts.reducer
-  })
+  });
   const store = configureStore({reducer})
 
   return (
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
-          <Route path ='/login' element={<Login/>}></Route>
-          <Route path ='' element={<Main/>}></Route>
-          <Route path ='*' element={<NotFound/>}></Route>
+          <Route path='/login' element={<Login/>}> </Route>
+          <Route path='/' element={<Main/>}> </Route>
+          <Route path='*' element={<NotFound/>}> </Route>
         </Routes>
       </BrowserRouter>
     </Provider>
