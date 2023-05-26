3f8fb04f0c882a5f9c8a20c5941c336fed515aab
Ed Code <edvard.ekstrom@gmail.com>
2023-05-26 16:49:13 +0200

    changed api url

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,7 +4,7 @@ import Profile from "./Profile"
 
 const token = localStorage.getItem('token');
 
-const API_URL = process.env.API_URL || 'http://127.0.0.1:8080'
+const API_URL = process.env.API_URL || 'https://project-auth-hboqolcftq-uc.a.run.app/'
 
 const Home = () => {
 
