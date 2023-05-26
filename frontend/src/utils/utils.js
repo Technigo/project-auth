57231b7cce285e4e1d3ccb818e00aa4606e1e957
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2023-05-26 18:27:20 +0200

    Delete /

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,3 +1,3 @@
-const BASE_URL = "https://project-auth-c7x23fdnsa-lz.a.run.app/";
+const BASE_URL = "https://project-auth-c7x23fdnsa-lz.a.run.app";
 
 export const API_URL = (param) => `${BASE_URL}/${param}`;
