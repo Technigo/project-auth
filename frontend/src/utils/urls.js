8f2f41e5c6befcbc2d83837ca0c8e4f967c9d595
Sammy Olsson <sammy.olsson@gmail.com>
2023-05-26 22:23:39 +0200

    Trying to change BASE URL

--- frontend/src/utils/urls.js
+++ frontend/src/utils/urls.js
@@ -1,3 +1,3 @@
-const BASE_URL = "http://localhost:8080"
+const BASE_URL = "https://project-auth-5g4lrfrwlq-lz.a.run.app" // http://localhost:8080"
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`
