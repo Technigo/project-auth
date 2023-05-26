fd10def904751440307a30b294801dce52873b2a
Andreas Axelsson <pererikandreas@gmail.com>
2023-05-26 19:15:32 +0200

    added css

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -3,112 +3,186 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { user } from "reducers/user";
 import { API_URL } from "utils/urls";
+import "./Login.css";
 
 export const Login = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [mode, setMode] = useState("login");
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const accessToken = useSelector((store) => store.user.accessToken);
+    // State variables for login and registration
+    const [loginUsername, setLoginUsername] = useState("");
+    const [loginPassword, setLoginPassword] = useState("");
+    const [registerUsername, setRegisterUsername] = useState("");
+    const [registerPassword, setRegisterPassword] = useState("");
+    const [mode, setMode] = useState("login");
 
-  useEffect(() => {
-    if (accessToken) {
-      navigate("/");
-    }
-  }, [accessToken, navigate]);
+    // Redux hooks
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const accessToken = useSelector((store) => store.user.accessToken);
 
-  const onFormSubmit = (event) => {
-    event.preventDefault();
-    const options = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ username: username, password: password }),
+    // Check if user is already logged in, then redirect to home page
+    useEffect(() => {
+        if (accessToken) {
+            navigate("/");
+        }
+    }, [accessToken]);
+
+    // Handle login form submission
+    const onLoginFormSubmit = (event) => {
+        event.preventDefault();
+
+        // Prepare request options
+        const options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ username: loginUsername, password: loginPassword }),
+        };
+
+        // Send login request to the server
+        fetch(API_URL(mode), options)
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.success) {
+                    // Update user state with access token, username, and user ID
+                    dispatch(user.actions.setAccessToken(data.response.accessToken));
+                    dispatch(user.actions.setUsername(data.response.username));
+                    dispatch(user.actions.setUserId(data.response.id));
+                    dispatch(user.actions.setError(null));
+                    navigate("/"); // Redirect to home page
+                } else {
+                    // Handle login error
+                    dispatch(user.actions.setAccessToken(null));
+                    dispatch(user.actions.setUsername(null));
+                    dispatch(user.actions.setUserId(null));
+                    dispatch(user.actions.setError(data.response));
+                }
+            });
     };
 
-    fetch(API_URL(mode), options)
-      .then((response) => response.json())
-      .then((data) => {
-        if (data.success) {
-          console.log(data);
-          dispatch(user.actions.setAccessToken(data.response.accessToken));
-          dispatch(user.actions.setUsername(data.response.username));
-          dispatch(user.actions.setUserId(data.response.id));
-          dispatch(user.actions.setError(null));
-        } else {
-          dispatch(user.actions.setAccessToken(null));
-          dispatch(user.actions.setUsername(null));
-          dispatch(user.actions.setUserId(null));
-          dispatch(user.actions.setError(data.response));
-        }
-      });
-  };
+    // Handle registration form submission
+    const onRegisterFormSubmit = (event) => {
+        event.preventDefault();
 
-  return (
-    <form className="form" onSubmit={onFormSubmit}>
-      <p id="heading">Login</p>
-      <div className="field">
-        <svg
-          className="input-icon"
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="currentColor"
-          viewBox="0 0 16 16"
-        >
-          <path d="M13.106 7.222c0-2.967-2.249-5.032-5.482-5.032-3.35 0-5.646 2.318-5.646 5.702 0 3.493 2.235 5.708 5.762 5.708.862 0 1.689-.123 2.304-.335v-.862c-.43.199-1.354.328-2.29.328-2.926 0-4.813-1.88-4.813-4.798 0-2.844 1.921-4.881 4.594-4.881 2.735 0 4.608 1.688 4.608 4.156 0 1.682-.554 2.769-1.416 2.769-.492 0-.772-.28-.772-.76V5.206H8.923v.834h-.11c-.266-.595-.881-.964-1.6-.964-1.4 0-2.378 1.162-2.378 2.823 0 1.737.957 2.906 2.379 2.906.8 0 1.415-.39 1.709-1.087h.11c.081.67.703 1.148 1.503 1.148 1.572 0 2.57-1.415 2.57-3.643zm-7.177.704c0-1.197.54-1.907 1.456-1.907.93 0 1.524.738 1.524 1.907S8.308 9.84 7.371 9.84c-.895 0-1.442-.725-1.442-1.914z"></path>
-        </svg>
-        <input
-          className="input"
-          type="text"
-          name="username"
-          placeholder="Username"
-          autoComplete="off"
-          value={username}
-          onChange={(event) => setUsername(event.target.value)}
-          required
-        />
-      </div>
-      <div className="field">
-        <svg
-          className="input-icon"
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          fill="currentColor"
-          viewBox="0 0 16 16"
-        >
-          <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z"></path>
-        </svg>
-        <input
-          className="input"
-          type="password"
-          name="password"
-          placeholder="Password"
-          autoComplete="off"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-          required
-        />
-      </div>
-      <div className="btn">
-        <button
-          className="button1"
-          type="submit"
-          onClick={() => setMode("login")}
-        >
-          Login
-        </button>
-        <button
-          className="button2"
-          type="submit"
-          onClick={() => setMode("register")}
-        >
-          Sign Up
-        </button>
-      </div>
-    </form>
-  );
+        // Prepare request options
+        const options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ username: registerUsername, password: registerPassword }),
+        };
+
+        // Send registration request to the server
+        fetch(API_URL("register"), options)
+            .then((response) => response.json())
+            .then((data) => {
+                if (data.success) {
+                    // Update user state with access token, username, and user ID
+                    dispatch(user.actions.setAccessToken(data.response.accessToken));
+                    dispatch(user.actions.setUsername(data.response.username));
+                    dispatch(user.actions.setUserId(data.response.id));
+                    dispatch(user.actions.setError(null));
+                    navigate("/"); // Redirect to home page
+                } else {
+                    // Handle registration error
+                    dispatch(user.actions.setAccessToken(null));
+                    dispatch(user.actions.setUsername(null));
+                    dispatch(user.actions.setUserId(null));
+                    dispatch(user.actions.setError(data.response));
+                }
+            });
+    };
+
+    // Handle login button click
+    const handleLoginButtonClick = () => {
+        setMode("login");
+    };
+
+    // Handle register button click
+    const handleRegisterButtonClick = () => {
+        setMode("register");
+    };
+
+    return (
+        <div className="loginContainer">
+            <div className="main">
+                <input type="checkbox" id="chk" aria-hidden="true" />
+
+                {/* Login form */}
+                <div className="login">
+                    <form className="form" onSubmit={onLoginFormSubmit}>
+                        <label htmlFor="chk" aria-hidden="true">
+                            Log in
+                        </label>
+                        <input
+                            className="input"
+                            type="text"
+                            name="username"
+                            placeholder="Username"
+                            autoComplete="off"
+                            value={loginUsername}
+                            onChange={(event) => setLoginUsername(event.target.value)}
+                            required
+                        />
+                        <input
+                            className="input"
+                            type="password"
+                            name="password"
+                            placeholder="Password"
+                            autoComplete="off"
+                            value={loginPassword}
+                            onChange={(event) => setLoginPassword(event.target.value)}
+                            required
+                        />
+                        <button className="submitBtn" type="submit" value="Sign In">
+                            Sign In
+                        </button>
+                    </form>
+                </div>
+
+                {/* Registration form */}
+                <div className="register">
+                    <form className="form" onSubmit={onRegisterFormSubmit}>
+                        <label htmlFor="chk" aria-hidden="true">
+                            Register
+                        </label>
+                        <input
+                            className="input"
+                            type="text"
+                            name="username"
+                            placeholder="Username"
+                            autoComplete="off"
+                            value={registerUsername}
+                            onChange={(event) => setRegisterUsername(event.target.value)}
+                            required
+                        />
+                        <input
+                            className="input"
+                            type="password"
+                            name="password"
+                            placeholder="Password"
+                            autoComplete="off"
+                            value={registerPassword}
+                            onChange={(event) => setRegisterPassword(event.target.value)}
+                            required
+                        />
+                        <button className="submitBtn" type="submit" value="Register">
+                            Register
+                        </button>
+                    </form>
+                </div>
+
+                {/* Switch between login and register forms */}
+                <div className="switchButtons">
+                    <button className="switchBtn" type="button" onClick={handleLoginButtonClick}>
+                        Log in
+                    </button>
+                    <button className="switchBtn" type="button" onClick={handleRegisterButtonClick}>
+                        Register
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+
 };
