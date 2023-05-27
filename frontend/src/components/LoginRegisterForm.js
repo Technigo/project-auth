3e52be2b6b2acc889c0b1154f8f7a4a9917fd0aa
SandraMadeleine <sandramohlsson@yahoo.com>
2023-05-27 10:09:10 +0200

    style in profile

--- frontend/src/components/LoginRegisterForm.js
+++ frontend/src/components/LoginRegisterForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import styled from "styled-components"
+import styled from "styled-components";
 
 const LoginRegisterForm = ({ state, name, setName, email, setEmail, password, setPassword, handleFormSubmit }) => {
 
--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 
 const token = localStorage.getItem("token");
 
@@ -28,15 +29,46 @@ const Profile = ({ API_URL }) => {
 
   return (
     <>
- 
+      <ProfileContainer>
       {responseData.user  && (
         <h1>
           Hi {responseData.user.name}, {responseData.secret}
         </h1>
       )}
-      <button type="button" onClick={handleLogout}>Logout</button>
+      <ProfileButton type="button" onClick={handleLogout}>Logout</ProfileButton>
+      </ProfileContainer>
     </>
   );
 };
 
 export default Profile;
+
+const ProfileContainer = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+
+h1 {
+  font-size: 3rem;
+  color: #ffb140;
+}
+`
+
+const ProfileButton = styled.button`
+border-radius: 5px;
+border: 1px solid #ffffff;
+cursor: pointer;
+width: 7rem;
+background-color: #a7cdbd;
+color: #ffb140;
+font-size: 1rem;
+font-weight: bold;
+margin-top: 1.5rem;
+padding: 0.8rem;
+
+@media (max-width: 664px) {
+  border-radius: 10px;
+  padding: 0.3rem;
+  }
+`;
