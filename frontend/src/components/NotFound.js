4c8f4d49c97da0d59eee91960508a3c3d7d0d6b4
Nora Terbocs <nora.terbocs@gmail.com>
2023-05-27 13:33:50 +0200

    Added animation to not found page

--- frontend/src/components/NotFound.js
+++ frontend/src/components/NotFound.js
@@ -1,24 +1,32 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import styled from 'styled-components/macro';
 import { Container } from '@mui/material';
+import { Player } from '@lottiefiles/react-lottie-player';
+import HomeIcon from '@mui/icons-material/Home';
+import notFoundAnimation from '../assets/notFoundAnimation.json';
 
 export const NotFound = () => {
   return (
     <Container>
-      <Link to="/">Go Home</Link>
-      <BackgroundImg alt="Page not found" />
-      {/* <Typography variant="h1">404 - Looks like you are lost.</Typography> */}
+      <Link to="/"><HomeIcon fontSize="large" /></Link>
+      <Player
+        loop
+        autoplay
+        className="loading"
+        src={notFoundAnimation}
+        speed={1}
+        style={{
+          position: 'absolute',
+          zIndex: '10',
+          height: '300px',
+          width: '300px',
+          top: '50%',
+          left: '0',
+          right: '0',
+          margin: 'auto',
+          transform: 'translateY(-50%)'
+        }}
+        background="transparent" />
     </Container>
   )
 }
-
-const BackgroundImg = styled.div`
-    background-size:cover;
-    width:100%;
-    height:100%;
-    background-repeat: no-repeat;
-    position: absolute;
-    top: 0;
-    z-index: -1;
-`
