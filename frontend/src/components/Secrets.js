0bb7402362bc9c17342ef9b4651f51c4aa14cdf1
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2023-05-27 11:43:24 +0200

    Add log out button

--- frontend/src/components/Secrets.js
+++ frontend/src/components/Secrets.js
@@ -1,18 +1,20 @@
 import React, { useEffect, useState } from 'react';
-import styled  from "styled-components";
-import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+import { useDispatch, useSelector } from 'react-redux';
+import user from '../reducers/user';
+import { batch } from 'react-redux';
 import { API_URL } from '../utils/utils';
 
-const OuterWrapper = styled.div `
+const OuterWrapper = styled.div`
   display: flex;
   height: 100vh;
   width: 100%;
   justify-content: center;
   text-align: center;
-  
-`
+`;
 
 const Secrets = () => {
+  const dispatch = useDispatch();
   const accessToken = useSelector((store) => store.user.accessToken);
   const [secretMessage, setSecretMessage] = useState(null);
 
@@ -38,13 +40,29 @@ const Secrets = () => {
       });
   }, [accessToken]);
 
+  const logOutButton = () => {
+    // Clear user data from the Redux store
+    batch(() => {
+      dispatch(user.actions.setAccessToken(null));
+      dispatch(user.actions.setUsername(null));
+      dispatch(user.actions.setError(null));
+    });
+
+    // Clear the persisted user data from localStorage
+    localStorage.removeItem('persist:root');
+
+    // Reload the page or perform any necessary actions after logging out
+    // For example: window.location.reload();
+  };
+
   return (
     <OuterWrapper>
-    <div>
-      <h1>This is a Secret Page</h1>
-      {/* <p>{secretMessage}</p> */}
-      <p>This is the secret page! Woop woop</p>
-    </div>
+      <div>
+        <h1>This is a Secret Page</h1>
+        {/* <p>{secretMessage}</p> */}
+        <p>This is the secret page! Woop woop</p>
+        <button onClick={logOutButton}>Log Out</button>
+      </div>
     </OuterWrapper>
   );
 };
