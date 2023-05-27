5a107882938e384187546309224269e2a5666345
Nora Terbocs <nora.terbocs@gmail.com>
2023-05-27 13:09:16 +0200

    Added loading spinner to form button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,32 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import NotFound from 'components/NotFound';
-import Main from 'components/Main';
-import Login from 'components/Login';
+import { NotFound } from 'components/NotFound';
+import { Main } from 'components/Main';
+import { Login } from 'components/Login';
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import {user} from 'components/reducers/user';
-import score from 'components/reducers/leaderboard';
+import { user } from 'components/reducers/user';
 import { Provider } from 'react-redux';
 import { loading } from 'components/reducers/loading';
-import leaderboard from 'components/reducers/leaderboard';
-import { Loading } from 'components/Loading';
-
+import { leaderboard } from 'components/reducers/leaderboard';
 
 export const App = () => {
-
   const reducer = combineReducers({
     user: user.reducer,
     leaderboard: leaderboard.reducer,
-    loading: loading.reducer,
+    loading: loading.reducer
 
   });
-  const store = configureStore({reducer})
+  const store = configureStore({ reducer })
 
   return (
-  <Provider store={store}>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/login' element={<Login />}></Route>
-        <Route path='/' element={<Main />}></Route>
-        <Route path='*' element={<NotFound />}></Route>
-      </Routes>
-    </BrowserRouter>
- </Provider>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Main />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
--- frontend/src/components/reducers/leaderboard.js
+++ frontend/src/components/reducers/leaderboard.js
@@ -1,21 +1,18 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
-const leaderboard = createSlice({
-    name:"leaderboard",
-    initialState:{
-        items:[],
-        leaderboard:'',
-        error:null
+export const leaderboard = createSlice({
+  name: 'leaderboard',
+  initialState: {
+    items: [],
+    leaderboard: '',
+    error: null
+  },
+  reducers: {
+    setError: (store, action) => {
+      store.error = action.payload
     },
-    reducers:{
-        setError:(store, action) =>{
-            store.error = action.payload
-        },
-        setLeaderboard:(store, action) =>{
-            store.leaderboard = action.payload
-        }
+    setLeaderboard: (store, action) => {
+      store.leaderboard = action.payload
     }
+  }
 });
-
-
-export default leaderboard
