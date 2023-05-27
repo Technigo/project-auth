4b4257b8a9330cda2e739a770f753325fff88767
smirrebinx <48405465+smirrebinx@users.noreply.github.com>
2023-05-27 10:12:04 +0200

    Add NotFound

---
+++ frontend/src/components/NotFound.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NotFound = () => {
+
+    return(
+        <>
+            <p>
+                <Link to="/login">Go to Login</Link>
+            </p>
+        </>
+    );
+}
+
+export default NotFound;
