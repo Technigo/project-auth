7cd78ee4b3aea00a55bb937e7dc7fb1b4d6bc5f4
camcron <camillacronqvist@hotmail.com>
2023-05-27 16:29:12 +0200

    Added more styling and a flipcard in /secrets

--- frontend/src/components/Startpage.js
+++ frontend/src/components/Startpage.js
@@ -4,20 +4,21 @@ import { StyledButton } from "./Styled components/StyledButton";
 import { StyledBox } from "./Styled components/StyledBox";
 import { StyledButtonContainer } from "./Styled components/StyledButtonContainer";
 import { MainContainer } from "./Styled components/MainContainer";
+import { StyledLink } from "./Styled components/StyledButton";
 
 export const Startpage = () => {
     return (
         <MainContainer imageUrl="https://images.unsplash.com/photo-1483706600674-e0c87d3fe85b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1814&q=80">
             <StyledBox>
-                <h1>Do yo wanna know a secret?</h1>
+                <h1>Do yo want to know a secret?</h1>
                 <StyledButtonContainer>
-                    <Link to="/register">
+                    <StyledLink to="/register">
                         <StyledButton variant="outlined">REGISTER</StyledButton>
-                    </Link>
+                    </StyledLink>
 
-                    <Link to="/login">
+                    <StyledLink to="/login">
                         <StyledButton variant="outlined">LOGIN</StyledButton>
-                    </Link>
+                    </StyledLink>
                 </StyledButtonContainer>
             </StyledBox>
         </MainContainer>  
