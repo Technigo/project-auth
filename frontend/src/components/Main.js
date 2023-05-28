b92fd68c7b37b1ee7acbf5bc204cc37b2c97c993
AjmalZ <ajmal.zahir@hotmail.com>
2023-05-28 02:14:16 +0200

    added styling

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -5,6 +5,7 @@ import { thoughts } from 'reducers/thoughts';
 import { API_URL } from 'utils/urls';
 import { user } from 'reducers/user';
 import { StickyNotes } from './StickyNotes';
+import './Main.css';
 
 export const Main = () => {
     const thoughtItems = useSelector((store) => store.thoughts.items);
@@ -76,10 +77,11 @@ export const Main = () => {
 
     return (
         <>
-            <button type="button" onClick={onLogoutButtonClick}>
-                LOGOUT
+            <button className="logOutBtn" type="button" onClick={onLogoutButtonClick}>
+                <div class="sign"><svg viewBox="0 0 512 512"><path d="M377.9 105.9L500.7 228.7c7.2 7.2 11.3 17.1 11.3 27.3s-4.1 20.1-11.3 27.3L377.9 406.1c-6.4 6.4-15 9.9-24 9.9c-18.7 0-33.9-15.2-33.9-33.9l0-62.1-128 0c-17.7 0-32-14.3-32-32l0-64c0-17.7 14.3-32 32-32l128 0 0-62.1c0-18.7 15.2-33.9 33.9-33.9c9 0 17.6 3.6 24 9.9zM160 96L96 96c-17.7 0-32 14.3-32 32l0 256c0 17.7 14.3 32 32 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32l-64 0c-53 0-96-43-96-96L0 128C0 75 43 32 96 32l64 0c17.7 0 32 14.3 32 32s-14.3 32-32 32z"></path></svg></div>
+                <div className='logOutText'>LOG OUT</div>
             </button>
-            {username ? <h2>THESE ARE THE THOUGHTS OF {username.toUpperCase()}</h2> : ''}
+            {username ? <h1>THESE ARE THE THOUGHTS OF {username.toUpperCase()}</h1> : ''}
             <div className='sticky-notes-container'>
                 <form className='messageForm' onSubmit={postNewThought}>
                     <input
@@ -88,7 +90,7 @@ export const Main = () => {
                         onChange={(e) => setMessage(e.target.value)}
                         placeholder="Enter your message"
                     />
-                    <button type="submit">Post Message</button>
+                    <button className="postBtn" type="submit">Post Message</button>
                 </form>
                 <StickyNotes thoughts={thoughtItems} />
             </div>
