b757f0bff984ba0308015dd7ac265f7a584c297a
Sara <sara@MBP-di-Sara.homenet.telecomitalia.it>
2023-05-28 20:10:24 +0200

    fixed endpoints bug

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ mongoose.set('debug', true);
 // PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
+const listEndpoints = require('express-list-endpoints');
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
