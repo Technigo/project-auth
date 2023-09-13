ac7a0b02c848dcfde9cde8f6670545912c1dcf0b
Johanna <120407185+JohLeo@users.noreply.github.com>
2023-09-13 11:42:23 +0200

    Update urls.js

--- frontend/src/utils/urls.js
+++ frontend/src/utils/urls.js
@@ -1,3 +1,3 @@
-const BASE_URL = "https://project-auth-saosbqsgza-ew.a.run.app";
+const BASE_URL = "https://project-auth-kygdlvpbnq-lz.a.run.app";
 
 export const API_URL = (slug) => `${BASE_URL}/${slug}`;
