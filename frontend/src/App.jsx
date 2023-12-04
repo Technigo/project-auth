0d119f408cef87f07f8e504a9f98c0abeec6e7c0
Anna Robertsson <robertsson_anna@hotmail.com>
2023-12-04 23:48:54 +0100

    initial setup frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,13 @@
+// export const App = () => {
+//   return <div>Find me in src/app.jsx!</div>;
+// };
+
 export const App = () => {
-  return <div>Find me in src/app.jsx!</div>;
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <Register />
+      <Login />
+    </div>
+  );
 };
