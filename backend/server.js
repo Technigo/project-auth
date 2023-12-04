74afd3d9d13f097faa98c1dcf5036f9c52198901
Inna Kokic <inna@Innas-MacBook-Pro-2.local>
2023-12-04 18:55:38 +0100

    Added endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,50 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import crypto from "crypto";
+//import bcrypt from "bcryptjs"; //? 
+
+//Har kört npm i och install mongoose && bcryptjs
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
+
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+    unique: true
+  },
+  email: {
+    type: String,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  accessToken: {
+    type: String,
+    default: () => crypto.randomBytes(128).toString('hex')
+  }
+});
+
+const authenticateUser = async (req, res, next) => {
+  const user = await User.findOne({accessToken: req.header('Authorization')});
+  if (user) { 
+    req.user = user;
+    next();
+  } else {
+    res.status(400).json({loggedOut: true});
+  }
+};
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
+var bcrypt = require('bcryptjs'); //Hämtat från https://www.npmjs.com/package/bcryptjs som var länkad i technigos material
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
@@ -19,6 +53,32 @@ app.use(express.json());
 // Start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
+});
+
+app.post('/users', async (req, res) => {
+  try {
+    const {name, email, password} = req.body;
+    const user = new User({name, email, password: bcrypt.hashSync(password)});
+    user.save();
+    res.status(201).json({id: user._id, accessToken: user.accessToken})
+  }
+  catch (err) {
+    res.status(400).json({message: 'Could not create user', errors: err.errors});
+  }
+})
+
+app.get("/secrets", authenticateUser);
+app.get("/secrets", (req, res) => {
+  res.json({secret: "Super secret message"})
+});
+
+app.post('/sessions', async (req, res) => {
+  const user = await User.findOne({email: req.body.email});
+  if (user && bcrypt.compareSync(req.body.password , user.password)){
+    res.json({userId: user._id, accessToken: user.accessToken});
+  } else {
+    res.json({notFound:true});
+  }
 });
 
 // Start the server
