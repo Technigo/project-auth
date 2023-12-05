08f8ce5a774416dc6e935b78a72dfe73da731c8b
So Youn Choi <manghatuhyo@gmail.com>
2023-12-05 13:01:31 +0100

    testing input field for register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,30 @@
+import { useState } from "react";
+
 export const App = () => {
-  return <div>Find me in src/app.jsx!</div>;
+  const [inputID, setInputID] = useState('')
+
+  const postID = async () => {
+    try {
+      const response = await fetch('https://one8-y5ov.onrender.com/users', {
+        method: 'POST',
+        // do i need headers? what for?
+        headers: {
+          'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify({ data: inputID })
+      }) 
+    } catch (error) {
+      console.error('error:', error)
+    }
+  }
+
+  const handleInputChange = (event) => {
+    setInputID(event.target.value)
+  }
+
+  const handleButtonClick = () => {
+    // trigger the POST request when the button is clicked
+    postID()
+  }
+  return <div><input type="text" value={inputID} onChange={handleInputChange} /><button onClick={handleButtonClick}>submit</button></div>;
 };
