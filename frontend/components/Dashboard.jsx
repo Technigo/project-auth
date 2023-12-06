1999f1d2ebc7b10e6c67b4e79e9a823cd2a36e08
Malin <malinlundhe@hotmail.com>
2023-12-06 21:52:22 +0100

    trying to access logged in data

--- frontend/components/Dashboard.jsx
+++ frontend/components/Dashboard.jsx
@@ -1,15 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { Container, Header, StyledButton, Paragraph } from './StyledComponents';
+import { useNavigate } from 'react-router-dom';
 
 const API_CONTENT_URL = 'http://localhost:8080/api/users/content'; // Adjust the endpoint if necessary!!!
 
 const Dashboard = ({ user, setToken }) => {
   const [content, setContent] = useState('');
+  const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
 
   useEffect(() => {
-    console.log('User in Dashboard:', user);
     const fetchContent = async () => {
       const token = localStorage.getItem('token');
+      console.log('Token:', token)
       try {
         const response = await fetch(API_CONTENT_URL, {
           headers: {
@@ -23,6 +26,8 @@ const Dashboard = ({ user, setToken }) => {
         setContent(data.message); // Update this line based on your actual response structure
       } catch (error) {
         console.error('Error fetching content:', error);
+      } finally {
+        setLoading(false);
       }
     };
 
@@ -32,14 +37,16 @@ const Dashboard = ({ user, setToken }) => {
   const handleSignOut = () => {
     setToken(null);
     localStorage.removeItem('token');
+    navigate('/')
   };
 
   return (
     <Container>
-      <Header>Welcome, {user?.username}</Header>
-      {user && (
+      <Header>
+        {loading ? 'Loading...' : `Welcome, ${user?.username || 'Guest'}`}
+      </Header>
+      {!loading && (
         <>
-      
       <Paragraph>{content || "Loading content..."}</Paragraph>
       <StyledButton onClick={handleSignOut}>Sign Out</StyledButton>
       </>
