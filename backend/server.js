38b0bcb2536415406595b0b3b830630be73ac76d
FridafridaL <fridus.lindskog@gmail.com>
2023-12-06 10:36:10 +0100

    destructure

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,8 @@ import crypto from "crypto";
 import dotenv from "dotenv";
 import bcrypt from "bcrypt-nodejs";
 //import listEndpoints from "express-list-endpoints";
-import { User } from "../backend/models/user";
+//import { User } from "../backend/models/user";
+import { routes } from "../backend/routes/routes";
 
 dotenv.config();
 
@@ -13,34 +14,15 @@ const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-auth";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
 
-// const User = mongoose.model("User", {
-//   name: {
-//     type: String,
-//     unique: true,
-//   },
-//   email: {
-//     type: String,
-//     unique: true,
-//   },
-//   password: {
-//     type: String,
-//     required: true,
-//   },
-//   accessToken: {
-//     type: String,
-//     default: () => crypto.randomBytes(128).toString("hex"),
-//   },
-// });
-
-const authenticateUser = async (req, res, next) => {
-  const user = await User.findOne({ accessToken: req.header("Authorization") });
-  if (user) {
-    req.user = user;
-    next();
-  } else {
-    res.status(401).json({ loggedOut: true });
-  }
-};
+// const authenticateUser = async (req, res, next) => {
+//   const user = await User.findOne({ accessToken: req.header("Authorization") });
+//   if (user) {
+//     req.user = user;
+//     next();
+//   } else {
+//     res.status(401).json({ loggedOut: true });
+//   }
+// };
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -51,6 +33,7 @@ const app = express();
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
+app.use(routes);
 // app.use(express.urlencoded({ extended: false }));
 // app.use((req, res, next) => {
 //   if (mongoose.connection.readyState === 1) {
@@ -59,40 +42,6 @@ app.use(express.json());
 //     res.status(503).json({ error: "Service unavailable" });
 //   }
 // });
-
-const listEndpoints = require("express-list-endpoints");
-
-//routes
-app.get("/", (req, res) => {
-  res.send(listEndpoints(app));
-});
-
-app.post("/users", async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
-    const user = new User({ name, email, password: bcrypt.hashSync(password) });
-    await user.save();
-    res.status(201).json({ id: user._id, accessToken: user.accessToken });
-  } catch (err) {
-    res
-      .status(400)
-      .json({ message: "Could not create user", errors: err.errors });
-  }
-});
-
-app.get("/secrets", authenticateUser);
-app.get("/secrets", (req, res) => {
-  res.json({ secret: "This is super secret message." });
-});
-
-app.post("/sessions", async (req, res) => {
-  const user = await User.findOne({ email: req.body.email });
-  if (user && bcrypt.compareSync(req.body.password, user.password)) {
-    res.json({ userId: user._id, accessToken: user.accessToken });
-  } else {
-    res.json({ notFound: true });
-  }
-});
 
 // Start the server
 app.listen(port, () => {
