a83fcc161f89ed6dfdde9be3b31f2183c94f518e
nguyenvuhang@gmail.com <nguyenvuhang@gmail.com>
2023-12-07 22:27:04 +0100

    set up frontend structure

---
+++ frontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import { Home } from "../pages/Home";
+import { LandingPage } from "../pages/LandingPage";
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<Home />} />
+    </Routes>
+  )
+}
