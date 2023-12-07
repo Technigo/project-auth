0c83711a91a27e4f6fe151aa95a4b170390b45d7
Isa Robertini <robertiniisa@gmail.com>
2023-12-07 16:42:36 +0100

    changed

--- backend/server.js
+++ backend/server.js
@@ -75,23 +75,39 @@ app.post('/users', async (req, res) => {
     res.status(400).json({ message: 'Could not create user', errors: err.errors })
   }
 })
+const secrets = [
+  'I have a hidden talent for juggling flaming torches while riding a unicycle.',
+  'Every Friday night, I secretly host a karaoke party for my collection of talking houseplants.',
+  'I once saved the day by using my expert knowledge of obscure trivia during a pub quiz.',
+  'My guilty pleasure is binge-watching cheesy 80s sitcoms in my pajamas while eating ice cream straight from the carton.',
+  'I have a secret stash of chocolate hidden in my sock drawer that I only indulge in when nobody is watching.',
+  'I am a closet poet and have written a collection of love poems dedicated to my favorite type of pizza.',
+  'I have a pet rock named Rocky, and we have deep philosophical conversations when no one is around.',
+  'I am convinced that I have a secret admirer who leaves mysterious notes in my mailbox, but I have never found out who it is.',
+  'I can speak fluent pig Latin and often use it to have private conversations in crowded places.',
+  'I believe in the existence of a parallel universe where everyone communicates through interpretive dance.'
+];
 
 // Protected endpoint, accessible only when the user is logged in
 app.get('/secrets', authenticateUser);
 app.get('/secrets', (req, res) => {
   // The authenticateUser middleware ensures that only logged-in users reach this point
-  res.json({ secret: 'this is a secret message :-)' });
+  // Randomly select a secret from the array
+  const randomSecret = secrets[Math.floor(Math.random() * secrets.length)];
+  res.json({ secret: randomSecret });
 });
 
 
 app.post('/sessions', async (req, res) => {
-  const user = await User.findOne({ email: req.body.email });
-  if (user && bcrypt.compareSync(req.body.password, user.password)) {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user && bcrypt.compareSync(password, user.password)) {
     res.json({ userId: user._id, accessToken: user.accessToken, name: user.name });
   } else {
-    res.json({ notFound: true });
+    res.status(401).json({ error: 'Invalid email or password' });
   }
-})
+});
 // Start the server 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
