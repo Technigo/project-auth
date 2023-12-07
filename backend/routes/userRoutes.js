668ea014211bff71899d5f4398f5c249daaaf24a
JuliaHolm <juliat.holm@gmail.com>
2023-12-07 16:58:45 +0100

    added register route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -11,12 +11,12 @@ const router = express.Router();
 const listEndpoints = require("express-list-endpoints");
 
 // Function to generate a JWT token for user authentication.
-// const generateToken = (user) => {
-//   // Generate a JWT token containing the user's unique ID, with an optional secret key and a 24-hour expiration time.
-//   return jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-//     expiresIn: "24h", // Token expires in 24 hours.
-//   });
-// };
+const generateToken = (user) => {
+  // Generate a JWT token containing the user's unique ID, with an optional secret key and a 24-hour expiration time.
+  return jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "24h", // Token expires in 24 hours.
+  });
+};
 
 // ----- ROUTES STARTS HERE -----
 
@@ -33,10 +33,55 @@ router.post(
   asyncHandler(async (req, res) => {
     // Extract email, username and password from the request body
     const { username, password, email } = req.body;
+
+    try {
+      if (!username || !password || !email) {
+        res.status(400);
+        throw new Error("Please add all fields");
+      }
+      const existingUser = await UserModel.findOne({
+        $or: [{ username }, { email }],
+      });
+      if (existingUser) {
+        res.status(400);
+        throw new Error(
+          `user with ${
+            existingUser.username === username ? "username" : "email"
+          } already exists`
+        );
+      }
+      const salt = bcrypt.genSaltSync(10);
+      const hashedPassword = bcrypt.hashSync(password, salt);
+      const newUser = new UserModel({
+        username,
+        email,
+        password: hashedPassword,
+      });
+      await newUser.save();
+      res.status(201).json({
+        success: true,
+        response: {
+          username: newUser.username,
+          email: newUser.email,
+          id: newUser._id,
+          accessToken: newUser.accessToken,
+        },
+      });
+    } catch (e) {
+      // Handle any errors that occur during the registration process
+      res.status(500).json({ success: false, response: e.message });
+    }
   })
 );
 
 // SIGN-IN ROUTE ENDPOINT - authenticate a returning user.
+router.post(
+  "/login",
+  asyncHandler(async (req, res) => {
+    // Extract email, username and password from the request body
+    const { username, password } = req.body;
+  })
+);
 
 // AUTHENTICATED ENDPOINT - only returns content if the Authorization header with the user's token was correct.
 // Return a 401 or 403 with an error message if you try to access it without an Authentication access token or with an invalid token.
