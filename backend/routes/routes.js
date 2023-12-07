6911c5e35e704e232c0af9b182ee2606fb2fe7ae
FridafridaL <fridus.lindskog@gmail.com>
2023-12-07 10:46:13 +0100

    fixing routes

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,6 +1,4 @@
 import express from "express";
-// import listEndpoints from "express-list-endpoints";
-import cryto from "crypto";
 import bcrypt from "bcrypt-nodejs";
 import { User } from "../models/user";
 
@@ -13,60 +11,63 @@ const authenticateUser = async (req, res, next) => {
     req.user = user;
     next();
   } else {
-    res.status(401).json({ loggedOut: true });
+    res.status(401).json({ loggedOut: true, message: "Unauthorized access" });
   }
 };
 
-const listEndpoints = require("express-list-endpoints");
+// Register Route
+router.post("/users", async (req, res) => {
+  try {
+    // Basic input validation (can be enhanced)
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      res.status(400).json({ message: "All fields are required" });
+      return;
+    }
 
-//routes
-router.get("/", (req, res) => {
-  res.send(listEndpoints(router));
+    const hashedPassword = bcrypt.hashSync(password);
+    const user = new User({ name, email, password: hashedPassword });
+    await user.save();
+    res
+      .status(201)
+      .json({ success: true, id: user._id, accessToken: user.accessToken });
+  } catch (err) {
+    res
+      .status(400)
+      .json({ message: "Could not create user", error: err.message });
+  }
 });
 
+// Login Route
 router.post("/sessions", async (req, res) => {
   try {
     const { username, password } = req.body;
     const user = await User.findOne({ name: username });
 
-    if (user && bcrypt.compareSync(req.body.password, user.password)) {
-      res.json({ success: true, userId: user._id, accessToken: user.accessToken });
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res.json({
+        success: true,
+        userId: user._id,
+        accessToken: user.accessToken,
+      });
     } else {
-      res.json({ success: false, message: "Invalid username or password" });
+      res
+        .status(401)
+        .json({ success: false, message: "Invalid username or password" });
     }
   } catch (err) {
-    console.error("Error during login:", err);
     res.status(400).json({ message: "Login failed", error: err.message });
   }
 });
 
-
-/*router.post("/users", async (req, res) => {
-  console.log("Received registration request with body:", req.body);
-  try {
-    const user = new User({ name: req.body.name, email: req.body.email, password: bcrypt.hashSync(req.body.password) });
-    await user.save();
-    console.log("User created successfully:", user);
-    res.status(201).json({ success: true, id: user._id, accessToken: user.accessToken });
-  } catch (err) {
-    console.error("Error during registration:", err);
-    res.status(400).json({ message: "Could not create user", error: err.message });
-  }
-});*/
-
-
-router.get("/secrets", authenticateUser);
-router.get("/secrets", (req, res) => {
-  res.json({ secret: "This is super secret message." });
+// Authenticated Route
+router.get("/secrets", authenticateUser, (req, res) => {
+  res.json({ secret: "This is a super-secret message." });
 });
 
-router.post("/sessions", async (req, res) => {
-  const user = await User.findOne({ email: req.body.email });
-  if (user && bcrypt.compareSync(req.body.password, user.password)) {
-    res.json({ userId: user._id, accessToken: user.accessToken });
-  } else {
-    res.json({ notFound: true });
-  }
+router.get("/", (req, res) => {
+  res.send(listEndpoints(router));
 });
+
 export { router as routes };
 export default router;
