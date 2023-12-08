b5799303a9dc98b9b85bb1bd6deb00426bd778cb
Amanda Rambert Hammarström <amanda.rambert@gmail.com>
2023-12-08 22:50:11 +0100

    Added logic to Register and SignIn components

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,42 @@
+import { useState } from "react";
+
 const Register = () => {
+    const [username, setUserName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+    const handleRegister = async () => {
+        try {
+            setLoading(true);
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/register`, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ username, email, password }),
+            });
+
+            if (response.ok) {
+                const userData = await response.json();
+                console.log(userData);
+            } else {
+                setError(true);
+            }
+        } catch (err) {
+            console.error(err.message);
+            setError(true);
+        } finally {
+            setLoading(false);
+        }
+    };
+
     return (
         <div>
-            <h1>Register</h1>
+            <input placeholder="Username" value={username} onChange={(e) => setUserName(e.target.value)} />
+            <input placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <input placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            {error === true && <p>Something went wrong!</p>}
+            <button disabled={loading} onClick={handleRegister}>Register</button>
         </div>
     )
 };
--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,10 +1,42 @@
-const Placeholder = () => {
+import { useState } from "react";
+
+const SignIn = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+    const handleSignIn = async () => {
+        try {
+            setLoading(true);
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/signin`, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ email, password }),
+            });
+
+            if (response.ok) {
+                const userData = await response.json();
+                console.log(userData);
+            } else {
+                setError(true);
+            }
+        } catch (err) {
+            console.error(err.message);
+            setError(true);
+        } finally {
+            setLoading(false);
+        }
+    };
+
     return (
         <div>
-            <h1>This is a placeholder component</h1>
-            <p>Add your content here</p>
+            <input placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <input placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            {error === true && <p>Something went wrong!</p>}
+            <button disabled={loading} onClick={handleSignIn}>Sign In</button>
         </div>
     );
 };
 
-export default Placeholder;
+export default SignIn;
