921a0753953ffd4725073d40652941a73b3c3813
stenlisuryadinata <elim_berkat@yahoo.co.id>
2023-12-08 07:58:10 +0700

    Update home.jsx , App.css
    Update home.jsx , App.css

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,8 +5,9 @@ import { Link } from "react-router-dom";
 
 export const Home = () => {
   const text = {
-    heading: "Vite + React + React Router + Minimal CSS",
+    heading: "Project-Auth",
     subheading: "Home Page",
+    intro: "The task involves creating a full-stack application with a backend API and a React frontend for a task management app. The backend API should have endpoints for user registration, user login, and an authenticated endpoint that returns content accessible only to logged-in users. The frontend needs to include registration and sign-in forms, a page to display authenticated content, and a sign-out button. Key requirements include bcrypt-encrypted passwords, proper handling of authentication errors, and validation of user input. Stretch goals involve storing data in the database, enhancing error messaging in the frontend, implementing Google authentication with Firebase, adding more routes for logged-in users, and improving backend validations for unique email addresses and email format. Advanced stretch goals include creating additional routes for database operations and further enhancing backend validations. The technology stack includes Vite, React, React Router, and minimal CSS. The focus is on building a secure and functional task app with both essential and optional features to improve the overall user experience."
   };
   const storeHandleLogout = userStore((state) => state.handleLogout);
 
@@ -30,23 +31,27 @@ export const Home = () => {
 
   return (
     <>
-      <nav className="Nav-wrapper">
+      <nav className="app-button-wrapper">
         <ul className="app-ul">
           <li className="app-li">
-            <Link to="/home" >Home</Link>
+            <Link to="/home" className="app-link">Home</Link>
           </li>
           <li className="app-li">
-            <Link to="/tasks">Tasks</Link>
+            <Link to="/tasks" className="app-link">Tasks</Link>
           </li>
+          <div className="app-button-li">
           <li className="app-li">
-            <button onClick={onLogoutClick}>Sign Out</button>
+            <button onClick={onLogoutClick} className="app-button">Sign Out</button>
           </li>
+          </div>
         </ul>
       </nav>
       <Logos />
+      <div className="card-container">
       <h1 className="heading">{text.heading}</h1>
       <h2>{text.subheading}</h2>
       <p>{text.intro}</p>
+      </div>
     </>
   );
 };
