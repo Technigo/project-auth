ccbda036bb46006a2c0d2b3c7249179f761d94c3
jforjanice <jjanice.cheng@gmail.com>
2023-12-09 18:04:42 +0100

    Start to fix front end

--- frontend/src/assets/Pages/Registration.jsx
+++ frontend/src/assets/Pages/Registration.jsx
@@ -1,7 +1,46 @@
 import { useState } from "react";
+import { userStore } from "../../store/userStore";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Registration = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  // Function to handle the click event of the signup button
+  const storeHandleSignup = userStore((state) => state.handleSignup);
+
+  // Combined function for handling the signup click event
+  const onSignupClick = async () => {
+    if (!username || !password || !email) {
+      alert("Please enter email, username and password");
+      return;
+    }
+
+    // Handle password length and special requirement
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[\S]{6,}$/;
+    if (!passwordRegex.test(password)) {
+      alert(
+        "Password must be at least 6 characters and include lowercase, uppercase, and a number."
+      );
+      return;
+    }
+
+    try {
+      await storeHandleSignup(username, password, email);
+      if (username && password) {
+        //Once the user has successfully sign up, the user will be directed to the log in page
+        navigate("/");
+      }
+    } catch (error) {
+      // Handle any errors that occur during signup
+      console.error("Signup error:", error);
+      alert("An error occurred during signup");
+    }
+  };
+
   return (
     <div>
       <div>
@@ -10,14 +49,34 @@ const Registration = () => {
       <form className="login-form">
         <label className="username">
           UserName:
-          <input type="text" name="username" />
+          <input
+            type="text"
+            placeholder="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </label>
         <br />
         <label className="pw">
           Password:
-          <input type="text" name="password" />
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </label>
-        <button>Sign up</button>
+        <br />
+        <label className="email">
+          E-mail:
+          <input
+            type="text"
+            placeholder="e-mail"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <button onClick={onSignupClick}>Sign up</button>
       </form>
       <h5>Already a user?</h5>
       <Link to="/">HOME</Link>
