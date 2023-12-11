bbd23834e26a347e367cfc66d0d75392bc5177a8
mariateresepettersson <mariateresepettersson>
2023-12-11 16:57:48 +0100

    Debugging middleware

--- backend/middlewares/authenticateUser.js
+++ backend/middlewares/authenticateUser.js
@@ -3,10 +3,11 @@ import { UserModel } from "../models/UserModel";
 
 export const authenticateUser = async (req, res, next) => {
     //retrieve the access token from the request header (why is it sometimes header and not body?)
-    const accessToken = req.header("Autorization");
+    const accessToken = req.header("Authorization");
     try {
         const user = await UserModel.findOneAndDelete({ accessToken: accessToken });
         if (user) {
+            req.user = user; // Store the user in the request for later use
             next(); //Continue to the next middleware routee
         } else {
             //if user not found show message
