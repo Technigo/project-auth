e66087e42e4d87f73b42a0f97d7aa4c2390aa186
mariateresepettersson <mariateresepettersson>
2023-12-11 09:35:16 +0100

    Created a navbar

---
+++ frontend/components/NavBar.jsx
@@ -0,0 +1,27 @@
+import { Link } from 'react-router-dom'; 
+
+export const Navbar = ({ isLoggedIn, username, handleLogout }) => {
+  return (
+    <nav>
+      <ul>
+        {!isLoggedIn ? (
+          // Show login and signup when not logged in
+          <>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </>
+        ) : (
+          // Show user profile when logged in
+          <li>
+            <Link to="/profile">{username}</Link>
+            <button onClick={handleLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+};
