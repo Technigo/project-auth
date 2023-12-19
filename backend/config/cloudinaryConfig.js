a23f6a8311324683ec4b7c3648c89dffd65c96ad
mariateresepettersson <mariateresepettersson>
2023-12-19 18:09:21 +0100

    Adding clodinary import in config

--- backend/config/cloudinaryConfig.js
+++ backend/config/cloudinaryConfig.js
@@ -3,7 +3,8 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-cloudinary.config({
+// Correct the usage here
+cloudinaryFramework.v2.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
