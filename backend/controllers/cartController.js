b907eb55c664112b4d5168f397615d3b84ee47a6
sheryll.poo-cheong <shey_pc@gmail.com>
2023-12-21 09:48:29 +0100

    adding PUT request (dead-end)+ interrogations on utility of DELETE/PUT/GET request for user's access

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -2,7 +2,7 @@ import asyncHandler from "express-async-handler";
 import { UserModel } from "../models/UserModel";
 import { CartModel } from "../models/CartModel";
 
-// @desc Post user's cart information
+// @desc Post user's cart information (with confirm button on frontend)
 // @route POST /cart/:user_id
 // @access Private
 export const addUserCartController = asyncHandler(async (req, res) => {
@@ -38,7 +38,7 @@ export const addUserCartController = asyncHandler(async (req, res) => {
 });
 
 
-// @desc Delete user's cart information
+// @desc Delete all user's shopping carts (note: no GUI for neither delete all nor specific cart in DB and not fit for user usage/access. Also, no method of data transfer from product page to cart page yet)
 // @route DELETE /cart/:user_id
 // @access Private
 export const deleteUserCartController = asyncHandler(async (req, res) => {
@@ -67,7 +67,7 @@ export const deleteUserCartController = asyncHandler(async (req, res) => {
   };
 });
 
-// @desc Get user's cart information
+// @desc Get user's cart information (Note: No GUI for this request neither)
 // @route Get /cart/:user_id
 // @access Private
 export const getUserCartController = asyncHandler(async (req, res) => {
@@ -92,3 +92,50 @@ export const getUserCartController = asyncHandler(async (req, res) => {
       res.status(500).json({ success: false, error: error.message });
     }
   });
+
+  // @desc Update user's cart information (Note: Dead-end with findOneAndUpdate method that has no save() to work with save() hook for maths logic.  Also, updates only already made purchases- not fit for user usage/access)
+// @route PUT (or PATCH?) /cart/:cart_id
+// @access Private
+export const updateUserCartController = asyncHandler(async (req, res) => {
+  //to update flower and/or options
+
+  try {
+    console.log("updating cart values")
+    // have the _id of the element the cart and the actuall element changed
+    // for example you want to update the flower then the input json would look like :
+    // {"_id": ####,
+    // "flower": updated_value
+    // }
+    console.log(req.params.cart_id) // Comes from /cart/:user_id
+    console.log(req.user.id) // comes from decoded token in authenticateUser.js
+    const cart_id = req.params.cart_id ;
+    if (!cart_id){
+      return res.status(404).json({success:false, response: `Error occured: Your shopping cart no ${cart_id} was not found.`})
+    }
+    const updateFlower = req.body.flower;
+    const updateOption = req.body.options;
+    console.log(updateFlower);
+    console.log(updateOption);
+    let updatedCart ;
+    if(updateFlower){
+      console.log(updateFlower);
+       updatedCart = await CartModel.findOneAndUpdate(
+          {_id:cart_id} ,
+        {flower: updateFlower},
+        { new: true } // Return the updated document
+      );
+      
+    }
+    if(updateOption){
+       updatedCart = await CartModel.findByIdAndUpdate(
+         cart_id,
+        {options: updateOption},
+        { new: true } // Return the updated document
+      );    
+    }
+    
+    res.status(200).json({ success: true, response: updatedCart });
+    } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+});
