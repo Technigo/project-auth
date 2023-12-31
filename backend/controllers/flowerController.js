053959b069a3448551de2466627baf4aa26a3997
veronica astrom <v.aastrom@gmail.com>
2023-12-31 10:57:03 +0100

    Improved code for flowerController and flowerRoutes

--- backend/controllers/flowerController.js
+++ backend/controllers/flowerController.js
@@ -1,32 +1,19 @@
 import asyncHandler from 'express-async-handler';
-import { FlowerModel } from '../models/FlowerModel.js'; 
-
-// @desc Get all flowers
-// @route GET /flowers
-// @access Public
-export const getAllFlowers = asyncHandler(async (req, res) => {
-    try {
-        const flowers = await FlowerModel.find();
-        res.status(200).json({ success: true, response: flowers });
-    } catch (error) {
-        res.status(500).json({ success: false, error: error.message });
-    }
-});
-
+import { FlowerModel } from '../models/FlowerModel.js';
 
 // @desc Get a flower by type
 // @route GET /flowers/:type
 // @access Public
-export const getFlowerByType = asyncHandler(async (type, req, res) => {
+export const getFlowerByType = asyncHandler(async (req, res) => {
+    const type = req.params.type;
     try {
         const flower = await FlowerModel.findOne({ type: type });
         if (!flower) {
-            res.status(404).json({ success: false, response: 'Flower not found' });
+            return res.status(404).json({ success: false, response: 'Flower not found' });
         } else {
-            res.status(200).json({ success: true, response: flower });
+            return res.status(200).json({ success: true, response: flower });
         }
     } catch (error) {
-        res.status(500).json({ success: false, error: error.message });
+        return res.status(500).json({ success: false, error: error.message });
     }
 });
-
