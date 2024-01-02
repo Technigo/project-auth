ee4531d11710943d4763bbcb4dd624c63dd7f012
sheryll.poo-cheong <shey_pc@gmail.com>
2024-01-02 22:32:48 +0100

    embedded greeting form into Cart.jsx

--- frontend/src/pages/cart/Cart.jsx
+++ frontend/src/pages/cart/Cart.jsx
@@ -1,8 +1,9 @@
 import { Footer } from "../../components/footer/Footer";
 import { Logo } from "../../components/logo/Logo";
+import { useState, useEffect } from "react";
 import { Link, useParams, useNavigate } from "react-router-dom";
 import { userStore } from "../../stores/userStore";
-// import { GreetingMessage } from "./GreetingMessage";
+import styles from "./cart.module.css";
 //import { cartStore } from "../../stores/cartStore";
 export const Cart = () => {
   const { id } = useParams();
@@ -26,6 +27,52 @@ export const Cart = () => {
   // const sendForm = () => {
   //   // post backend
   // };
+
+  //-------- Logic regarding greeting message in form  -------
+
+  const [newGreeting, setNewGreeting] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  //useEffect hook to check max length criteria for greeting message
+  useEffect(() => {
+    if (newGreeting.length >= 101) {
+      setErrorMessage("Your message is too long.");
+    } else {
+      setErrorMessage("");
+    }
+  }, [newGreeting]); //dependency array with effect only running when "newGreeting" changes
+
+  const handleSubmit = async (event) => {
+    event.preventDefault(); //preventing form's default submit behaviour
+    const backendApi = import.meta.env.VITE_BACKEND_API;
+
+    //Checking minimum length criteria for greeting message
+    if (newGreeting.length <= 4) {
+      setErrorMessage("Your message is too short.");
+    } else {
+      //Configuring the fetch request -POST method
+      const greeting = {
+        method: "POST",
+        //Stringifying "newGreeting" and setting it to request body
+        body: JSON.stringify({
+          message: `${newGreeting}`,
+        }),
+        headers: { "Content-Type": "application/json" },
+      };
+
+      //Making a POST request to API endpoint with configured greeting
+      await fetch(`${backendApi}/cart/${id}`, greeting)
+        .then((res) => res.json()) //parsing response as json
+        .then((data) => {
+          newGreeting(data); //calling "newGreeting" function (passed prop) with the parsed data
+          setNewGreeting(""); //clearing textarea
+        })
+        .catch((error) => {
+          console.error("Error occured in creating greeting:", error);
+          alert("An error occurred while creating your greeting message.");
+        });
+    }
+  };
 
   return (
     <div>
@@ -64,22 +111,42 @@ export const Cart = () => {
         sum:<span></span>kr
       </p>
       {/*<GreetingMessage dataToshow/>*/}
-      <form>
-        {/* component typography? */}
-        <p>
-          If you want to send flowers to someone or convey your feelings for the
-          week through FloraEcho, please leave a message below. We will
-          customize a secret floral based on your message. There will be
-          different surprises every week!
-        </p>
-        <label>
+      <p>
+        If you want to send flowers to someone or convey your feelings for the
+        week through FloraEcho, please leave a message below. We will customize
+        a secret floral based on your message. There will be different surprises
+        every week!
+      </p>
+
+      {/* Form element with onSubmit event handler set to "handleSubmit" */}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="greeting">
           <textarea
+            id="greeting"
             rows="5"
             cols="50"
             placeholder="Type your text here"
+            value={newGreeting}
+            onChange={(event) => setNewGreeting(event.target.value)}
             className=""
           />
         </label>
+        <div>
+          <p>{errorMessage}</p>
+          <p
+            className={`length ${newGreeting.length >= 100 ? styles.red : ""}`}
+          >
+            {newGreeting.length}/100
+          </p>
+        </div>
+        <button
+          type="submit"
+          id="submitPostButton"
+          aria-label="click to submit your subscription order"
+          disabled={newGreeting.length < 4 || newGreeting.length > 100}
+        >
+          Confirm order
+        </button>
       </form>
       <Footer />
     </div>
