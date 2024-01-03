23f499521095b56ed6a8fe7abbc35dace463298b
sheryll.poo-cheong <shey_pc@gmail.com>
2024-01-03 23:39:50 +0100

    commenting unused controllers and routes

--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
 import {
     addUserCartController,
-    getUserCartController,
-    deleteAllUserCartsController,
-    deleteUserCartController,
-    updateUserCartController
+    // getUserCartController,
+    // deleteAllUserCartsController,
+    // deleteUserCartController,
+    // updateUserCartController
 } from "../controllers/cartController";
 import { authenticateUser } from "../middleware/authenticateUser";
 
@@ -12,9 +12,9 @@ const router = express.Router();
 
 // Endpoints
 router.post("/cart/:user_id", authenticateUser, addUserCartController);
-router.get("/cart/:user_id", authenticateUser, getUserCartController);
-router.delete("/cart/all/:user_id", authenticateUser, deleteAllUserCartsController);
-router.delete("/cart/:cart_id", authenticateUser, deleteUserCartController);
-router.put("/cart/:cart_id", authenticateUser,updateUserCartController);
+// router.get("/cart/:user_id", authenticateUser, getUserCartController);
+// router.delete("/cart/all/:user_id", authenticateUser, deleteAllUserCartsController);
+// router.delete("/cart/:cart_id", authenticateUser, deleteUserCartController);
+// router.put("/cart/:cart_id", authenticateUser,updateUserCartController);
 
 export default router;
