af57f494244c350169639eda09b92a7ac1b7bc2c
Your Name alexandrameija@hotmail.com <alexandrameija@hotmail.com.com>
2024-01-04 11:16:44 +0100

    -m made changes

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import bcrypt from 'bcryptjs';
+import bcrypt from 'bcrypt';
 import asyncHandler from 'express-async-handler';
 import UserModel from '../models/UserModel.js';
 import jwt from 'jsonwebtoken';
