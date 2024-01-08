d429aa269ee958c14916d5396ccff67ddee62bed
sheryll.poo-cheong <shey_pc@gmail.com>
2024-01-08 15:01:18 +0100

    removed uncommented parts in navbar

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -6,12 +6,9 @@ import { Logo } from "../../components/logo/Logo";
 import down from "../../assets/icons/icons8-down-64.png";
 import hamburgerIcon from "../../assets/icons/hamburger.png";
 
-// import { useParams } from "react-router-dom";
-
 export const Navbar = () => {
   const [showOptions, setShowOptions] = useState(false);
   const [showMobileMenu, setShowMobileMenu] = useState(false);
-  // const { id } = useParams();
   const { id } = userStore((state) => ({
     id: state.id,
   }));
