bd3668980c868832bc9f8abd8399d2cff396c0ee
sheryll.poo-cheong <shey_pc@gmail.com>
2024-01-09 10:06:57 +0100

    cleaning up code cart backend

--- backend/models/CartModel.js
+++ backend/models/CartModel.js
@@ -52,45 +52,5 @@ const cartSchema = mongoose.Schema (
   }
   )
   
-  // //------Mongoose model middleware with pre-save hook ------
-  //   //Base price for the different flower options
-  // cartSchema.pre("save", function (next) {
-  //   let basePrice ;
-  //   if (this.flower === "basic") {
-  //     basePrice = 150;
-  //   } else if (this.flower === "standard") {
-  //     basePrice = 250;
-  //   } else if (this.flower === "large") {
-  //     basePrice = 350;
-  //   } else {
-  //     // Handle any other cases or throw an error if needed
-  //     return next(new Error("Invalid flower option."));
-  //   }
-  //   //Quantity of bouquets purchased according to the subscription option
-  //   if (this.options === "weekly") {
-  //     this.quantity = 1;
-  //   } else if (this.options === "monthly") {
-  //     this.quantity = 4;
-  //   } else if (this.options === "yearly") {
-  //     this.quantity = 52;
-  //   } else {
-  //     // Handle any other cases or throw an error if needed
-  //     return next(new Error("Invalid subscription plan."));
-  //   }
-  //    // Total price for all flower services
-  //   if (this.options === "weekly") {
-  //     this.price = basePrice * this.quantity;
-  //   } else if (this.options === "monthly") {
-  //     this.price = basePrice * this.quantity;
-  //   } else if (this.options === "yearly") {
-  //     this.price = basePrice * this.quantity;
-  //   } else {
-  //     // Handle any other cases or throw an error if needed
-  //     return next(new Error("Error occured while ordering."));
-  //   }
-  //   this.sum = this.price + this.deliveryCost
-  //   next();
-  // });
- // This middleware ensures that the necessary calculations and validations are performed before saving a cart document, encapsulating logic related to the document before it gets saved to the database.
 export const CartModel = mongoose.model("Cart", cartSchema);
 
--- backend/routes/cartRoutes.js
+++ backend/routes/cartRoutes.js
@@ -1,20 +1,10 @@
 import express from "express";
-import {
-    addUserCartController,
-    // getUserCartController,
-    // deleteAllUserCartsController,
-    // deleteUserCartController,
-    // updateUserCartController
-} from "../controllers/cartController";
+import { addUserCartController } from "../controllers/cartController";
 import { authenticateUser } from "../middleware/authenticateUser";
 
 const router = express.Router();
 
-// Endpoints
+// Endpoint
 router.post("/cart/:user_id", authenticateUser, addUserCartController);
-// router.get("/cart/:user_id", authenticateUser, getUserCartController);
-// router.delete("/cart/all/:user_id", authenticateUser, deleteAllUserCartsController);
-// router.delete("/cart/:cart_id", authenticateUser, deleteUserCartController);
-// router.put("/cart/:cart_id", authenticateUser,updateUserCartController);
 
 export default router;
