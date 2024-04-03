fec87d388c326bb5e65af364f28bb97b5b71f77b
EvelynDelCarmen <hey@evelyndelcarmen.com>
2024-04-03 20:49:18 +0200

    changed the logic in the backend

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,38 +1,93 @@
-// userModel.js
-import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
+import mongoose from "mongoose";
+import crypto from "crypto"; //  Imports the Node.js crypto library for generating secure random strings.
 
-const userSchema = new mongoose.Schema({
-    username: { type: String, unique: true, required: true },
-    email: { type: String, unique: true, required: true },
-    password: { type: String, required: true },
-    secretKey: { type: String },
-});
+// Import the Schema class from the Mongoose library
+// Destructures the Schema class from the Mongoose library, allowing us to create a schema.
+const { Schema } = mongoose;
 
-userSchema.pre('save', async function (next) {
-    const user = this;
-    if (user.isModified('password')) {
-        try {
-            const hashedPassword = await bcrypt.hash(user.password, 10);
-            user.password = hashedPassword;
-        } catch (error) {
-            next(error);
-        }
+// Create a new Mongoose schema named 'userSchema'
+// Creates a new Mongoose schema named userSchema that defines the structure of a user document in the MongoDB collection. It includes fields like username, password, and accessToken, specifying their data types, validation rules, and default values.
+const userSchema = new Schema(
+    {
+        // Define the 'username' field with a String data type
+        username: {
+            type: String, // Specifies that 'username' should be a string
+            required: true, // Indicates that 'username' is a required field
+            unique: true, // Ensures that 'username' values are unique
+            minlength: 2, // Sets a minimum length of 2 characters for 'username'
+        },
+        // Define the 'password' field with a String data type
+        password: {
+            type: String, // Specifies that 'password' should be a string
+            required: true, // Indicates that 'password' is a required field
+            minlength: 6, // Sets a minimum length of 6 characters for 'password'
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        //Define the 'accessToken' field with a String data type
+        accessToken: {
+            type: String, // Specifies that 'accessToken' should be a string
+            default: () => crypto.randomBytes(128).toString("hex"), // Sets a default value using a cryptographic random string
+        },
+    },
+    {
+        timestamps: true,
     }
-    next();
-});
+);
 
-userSchema.methods.comparePassword = async function (enteredPassword) {
-    try {
-        return await bcrypt.compare(enteredPassword, this.password);
-    } catch (error) {
-        throw new Error(error);
-    }
-};
+// Create a Mongoose model named 'UserModel' based on the 'userSchema' for the 'users' collection
+// This model is used to interact with the "users" collection in the MongoDB database. It allows you to perform CRUD operations on user documents and provides methods for data validation based on the schema.
+export const UserModel = mongoose.model("User", userSchema);
 
-const User = mongoose.model('User', userSchema);
+// In summary, this code defines a Mongoose schema (userSchema) that describes the structure of documents for users in a MongoDB collection. It also creates a Mongoose model (UserModel) associated with the "users" collection, which can be used to interact with the database and perform operations like creating, reading, updating, and deleting user documents.
 
-export default User;
+
+
+
+
+
+
+
+
+
+// // userModel.js
+// import mongoose from 'mongoose';
+// import bcrypt from 'bcrypt';
+
+// const userSchema = new mongoose.Schema({
+//     username: { type: String, unique: true, required: true },
+//     email: { type: String, unique: true, required: true },
+//     password: { type: String, required: true },
+//     secretKey: { type: String },
+// });
+
+// userSchema.pre('save', async function (next) {
+//     const user = this;
+//     if (user.isModified('password')) {
+//         try {
+//             const hashedPassword = await bcrypt.hash(user.password, 10);
+//             user.password = hashedPassword;
+//         } catch (error) {
+//             next(error);
+//         }
+//     }
+//     next();
+// });
+
+// userSchema.methods.comparePassword = async function (enteredPassword) {
+//     try {
+//         return await bcrypt.compare(enteredPassword, this.password);
+//     } catch (error) {
+//         throw new Error(error);
+//     }
+// };
+
+// const User = mongoose.model('User', userSchema);
+
+// export default User;
 
 
 
--- backend/server.js
+++ backend/server.js
@@ -1,41 +1,104 @@
-// server.js
-import express from 'express';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import authUserMiddleware from './middleware/authMiddleware';
-// import userRoutes from './models/userModel';
-import authUserRoutes from './routes/authUserRoutes';
-import secretRoute from './routes/secretRoutes'; // Add this line
-import listEndpoints from 'express-list-endpoints';
-import dotenv from 'dotenv';
-dotenv.config();
 
-const mongoUrl = process.env.MONGO_URI || "mongodb://localhost/project-mongo";
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.Promise = Promise;
+// Import necessary libraries and modules
+import express from "express"; // Import the Express.js framework
+import cors from "cors"; // Import the CORS middleware
+import dotenv from "dotenv"; // Import dotenv for environment variables
+import expressListEndpoints from "express-list-endpoints";
+dotenv.config(); // Load environment variables from the .env file
+import userRoutes from "./routes/userRoutes"; // Import custom user routes
+// import contactRoutes from "./routes/contactRoutes"
+// import mediaRoutes from "./routes/mediaRoutes"
+import { connectDB } from "./config/db"; // Import database connection function (not used here)
 
-const port = process.env.PORT || 8080;
-const app = express();
+// Defines the port the app will run on. Defaults to 8080, but can be overridden
+const port = process.env.PORT; // Set the port number for the server
+const app = express(); // Create an instance of the Express application
 
-app.use(cors());
-app.use(express.json());
+// Add middlewares to enable cors and json body parsing
+app.use(cors()); // Enable CORS (Cross-Origin Resource Sharing)
+app.use(express.json()); // Parse incoming JSON data
+app.use(express.urlencoded({ extended: false })); // Parse URL-encoded data
 
-app.use((req, res, next) => {
-  console.log(`Received ${req.method} request at ${req.url}`);
-  next();
-});
+// Use the routes for handling API requests
+// ROUTES - These routes USE controller functions ;)
+// Use the task-controlled routes for task-related requests
+app.use(userRoutes); // Use the user-controlled routes for user-related requests
 
-app.use('/auth', authUserMiddleware);
-app.use('/user', authUserRoutes);
-app.use('/secret', secretRoute); // Add this line
+//KANSKE SKA ANVÄNDA DOM HÄR
+// app.use(contactRoutes); //for the contactform
+// app.use(mediaRoutes); // for the images/film
 
+
+// Connection to the database through Mongoose
+connectDB();
+// Create a dedicated endpoint to view endpoints in the browser
 app.get("/", (req, res) => {
-  res.json(listEndpoints(app));
+  const endpoints = expressListEndpoints(app);
+  res.json(endpoints);
+  console.log("List of Endpoints:");
+  console.log(endpoints);
 });
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+app.use((err, req, res, next) => {
+  res.status(500).send(err);
 });
+
+// Start the server and listen for incoming requests on the specified port
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`); // Display a message when the server is successfully started
+});
+
+
+
+
+
+
+
+
+
+
+
+// // server.js
+// import express from 'express';
+// import cors from 'cors';
+// import mongoose from 'mongoose';
+// import authUserMiddleware from './middleware/authMiddleware';
+// // import userRoutes from './models/userModel';
+// import authUserRoutes from './routes/authUserRoutes';
+// import secretRoute from './routes/secretRoutes'; // Add this line
+// import listEndpoints from 'express-list-endpoints';
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+// console.log('JWT_SECRET:', process.env.JWT_SECRET);
+
+
+// const mongoUrl = process.env.MONGO_URI || "mongodb://localhost/project-mongo";
+// mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.Promise = Promise;
+
+// const port = process.env.PORT || 8080;
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// app.use((req, res, next) => {
+//   console.log(`Received ${req.method} request at ${req.url}`);
+//   next();
+// });
+
+// app.use('/auth', authUserMiddleware);
+// app.use('/user', authUserRoutes);
+// app.use('/secret', secretRoute);
+
+// app.get("/", (req, res) => {
+//   res.json(listEndpoints(app));
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server running on http://localhost:${port}`);
+// });
 
 
 
