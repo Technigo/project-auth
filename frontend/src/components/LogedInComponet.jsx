0de07d29bcf51c4158bf95425d25612fdb2063e6
hamdiolad <oladhamdi@gmail.com>
2024-04-23 19:23:09 +0200

    frontend done

--- frontend/src/components/LogedInComponet.jsx
+++ frontend/src/components/LogedInComponet.jsx
@@ -1,4 +1,56 @@
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { userStore } from "./UserStore";
+
+const apiEnv = import.meta.env.VITE_BACKEND_API;
+
+export const LogedInComp = () => {
+  const { accessToken, isLoggedIn, setIsLoggedIn } = userStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchLoggedInData = async () => {
+      try {
+        const response = await axios.get(`${apiEnv}/logged-in`, {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `${accessToken}`,
+          },
+        });
+
+        if (response.status === 200) {
+          const data = response.data;
+          console.log(data);
+          setIsLoggedIn(true);
+        } else {
+          console.error("Failed to fetch authenticated content");
+          navigate("/");
+        }
+      } catch (error) {
+        console.error("Error fetching authenticated content:", error);
+        navigate("/");
+      }
+    };
+
+    fetchLoggedInData();
+  }, [accessToken, isLoggedIn, setIsLoggedIn, navigate]);
+
+  return (
+    isLoggedIn && (
+      <div className="logged-in-content">
+        <img
+          src="https://cdn.pixabay.com/photo/2023/11/24/17/13/ai-generated-8410330_1280.png"
+          alt="AI Generated Image"
+          style={{ width: "50vh", height: "auto" }}
+        />
+        <p>Congratulations! You are logged in. Here is a cute puppy for you!</p>
+      </div>
+    )
+  );
+};
+
+/*import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import { userStore } from "./UserStore";
 
@@ -48,3 +100,4 @@ export const LogedInComp = () => {
     )
   );
 };
+*/
