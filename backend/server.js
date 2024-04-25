1b0385df2e7c6b413480d16248ece64101e8000c
hamdiolad <oladhamdi@gmail.com>
2024-04-25 22:24:01 +0200

    fixed env issues

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,43 @@
-import express, { urlencoded } from "express";
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose"; // Add this line
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 9090;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(userRoutes);
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Hello Technigo!");
+});
+
+// Database connection
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB Atlas");
+    // Start the server after successfully connecting to the database
+    app.listen(port, () => {
+      console.log(`Server running on http://localhost:${port}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB Atlas:", error);
+  });
+
+/*import express, { urlencoded } from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
@@ -6,7 +45,7 @@ dotenv.config();
 import userRoutes from "./routes/userRoutes.js";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-auth";
-//mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 
 mongoose.connect(mongoUrl);
 
@@ -26,3 +65,4 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+*/
