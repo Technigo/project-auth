cfe618ac4675bf27662b6a03b05bde23da25fc5f
hamdiolad <oladhamdi@gmail.com>
2024-04-26 05:12:21 +0200

    log out page

--- frontend/src/components/LogOut.jsx
+++ frontend/src/components/LogOut.jsx
@@ -1,12 +1,20 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { userStore } from "./UserStore";
 
 export const Logout = () => {
   const navigate = useNavigate();
+  const { isLoggedIn, setAccessToken, setIsLoggedIn, setUser } = userStore();
 
-  const { setAccessToken, setIsLoggedIn, setUser } = userStore();
+  useEffect(() => {
+    // Ensure that the Logout component remains visible even after logging in
+    // by checking the isLoggedIn state and taking appropriate action
+    if (isLoggedIn) {
+      // Redirect to the appropriate route after logout
+      navigate("/");
+    }
+  }, [isLoggedIn, navigate]);
 
   const handleLogOut = () => {
     setUser(null);
