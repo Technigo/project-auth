6760dde905092fb468255ccdcd78b08efe60a2b9
hamdiolad <oladhamdi@gmail.com>
2024-04-26 06:05:03 +0200

    loggot

--- frontend/src/components/LogOut.jsx
+++ frontend/src/components/LogOut.jsx
@@ -1,18 +1,20 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-//import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { userStore } from "./UserStore";
 
 export const Logout = () => {
-  // const navigate = useNavigate();
-  const { setAccessToken, setIsLoggedIn, setUser } = userStore();
+  const navigate = useNavigate();
+  const logout = userStore((state) => state.handleLogout);
+  //const { setAccessToken, setIsLoggedIn, setUser } = userStore();
 
   const handleLogOut = () => {
-    setUser(null);
-    setAccessToken(null);
-    setIsLoggedIn(false);
-    localStorage.removeItem("accessToken");
-
+    //setUser(null);
+    //setAccessToken(null);
+    //setIsLoggedIn(false);
+    //localStorage.removeItem("accessToken");
+    logout(); // This should clear the token and update the isLoggedIn state.
+    navigate("/"); // Redirect the user to the login page.
     alert("Log out successful!");
     //navigate("/logged-in");
     //navigate("/");
