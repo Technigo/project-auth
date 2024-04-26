4cba9765bd7c9fcc6274ead3cc02c730f37afaa7
hamdiolad <oladhamdi@gmail.com>
2024-04-26 05:55:38 +0200

    p

--- frontend/src/components/LogOut.jsx
+++ frontend/src/components/LogOut.jsx
@@ -1,10 +1,10 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import { useNavigate } from "react-router-dom";
+//import { useNavigate } from "react-router-dom";
 import { userStore } from "./UserStore";
 
 export const Logout = () => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   const { setAccessToken, setIsLoggedIn, setUser } = userStore();
 
   const handleLogOut = () => {
@@ -14,7 +14,7 @@ export const Logout = () => {
     localStorage.removeItem("accessToken");
 
     alert("Log out successful!");
-    navigate("/logged-in");
+    //navigate("/logged-in");
     //navigate("/");
   };
 
