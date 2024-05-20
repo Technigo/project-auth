31487af928e477996c4a8edb9ac0ec9e352a5af7
Your El1an3 <eliane_w@yahoo.com>
2024-05-20 20:45:49 +0200

    dependencies update and import

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,44 @@
 import cors from "cors";
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+import expressListEndpoints from "express-list-endpoints";
+
+dotenv.config();
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl);
 mongoose.Promise = Promise;
 
-// Defines the port the app will run on. Defaults to 8080, but can be overridden
-// when starting the server. Example command to overwrite PORT env variable value:
-// PORT=9000 npm start
+//defines the port the app will run on
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Add middlewares to enable cors and json body parsing
+//add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
 
-// Start defining your routes here
+//middleware to check if database is available
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState === 1) {
+    next();
+  } else {
+    res.status(503).json({ error: "service unavailable" });
+  }
+});
+
+//routes
+app.get("/", (req, res) => {
+  const endpoints = expressListEndpoints(app);
+  res.json(endpoints);
+});
+
+//start defining your routes here
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
 
-// Start the server
+//start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
