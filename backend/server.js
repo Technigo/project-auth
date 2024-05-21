61e901ca7ef2377c7862a7fbc9ee5e5c226447b7
Maria-source-cell <manuelabirnat@gmail.com>
2024-05-21 22:53:02 +0200

    in progress

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,55 @@
+import bcrypt from " bcrypt";
 import cors from "cors";
+import dotenv from "dotenv";
 import express from "express";
+import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-auth";
 mongoose.connect(mongoUrl);
 mongoose.Promise = Promise;
+
+dotenv.config();
+
+const userSchema = mongoose.model("User", {
+  username: {
+    type: String,
+    unique: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  token: {
+    type: String,
+  },
+});
+
+const User = model("User", userSchema);
+
+const generateAccessToken = (userId) => {
+  return jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, {
+    expriresIn: "1h",
+  });
+};
+
+const authethicateToken = async (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
+};
+
+try {
+  const decoded = jwt.verify(toke, process.env.ACCESS_TOKEN_SECRET);
+  req.user = await User.findById(decoded.userId);
+  next();
+} catch (err) {
+  return res.sendStatus(403);
+}
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -21,6 +66,18 @@ app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
 
+//Registration Endpoint
+app.post("/register", async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ username, email, password: hashedPassword });
+    const savedUser = await user.save();
+    res.status(201).json({ id: savedUser._id });
+  } catch (error) {
+    res.status(400), json({ error: "Registartion failed" });
+  }
+});
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
