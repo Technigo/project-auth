62f31c07d03df81ecf769f3fb4b3f54699cddaf2
Mai Kanetaka <maikanetaka@gmail.com>
2024-05-22 14:52:38 +0200

    import LoginForm component in App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import { SignupForm } from "./components/SignupForm";
+import { LoginForm } from "./components/LoginForm"
 
 export const App = () => {
   return (
     <div>
       <SignupForm />
+      <LoginForm/>
     </div>
   );
 };
--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
 
-export LoginForm = () => {
-  
+export const LoginForm = () => {
+  return null
 }
