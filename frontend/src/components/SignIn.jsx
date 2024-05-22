e6e10c2fc3a769204f8e9438c8f5049b214d3970
Your El1an3 <eliane_w@yahoo.com>
2024-05-22 12:54:12 +0200

    created components and reusables

--- frontend/src/components/SignIn.jsx
+++ frontend/src/components/SignIn.jsx
@@ -1,3 +1,19 @@
+//imports
+import { Form } from "../reusables/Form";
+import { Header } from "../reusables/Header";
+import { Button } from "../reusables/Button";
+
+//styling
+//use styled components here
+
+//component
+
 export const SignIn = () => {
-  return <div>SignIn</div>;
+  return (
+    <>
+      <Header />
+      <Form />
+      <Button />
+    </>
+  );
 };
--- frontend/src/components/SignOutButton.jsx
+++
@@ -1,3 +0,0 @@
-export const SignOutButton = () => {
-  return <div>SignOutButton</div>;
-};
