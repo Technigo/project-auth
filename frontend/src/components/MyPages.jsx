ad6ecc2b11c1d5e89e0699863d7dfd408743ef02
Erica Mechler <erica.mechler1@gmail.com>
2024-05-22 16:18:03 +0200

    implemented the authenticated content page

--- frontend/src/components/MyPages.jsx
+++ frontend/src/components/MyPages.jsx
@@ -1,10 +1,46 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
 export const MyPages = ({ user }) => {
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchMyPages = async () => {
+      const accessToken = localStorage.getItem("accessToken");
+      try {
+        const response = await fetch(
+          "https://auth-s0og.onrender.com/my-pages",
+          {
+            headers: {
+              Authorization: accessToken,
+            },
+          }
+        );
+        const data = await response.json();
+        if (response.ok) {
+          setMessage(data.message);
+        } else {
+          setError("Failed to fetch data. Please log in again");
+        }
+      } catch (error) {
+        setError("An error occurred. Please try again later");
+      }
+    };
+    fetchMyPages();
+  }, []);
+
+  {
+    error && (
+      <div className="error-container">
+        <p>{error}</p>
+      </div>
+    );
+  }
+
   return (
     <div className="container">
       <h1>Welcome, {user.name}</h1>
-      <p>This is your personal page.</p>
+      <p>{message}</p>
     </div>
   );
 };
