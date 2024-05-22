4e428be9cb989bd86d60a8ac4092ccca4c5bd2ce
Alma Herrstrom <almaherrstrom@hotmail.com>
2024-05-22 17:41:45 +0200

    buttons styling

--- frontend/src/components/WelcomePage.jsx
+++ frontend/src/components/WelcomePage.jsx
@@ -5,12 +5,12 @@ export const WelcomePage = () => {
   return (
     <div>
       <h2 className="title">Are you ready to get started?</h2>
-      <div className="welcome-buttons">
+      <div className="buttons-container">
         <Link to={"/registration"}>
-          <button>Register</button>
+          <button className="welcome-button">Register</button>
         </Link>
         <Link to={"/login"}>
-          <button>Log in</button>
+          <button className="welcome-button">Log in</button>
         </Link>
       </div>
     </div>
