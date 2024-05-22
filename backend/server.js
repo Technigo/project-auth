5cfae3fbcb17d5f7a05841b067a2831a419d46d5
Caracal23 <154092424+Caracal23@users.noreply.github.com>
2024-05-22 11:30:02 +0200

    starting up week project; manage to create a new user

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,30 @@
 import cors from "cors";
 import express from "express";
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl);
 mongoose.Promise = Promise;
+
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    unique: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  accessToken: {
+    type: String,
+    default: () => bcrypt.genSaltSync(),
+  },
+});
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -20,6 +40,33 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello Technigo!");
 });
+
+// Registration endpoint
+app.post("/users", async (req, res) => {
+  const salt = bcrypt.genSaltSync(10);
+
+  try {
+    const { name, email, password } = req.body;
+    const user = new User({
+      name,
+      email,
+      password: bcrypt.hashSync(password, salt),
+    });
+    user.save();
+    res.status(201).json({ id: user._id, accessToken: user.accessToken });
+  } catch (error) {
+    res.status(400).json({
+      response: error.message,
+      success: false,
+      message: "Could not create user",
+      errors: error.errors,
+    });
+  }
+});
+
+// Sign-in endpoint
+
+// Endpoint once user is signed in
 
 // Start the server
 app.listen(port, () => {
