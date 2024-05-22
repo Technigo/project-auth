e9faff70068f9a0be8a01539b7a8c821cfcebf7c
Your El1an3 <eliane_w@yahoo.com>
2024-05-22 13:02:07 +0200

    started form

--- frontend/src/reusables/Form.jsx
+++ frontend/src/reusables/Form.jsx
@@ -1,3 +1,18 @@
+//imports
+import styled from "styled-components";
+
+//styling
+const FormSection = styled.section`
+  display: flex;
+  background: red;
+
+  @media all and (min-width: 744px) {
+  }
+  @media all and (min-width: 1024px) {
+  }
+`;
+
+//component
 export const Form = () => {
-  return <div>Form</div>;
+  return <FormSection>Form</FormSection>;
 };
