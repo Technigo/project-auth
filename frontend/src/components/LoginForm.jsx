3e17d0d5403ef716d15fcdeda7823a186400a7d9
SofieFerrari <sofiestrahl@gmail.com>
2024-05-22 20:20:15 +0200

    worked on loginForm

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,5 +1,61 @@
-import { useState } from "react";
+import { useState } from 'react'
 
 export const LoginForm = () => {
-  return null
+	const [loginData, setLoginData] = useState({
+		username: '',
+		password: '',
+	})
+
+	const handleChange = (e) => {
+		setLoginData({
+			...loginData,
+			[e.target.name]: e.target.value,
+		})
+	}
+
+	const handleSubmit = async (e) => {
+		e.preventDefault()
+		try {
+			const response = await fetch('mongodb://localhost/project-mongo', {
+				method: POST,
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(loginData),
+			})
+			const result = await response.json()
+			if (response.ok) {
+				//return id and access token
+			} else {
+				throw new Error('Failed to login')
+			}
+		} catch (error) {
+			console.error('Error', error)
+		}
+	}
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<label>
+				Username:
+				<input
+					type="text"
+					name="username"
+					id=""
+					value={loginData.username}
+					onChange={handleChange}
+				/>
+			</label>
+			<label>
+				Password:
+				<input
+					type="text"
+					name="password"
+					id=""
+					value={loginData.password}
+					onChange={handleChange}
+				/>
+			</label>
+		</form>
+	)
 }
