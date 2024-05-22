392e98efff33e1a5456b73c7e9ae16bad3ecbf95
Alma Herrstrom <almaherrstrom@hotmail.com>
2024-05-22 17:20:48 +0200

    set up user page

--- frontend/src/components/UserPage.jsx
+++ frontend/src/components/UserPage.jsx
@@ -1,7 +1,14 @@
-//GET call from /user-page
+import Lottie from "lottie-react"
+import animationData from "../assets/lottie.json"
+
+//authorize with access token from /user-page
 
 export const UserPage = () => {
   return (
-    <div>UserPage</div>
+    <div className="user-page-container">
+      <h2 className="user-message">Congratulations! You are logged in!</h2>
+      <Lottie className="lottie" animationData={animationData} />
+      <button>Log out</button>
+    </div>
   )
 }
