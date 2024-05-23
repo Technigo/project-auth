f2cd33e56d50dcf37d3dfb9575efbeebefab6d94
Paulajungaker <paula010514@hotmail.com>
2024-05-23 17:51:50 +0200

    Creating homepage

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,5 +1,22 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
-  return;
+  return (
+    <div className="homeContainer">
+      <h1>Ready to see some more very cute kitties?</h1>
+      <p>To get access to the kittens you need to log in.</p>
+
+      <p>Already have an account?</p>
+      <Link to="/login">
+        <button>Login</button>
+      </Link>
+
+      <p>Need to create an account?</p>
+      <Link to="/register">
+        <button>Register</button>
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
