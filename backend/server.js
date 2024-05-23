e348afe0bd301c6053033a6794cb132cbf543ef2
unknown <linda.frischknecht@bluewin.ch>
2024-05-23 15:34:06 +0200

    adding endpoint and finishing backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcrypt";
 import cors from "cors";
 import express from "express";
+import expressListEndpoints from "express-list-endpoints";
 import mongoose from "mongoose";
 
 import { User } from "./models/userSchema";
@@ -13,7 +14,7 @@ mongoose.Promise = Promise;
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
 // PORT=9000 npm start
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8088;
 const app = express();
 
 const authenticateUser = async (req, res, next) => {
@@ -32,7 +33,8 @@ app.use(express.json());
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  const endpoints = expressListEndpoints(app);
+  res.json(endpoints);
 });
 
 app.post("/register", async (req, res) => {
