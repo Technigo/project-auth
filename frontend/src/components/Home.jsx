0886f20e219b15e684ca9e3b2d270e572755170b
Paulajungaker <paula010514@hotmail.com>
2024-05-23 18:47:09 +0200

    First styling of homepage

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,20 +1,43 @@
 import { Link } from "react-router-dom";
+import { FaLongArrowAltDown } from "react-icons/fa";
+import "../styling/Home.css";
 
 const Home = () => {
   return (
     <div className="homeContainer">
-      <h1>Ready to see some more very cute kitties?</h1>
-      <p>To get access to the kittens you need to log in.</p>
+      <div className="homeIntro">
+        <h1 className="homeTitle">Ready to see some very cute kitties?</h1>
+        <div className="homeSection">
+          <img
+            src="/public/homeKitten.jpg"
+            alt="cute kitten"
+            className="homeImage"
+          />
+          <p className="homeText">
+            "A cat will do what it wants, when it wants, and there is not a
+            thing you can do about it"
+          </p>
+        </div>
+      </div>
 
-      <p>Already have an account?</p>
-      <Link to="/login">
-        <button>Login</button>
-      </Link>
+      <div className="arrowIcon">
+        <FaLongArrowAltDown />
+      </div>
 
-      <p>Need to create an account?</p>
-      <Link to="/register">
-        <button>Register</button>
-      </Link>
+      <div className="homeAccount">
+        <p className="accountIntro">
+          To get access to the kittens you need to log in.
+        </p>
+        <p className="AccountText">Already have an account?</p>
+        <Link to="/login">
+          <button className="accountButton">Login</button>
+        </Link>
+
+        <p className="accountText">Need to create an account?</p>
+        <Link to="/register">
+          <button className="accountButton">Register</button>
+        </Link>
+      </div>
     </div>
   );
 };
