9001fc46e3aa89a45337ac791656990557d4bade
Mai Kanetaka <maikanetaka@gmail.com>
2024-05-23 09:45:16 +0200

    loginform component

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -15,47 +15,48 @@ export const LoginForm = () => {
 
 	const handleSubmit = async (e) => {
 		e.preventDefault()
+		console.log("Form submitted");
+    console.log("Login Data:", loginData);
 		try {
-			const response = await fetch('mongodb://localhost/project-mongo', {
-				method: POST,
+			const response = await fetch("http://localhost:8080/login", {
+				method: "POST",
 				headers: {
 					'Content-Type': 'application/json',
 				},
 				body: JSON.stringify(loginData),
 			})
+			if (!response.ok) throw new Error('Failed to login') 
 			const result = await response.json()
-			if (response.ok) {
-				//return id and access token
-			} else {
-				throw new Error('Failed to login')
-			}
-		} catch (error) {
+					console.log("Login successful:", result);
+
+			} catch (error) {
 			console.error('Error', error)
 		}
 	}
 
 	return (
-		<form onSubmit={handleSubmit}>
-			<label>
-				Username:
-				<input
-					type="text"
-					name="username"
-					id=""
-					value={loginData.username}
-					onChange={handleChange}
-				/>
-			</label>
-			<label>
-				Password:
-				<input
-					type="text"
-					name="password"
-					id=""
-					value={loginData.password}
-					onChange={handleChange}
-				/>
-			</label>
-		</form>
-	)
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input
+          type="text"
+          name="username"
+          id=""
+          value={loginData.username}
+          onChange={handleChange}
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          name="password"
+          id=""
+          value={loginData.password}
+          onChange={handleChange}
+        />
+      </label>
+      <button type="submit">Login</button>
+    </form>
+  );
 }
