4e36f26572ed9c27f59c927883a6cf8d7a4873d3
Paulajungaker <paula010514@hotmail.com>
2024-05-23 23:52:14 +0200

    Creating the Register component

--- frontend/src/components/auth/Register.jsx
+++ frontend/src/components/auth/Register.jsx
@@ -1,5 +1,52 @@
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 const Register = () => {
-  return;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "https://project-auth-ws3k.onrender.com/api/auth/register",
+        { username, password }
+      );
+      localStorage.setItem("token", response.data.token);
+      navigate("/protected");
+    } catch (error) {
+      setError("Registration failed. Please try again.");
+    }
+  };
+
+  return (
+    <div className="registerContainer">
+      <h2>Register</h2>
+      <form onSubmit={handleRegister}>
+        <label htmlFor="username">Username</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit">Register</button>
+        {error & <p className="errorMessage">{error}</p>}
+      </form>
+    </div>
+  );
 };
 
 export default Register;
