a30aa7c365172570f747f3e0c442643b5d3f4aad
Viktoria Olofsson <viktoriaolofsson@gmail.com>
2024-05-26 20:14:57 +0200

    updated the api url

--- frontend/src/store/useStore.jsx
+++ frontend/src/store/useStore.jsx
@@ -53,17 +53,20 @@ export const useStore = create((set, get) => ({
       return false;
     }
     try {
-      const response = await fetch("http://localhost:8080/users", {
-        method: "POST",
-        body: JSON.stringify({
-          name: signUpData.name,
-          username: signUpData.username,
-          email: signUpData.email,
-          password: signUpData.password,
-          address: constructedAddress,
-        }),
-        headers: { "Content-Type": "application/json" },
-      });
+      const response = await fetch(
+        "https://project-auth-kh49.onrender.com/users",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            name: signUpData.name,
+            username: signUpData.username,
+            email: signUpData.email,
+            password: signUpData.password,
+            address: constructedAddress,
+          }),
+          headers: { "Content-Type": "application/json" },
+        }
+      );
       if (!response.ok) {
         throw new Error("Network response was not ok");
       }
@@ -102,14 +105,17 @@ export const useStore = create((set, get) => ({
     event.preventDefault();
     const { loginData } = get();
     try {
-      const response = await fetch("http://localhost:8080/sessions", {
-        method: "POST",
-        body: JSON.stringify({
-          username: loginData.username,
-          password: loginData.password,
-        }),
-        headers: { "Content-Type": "application/json" },
-      });
+      const response = await fetch(
+        "https://project-auth-kh49.onrender.com/sessions",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            username: loginData.username,
+            password: loginData.password,
+          }),
+          headers: { "Content-Type": "application/json" },
+        }
+      );
       if (!response.ok) {
         throw new Error("Network response was not ok");
       }
@@ -129,13 +135,16 @@ export const useStore = create((set, get) => ({
 
   fetchLoggedInData: async (accessToken) => {
     try {
-      const response = await fetch("http://localhost:8080/logged-in", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: accessToken,
-        },
-      });
+      const response = await fetch(
+        "https://project-auth-kh49.onrender.com/logged-in",
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: accessToken,
+          },
+        }
+      );
       if (!response.ok) {
         throw new Error("Network response was not ok");
       }
