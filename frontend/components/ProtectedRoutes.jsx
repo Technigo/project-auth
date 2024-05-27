19d38d041f3391892c2e0457fa5535fc3a2608cb
Kathinka Sewell <4355167+kathinka@users.noreply.github.com>
2024-05-27 17:08:37 +0200

    added handling of successful verification of token

--- frontend/components/ProtectedRoutes.jsx
+++ frontend/components/ProtectedRoutes.jsx
@@ -2,7 +2,6 @@ import { useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { useEffect, useState } from 'react';
 
-// ProtectedRoute component handles the verification of the token for all protected routes
 export const ProtectedRoute = ({ children }) => {
   const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(true);
@@ -10,37 +9,33 @@ export const ProtectedRoute = ({ children }) => {
   const apiKey = import.meta.env.VITE_API_KEY;
   const API = apiKey + "/verify";
 
-  useEffect(() => {
-    const verifyToken = async () => {
-      try {
-        const response = await fetch(API, {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-            "Authorization": `Bearer ${token}`,
-          },
-        });
+  const verifyToken = async () => {
+    try {
+      const response = await fetch(API, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          "Authorization": `Bearer ${token}`,
+        },
+      });
 
-        if (!response.ok) {
-          throw new Error('Token verification failed');
-        }
-
-        setIsLoading(false);
-      } catch (error) {
-        sessionStorage.removeItem('token');
-        navigate('/home');
+      if (!response.ok) {
+        throw new Error('Token verification failed');
       }
-    };
 
-    if (token) {
-      verifyToken();
-    } else {
-      navigate('/home');
+      setIsLoading(false);
+    } catch (error) {
+      console.error(error);
+      navigate('/login');
     }
-  }, [navigate, token]);
+  };
 
-  if (isLoading || !token) {
-    return null;
+  useEffect(() => {
+    verifyToken();
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
   }
 
   return children;
