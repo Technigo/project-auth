19c6b214897e508050f56712e8e207c6e393f3d8
Johanna Catalini Smith <johannacatalini@gmail.com>
2024-05-27 20:47:13 +0200

    add login and signup components with form

--- frontend/src/LoginPage.jsx
+++ frontend/src/LoginPage.jsx
@@ -1,3 +1,25 @@
+import { useNavigate } from "react-router-dom";
+
 export const LoginPage = () => {
-  return <div>Pls log in</div>;
-}
+  const navigate = useNavigate();
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const { username, password } = event.target.elements;
+    console.log(username, password);
+    navigate("/private");
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>Login</h1>
+      <label>
+        Username:
+        <input type="text" name="username" />
+      </label>
+      <label>
+        Password:
+        <input type="password" name="password" />
+      </label>
+      <button type="submit">Login</button>
+    </form>
+  );
+};
