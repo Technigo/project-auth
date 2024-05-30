eccfdafade489ee4aea957a88fd197a5692473ae
Your El1an3 <eliane_w@yahoo.com>
2024-05-30 12:25:42 +0200

    styling fix

--- frontend/src/reusables/Loading.jsx
+++ frontend/src/reusables/Loading.jsx
@@ -1,11 +1,11 @@
 import styled from "styled-components";
 
 export const Loading = styled.div`
-  border: 4px solid rgba(0, 0, 0, 0.1);
+  border: 4px solid var(--darkgreen) (0, 0, 0, 0.1);
   width: 36px;
   height: 36px;
   border-radius: 50%;
-  border-left-color: #09f;
+  border-left-color: var(--lightgreen);
   animation: spin 1s ease infinite;
 
   @keyframes spin {
